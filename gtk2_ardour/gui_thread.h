462a534a5529bd40102ed553df5d368ce23e63a0
Taybin Rutkin <taybin@taybin.com>
2006-01-12 00:56:57 +0000

    Include gtkmm2ext/gtk_ui.h
    git-svn-id: svn://localhost/trunk/ardour2@264 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/gui_thread.h
+++ gtk2_ardour/gui_thread.h
@@ -1,6 +1,8 @@
 #ifndef __ardour_gtk_gui_thread_h__
 #define __ardour_gtk_gui_thread_h__
 
+#include <gtkmm2ext/gtk_ui.h>
+
 #define ENSURE_GUI_THREAD(slot) \
      if (!Gtkmm2ext::UI::instance()->caller_is_gui_thread()) {\
 	Gtkmm2ext::UI::instance()->call_slot ((slot));\
