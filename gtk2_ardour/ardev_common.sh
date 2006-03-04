2cdbfaddc415970e3d0f13d13f43914b0e1c4d12
Tim Mayberry <mojofunk@gmail.com>
2006-03-04 12:08:27 +0000

    Add new file for dev scripts.
    git-svn-id: svn://localhost/trunk/ardour2@345 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ gtk2_ardour/ardev_common.sh
@@ -0,0 +1,8 @@
+#export G_DEBUG=fatal_criticals
+export ARDOUR_RC=../ardour.rc
+export ARDOUR_GLADE_PATH=./glade
+export ARDOUR_UI=./ardour.menus
+export ARDOUR_UI_RC=./ardour2_ui.rc
+export ARDOUR_BINDINGS=./ardour.bindings
+export ARDOUR_COLORS=./ardour.colors
+export LD_LIBRARY_PATH=../libs/ardour:../libs/midi++2:../libs/pbd3:../libs/soundtouch:../libs/gtkmm2ext:$LD_LIBRARY_PATH
