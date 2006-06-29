481f7c39655afec832ac10430dd61a3bb464aa58
Taybin Rutkin <taybin@taybin.com>
2006-06-29 22:21:30 +0000

    Fixed i18n system. Renamed pbd3 back to pbd, since it's version 4.1 now. Very minor fixes
    git-svn-id: svn://localhost/ardour2/trunk@656 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/route_ui.cc
+++ gtk2_ardour/route_ui.cc
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2002 Paul Davis 
+    Copyright (C) 2002-2006 Paul Davis 
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
