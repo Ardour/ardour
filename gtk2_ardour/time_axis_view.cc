e46924420fb60823b96cbe297c588a32fa5cce88
Sampo Savolainen <v2@iki.fi>
2006-08-03 14:40:59 +0000

    Oops. forgot the most important file of previous commit.
    git-svn-id: svn://localhost/ardour2/trunk@751 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/time_axis_view.cc
+++ gtk2_ardour/time_axis_view.cc
@@ -128,6 +128,7 @@ TimeAxisView::TimeAxisView (ARDOUR::Session& sess, PublicEditor& ed, TimeAxisVie
 
 	controls_table.attach (name_hbox, 0, 4, 0, 1, Gtk::FILL|Gtk::EXPAND, Gtk::FILL|Gtk::EXPAND);
 	controls_table.show_all ();
+	controls_table.set_no_show_all ();
 
 	controls_vbox.pack_start (controls_table, false, false);
 	controls_vbox.show ();
