f9812be639d8935aa14b43359fb09bc8dff35749
Taybin Rutkin <taybin@taybin.com>
2006-10-24 15:41:46 +0000

    namespace fix.
    git-svn-id: svn://localhost/ardour2/trunk@1005 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/i18n.h
+++ gtk2_ardour/i18n.h
@@ -11,6 +11,6 @@
 #define _(Text)  dgettext (PACKAGE,Text)
 #define N_(Text) gettext_noop (Text)
 #define X_(Text) Text
-#define I18N(Array) internationalize (PACKAGE, Array)
+#define I18N(Array) PBD::internationalize (PACKAGE, Array)
 
 #endif // __i18n_h__
