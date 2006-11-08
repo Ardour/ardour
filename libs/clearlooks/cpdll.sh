1e86b8406639afb8a315c1f054d47a21ee0f2f8a
Doug McLain <doug@nostar.net>
2006-11-08 10:18:04 +0000

    Removed un necessary version and host subdirectories
    git-svn-id: svn://localhost/ardour2/trunk@1094 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/clearlooks/cpdll.sh
+++ libs/clearlooks/cpdll.sh
@@ -1,2 +1,2 @@
-mkdir -p `pkg-config --variable=gtk_binary_version gtk+-2.0`/`pkg-config --variable=gtk_host gtk+-2.0`/engines
-cp libclearlooks.so `pkg-config --variable=gtk_binary_version gtk+-2.0`/`pkg-config --variable=gtk_host gtk+-2.0`/engines
+mkdir engines
+cp libclearlooks.so engines
