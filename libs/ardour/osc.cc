9dfa70945c23df676b52862b0d9b8cc2366ece17
David Robillard <d@drobilla.net>
2007-01-10 03:45:22 +0000

    Actually pass attr's to pthread_create...
    git-svn-id: svn://localhost/ardour2/trunk@1294 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/osc.cc
+++ libs/ardour/osc.cc
@@ -203,7 +203,7 @@ OSC::init_osc_thread ()
 	pthread_attr_init(&attr);
 	pthread_attr_setstacksize(&attr, 500000);
 
-	pthread_create (&_osc_thread, NULL, &OSC::_osc_receiver, this);
+	pthread_create (&_osc_thread, &attr, &OSC::_osc_receiver, this);
 	if (!_osc_thread) {
 		return false;
 	}
