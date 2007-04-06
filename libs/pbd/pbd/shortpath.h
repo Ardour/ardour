8aab3489da7ce4edc6f2bd293420ced9c874e640
Paul Davis <paul@linuxaudiosystems.com>
2007-04-06 02:33:51 +0000

    added moved code for shortpath
    git-svn-id: svn://localhost/ardour2/trunk@1671 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ libs/pbd/pbd/shortpath.h
@@ -0,0 +1,8 @@
+#ifndef __pbd_shortpath_h__
+#define __pbd_shortpath_h__
+
+#include <glibmm/ustring.h>
+
+Glib::ustring short_path (const Glib::ustring& path, Glib::ustring::size_type target_characters);
+
+#endif /* __pbd_shortpath_h__ */
