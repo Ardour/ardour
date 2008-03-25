97ade208b0a9c0d639a7a4dffa219b8b606de4da
Paul Davis <paul@linuxaudiosystems.com>
2008-03-25 14:58:16 +0000

    ignore dbl-clicks on mute/solo/rec-enable buttons since they make the user-experience feel inconsistent
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@3180 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/route_ui.cc
+++ gtk2_ardour/route_ui.cc
@@ -124,6 +124,10 @@ RouteUI::~RouteUI()
 bool
 RouteUI::mute_press(GdkEventButton* ev)
 {
+	if (ev->type == GDK_2BUTTON_PRESS) {
+		return true;
+	}
+
 	if (!ignore_toggle) {
 
 		if (Keyboard::is_context_menu_event (ev)) {
@@ -201,6 +205,12 @@ RouteUI::mute_release(GdkEventButton* ev)
 bool
 RouteUI::solo_press(GdkEventButton* ev)
 {
+	/* ignore double clicks */
+
+	if (ev->type == GDK_2BUTTON_PRESS) {
+		return true;
+	}
+
 	if (!ignore_toggle) {
 
 		if (Keyboard::is_context_menu_event (ev)) {
@@ -270,7 +280,6 @@ RouteUI::solo_press(GdkEventButton* ev)
 				} else {
 
 					/* click: solo this route */
-					
 					reversibly_apply_route_boolean ("solo change", &Route::set_solo, !_route->soloed(), this);
 				}
 			}
@@ -299,6 +308,10 @@ RouteUI::solo_release(GdkEventButton* ev)
 bool
 RouteUI::rec_enable_press(GdkEventButton* ev)
 {
+	if (ev->type == GDK_2BUTTON_PRESS) {
+		return true;
+	}
+
 	if (!_session.engine().connected()) {
 	        MessageDialog msg (_("Not connected to JACK - cannot engage record"));
 		msg.run ();
@@ -367,7 +380,7 @@ RouteUI::update_solo_display ()
 		ignore_toggle = true;
 		solo_button->set_active(x);
 		ignore_toggle = false;
-	}
+	} 
 	
 	if (_route->solo_safe()) {
 		solo_button->set_visual_state (2);
