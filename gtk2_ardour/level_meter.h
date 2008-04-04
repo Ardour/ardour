83bebc0af54dae691d2441a75b57615ddba4ed3f
Doug McLain <doug@nostar.net>
2008-04-04 13:12:55 +0000

    Integrate level meters into the mixerstrip gain meters for real this time. The LevelMeter class becomes a direct replacement for the meter_packer hbox, and all of its contents
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@3213 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/level_meter.h
+++ gtk2_ardour/level_meter.h
@@ -61,11 +61,11 @@ class LevelMeter : public Gtk::HBox
 
 	void update_gain_sensitive ();
 
-	void update_meters ();
+	float update_meters ();
 	void update_meters_falloff ();
 	void clear_meters ();
 	void hide_meters ();
-	void setup_meters (int len=0);
+	void setup_meters (int len=0, int width=3);
 
   private:
 
@@ -89,11 +89,10 @@ class LevelMeter : public Gtk::HBox
 	    }
 	};
 
-	static const guint16 regular_meter_width = 3;
+	guint16 regular_meter_width;
 	static const guint16 thin_meter_width = 2;
 	vector<MeterInfo>    meters;
-
-	//Gtk::HBox   meter_packer;
+	float       max_peak;
 	
 	void hide_all_meters ();
 
