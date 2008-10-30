2adbb344849725cd9f256d6d236bd22d58a847c0
Nick Mainsbridge <beatroute@iprimus.com.au>
2008-10-30 11:17:51 +0000

    Show loop/punch ruler in initial session.
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@4034 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/editor_actions.cc
+++ gtk2_ardour/editor_actions.cc
@@ -699,6 +699,7 @@ Editor::register_actions ()
 	ruler_tempo_action->set_active (true);
 	ruler_marker_action->set_active (true);
 	ruler_range_action->set_active (true);
+	ruler_loop_punch_action->set_active (true);
 	if (Profile->get_sae()) {
 		ruler_cd_marker_action->set_active (false);
 		ruler_timecode_action->set_active (false);
