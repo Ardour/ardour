47f1cba718acbf82240d447a11a1e75a80dbe53b
Paul Davis <paul@linuxaudiosystems.com>
2008-10-31 08:25:09 +0000

    fix (?) confusing visual state(s) of bypass button by making it into a completely regular button
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@4072 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/plugin_ui.cc
+++ gtk2_ardour/plugin_ui.cc
@@ -408,11 +408,6 @@ PlugUIBase::bypass_toggled ()
 
 	if ((x = bypass_button.get_active()) == insert->active()) {
 		insert->set_active (!x, this);
-		if (insert->active()) {
-			bypass_button.set_label (_("Bypass"));
-		} else {
-			bypass_button.set_label (_("Active"));
-		}
 	}
 }
 
