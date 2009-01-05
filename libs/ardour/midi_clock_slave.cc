ea54d26111e1c3e2ba46809caa7f8a8aaaa93dbc
Hans Baier <hansfbaier@googlemail.com>
2009-01-05 18:43:22 +0000

    *fixed typo * set copyright to Hans Baier for midi_clock_slave.cc
    git-svn-id: svn://localhost/ardour2/branches/3.0@4387 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/midi_clock_slave.cc
+++ libs/ardour/midi_clock_slave.cc
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2002-4 Paul Davis
+    Copyright (C) 2008 Hans Baier
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- libs/ardour/session_process.cc
+++ libs/ardour/session_process.cc
@@ -50,7 +50,7 @@ using namespace std;
 void
 Session::process (nframes_t nframes)
 {
-	// This is no more the appriopriate place to call cycle
+	// This is no more the appropriate place to call cycle
 	// start. cycle_start needs to be called at the Route::roll()
 	// where the signals which we want to mixdown have been calculated.
 	//
