1c043e33041326671a11cd39151bf7278670639f
Hans Baier <hansfbaier@googlemail.com>
2009-02-09 22:32:38 +0000

    * filename conventions * added (blank) SMFTest.cpp/hpp: To be implemented
    git-svn-id: svn://localhost/ardour2/branches/3.0@4509 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ libs/evoral/test/SMFTest.cpp
@@ -0,0 +1,1 @@
+#include "SMFTest.hpp"
---
+++ libs/evoral/test/SMFTest.hpp
@@ -0,0 +1,8 @@
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <evoral/SMF.hpp>
+
+#include <sigc++/sigc++.h>
+
+#include <cassert>
