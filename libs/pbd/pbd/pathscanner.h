f613d3fe1478533ca770c1f3439cf1c670177210
Paul Davis <paul@linuxaudiosystems.com>
2009-03-02 16:52:10 +0000

    recursive operation for PBD::PathScanner, backported from 2.X
    git-svn-id: svn://localhost/ardour2/branches/3.0@4712 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/pbd/pbd/pathscanner.h
+++ libs/pbd/pbd/pathscanner.h
@@ -31,55 +31,65 @@ class PathScanner
 
 {
   public:
-    vector<string *> *operator() (const string &dirpath,
-				  bool (*filter)(const string &, void *arg),
-				  void *arg, 
-				  bool match_fullpath = true,
-				  bool return_fullpath = true,
-				  long limit = -1) {
-	    return run_scan (dirpath,
-			     (bool (PathScanner::*)(const string &)) 0, 
-			     filter, 
-			     arg,
-			     match_fullpath,
-			     return_fullpath, 
-			     limit);
-    }
+	vector<string *> *operator() (const string &dirpath,
+				      bool (*filter)(const string &, void *arg),
+				      void *arg, 
+				      bool match_fullpath = true,
+				      bool return_fullpath = true,
+				      long limit = -1,
+				      bool recurse = false) {
+		return run_scan (dirpath,
+				 (bool (PathScanner::*)(const string &)) 0, 
+				 filter, 
+				 arg,
+				 match_fullpath,
+				 return_fullpath, 
+				 limit, recurse);
+	}
 
-    vector<string *> *operator() (const string &dirpath,
-				  const string &regexp,
-				  bool match_fullpath = true,
-				  bool return_fullpath = true,
-				  long limit = -1);
-
+	vector<string *> *operator() (const string &dirpath,
+				      const string &regexp,
+				      bool match_fullpath = true,
+				      bool return_fullpath = true,
+				      long limit = -1,
+				      bool recurse = false);
     
-    string *find_first (const string &dirpath,
-			const string &regexp,
-			bool match_fullpath = true,
-			bool return_fullpath = true);
-
-    string *find_first (const string &dirpath,
-			bool (*filter)(const string &, void *),
-			void *arg,
-			bool match_fullpath = true,
-			bool return_fullpath = true);
-
+	string *find_first (const string &dirpath,
+			    const string &regexp,
+			    bool match_fullpath = true,
+			    bool return_fullpath = true);
+	
+	string *find_first (const string &dirpath,
+			    bool (*filter)(const string &, void *),
+			    void *arg,
+			    bool match_fullpath = true,
+			    bool return_fullpath = true);
+	
   private:
-    regex_t compiled_pattern;
-    
-    bool regexp_filter (const string &str) {
-	    return regexec (&compiled_pattern, str.c_str(), 0, 0, 0) == 0;
-    }
+	regex_t compiled_pattern;
+	
+	bool regexp_filter (const string &str) {
+		return regexec (&compiled_pattern, str.c_str(), 0, 0, 0) == 0;
+	}
+	
+	vector<string *> *run_scan (const string &dirpath,
+				    bool (PathScanner::*mfilter) (const string &),
+				    bool (*filter)(const string &, void *),
+				    void *arg,
+				    bool match_fullpath,
+				    bool return_fullpath,
+				    long limit,
+				    bool recurse = false);
 
-    vector<string *> *run_scan (const string &dirpath,
-				bool (PathScanner::*mfilter) (const string &),
-				bool (*filter)(const string &, void *),
-				void *arg,
-				bool match_fullpath,
-				bool return_fullpath,
-				long limit);
-    
-
+	vector<string *> *run_scan_internal (vector<string*>*, 
+					     const string &dirpath,
+				    bool (PathScanner::*mfilter) (const string &),
+				    bool (*filter)(const string &, void *),
+				    void *arg,
+				    bool match_fullpath,
+				    bool return_fullpath,
+				    long limit,
+				    bool recurse = false);
 };
 
 #endif // __libmisc_pathscanner_h__
