960f266b9f6f4bf59abaeee51a5b0c42e7eebb31
Carl Hetherington <carl@carlh.net>
2009-06-08 23:58:46 +0000

    Fix tranzport and SCons builds.
    git-svn-id: svn://localhost/ardour2/branches/3.0@5133 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/surfaces/tranzport/general.cc
+++ libs/surfaces/tranzport/general.cc
@@ -121,7 +121,7 @@ int TranzportControlProtocol::lights_show_normal()
 	/* Global settings */
 
 	lights_pending[LightLoop]        = session->get_play_loop(); 
-	lights_pending[LightPunch]       = Config->get_punch_in() || Config->get_punch_out();
+	lights_pending[LightPunch]       = session->config.get_punch_in() || session->config.get_punch_out();
 	lights_pending[LightRecord]      = session->get_record_enabled();
 	lights_pending[LightAnysolo]     = session->soloing();
 
