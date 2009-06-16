5924788df5b87b287e75d6ef31088ac0951f8799
Taybin Rutkin <taybin@taybin.com>
2009-06-16 01:52:22 +0000

    add CrashReporter cocoa project template
    git-svn-id: svn://localhost/ardour2/branches/3.0@5199 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ tools/CrashReporter/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CrashReporter
+//
+//  Created by Taybin Rutkin on 6/10/09.
+//  Copyright Penguin Sounds 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
