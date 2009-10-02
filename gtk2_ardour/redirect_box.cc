91cb4157b9397da1637b7a7b21dd341c181fb419
Paul Davis <paul@linuxaudiosystems.com>
2009-10-02 21:36:32 +0000

    better fix for send/redirect renaming, plus (sorry) small comment change in public_editor.h and you know that means
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@5725 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/redirect_box.cc
+++ gtk2_ardour/redirect_box.cc
@@ -918,13 +918,13 @@ RedirectBox::rename_redirect (boost::shared_ptr<Redirect> redirect)
 	switch (name_prompter.run ()) {
 
 	case Gtk::RESPONSE_ACCEPT:
-        name_prompter.get_result (result);
-        if (result.length()) {
-			if (ARDOUR_UI::instance()->the_editor().get_named_time_axis(result) != 0) {
+		name_prompter.get_result (result);
+		if (result.length()) {
+			if (_session.route_by_name (result)) {
 				ARDOUR_UI::instance()->popup_error (_("A track already exists with that name"));
 				return;
 			}
-
+			
 			redirect->set_name (result, this);
 		}	
 		break;
