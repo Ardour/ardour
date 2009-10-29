67b57c8df9b6202dd9c3e95b450d4d5c214023c7
David Robillard <d@drobilla.net>
2009-10-29 18:26:03 +0000

    Remove pointless and broken PluginInfo copy constructor. Never do this...
    git-svn-id: svn://localhost/ardour2/branches/3.0@5972 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/ardour/plugin.h
+++ libs/ardour/ardour/plugin.h
@@ -52,15 +52,6 @@ typedef boost::shared_ptr<Plugin> PluginPtr;
 class PluginInfo {
   public:
 	PluginInfo () { }
-	PluginInfo (const PluginInfo &o)
-		: name(o.name),
-		category (o.category),
-		creator (o.creator),
-		path (o.path),
-		n_inputs(o.n_inputs),
-		n_outputs(o.n_outputs),
-		unique_id(o.unique_id),
-		index(o.index) {}
 	virtual ~PluginInfo () { }
 
 	std::string name;
