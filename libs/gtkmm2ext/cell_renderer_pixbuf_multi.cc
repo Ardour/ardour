7ec2888c32de1a9f7ac77f82467399a916d3454c
Paul Davis <paul@linuxaudiosystems.com>
2009-11-21 19:25:54 +0000

    new multistate custom cell pixbuf renderer
    git-svn-id: svn://localhost/ardour2/branches/3.0@6147 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ libs/gtkmm2ext/cell_renderer_pixbuf_multi.cc
@@ -0,0 +1,91 @@
+/*
+    Copyright (C) 2009 Paul Davis
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    $Id: cell_renderer_toggle_pixbuf.cc $
+*/
+
+#include <iostream>
+#include <gtkmm.h>
+
+#include <gtkmm2ext/cell_renderer_pixbuf_multi.h>
+
+using namespace std;
+using namespace Gtk;
+using namespace Gdk;
+using namespace Glib;
+using namespace Gtkmm2ext;
+
+
+CellRendererPixbufMulti::CellRendererPixbufMulti() : 
+	Glib::ObjectBase( typeid(CellRendererPixbufMulti) ),
+	Gtk::CellRenderer(),
+	property_state_(*this, "active", 0)
+{
+	property_mode() = Gtk::CELL_RENDERER_MODE_ACTIVATABLE;
+	property_xpad() = 2;
+	property_ypad() = 2;
+	property_sensitive() = false;
+}
+
+Glib::PropertyProxy<uint32_t> 
+CellRendererPixbufMulti::property_state()
+{
+	return property_state_.get_proxy();
+} 
+
+// Overridden methods of the parent CellRenderer  
+Glib::PropertyProxy_Base 
+CellRendererPixbufMulti::_property_renderable()
+{
+	return property_state();
+}  
+
+bool 
+CellRendererPixbufMulti::activate_vfunc(GdkEvent*, Gtk::Widget&, const Glib::ustring& path, const Gdk::Rectangle&, const Gdk::Rectangle&, Gtk::CellRendererState)
+{
+	signal_changed_(path);
+	return true;
+}
+
+void 
+CellRendererPixbufMulti::render_vfunc (const Glib::RefPtr<Gdk::Drawable>& window, Gtk::Widget& /*widget*/, const Gdk::Rectangle& /*background_area*/, const Gdk::Rectangle& cell_area, const Gdk::Rectangle& /*expose_area*/, Gtk::CellRendererState /*flags*/)
+{
+	int offset_width = 0;
+	int offset_height = 0;
+	Glib::RefPtr<Pixbuf> pb = _pixbufs[property_state()];
+
+	offset_width = cell_area.get_x() +  (int)(cell_area.get_width() - pb->get_width())/2;
+	offset_height = cell_area.get_y() + (int)(cell_area.get_height() - pb->get_height())/2;
+	
+	window->draw_pixbuf (RefPtr<GC>(), pb, 0, 0, offset_width, offset_height, -1, -1, Gdk::RGB_DITHER_NORMAL, 0, 0);
+}
+
+void 
+CellRendererPixbufMulti::get_size_vfunc (Gtk::Widget& /*widget*/, const Gdk::Rectangle* /*cell_area*/, int* /*x_offset*/, int* /*y_offset*/, int* /*width*/, int* /*height*/) const
+{
+}
+
+void
+CellRendererPixbufMulti::set_pixbuf(uint32_t which, Glib::RefPtr<Gdk::Pixbuf> pixbuf){
+	_pixbufs[which] = pixbuf;
+}
+
+CellRendererPixbufMulti::SignalChanged& 
+CellRendererPixbufMulti::signal_changed()
+{
+  return signal_changed_;
+}
---
+++ libs/gtkmm2ext/gtkmm2ext/cell_renderer_pixbuf_multi.h
@@ -0,0 +1,79 @@
+/*
+    Copyright (C) 2000-2009 Paul Davis 
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef __gtkmm2ext_cell_renderer_pixbuf_multi_h__ 
+#define __gtkmm2ext_cell_renderer_pixbuf_multi_h__
+
+#ifdef GTKOSX
+#include <MacTypes.h>
+#if defined(Style)
+#undef style
+#endif
+#endif
+
+#include <map>
+
+#include <gtkmm/drawingarea.h>
+#include <gtkmm/adjustment.h>
+#include <gtkmm/widget.h>
+#include <gtkmm/cellrenderer.h>
+#include <gdkmm.h>
+
+using namespace Gtk;
+
+namespace Gtkmm2ext {
+
+class CellRendererPixbufMulti : public Gtk::CellRenderer
+{
+  public:
+
+	CellRendererPixbufMulti();  
+	virtual ~CellRendererPixbufMulti(){};
+
+	virtual void render_vfunc (const Glib::RefPtr<Gdk::Drawable>& window, Gtk::Widget& widget, const Gdk::Rectangle& background_area, const Gdk::Rectangle& cell_area, const Gdk::Rectangle& expose_area, Gtk::CellRendererState flags);
+
+	virtual void get_size_vfunc (Gtk::Widget& widget, const Gdk::Rectangle* cell_area, int* x_offset, int* y_offset, int* width, int* height) const;
+
+	virtual bool activate_vfunc(GdkEvent*, Gtk::Widget&, const Glib::ustring& path, const Gdk::Rectangle&, const Gdk::Rectangle&, Gtk::CellRendererState);
+
+	Glib::PropertyProxy_Base _property_renderable();
+
+	Glib::PropertyProxy<uint32_t> property_state();
+
+	void set_pixbuf(uint32_t state, Glib::RefPtr<Gdk::Pixbuf> pixbuf);
+	
+	typedef sigc::signal<void, const Glib::ustring&> SignalChanged;
+
+	SignalChanged& signal_changed();
+
+  protected:
+  
+  private:
+	Glib::Property<uint32_t> property_state_; 
+
+	std::map<uint32_t,Glib::RefPtr<Gdk::Pixbuf> > _pixbufs;
+
+	//void on_cell_toggled(const Glib::ustring& path_string);
+
+	SignalChanged signal_changed_;
+};
+
+} // namespace
+
+#endif /* __gtkmm2ext_cell_renderer_pixbuf_multi_h__ */
