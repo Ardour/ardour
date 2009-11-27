7c83541a1155431e893ba6511ebdadca09369947
Paul Davis <paul@linuxaudiosystems.com>
2009-11-27 17:28:19 +0000

    use older gtkmm API to give key focus ability to the processor box
    git-svn-id: svn://localhost/ardour2/branches/3.0@6188 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/processor_box.cc
+++ gtk2_ardour/processor_box.cc
@@ -284,7 +284,7 @@ ProcessorBox::ProcessorBox (ARDOUR::Session& sess, sigc::slot<PluginSelector*> g
 	processor_scroller.add (processor_display);
 	pack_start (processor_scroller, true, true);
 
-	processor_display.set_can_focus ();
+	processor_display.set_flags (CAN_FOCUS);
 	processor_display.set_name ("ProcessorSelector");
 	processor_display.set_size_request (48, -1);
 	processor_display.set_data ("processorbox", this);
