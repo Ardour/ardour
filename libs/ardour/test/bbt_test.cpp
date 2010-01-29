2f09d2353e47e66dd00d81847bbdd0e41fcf66c3
Hans Baier <hansfbaier@googlemail.com>
2010-01-29 05:17:57 +0000

    BBTTest: make it pass
    git-svn-id: svn://localhost/ardour2/branches/3.0@6585 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/test/bbt_test.cpp
+++ libs/ardour/test/bbt_test.cpp
@@ -11,10 +11,22 @@ void
 BBTTest::addTest ()
 {
 	TempoMap map(48000);
+	Tempo    tempo(120);
+	Meter    meter(4.0, 4.0);
+
+	map.add_meter (meter, BBT_Time(1, 1, 0));
 
 	// Test basic operations with a flat tempo map
-	CPPUNIT_ASSERT(map.bbt_add(BBT_Time(0, 0, 0), BBT_Time(1, 2, 3)) == BBT_Time(1, 2, 3));
-	CPPUNIT_ASSERT(map.bbt_add(BBT_Time(1, 2, 3), BBT_Time(0, 0, 0)) == BBT_Time(1, 2, 3));
+	BBT_Time time = map.bbt_add(BBT_Time(1, 1, 0), BBT_Time(1, 2, 3));
+	//cerr << "result: BBT_Time(" << time.bars << ", " << time.beats << ", " 
+	//     << time.ticks << ")" << endl;
+	CPPUNIT_ASSERT(time == BBT_Time(2, 3, 0));
+
+
+	time = map.bbt_add(BBT_Time(1, 2, 3), BBT_Time(2, 2, 3));
+	//cerr << "result: BBT_Time(" << time.bars << ", " << time.beats << ", " 
+	//     << time.ticks << ")" << endl;
+	CPPUNIT_ASSERT(time == BBT_Time(3, 4, 3));
 }
 
 void
