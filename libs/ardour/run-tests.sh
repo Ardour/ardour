075a4d94507f9209cb630e55570ab08ef141a17b
Hans Baier <hansfbaier@googlemail.com>
2010-01-29 05:18:03 +0000

    libs/ardour/run-tests.h: add gdb debugging option
    git-svn-id: svn://localhost/ardour2/branches/3.0@6586 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/run-tests.sh
+++ libs/ardour/run-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! -f './tempo.cc' ]; then
     echo "This script must be run from within the libs/ardour directory";
@@ -12,6 +12,11 @@ libs='libs'
 
 export LD_LIBRARY_PATH=$libs/audiographer:$libs/vamp-sdk:$libs/surfaces:$libs/surfaces/control_protocol:$libs/ardour:$libs/midi++2:$libs/pbd:$libs/rubberband:$libs/soundtouch:$libs/gtkmm2ext:$libs/sigc++2:$libs/glibmm2:$libs/gtkmm2/atk:$libs/gtkmm2/pango:$libs/gtkmm2/gdk:$libs/gtkmm2/gtk:$libs/libgnomecanvasmm:$libs/libsndfile:$libs/appleutility:$libs/cairomm:$libs/taglib:$libs/evoral:$libs/evoral/src/libsmf:$LD_LIBRARY_PATH
 
-echo LD_LIBRARY_PATH = $LD_LIBRARY_PATH
+echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
-./libs/ardour/run-tests
+if [ "$1" == "--debug" ]
+then
+        gdb ./libs/ardour/run-tests
+else
+        ./libs/ardour/run-tests
+fi
