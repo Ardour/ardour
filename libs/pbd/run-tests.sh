2fa02997dd7fdc88270f2f35256ce37459087b0b
Carl Hetherington <carl@carlh.net>
2010-04-02 14:55:34 +0000

    Add script to run libpbd tests.
    git-svn-id: svn://localhost/ardour2/branches/3.0@6832 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ libs/pbd/run-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -f './id.cc' ]; then
+    echo "This script must be run from within the libs/pbd directory";
+    exit 1;
+fi
+
+srcdir=`pwd`
+cd ../../build/default
+
+libs='libs'
+
+export LD_LIBRARY_PATH=$libs/audiographer:$libs/vamp-sdk:$libs/surfaces:$libs/surfaces/control_protocol:$libs/ardour:$libs/midi++2:$libs/pbd:$libs/rubberband:$libs/soundtouch:$libs/gtkmm2ext:$libs/sigc++2:$libs/glibmm2:$libs/gtkmm2/atk:$libs/gtkmm2/pango:$libs/gtkmm2/gdk:$libs/gtkmm2/gtk:$libs/libgnomecanvasmm:$libs/libsndfile:$libs/appleutility:$libs/cairomm:$libs/taglib:$libs/evoral:$libs/evoral/src/libsmf:$LD_LIBRARY_PATH
+
+if [ "$1" == "--debug" ]
+then
+        gdb ./libs/pbd/run-tests
+else
+        ./libs/pbd/run-tests
+fi
