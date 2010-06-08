2067937ecfb9e2db2e22631478c5c4548e0c47c7
Carl Hetherington <carl@carlh.net>
2010-06-08 01:30:03 +0000

    Copy-edit.
    git-svn-id: svn://localhost/ardour2/branches/3.0@7244 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/route_time_axis.cc
+++ gtk2_ardour/route_time_axis.cc
@@ -436,7 +436,7 @@ RouteTimeAxisView::build_display_menu ()
 	MenuList& items = display_menu->items();
 	display_menu->set_name ("ArdourContextMenu");
 
-	items.push_back (MenuElem (_("Color"), sigc::mem_fun(*this, &RouteTimeAxisView::select_track_color)));
+	items.push_back (MenuElem (_("Color..."), sigc::mem_fun(*this, &RouteTimeAxisView::select_track_color)));
 
 	items.push_back (SeparatorElem());
 
