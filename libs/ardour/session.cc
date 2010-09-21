e6ecd36b266c173a65371d50cde5b955072a98ff
Carl Hetherington <carl@carlh.net>
2010-09-21 15:02:45 +0000

    Revert previous commit, adding comment as to why.
    git-svn-id: svn://localhost/ardour2/branches/3.0@7822 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/session.cc
+++ libs/ardour/session.cc
@@ -1095,6 +1095,14 @@ Session::maybe_enable_record ()
 
 	g_atomic_int_set (&_record_status, Enabled);
 
+	/* This function is currently called from somewhere other than an RT thread.
+	   This save_state() call therefore doesn't impact anything.  Doing it here
+	   means that we save pending state of which sources the next record will use,
+	   which gives us some chance of recovering from a crash during the record.
+	*/
+	
+	save_state ("", true);
+	
 	if (_transport_speed) {
 		if (!config.get_punch_in()) {
 			enable_record ();
