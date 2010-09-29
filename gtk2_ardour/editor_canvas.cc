dea1c24d6c96fb4c089069b7d85d4adab98bc4a1
Paul Davis <paul@linuxaudiosystems.com>
2010-09-29 15:47:41 +0000

    compute height of track controls layout from track_views, not the route list ListStore - more robust against future possible changes in mixbus and trunk
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@7859 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/editor_canvas.cc
+++ gtk2_ardour/editor_canvas.cc
@@ -377,16 +377,12 @@ Editor::track_canvas_size_allocated ()
 void
 Editor::controls_layout_size_request (Requisition* req)
 {
-	TreeModel::Children rows = route_display_model->children();
-	TreeModel::Children::iterator i;
 	double pos;
 	bool changed = false;
 
-	for (pos = 0, i = rows.begin(); i != rows.end(); ++i) {
-		TimeAxisView *tv = (*i)[route_display_columns.tv];
-		if (tv != 0) {
-			pos += tv->effective_height;
-		}
+	pos = 0;
+	for (TrackViewList::iterator i = track_views.begin(); i != track_views.end(); ++i) {
+		pos += (*i)->effective_height;
 	}
 
 	gint height = min ((gint) pos, (gint) (physical_screen_height (get_window()) - 600));
