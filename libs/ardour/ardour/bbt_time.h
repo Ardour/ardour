844b7d0f684eaf01a8d553f935027f496a09c3ad
Carl Hetherington <carl@carlh.net>
2010-11-04 20:37:36 +0000

    Reformat.
    git-svn-id: svn://localhost/ardour2/branches/3.0@7967 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/ardour/bbt_time.h
+++ libs/ardour/ardour/bbt_time.h
@@ -26,30 +26,30 @@
 namespace ARDOUR {
 
 struct BBT_Time {
-    uint32_t bars;
-    uint32_t beats;
-    uint32_t ticks;
-    
-    BBT_Time() {
-	    bars = 1;
-	    beats = 1;
-	    ticks = 0;
-    }
-    
-    BBT_Time (uint32_t ba, uint32_t be, uint32_t t)
-         : bars (ba), beats (be), ticks (t) {}
-
-    bool operator< (const BBT_Time& other) const {
-	    return bars < other.bars ||
-		    (bars == other.bars && beats < other.beats) ||
-		    (bars == other.bars && beats == other.beats && ticks < other.ticks);
-    }
-    
-    bool operator== (const BBT_Time& other) const {
-	    return bars == other.bars && beats == other.beats && ticks == other.ticks;
-    }
+	uint32_t bars;
+	uint32_t beats;
+	uint32_t ticks;
+	
+	BBT_Time() {
+		bars = 1;
+		beats = 1;
+		ticks = 0;
+	}
+	
+	BBT_Time (uint32_t ba, uint32_t be, uint32_t t)
+		: bars (ba), beats (be), ticks (t) {}
+	
+	bool operator< (const BBT_Time& other) const {
+		return bars < other.bars ||
+			(bars == other.bars && beats < other.beats) ||
+			(bars == other.bars && beats == other.beats && ticks < other.ticks);
+	}
+	
+	bool operator== (const BBT_Time& other) const {
+		return bars == other.bars && beats == other.beats && ticks == other.ticks;
+	}
 };
-
+	
 }
 
 inline std::ostream&
