f3ba8e83b572c93ac8e5b8928dda132a8d4d3d9a
Paul Davis <paul@linuxaudiosystems.com>
2010-12-05 21:01:49 +0000

    debugging with fkeymo 3
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@8180 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/crossfade.cc
+++ libs/ardour/crossfade.cc
@@ -803,10 +803,10 @@ Crossfade::set_state (const XMLNode& node)
 			_fade_in.add (x, y);
 		}
 	}
-#endif
 
 	_fade_in.front()->value=0.0;
 	_fade_in.back()->value=1.0;
+#endif
 	_fade_in.thaw ();
 	
         /* fade out */
