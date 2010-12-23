203257bd6e54bc427d9e5e9acaf438c9d91af71f
Paul Davis <paul@linuxaudiosystems.com>
2010-12-23 04:11:23 +0000

    a few bits of EPA debugging
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@8336 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/pbd/epa.cc
+++ libs/pbd/epa.cc
@@ -1,4 +1,24 @@
+/*
+    Copyright (C) 2010 Paul Davis 
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
 #include <cstdlib>
+#include <iostream>
 
 #include "pbd/epa.h"
 
@@ -6,6 +26,8 @@ extern char** environ;
 
 using namespace PBD;
 using namespace std;
+
+EnvironmentalProtectionAgency* EnvironmentalProtectionAgency::_global_epa = 0;
 
 EnvironmentalProtectionAgency::EnvironmentalProtectionAgency ()
 {
@@ -35,6 +57,8 @@ EnvironmentalProtectionAgency::save ()
                 string before = estring.substr (0, equal);
                 string after = estring.substr (equal+1);
 
+                cerr << "Save [" << before << "] = " << after << endl;
+
                 e.insert (pair<string,string>(before,after));
         }
 }                         
@@ -42,6 +66,7 @@ void
 EnvironmentalProtectionAgency::restore ()
 {
         for (map<string,string>::iterator i = e.begin(); i != e.end(); ++i) {
+                cerr << "Restore [" << i->first << "] = " << i->second << endl;
                 setenv (i->first.c_str(), i->second.c_str(), 1);
         }
 }                         
