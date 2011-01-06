97abd700dacdcf846765f8faaeda7f8c6ac9e059
Todd Naugle <toddn@glw.com>
2011-01-06 22:11:04 +0000

    Add release and svn version numbers to bundles and packages. Added a script to create a package with installer scripts inside.
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@8472 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- tools/linux_packaging/install.sh
+++ tools/linux_packaging/install.sh
@@ -4,7 +4,13 @@
 # the real install script.
 
 if [ -z $WINDOWID ]; then
-	exec xterm -e ./stage2.run
+	if which xterm > /dev/null; then
+		exec xterm -e ./stage2.run
+	elif which gnome-terminal > /dev/null; then
+		exec gnome-terminal -e ./stage2.run
+	elif which konsole > /dev/null; then
+		exec konsole -e ./stage2.run
+	fi
 else
 	./stage2.run
 fi
