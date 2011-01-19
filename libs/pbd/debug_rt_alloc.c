ba78410f13abfcba3388eee38d686679a015b504
Carl Hetherington <carl@carlh.net>
2011-01-19 21:49:14 +0000

    Missing files.
    git-svn-id: svn://localhost/ardour2/branches/3.0@8550 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ libs/pbd/debug_rt_alloc.c
@@ -0,0 +1,78 @@
+/*
+    Copyright (C) 2011 Paul Davis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifdef DEBUG_RT_ALLOC
+
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+int (*pbd_alloc_allowed) () = 0;
+
+/** Thread-local key whose value is set to 1 if malloc checking is disabled
+ *  for this thread, 0 otherwise.
+ */
+
+static pthread_key_t disabled;
+
+static pthread_once_t once;
+
+static void
+make_key (void)
+{
+	(void) pthread_key_create (&disabled, NULL);
+}
+
+/** This is our malloc which overrides the system one */
+void* malloc (size_t s)
+{
+	static void * (*real_malloc) (size_t) = NULL;
+	if (!real_malloc) {
+		/* find the system malloc */
+		real_malloc = dlsym (RTLD_NEXT, "malloc");
+	}
+
+	(void) pthread_once (&once, make_key);
+
+	if (pthread_getspecific (disabled) == NULL && pbd_alloc_allowed && !pbd_alloc_allowed ()) {
+		/* pbd_alloc_allowed says that this malloc is not permitted */
+		abort ();
+	}
+
+	/* Pass through to the system malloc */
+	return real_malloc (s);
+}
+
+void
+suspend_rt_malloc_checks ()
+{
+	(void) pthread_once (&once, make_key);
+	pthread_setspecific (disabled, (void *) 1);
+}
+
+void
+resume_rt_malloc_checks ()
+{
+	(void) pthread_once (&once, make_key);
+	pthread_setspecific (disabled, (void *) 0);
+}
+
+#endif
---
+++ libs/pbd/pbd/debug_rt_alloc.h
@@ -0,0 +1,49 @@
+/*
+    Copyright (C) 2011 Paul Davis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifdef DEBUG_RT_ALLOC
+
+#ifndef __pbd_debug_rt_alloc_h__
+#define __pbd_debug_rt_alloc_h__
+
+extern "C" {
+
+/** Should be set to point to a function which returns non-0 if a malloc is
+ *  allowed in the current situation, or 0 if not.
+ */
+extern int (*pbd_alloc_allowed) ();
+
+/** Call this to suspend malloc checking until a call to resume_rt_malloc_checks */
+extern void suspend_rt_malloc_checks ();
+
+/** Resume malloc checking after a suspension */	
+extern void resume_rt_malloc_checks ();
+
+}
+
+#endif
+
+#else
+
+#define suspend_rt_malloc_checks () {}
+#define resume_rt_malloc_checks () {}
+
+#endif
+
+
