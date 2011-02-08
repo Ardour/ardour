b7a26970d43a18b16b824836bb9467bed0faa99e
Todd Naugle <toddn@glw.com>
2011-02-08 22:23:44 +0000

    A more robust way to ensure the PWD is correct. Look for gnome or kde terminals before falling back to xterm
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@8780 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- tools/linux_packaging/install.sh
+++ tools/linux_packaging/install.sh
@@ -6,19 +6,20 @@
 # Some systems don't correctly set the PWD when a script is double-clicked,
 # so go ahead and figure out our path and make sure we are in that directory.
 
+SAVED_PWD=$PWD
 PKG_PATH=$(dirname $(readlink -f $0))
-pushd ${PKG_PATH}
+cd ${PKG_PATH}
 
 if [ -z "$TERM" ] || [ "$TERM" == "dumb" ]; then
-	if which xterm > /dev/null; then
-		exec xterm -e ${PKG_PATH}/stage2.run
-	elif which gnome-terminal > /dev/null; then
-		exec gnome-terminal -e ${PKG_PATH}/stage2.run
+	if which gnome-terminal > /dev/null; then
+		exec gnome-terminal -e ${PKG_PATH}/.stage2.run
 	elif which konsole > /dev/null; then
-		exec konsole -e ${PKG_PATH}/stage2.run
+		exec konsole -e ${PKG_PATH}/.stage2.run
+	elif which xterm > /dev/null; then
+		exec xterm -e ${PKG_PATH}/.stage2.run
 	fi
 else
-	${PKG_PATH}/stage2.run
+	${PKG_PATH}/.stage2.run
 fi
 
-popd
+cd ${SAVED_PWD}
