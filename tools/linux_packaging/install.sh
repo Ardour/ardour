c6af72af12a2113f60a3a300636bd03f178fd832
Todd Naugle <toddn@glw.com>
2011-02-09 16:24:20 +0000

    switch back to xterm being the first choice of terminals to use. This solves an issues with avlinux
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@8807 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- tools/linux_packaging/install.sh
+++ tools/linux_packaging/install.sh
@@ -11,12 +11,12 @@ PKG_PATH=$(dirname $(readlink -f $0))
 cd ${PKG_PATH}
 
 if [ -z "$TERM" ] || [ "$TERM" = "dumb" ]; then
-	if which gnome-terminal > /dev/null; then
+	if which xterm > /dev/null; then
+		exec xterm -e ${PKG_PATH}/.stage2.run
+	elif which gnome-terminal > /dev/null; then
 		exec gnome-terminal -e ${PKG_PATH}/.stage2.run
 	elif which konsole > /dev/null; then
 		exec konsole -e ${PKG_PATH}/.stage2.run
-	elif which xterm > /dev/null; then
-		exec xterm -e ${PKG_PATH}/.stage2.run
 	fi
 else
 	${PKG_PATH}/.stage2.run
