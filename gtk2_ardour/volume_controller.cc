fe6ca1786f0a1d494ddc379acc3a9e4a34738acf
Paul Davis <paul@linuxaudiosystems.com>
2011-02-11 18:02:15 +0000

    add back working version of VolumeController
    git-svn-id: svn://localhost/ardour2/branches/3.0@8827 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ gtk2_ardour/volume_controller.cc
@@ -0,0 +1,72 @@
+/*
+    Copyright (C) 1998-2007 Paul Davis
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    $Id: volume_controller.cc,v 1.4 2000/05/03 15:54:21 pbd Exp $
+*/
+
+#include <string.h>
+#include <limits.h>
+
+#include "pbd/controllable.h"
+
+#include "gtkmm2ext/gui_thread.h"
+#include "ardour/utils.h"
+
+#include "volume_controller.h"
+
+using namespace Gtk;
+
+VolumeController::VolumeController (Glib::RefPtr<Gdk::Pixbuf> p,
+				    Gtk::Adjustment *adj,
+				    bool with_numeric,
+                                    int subw, int subh)
+
+	: MotionFeedback (p, MotionFeedback::Rotary, "", adj, with_numeric, subw, subh)
+{			  
+	get_adjustment()->signal_value_changed().connect (mem_fun (*this,&VolumeController::adjustment_value_changed));
+}
+
+void
+VolumeController::set_controllable (boost::shared_ptr<PBD::Controllable> c)
+{
+        MotionFeedback::set_controllable (c);
+
+        controllable_connection.disconnect ();
+
+        if (c) {
+                c->Changed.connect (controllable_connection, MISSING_INVALIDATOR, boost::bind (&VolumeController::controllable_value_changed, this), gui_context());
+        }
+}
+
+void
+VolumeController::controllable_value_changed ()
+{
+        boost::shared_ptr<PBD::Controllable> c = controllable();
+        if (c) {
+                get_adjustment()->set_value (gain_to_slider_position (c->get_value ()));
+        }
+}
+
+void
+VolumeController::adjustment_value_changed ()
+{
+        boost::shared_ptr<PBD::Controllable> c = controllable();
+        if (c) {
+                c->set_value (slider_position_to_gain (get_adjustment()->get_value()));
+        }
+}
+
+
---
+++ gtk2_ardour/volume_controller.h
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) 1998-2007 Paul Davis 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    $Id: volume_controller.h,v 1.4 2000/05/03 15:54:21 pbd Exp $
+*/
+
+#ifndef __gtk_ardour_vol_controller_h__
+#define __gtk_ardour_vol_controller_h__
+
+#include <gtkmm/adjustment.h>
+
+#include "gtkmm2ext/motionfeedback.h"
+
+// march 2010: this exists as a placeholder to add a controllable, but maybe it will
+// end up in MotionFeedback
+
+class VolumeController : public Gtkmm2ext::MotionFeedback
+{
+  public:
+	VolumeController (Glib::RefPtr<Gdk::Pixbuf>,
+			  Gtk::Adjustment *adj,
+			  bool with_numeric = true,
+                          int image_width = 40,
+                          int image_height = 40);
+
+        virtual ~VolumeController () {}
+        void set_controllable (boost::shared_ptr<PBD::Controllable> c);
+
+  private:
+	Gtk::Adjustment *adjustment;
+        PBD::ScopedConnection controllable_connection;
+
+	void adjustment_value_changed ();
+	void controllable_value_changed ();
+};
+
+#endif // __gtk_ardour_vol_controller_h__		
+
+
