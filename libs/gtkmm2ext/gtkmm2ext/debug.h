d5e44d00a61ce80767393fcc4b821d2ce093d097
Carl Hetherington <carl@carlh.net>
2011-03-01 23:28:02 +0000

    Missing files.
    git-svn-id: svn://localhost/ardour2/branches/3.0@9020 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ libs/gtkmm2ext/gtkmm2ext/debug.h
@@ -0,0 +1,33 @@
+/*
+    Copyright (C) 2011 Paul Davis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef __libgtkmm2ext_debug_h__
+#define __libgtkmm2ext_debug_h__
+
+#include <stdint.h>
+
+namespace Gtkmm2ext {
+	namespace DEBUG {
+                extern uint64_t Keyboard;
+                extern uint64_t Bindings;
+	}
+}
+
+#endif /* __libgtkm2ext_debug_h__ */
+
