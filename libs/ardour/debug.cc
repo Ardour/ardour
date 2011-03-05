faeeb292020dc5aab162557409d13dd57c7b42ff
Paul Davis <paul@linuxaudiosystems.com>
2011-03-05 23:15:45 +0000

    some LV2 debug tracing, part three
    git-svn-id: svn://localhost/ardour2/branches/3.0@9080 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/debug.cc
+++ libs/ardour/debug.cc
@@ -46,4 +46,5 @@ uint64_t PBD::DEBUG::Monitor = PBD::new_debug_bit ("monitor");
 uint64_t PBD::DEBUG::Solo = PBD::new_debug_bit ("solo");
 uint64_t PBD::DEBUG::AudioPlayback = PBD::new_debug_bit ("audioplayback");
 uint64_t PBD::DEBUG::Panning = PBD::new_debug_bit ("panning");
+uint64_t PBD::DEBUG::LV2 = PBD::new_debug_bit ("lv2");
 
