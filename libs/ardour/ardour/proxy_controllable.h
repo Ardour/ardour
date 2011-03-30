832e6fb21adda8fa05db725bd8434789bd62b89d
Carl Hetherington <carl@carlh.net>
2011-03-30 12:37:05 +0000

    Whitespace, add GPL boilerplate.
    git-svn-id: svn://localhost/ardour2/branches/3.0@9242 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/ardour/proxy_controllable.h
+++ libs/ardour/ardour/proxy_controllable.h
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2010-2011 Paul Davis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
 #ifndef __libardour_proxy_controllable_h__
 #define __libardour_proxy_controllable_h__
 
@@ -12,23 +31,23 @@ namespace ARDOUR {
 */
 
 class ProxyControllable : public PBD::Controllable {
-  public:
-    ProxyControllable (const std::string& name, PBD::Controllable::Flag flags,
-                                 boost::function1<void,double> setter,
-                                 boost::function0<double> getter)
-            : PBD::Controllable (name, flags)
-            , _setter (setter)
-            , _getter (getter)
-    {}
-    
-    void set_value (double v) { _setter (v); }
-    double get_value () const { return _getter (); }
-
-  private:
-    boost::function1<void,double> _setter;
-    boost::function0<double> _getter;
+public:
+	ProxyControllable (const std::string& name, PBD::Controllable::Flag flags,
+			   boost::function1<void,double> setter,
+			   boost::function0<double> getter)
+		: PBD::Controllable (name, flags)
+		, _setter (setter)
+		, _getter (getter)
+	{}
+	
+	void set_value (double v) { _setter (v); }
+	double get_value () const { return _getter (); }
+	
+private:
+	boost::function1<void,double> _setter;
+	boost::function0<double> _getter;
 };
-
+	
 } // namespace
 
 #endif /* __libardour_proxy_controllable_h__ */
