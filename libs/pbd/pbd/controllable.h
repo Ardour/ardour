05cc0f0db798523cb8c760d8a3f73ae47753f39a
Carl Hetherington <carl@carlh.net>
2011-03-30 12:36:53 +0000

    Spurious character in GPL boilerplate.
    git-svn-id: svn://localhost/ardour2/branches/3.0@9241 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/pbd/pbd/controllable.h
+++ libs/pbd/pbd/controllable.h
@@ -2,7 +2,7 @@
     Copyright (C) 2000-2007 Paul Davis 
 
     This program is free software; you can redistribute it and/or modify
-v    it under the terms of the GNU General Public License as published by
+    it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 
