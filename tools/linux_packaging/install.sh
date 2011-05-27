0f28a081de95f449c6242b0b8a9e6c3bcf31161f
Todd Naugle <toddn@glw.com>
2011-05-27 19:46:51 +0000

    Properly handle spaces in filenames along the path to the installer.
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@9614 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- tools/linux_packaging/install.sh
+++ tools/linux_packaging/install.sh
@@ -7,19 +7,19 @@
 # so go ahead and figure out our path and make sure we are in that directory.
 
 SAVED_PWD=$PWD
-PKG_PATH=$(dirname $(readlink -f $0))
-cd ${PKG_PATH}
+PKG_PATH=$(dirname "$(readlink -f "$0")")
+cd "${PKG_PATH}"
 
 if [ -z "$TERM" ] || [ "$TERM" = "dumb" ]; then
 	if which xterm > /dev/null; then
-		exec xterm -e ${PKG_PATH}/.stage2.run
+		exec xterm -e "${PKG_PATH}/.stage2.run"
 	elif which gnome-terminal > /dev/null; then
-		exec gnome-terminal -e ${PKG_PATH}/.stage2.run
+		exec gnome-terminal -e "${PKG_PATH}/.stage2.run"
 	elif which konsole > /dev/null; then
-		exec konsole -e ${PKG_PATH}/.stage2.run
+		exec konsole -e "${PKG_PATH}/.stage2.run"
 	fi
 else
-	${PKG_PATH}/.stage2.run
+	"${PKG_PATH}/.stage2.run"
 fi
 
-cd ${SAVED_PWD}
+cd "${SAVED_PWD}"
