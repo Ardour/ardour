260c1c1a1360dac54c09baed692e7debb2d95125
Paul Davis <paul@linuxaudiosystems.com>
2011-06-22 14:38:32 +0000

    remove some potential HCI confusion for Gtkmm2ext::UI::popup_error() by using a standard MessageDialog (as noticed by thorwil)
    git-svn-id: svn://localhost/ardour2/branches/3.0@9756 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/gtkmm2ext/gtk_ui.cc
+++ libs/gtkmm2ext/gtk_ui.cc
@@ -650,19 +650,18 @@ UI::handle_fatal (const char *message)
 void
 UI::popup_error (const string& text)
 {
-	PopUp *pup;
-
 	if (!caller_is_ui_thread()) {
 		error << "non-UI threads can't use UI::popup_error"
 		      << endmsg;
 		return;
 	}
 
-	pup = new PopUp (WIN_POS_MOUSE, 0, true);
-	pup->set_text (text);
-	pup->touch ();
+	MessageDialog msg (text);
+	msg.set_title (string_compose (_("I'm sorry %1, I can't do that"), g_get_user_name()));
+	msg.set_wmclass (X_("error"), name());
+	msg.set_position (WIN_POS_MOUSE);
+	msg.run ();
 }
-
 
 void
 UI::flush_pending ()
