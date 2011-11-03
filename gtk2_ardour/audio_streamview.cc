3b6a1adb90da88b314d3b995efa5063d23e51fa0
Ben Loftis <ben@glw.com>
2011-11-03 15:36:23 +0000

    fix for previous patch
    git-svn-id: svn://localhost/ardour2/branches/2.0-ongoing@10419 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/audio_streamview.cc
+++ gtk2_ardour/audio_streamview.cc
@@ -73,7 +73,7 @@ AudioStreamView::AudioStreamView (AudioTimeAxisView& tv)
 AudioStreamView::~AudioStreamView ()
 {
 	for (CrossfadeViewList::iterator xi = crossfade_views.begin(); xi != crossfade_views.end(); ++xi) {
-		delete xi->second;
+		delete (*xi);
 	}
 }
 
