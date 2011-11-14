5de95fc822300102d03c43b376cd071022b824fc
Carl Hetherington <carl@carlh.net>
2011-11-14 11:31:22 +0000

    Fix unit test build by adding some dummy LXVST methods.
    git-svn-id: svn://localhost/ardour2/branches/3.0@10576 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ libs/ardour/test/dummy_lxvst.cc
@@ -0,0 +1,33 @@
+/* Dummy LXVST methods so that libardour can be linked against the test code */
+
+#include "ardour/vstfx.h"
+
+int
+vstfx_init (void* ptr)
+{
+	return 0;
+}
+
+VSTFX *
+vstfx_instantiate (VSTFXHandle* fhandle, audioMasterCallback amc, void* userptr)
+{
+	return 0;
+}
+
+void
+vstfx_close (VSTFX* vstfx)
+{
+
+}
+
+VSTFXHandle *
+vstfx_load (const char *path)
+{
+	return 0;
+}
+
+int
+vstfx_unload (VSTFXHandle* fhandle)
+{
+	return -1;
+}
