22f6b991300e265ecfe0e8fb9e52f36a0cd2912a
Paul Davis <paul@linuxaudiosystems.com>
2011-11-21 16:36:05 +0000

    #ifdef out some expensive code from midibuffer merging (debug only, and we pretty much know it works)
    git-svn-id: svn://localhost/ardour2/branches/3.0@10733 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/midi_buffer.cc
+++ libs/ardour/midi_buffer.cc
@@ -284,6 +284,7 @@ MidiBuffer::merge_in_place(const MidiBuffer &other)
 	}
 
 #ifndef NDEBUG
+#ifdef  TEST_MIDI_MERGE
 	size_t   test_orig_us_size   = _size;
 	size_t   test_orig_them_size = other._size;
 	TimeType test_time           = 0;
@@ -302,6 +303,7 @@ MidiBuffer::merge_in_place(const MidiBuffer &other)
 		test_time = (*i).time();
 		++test_them_count;
 	}
+#endif
 #endif
 
 	const_iterator them = other.begin();
@@ -362,6 +364,7 @@ MidiBuffer::merge_in_place(const MidiBuffer &other)
 	}
 
 #ifndef NDEBUG
+#ifdef  TEST_MIDI_MERGE
 	assert(_size == test_orig_us_size + test_orig_them_size);
 	size_t test_final_count = 0;
 	test_time = 0;
@@ -373,6 +376,7 @@ MidiBuffer::merge_in_place(const MidiBuffer &other)
 		++test_final_count;
 	}
 	assert(test_final_count = test_us_count + test_them_count);
+#endif
 #endif
 
 	return true;
