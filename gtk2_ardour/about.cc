00efc7db6ed7d3f439839106931c3368d6028f92
Carl Hetherington <carl@carlh.net>
2011-11-22 12:48:14 +0000

    Another missing translator.
    git-svn-id: svn://localhost/ardour2/branches/3.0@10771 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/about.cc
+++ gtk2_ardour/about.cc
@@ -181,7 +181,8 @@ static const char* translators[] = {
 \n\tSebastian Arnold <mail@sebastian-arnold.net>\
 \n\tRobert Schwede <schwede@ironshark.com>\
 \n\tBenjamin Scherrer <realhangman@web.de>\
-\n\tEdgar Aichinger\n"),
+\n\tEdgar Aichinger\
+\n\tRichard Oax <richard@pagliacciempire.de>\n"),
 	N_("Italian:\n\tFilippo Pappalardo <filippo@email.it>\n"),
 	N_("Portuguese:\n\tRui Nuno Capela <rncbc@rncbc.org>\n"),
 	N_("Brazilian Portuguese:\n\tAlexander da Franca Fernandes <alexander@nautae.eti.br>\
