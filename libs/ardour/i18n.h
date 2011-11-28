c05e30c04327b1972d51e6186117e36492566cd9
Paul Davis <paul@linuxaudiosystems.com>
2011-11-28 20:38:23 +0000

    add _S() macro to i18n.h for libs/ardour and libs/gtkmm2ext
    git-svn-id: svn://localhost/ardour2/branches/3.0@10834 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/i18n.h
+++ libs/ardour/i18n.h
@@ -12,5 +12,9 @@
 #define N_(Text) gettext_noop (Text)
 #define X_(Text) Text
 #define I18N(Array) PBD::internationalize (PACKAGE, Array)
+/** Use this to translate strings that have different meanings in different places.
+ *  Text should be of the form Context|Message.
+ */
+#define S_(Text) PBD::sgettext (PACKAGE, Text)
 
 #endif // __i18n_h__
--- libs/gtkmm2ext/i18n.h
+++ libs/gtkmm2ext/i18n.h
@@ -7,5 +7,9 @@
 #define _(Text) dgettext (PACKAGE, Text)
 #define N_(Text) gettext_noop (Text)
 #define X_(Text) (Text)
+/** Use this to translate strings that have different meanings in different places.
+ *  Text should be of the form Context|Message.
+ */
+#define S_(Text) PBD::sgettext (PACKAGE, Text)
 
 #endif // __i18n_h__
