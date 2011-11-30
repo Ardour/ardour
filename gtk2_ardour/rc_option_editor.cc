6fe7e0dea117988e777131b33e4901894ba51504
Paul Davis <paul@linuxaudiosystems.com>
2011-11-30 01:54:13 +0000

    fix segfault in preferences dialog caused by incorrect use of S_(). i've checked all other uses in gtk2_ardour and cleared them
    git-svn-id: svn://localhost/ardour2/branches/3.0@10839 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/rc_option_editor.cc
+++ gtk2_ardour/rc_option_editor.cc
@@ -310,7 +310,9 @@ public:
 
 		for (int x = 0; modifiers[x].name; ++x) {
 			if (modifiers[x].modifier == Keyboard::edit_modifier ()) {
-				_edit_modifier_combo.set_active_text (S_(modifiers[x].name));
+				string lookup_str = "key|";
+				lookup_str += modifiers[x].name;
+				_edit_modifier_combo.set_active_text (S_(lookup_str.c_str()));
 				break;
 			}
 		}
