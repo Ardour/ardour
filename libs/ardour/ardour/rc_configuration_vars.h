a1cf2bba3622e6301c9f5fc2ecb649141ddfba28
Paul Davis <paul@linuxaudiosystems.com>
2011-12-02 19:33:46 +0000

    new configuration variable to control display of periodic MIDI messages
    git-svn-id: svn://localhost/ardour2/branches/3.0@10864 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/ardour/rc_configuration_vars.h
+++ libs/ardour/ardour/rc_configuration_vars.h
@@ -169,6 +169,7 @@ CONFIG_VARIABLE (int32_t, processor_usage, "processor-usage", -1)
 CONFIG_VARIABLE (bool, color_regions_using_track_color, "color-regions-using-track-color", false)
 CONFIG_VARIABLE (gain_t, max_gain, "max-gain", 2.0) /* +6.0dB */
 CONFIG_VARIABLE (bool, update_editor_during_summary_drag, "update-editor-during-summary-drag", true)
+CONFIG_VARIABLE (bool, never_display_periodic_midi, "never-display-periodic-midi", true)
 
 /* denormal management */
 
