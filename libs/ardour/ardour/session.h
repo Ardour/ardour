b7688699efbf0b6e185b1a9965a1e7d8f60b9ed3
Carl Hetherington <carl@carlh.net>
2011-12-23 01:38:32 +0000

    Remove unused declaration.
    git-svn-id: svn://localhost/ardour2/branches/3.0@11065 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/ardour/session.h
+++ libs/ardour/ardour/session.h
@@ -369,8 +369,6 @@ class Session : public PBD::StatefulDestructible, public PBD::ScopedConnectionLi
 	void set_auto_loop_location (Location *);
 	int location_name(std::string& result, std::string base = std::string(""));
 
-	void reset_input_monitor_state ();
-
 	pframes_t get_block_size()        const { return current_block_size; }
 	framecnt_t worst_output_latency () const { return _worst_output_latency; }
 	framecnt_t worst_input_latency ()  const { return _worst_input_latency; }
