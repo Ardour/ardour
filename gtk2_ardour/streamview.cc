54ff6901a96c7b77fbdc0abdc79224b763872c13
Carl Hetherington <carl@carlh.net>
2011-12-31 00:19:08 +0000

    Restore update of coverage frames on playlist contents changed, which is needed for trim.
    git-svn-id: svn://localhost/ardour2/branches/3.0@11124 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/streamview.cc
+++ gtk2_ardour/streamview.cc
@@ -338,7 +338,9 @@ StreamView::playlist_switched (boost::weak_ptr<Track> wtr)
 	tr->playlist()->LayeringChanged.connect (playlist_connections, invalidator (*this), boost::bind (&StreamView::playlist_layered, this, boost::weak_ptr<Track> (tr)), gui_context());
 	tr->playlist()->RegionAdded.connect (playlist_connections, invalidator (*this), ui_bind (&StreamView::add_region_view, this, _1), gui_context());
 	tr->playlist()->RegionRemoved.connect (playlist_connections, invalidator (*this), ui_bind (&StreamView::remove_region_view, this, _1), gui_context());
+	tr->playlist()->ContentsChanged.connect (playlist_connections, invalidator (*this), ui_bind (&StreamView::update_coverage_frames, this), gui_context());
 }
+
 
 void
 StreamView::diskstream_changed ()
