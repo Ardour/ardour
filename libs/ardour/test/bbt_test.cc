ce5de59c77a48f10bc60130509db888da3cb2bed
Paul Davis <paul@linuxaudiosystems.com>
2012-01-03 17:36:40 +0000

    remove body of no-longer-relevant tempo-related test
    git-svn-id: svn://localhost/ardour2/branches/3.0@11143 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/test/bbt_test.cc
+++ libs/ardour/test/bbt_test.cc
@@ -17,18 +17,8 @@ BBTTest::addTest ()
 	Meter    meter(4.0, 4.0);
 
 	map.add_meter (meter, BBT_Time(1, 1, 0));
-
-	// Test basic operations with a flat tempo map
-	BBT_Time time = map.bbt_add(BBT_Time(1, 1, 0), BBT_Time(1, 2, 3));
-	//cout << "result: BBT_Time(" << time.bars << ", " << time.beats << ", " 
-	//     << time.ticks << ")" << endl;
-	CPPUNIT_ASSERT(time == BBT_Time(2, 3, 3));
-
-
-	time = map.bbt_add(BBT_Time(1, 2, 3), BBT_Time(2, 2, 3));
-	//cerr << "result: BBT_Time(" << time.bars << ", " << time.beats << ", " 
-	//     << time.ticks << ")" << endl;
-	CPPUNIT_ASSERT(time == BBT_Time(3, 4, 6));
+	
+	/* add some good stuff here */
 }
 
 void
