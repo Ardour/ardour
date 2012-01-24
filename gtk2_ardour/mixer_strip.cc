05451a80f2dd90d2d5ea9f1a529d0573ac24587c
Carl Hetherington <carl@carlh.net>
2012-01-24 21:02:34 +0000

    Remove some never-seen tooltips (overridden by other things).  Clip output button labels to 7 characters rather than 6 to fit the English translation of monitor in (#4627).
    git-svn-id: svn://localhost/ardour2/branches/3.0@11340 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/mixer_strip.cc
+++ gtk2_ardour/mixer_strip.cc
@@ -157,12 +157,10 @@ MixerStrip::init ()
 	ARDOUR_UI::instance()->set_tip (&hide_button, _("Hide this mixer strip"));
 
 	input_button.set_text (_("Input"));
-	ARDOUR_UI::instance()->set_tip (&input_button, _("Button 1 to choose inputs from a port matrix, button 3 to select inputs from a menu"), "");
 	input_button.set_name ("mixer strip button");
 	input_button_box.pack_start (input_button, true, true);
 
 	output_button.set_text (_("Output"));
-	ARDOUR_UI::instance()->set_tip (&output_button, _("Button 1 to choose outputs from a port matrix, button 3 to select inputs from a menu"), "");
 	output_button.set_name ("mixer strip button");
 	Gtkmm2ext::set_size_request_to_display_given_text (output_button, longest_label.c_str(), 4, 4);
 
@@ -1207,7 +1205,7 @@ MixerStrip::update_io_button (boost::shared_ptr<ARDOUR::Route> route, Width widt
 
 	switch (width) {
 	case Wide:
-		label_string = label.str().substr(0, 6);
+		label_string = label.str().substr(0, 7);
 		break;
 	case Narrow:
 		label_string = label.str().substr(0, 3);
