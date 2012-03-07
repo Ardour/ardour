fd1ff3d7536758283c9597642de5d74e600b3273
Carl Hetherington <carl@carlh.net>
2012-03-07 14:40:33 +0000

    Remove unused color_from_style / gc_from_style.
    git-svn-id: svn://localhost/ardour2/branches/3.0@11618 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/utils.h
+++ gtk2_ardour/utils.h
@@ -56,10 +56,6 @@ Pango::FontDescription get_font_for_style (std::string widgetname);
 
 uint32_t rgba_from_style (std::string, uint32_t, uint32_t, uint32_t, uint32_t, std::string = "fg", int = Gtk::STATE_NORMAL, bool = true);
 
-Gdk::Color color_from_style (std::string widget_style_name, int state, std::string attr);
-Glib::RefPtr<Gdk::GC> gc_from_style (std::string widget_style_name, int state, std::string attr);
-
-
 void decorate (Gtk::Window& w, Gdk::WMDecoration d);
 
 bool canvas_item_visible (ArdourCanvas::Item* item);
