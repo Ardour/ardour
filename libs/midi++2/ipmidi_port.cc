50f179b33e7eed095380950e31b41e8792c8942f
Paul Davis <paul@linuxaudiosystems.com>
2012-04-24 03:19:10 +0000

    ipMIDI: credit where credit is due
    git-svn-id: svn://localhost/ardour2/branches/3.0@12074 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/midi++2/ipmidi_port.cc
+++ libs/midi++2/ipmidi_port.cc
@@ -1,5 +1,7 @@
 /*
     Copyright (C) 2012 Paul Davie
+
+    Using code from Rui Nuno Capela's qmidinet as inspiration.
     
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
