ffa2c2cf4069b12d2c981e6d1c8cbef6a8e163a2
Paul Davis <paul@linuxaudiosystems.com>
2012-05-05 18:14:50 +0000

    moron
    git-svn-id: svn://localhost/ardour2/branches/3.0@12193 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/main.cc
+++ gtk2_ardour/main.cc
@@ -370,14 +370,14 @@ fixup_bundle_environment (int /*argc*/, char* argv[])
 
 	if (Glib::file_test ("/etc/fonts/fonts.conf", Glib::FILE_TEST_EXISTS)) {
 		setenv ("FONTCONFIG_FILE", "/etc/fonts/fonts.conf", 1);
-		setenv ("FONTCONFIG_PATH, "/etc/fonts", 1);
+		setenv ("FONTCONFIG_PATH", "/etc/fonts", 1);
 	} else {
 		/* use the one included in the bundle */
 		
 		path = Glib::build_filename (dir_path, "etc/fonts/fonts.conf");
 		setenv ("FONTCONFIG_FILE", path.c_str(), 1);
 		path = Glib::build_filename (dir_path, "etc/fonts");
-		setenv ("FONTCONFIG_PATH, "/etc/fonts", 1);
+		setenv ("FONTCONFIG_PATH", "/etc/fonts", 1);
 	}
 
 	/* write a pango.rc file and tell pango to use it. we'd love
