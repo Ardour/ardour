6beff737fe50ca79b0903a8296d2c2771df52457
Carl Hetherington <carl@carlh.net>
2012-05-19 14:46:50 +0000

    Fix up tests after path changes.
    git-svn-id: svn://localhost/ardour2/branches/3.0@12331 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/ardour/run-tests.sh
+++ libs/ardour/run-tests.sh
@@ -8,16 +8,20 @@ if [ ! -f './tempo.cc' ]; then
     exit 1;
 fi
 
-srcdir=`pwd`
-cd ../../build
+cd ../..
+top=`pwd`
+cd build
 
 libs='libs'
 
 export LD_LIBRARY_PATH=$libs/audiographer:$libs/vamp-sdk:$libs/surfaces:$libs/surfaces/control_protocol:$libs/ardour:$libs/midi++2:$libs/pbd:$libs/rubberband:$libs/soundtouch:$libs/gtkmm2ext:$libs/appleutility:$libs/taglib:$libs/evoral:$libs/evoral/src/libsmf:$libs/timecode:/usr/local/lib:/usr/local/lib64:$LD_LIBRARY_PATH
 
+export ARDOUR_CONFIG_PATH=$top:$top/gtk2_ardour:$libs/..:$libs/../gtk2_ardour
 export ARDOUR_PANNER_PATH=$libs/panners/2in2out:$libs/panners/1in2out:$libs/panners/vbap
 export ARDOUR_SURFACES_PATH=$libs/surfaces/osc:$libs/surfaces/generic_midi:$libs/surfaces/tranzport:$libs/surfaces/powermate:$libs/surfaces/mackie
 export ARDOUR_MCP_PATH="../mcp"
+export ARDOUR_DLL_PATH=$libs
+export ARDOUR_DATA_PATH=$top/gtk2_ardour:$top/build/gtk2_ardour:.
 
 if [ "$1" == "--debug" ]; then
         gdb ./libs/ardour/run-tests
