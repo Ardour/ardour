4d1246e0ee7267ae935c1cd4a0b6087978fab0c2
Carl Hetherington <carl@carlh.net>
2012-06-19 00:25:26 +0000

    Add load_session to profiling.
    git-svn-id: svn://localhost/ardour2/branches/3.0@12775 d708f5d6-7413-0410-9779-e7cbd77b26cf

---
+++ libs/ardour/test/profiling/load_session.cc
@@ -0,0 +1,48 @@
+#include "test_util.h"
+#include "pbd/failed_constructor.h"
+#include "ardour/ardour.h"
+#include "ardour/audioengine.h"
+#include "ardour/session.h"
+#include "midi++/manager.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+using namespace ARDOUR;
+
+int main (int argc, char* argv[])
+{
+	if (argc != 3) {
+		cerr << "Syntax: " << argv[0] << " <dir> <snapshot-name>\n";
+		exit (EXIT_FAILURE);
+	}
+
+	ARDOUR::init (false, true);
+
+	Session* s = 0;
+	
+	try {
+		s = load_session (argv[1], argv[2]);
+	} catch (failed_constructor& e) {
+		cerr << "failed_constructor: " << e.what() << "\n";
+		exit (EXIT_FAILURE);
+	} catch (AudioEngine::PortRegistrationFailure& e) {
+		cerr << "PortRegistrationFailure: " << e.what() << "\n";
+		exit (EXIT_FAILURE);
+	} catch (exception& e) {
+		cerr << "exception: " << e.what() << "\n";
+		exit (EXIT_FAILURE);
+	} catch (...) {
+		cerr << "unknown exception.\n";
+		exit (EXIT_FAILURE);
+	}
+
+	AudioEngine::instance()->remove_session ();
+	delete s;
+	AudioEngine::instance()->stop (true);
+
+	MIDI::Manager::destroy ();
+	AudioEngine::destroy ();
+
+	return 0;
+}
