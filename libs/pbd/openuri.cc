61f879bcee4c8ba471837ad7f6a28339205b85fa
Todd Naugle <toddn@glw.com>
2012-06-21 20:15:38 +0000

    Fixing issues with opening a browser when using the bundle. Use xdg-open instead of glib functions. Fix to epa to ensure that the enviroment is restored correctly. The current enviroment has to be cleared. Otherwise any variables that are set in the current enviroment, but do not have a setting in the restored version will be left set.
    git-svn-id: svn://localhost/ardour2/branches/3.0@12812 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- libs/pbd/openuri.cc
+++ libs/pbd/openuri.cc
@@ -10,20 +10,27 @@
 bool
 PBD::open_uri (const char* uri)
 {
-#ifdef HAVE_GTK_OPEN_URI
-	GError* err;
-	return gtk_open_uri (0, uri, GDK_CURRENT_TIME, &err);
-#else
 #ifdef __APPLE__
 	extern bool cocoa_open_url (const char*);
 	return cocoa_open_url (uri);
 #else
+	EnvironmentalProtectionAgency* global_epa = EnvironmentalProtectionAgency::get_global_epa ();
+	boost::scoped_ptr<EnvironmentalProtectionAgency> current_epa;
+
+	/* revert all environment settings back to whatever they were when ardour started
+	 */
+
+	if (global_epa) {
+		current_epa.reset (new EnvironmentalProtectionAgency(true)); /* will restore settings when we leave scope */
+		global_epa->restore ();
+	}
+
 	std::string command = "xdg-open ";
 	command += uri;
-        Glib::spawn_command_line_async (command);
+	command += " &";
+	system (command.c_str());
 
 	return true;
-#endif
 #endif
 }
 
