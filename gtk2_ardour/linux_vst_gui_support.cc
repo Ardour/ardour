2eb43c83100010c1214925b3a306ef1a6657c184
Paul Davis <paul@linuxaudiosystems.com>
2013-01-16 18:22:15 +0000

    add copyright comments
    git-svn-id: svn://localhost/ardour2/branches/3.0@13859 d708f5d6-7413-0410-9779-e7cbd77b26cf

--- gtk2_ardour/linux_vst_gui_support.cc
+++ gtk2_ardour/linux_vst_gui_support.cc
@@ -1,9 +1,26 @@
+/*
+    Copyright (C) 2012 Paul Davis 
+    Based on code by Paul Davis, Torben Hohn as part of FST
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
 /******************************************************************/
 /** VSTFX - An engine based on FST for handling linuxVST plugins **/
 /******************************************************************/
-
-/*This is derived from the original FST (C code) with some tweaks*/
-
 
 /** EDITOR tab stops at 4 **/
 
