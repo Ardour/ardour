5596f1837d9f212732ca17a5d3c3e57bf7315fb8
Paul Davis <paul@linuxaudiosystems.com>
2013-03-12 17:19:39 -0400

    test master push email

--- gtk2_ardour/ardour_ui.cc
+++ gtk2_ardour/ardour_ui.cc
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 1999-2007 Paul Davis
+    Copyright (C) 1999-2013 Paul Davis
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
