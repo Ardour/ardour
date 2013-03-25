7aca3a92217ff02be905dc387bf432e7a2f103c1
Paul Davis <paul@linuxaudiosystems.com>
2013-03-25 09:49:07 -0400

    possible new naming convention, plus factor out version/DEBUG info into a sourced shell script fragment that can be shared by build+package

---
+++ tools/linux_packaging/define_versions.sh
@@ -0,0 +1,21 @@
+# 
+# this is sourced by build and package, and executed from within build/linux_packaging
+#
+
+release_version=`grep -m 1 '[^A-Za-z_]LINUX_VERSION = ' ../../wscript | awk '{print $3}' | sed "s/'//g"`
+r=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed -e 1d -e "s/$release_version-//"`
+revcount=`echo $r | cut -d- -f1`
+commit=`echo $r | cut -d- -f2`
+version=${release_version}.${revcount}
+
+#
+# Figure out the Build Type
+#
+# Note that the name of the cache file may vary from to time
+#
+
+if grep -q "DEBUG = True" ../../build/c4che/_cache.py; then
+	DEBUG="T"
+else
+	DEBUG="F"
+fi
