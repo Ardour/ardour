d88999312a97a04548d2fc457d11a3303789c171
Paul Davis <paul@linuxaudiosystems.com>
2013-05-02 10:10:05 -0400

    force ArdourWindows to be TYPE_UTILITY so that they float "with" TYPE_DIALOG windows (at least on OS X - still to be tested on X Window

--- gtk2_ardour/ardour_window.cc
+++ gtk2_ardour/ardour_window.cc
@@ -75,5 +75,15 @@ void
 ArdourWindow::init ()
 {
 	set_border_width (10);
+
+        /* ArdourWindows are not dialogs (they have no "OK" or "Close" button) but
+           they should be considered part of the same "window level" as a dialog. This
+           works on X11 and Quartz, in that:
+           
+           (a) utility & dialog windows are considered to be part of the same level
+           (b) they will float above normal windows without any particular effort
+        */
+
+        set_type_hint (Gdk::WINDOW_TYPE_HINT_UTILITY);
 }
 
