fe9650d8939db9474a8dbd13a33911c18fd254c3
Robin Gareus <robin@gareus.org>
2013-07-09 03:36:06 +0200

    clean up meterbridge options

--- gtk2_ardour/session_option_editor.cc
+++ gtk2_ardour/session_option_editor.cc
@@ -272,60 +272,66 @@ SessionOptionEditor::SessionOptionEditor (Session* s)
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_glue_new_regions_to_bars_and_beats)
 			    ));
 
-	add_option (_("Meterbridge"), new BoolOption (
-			    "show-name-on-meterbridge",
-			    _("Show Track Name on Meterbridge"),
-			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_name_on_meterbridge),
-			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_name_on_meterbridge)
-			    ));
-
-	add_option (_("Meterbridge"), new BoolOption (
-			    "show-id-on-meterbridge",
-			    _("Show Track Id on Meterbridge"),
-			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_id_on_meterbridge),
-			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_id_on_meterbridge)
-			    ));
+	add_option (_("Meterbridge"), new OptionEditorHeading (_("Route Display")));
 
 	add_option (_("Meterbridge"), new BoolOption (
 			    "show-midi-on-meterbridge",
-			    _("Include Midi Tracks on Meterbridge"),
+			    _("Show Midi Tracks"),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_midi_on_meterbridge),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_midi_on_meterbridge)
 			    ));
 
 	add_option (_("Meterbridge"), new BoolOption (
 			    "show-busses-on-meterbridge",
-			    _("Include Busses on Meterbridge"),
+			    _("Show Busses"),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_busses_on_meterbridge),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_busses_on_meterbridge)
 			    ));
 
 	add_option (_("Meterbridge"), new BoolOption (
 			    "show-master-on-meterbridge",
-			    _("Include Master Bus on Meterbridge"),
+			    _("Include Master Bus"),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_master_on_meterbridge),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_master_on_meterbridge)
 			    ));
 
+	add_option (_("Meterbridge"), new OptionEditorHeading (_("Button Area")));
+
 	add_option (_("Meterbridge"), new BoolOption (
 			    "show-rec-on-meterbridge",
-			    _("Show Rec-enable Button on Meterbridge"),
+			    _("Rec-enable Button"),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_rec_on_meterbridge),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_rec_on_meterbridge)
 			    ));
 
 	add_option (_("Meterbridge"), new BoolOption (
 			    "show-mute-on-meterbridge",
-			    _("Include Mute Button on Meterbridge"),
+			    _("Mute Button"),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_mute_on_meterbridge),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_mute_on_meterbridge)
 			    ));
 
 	add_option (_("Meterbridge"), new BoolOption (
 			    "show-solo-on-meterbridge",
-			    _("Show Solo Button on Meterbridge"),
+			    _("Solo Button"),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_solo_on_meterbridge),
 			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_solo_on_meterbridge)
+			    ));
+
+	add_option (_("Meterbridge"), new OptionEditorHeading (_("Name Labels")));
+
+	add_option (_("Meterbridge"), new BoolOption (
+			    "show-name-on-meterbridge",
+			    _("Track Name"),
+			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_name_on_meterbridge),
+			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_name_on_meterbridge)
+			    ));
+
+	add_option (_("Meterbridge"), new BoolOption (
+			    "show-id-on-meterbridge",
+			    _("Track ID"),
+			    sigc::mem_fun (*_session_config, &SessionConfiguration::get_show_id_on_meterbridge),
+			    sigc::mem_fun (*_session_config, &SessionConfiguration::set_show_id_on_meterbridge)
 			    ));
 
 }
