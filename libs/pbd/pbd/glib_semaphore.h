29f178aa4b1a35449ddcfcc98a1ffd9d0e648dbf
Paul Davis <paul@linuxaudiosystems.com>
2013-07-11 14:38:53 -0400

    Add PBD::GlibSemaphore class that uses PBD::atomic_counter etc.
    PBD::Semaphore can really be used instead of this class now,
    but it didn't exist when I needed this class and it is header only, which
    causes some problems because windows.h is included

---
+++ libs/pbd/pbd/glib_semaphore.h
@@ -0,0 +1,56 @@
+/*
+    Copyright (C) 2010 Tim Mayberry
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef PBD_GLIB_SEMAPHORE_H
+#define PBD_GLIB_SEMAPHORE_H
+
+#include <glibmm/threads.h>
+
+#include "atomic_counter.h"
+
+namespace PBD {
+
+class GlibSemaphore
+{
+
+	// prevent copying and assignment
+	GlibSemaphore(const GlibSemaphore& sema);
+	GlibSemaphore& operator= (const GlibSemaphore& sema);
+
+public:
+
+	GlibSemaphore (gint initial_val = 1);
+
+	void wait ();
+
+	bool try_wait ();
+
+	void post ();
+
+private:
+
+	atomic_counter           m_counter;
+	Glib::Threads::Cond      m_cond;
+	Glib::Threads::Mutex     m_mutex;
+
+};
+
+} // namespace PBD
+
+#endif // PBD_SEMAPHORE_H
