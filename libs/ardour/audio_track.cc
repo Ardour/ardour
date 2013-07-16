15aa2a224e22a70eea18b79ff4cb06db66ca8951
Robin Gareus <robin@gareus.org>
2013-07-16 19:36:57 +0200

    zero meters for inactive tracks

--- libs/ardour/audio_track.cc
+++ libs/ardour/audio_track.cc
@@ -325,6 +325,9 @@ AudioTrack::roll (pframes_t nframes, framepos_t start_frame, framepos_t end_fram
 
 	if (!_active) {
 		silence (nframes);
+		if (_meter_point == MeterInput && (_monitoring & MonitorInput || _diskstream->record_enabled())) {
+			_meter->reset();
+		}
 		return 0;
 	}
 
