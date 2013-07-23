8da6a70ead63767b701a00aeaf859569b0d31ee4
Robin Gareus <robin@gareus.org>
2013-07-23 21:20:51 +0200

    implement meter-lineup and VU standard preferences

--- libs/ardour/ardour/types.h
+++ libs/ardour/ardour/types.h
@@ -383,6 +383,12 @@ namespace ARDOUR {
 		MeteringRoute  ///< meter what is going through the route
 	};
 
+	enum VUMeterStandard {
+		MeteringVUfrench,   // + 2
+		MeteringVUamerican, // +-0
+		MeteringVUstandard  // -4
+	};
+
 	enum MeterLineUp {
 		MeteringLineUp24,
 		MeteringLineUp20,
@@ -590,6 +596,7 @@ std::istream& operator>>(std::istream& o, ARDOUR::HeaderFormat& sf);
 std::istream& operator>>(std::istream& o, ARDOUR::AutoConnectOption& sf);
 std::istream& operator>>(std::istream& o, ARDOUR::EditMode& sf);
 std::istream& operator>>(std::istream& o, ARDOUR::MonitorModel& sf);
+std::istream& operator>>(std::istream& o, ARDOUR::VUMeterStandard& sf);
 std::istream& operator>>(std::istream& o, ARDOUR::MeterLineUp& sf);
 std::istream& operator>>(std::istream& o, ARDOUR::PFLPosition& sf);
 std::istream& operator>>(std::istream& o, ARDOUR::AFLPosition& sf);
@@ -612,6 +619,7 @@ std::ostream& operator<<(std::ostream& o, const ARDOUR::HeaderFormat& sf);
 std::ostream& operator<<(std::ostream& o, const ARDOUR::AutoConnectOption& sf);
 std::ostream& operator<<(std::ostream& o, const ARDOUR::EditMode& sf);
 std::ostream& operator<<(std::ostream& o, const ARDOUR::MonitorModel& sf);
+std::ostream& operator<<(std::ostream& o, const ARDOUR::VUMeterStandard& sf);
 std::ostream& operator<<(std::ostream& o, const ARDOUR::MeterLineUp& sf);
 std::ostream& operator<<(std::ostream& o, const ARDOUR::PFLPosition& sf);
 std::ostream& operator<<(std::ostream& o, const ARDOUR::AFLPosition& sf);
