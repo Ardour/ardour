16339aa4464ddadbbaeb19cc16b8bb146d2ddc98
Robin Gareus <robin@gareus.org>
2013-07-28 03:29:16 +0200

    add dedicated line-up level for IEC1/DIN meter (15dbFS)

--- libs/ardour/ardour/rc_configuration_vars.h
+++ libs/ardour/ardour/rc_configuration_vars.h
@@ -151,6 +151,7 @@ CONFIG_VARIABLE (float, meter_hold, "meter-hold", 100.0f)
 CONFIG_VARIABLE (float, meter_falloff, "meter-falloff", 32.0f)
 CONFIG_VARIABLE (VUMeterStandard, meter_vu_standard, "meter-vu-standard", MeteringVUstandard)
 CONFIG_VARIABLE (MeterLineUp, meter_line_up_level, "meter-line-up-level", MeteringLineUp18)
+CONFIG_VARIABLE (MeterLineUp, meter_line_up_din, "meter-line-up-din", MeteringLineUp15)
 CONFIG_VARIABLE (float, meter_peak, "meter-peak", 0.0f)
 CONFIG_VARIABLE (bool, meter_style_led, "meter-style-led", true)
 
