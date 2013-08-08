83a0c30c24ce6bb6e3e718c267a82fbaffc33b4b
Paul Davis <paul@linuxaudiosystems.com>
2013-08-07 22:21:36 -0400

    major redesign of MIDI port heirarchy and management
    basic, very flaky functionality is back. program unstable at present

---
+++ libs/ardour/ardour/midiport_manager.h
@@ -0,0 +1,94 @@
+/*
+    Copyright (C) 1998 Paul Barton-Davis
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef __midiport_manager_h__
+#define __midiport_manager_h__
+
+#include <list>
+
+#include <string>
+
+#include "pbd/rcu.h"
+
+#include "midi++/types.h"
+#include "midi++/port.h"
+
+#include "ardour/types.h"
+
+namespace ARDOUR {
+
+class MidiPort;
+class Port;
+
+class MidiPortManager {
+  public:
+    MidiPortManager();
+    virtual ~MidiPortManager ();
+	
+    MidiPort* add_port (MidiPort *);
+    void remove_port (MidiPort *);
+
+    MidiPort* port (const std::string&);
+    
+    /* Ports used for control. These are read/written to outside of the
+     * process callback (asynchronously with respect to when data
+     * actually arrives). 
+     *
+     * More detail: we do actually read/write data for these ports
+     * inside the process callback, but incoming data is only parsed
+     * and outgoing data is only generated *outside* the process
+     * callback. 
+     */
+
+    MIDI::Port* midi_input_port () { return _midi_input_port; }
+    MIDI::Port* midi_output_port () { return _midi_output_port; }
+    
+    /* Ports used for synchronization. These have their I/O handled inside the
+     * process callback.
+     */
+
+    boost::shared_ptr<MidiPort> mtc_input_port() const { return _mtc_input_port; }
+    boost::shared_ptr<MidiPort> mtc_output_port() const { return _mtc_output_port; }
+    boost::shared_ptr<MidiPort> midi_clock_input_port() const { return _midi_clock_input_port; }
+    boost::shared_ptr<MidiPort> midi_clock_output_port() const { return _midi_clock_output_port; }
+    
+    void set_port_states (std::list<XMLNode*>);
+
+    PBD::Signal0<void> PortsChanged;
+
+  protected:
+    /* asynchronously handled ports: MIDI::Port */
+    MIDI::Port* _midi_input_port;
+    MIDI::Port* _midi_output_port;
+    boost::shared_ptr<Port> _midi_in;
+    boost::shared_ptr<Port> _midi_out;
+
+    /* synchronously handled ports: ARDOUR::MidiPort */
+    boost::shared_ptr<MidiPort> _mtc_input_port;
+    boost::shared_ptr<MidiPort> _mtc_output_port;
+    boost::shared_ptr<MidiPort> _midi_clock_input_port;
+    boost::shared_ptr<MidiPort> _midi_clock_output_port;
+
+    void create_ports ();
+
+};
+
+} // namespace MIDI
+
+#endif  // __midi_port_manager_h__
---
+++ libs/ardour/midiport_manager.cc
@@ -0,0 +1,89 @@
+/*
+    Copyright (C) 1998-99 Paul Barton-Davis 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    $Id$
+*/
+
+#include "ardour/audioengine.h"
+#include "ardour/async_midi_port.h"
+#include "ardour/midiport_manager.h"
+
+#include "i18n.h"
+
+using namespace ARDOUR;
+using namespace std;
+using namespace MIDI;
+using namespace PBD;
+
+
+MidiPortManager::MidiPortManager ()
+{
+}
+
+MidiPortManager::~MidiPortManager ()
+{
+	if (_midi_in) {
+		AudioEngine::instance()->unregister_port (_midi_in);
+	}
+	if (_midi_in) {
+		AudioEngine::instance()->unregister_port (_midi_in);
+	}
+	if (_mtc_input_port) {
+		AudioEngine::instance()->unregister_port (_mtc_input_port);
+	}
+	if (_mtc_output_port) {
+		AudioEngine::instance()->unregister_port (_mtc_output_port);
+	}
+	if (_midi_clock_input_port) {
+		AudioEngine::instance()->unregister_port (_midi_clock_input_port);
+	}
+	if (_midi_clock_output_port) {
+		AudioEngine::instance()->unregister_port (_midi_clock_output_port);
+	}
+
+}
+
+MidiPort*
+MidiPortManager::port (string const & n)
+{
+	boost::shared_ptr<MidiPort> mp =  boost::dynamic_pointer_cast<MidiPort> (AudioEngine::instance()->get_port_by_name (n));
+	return mp.get();
+}
+
+void
+MidiPortManager::create_ports ()
+{
+	_midi_in  = AudioEngine::instance()->register_input_port (DataType::MIDI, _("MIDI control in"), true);
+	_midi_out = AudioEngine::instance()->register_output_port (DataType::MIDI, _("MIDI control out"), true);
+	
+	_midi_input_port = boost::dynamic_pointer_cast<AsyncMIDIPort>(_midi_in).get();
+	_midi_output_port = boost::dynamic_pointer_cast<AsyncMIDIPort>(_midi_out).get();
+}
+
+void
+MidiPortManager::set_port_states (list<XMLNode*> s)
+{
+	PortManager::PortList pl;
+
+	AudioEngine::instance()->get_ports (DataType::MIDI, pl);
+	
+	for (list<XMLNode*>::iterator i = s.begin(); i != s.end(); ++i) {
+		for (PortManager::PortList::const_iterator j = pl.begin(); j != pl.end(); ++j) {
+			// (*j)->set_state (**i);
+		}
+	}
+}
+
