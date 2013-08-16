7e42cafe395e934d966938728e72b03c1167fb42
Tim Mayberry <mojofunk@gmail.com>
2013-08-16 20:52:45 +1000

    Add ability to run single libardour tests using wine

--- tools/windows_packaging/wine-ardour-tests.sh
+++ tools/windows_packaging/wine-ardour-tests.sh
@@ -2,4 +2,19 @@
 
 . ./wine-env.sh
 
-wine run-tests.exe
+if [ "$1" == "--single" ] || [ "$2" == "--single" ]; then
+        if [ "$1" == "--single" ]; then
+	        TESTS="test_*$2*"
+        elif [ "$2" == "--single" ]; then
+	        TESTS="test_*$3*"
+	else
+                TESTS='test_*'
+        fi
+	for test_program in `find . -name "$TESTS" -type f -perm /u+x`;
+	do
+		echo "Running $test_program..."
+	        wine "$test_program"
+	done
+else
+        wine run-tests.exe
+fi
