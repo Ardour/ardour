6b23417ae0a93e5ff26477a62e3abcbcaa5efd64
Paul Davis <paul@linuxaudiosystems.com>
2013-09-14 16:17:24 -0400

    move creation of Graph till after we have the engine running, since we need access to the backend

--- libs/ardour/session_state.cc
+++ libs/ardour/session_state.cc
@@ -177,13 +177,6 @@ Session::pre_engine_init (string fullpath)
 
 	set_history_depth (Config->get_history_depth());
 	
-	if (how_many_dsp_threads () > 1) {
-		/* For now, only create the graph if we are using >1 DSP threads, as
-		   it is a bit slower than the old code with 1 thread.
-		*/
-		_process_graph.reset (new Graph (*this));
-	}
-
         /* default: assume simple stereo speaker configuration */
 
         _speakers->setup_default_speakers (2);
@@ -216,6 +209,13 @@ Session::post_engine_init ()
 
 	set_block_size (_engine.samples_per_cycle());
 	set_frame_rate (_engine.sample_rate());
+
+	if (how_many_dsp_threads () > 1) {
+		/* For now, only create the graph if we are using >1 DSP threads, as
+		   it is a bit slower than the old code with 1 thread.
+		*/
+		_process_graph.reset (new Graph (*this));
+	}
 
 	n_physical_outputs = _engine.n_physical_outputs ();
 	n_physical_inputs =  _engine.n_physical_inputs ();
