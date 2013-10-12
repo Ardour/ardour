be8d06f848914a6383e3c3053a2608dfe8aa7142
Paul Davis <paul@linuxaudiosystems.com>
2013-10-12 11:58:22 -0400

    post a dialog about a session being opened in read-only mode (if it happens)

--- gtk2_ardour/ardour_ui.cc
+++ gtk2_ardour/ardour_ui.cc
@@ -2851,6 +2851,22 @@ ARDOUR_UI::load_session (const std::string& path, const std::string& snap_name, 
 		}
 	}
 
+	if (!new_session->writable()) {
+		MessageDialog msg (_("This session has been opened in read-only mode.\n\nYou will not be able to record or save."),
+				   true,
+				   Gtk::MESSAGE_INFO,
+				   BUTTONS_OK);
+		
+		msg.set_keep_above (true);
+		msg.set_title (_("Read-only Session"));
+		msg.set_position (Gtk::WIN_POS_CENTER);
+		pop_back_splash (msg);
+		msg.present ();
+		(void) msg.run ();
+		msg.hide ();
+	}
+	
+
 	/* Now the session been created, add the transport controls */
 	new_session->add_controllable(roll_controllable);
 	new_session->add_controllable(stop_controllable);
