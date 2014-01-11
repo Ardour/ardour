60fe9606f404cc723c0aa33110f0e15c5324a97d
Robin Gareus <robin@gareus.org>
2014-01-11 23:29:36 +0100

    VBAP GUI depends on signal-position (not parameter changes)

--- libs/panners/vbap/vbap.cc
+++ libs/panners/vbap/vbap.cc
@@ -189,6 +189,8 @@ VBAPanner::update ()
                 s->direction = AngularVector (center, elevation);
                 compute_gains (s->desired_gains, s->desired_outputs, s->direction.azi, s->direction.ele);
         }
+
+        SignalPositionChanged(); /* emit */
 }
 
 void 
