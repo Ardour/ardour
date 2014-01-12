61c692baa1a56b0f00d5d6e391072324e337b0ed
John Emmas <johne53@tiscali.co.uk>
2014-01-12 17:40:25 +0000

    'libs/evoral' - Move a couple of 'IdentityConverter' functions into their own dedicated C++ source file. This allows them to be used correctly with __dllimport and __dllexport. Hopefully, this technique will also work for gcc / mingw

---
+++ libs/evoral/src/IdentityConverter.cpp
@@ -0,0 +1,42 @@
+/* This file is part of Evoral.
+ * Copyright (C) 2008 David Robillard <http://drobilla.net>
+ * Copyright (C) 2000-2008 Paul Davis
+ *
+ * Evoral is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Evoral is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "evoral/TimeConverter.hpp"
+
+typedef long long framepos_t;
+
+namespace Evoral {
+
+template<typename A, typename B>
+B
+IdentityConverter<A,B>::to(A a)   const
+{
+	return static_cast<B>(a);
+}
+
+template<typename A, typename B>
+A
+IdentityConverter<A,B>::from(B b) const
+{
+	return static_cast<A>(b);
+}
+
+template class IdentityConverter<double, framepos_t>;
+template class TimeConverter<double, framepos_t>;
+
+} // namespace Evoral
