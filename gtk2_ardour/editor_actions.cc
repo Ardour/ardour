1701c9c78bd2dae6b033c1c4b2ac56dc3fcb8ec9
Robin Gareus <robin@gareus.org>
2014-02-20 00:53:23 +0100

    the forgotten second (amend to prev commit)

--- gtk2_ardour/editor_actions.cc
+++ gtk2_ardour/editor_actions.cc
@@ -1155,7 +1155,7 @@ Editor::prev_snap_choice ()
 		set_snap_to (Editing::SnapToTimecodeSeconds);
 		break;
 	case Editing::SnapToSeconds:
-		set_snap_to (Editing::SnapToSeconds);
+		set_snap_to (Editing::SnapToTimecodeMinutes);
 		break;
 	case Editing::SnapToMinutes:
 		set_snap_to (Editing::SnapToSeconds);
