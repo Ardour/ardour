46e86f669402d88407062e461dbf4f7bcdccba67
Paul Davis <paul@linuxaudiosystems.com>
2014-02-19 13:16:02 -0500

    Revert "add a hack to deal with device discovery race, in which the devices were not actually known to be ready before the last update_surfaces() call (which is where binding between strips and routes occurs) ; add support for Mackie Control touch on/off messages"
    This reverts commit 06a25ffd5695697db9869a19e46c2f93c6c17458.

--- gtk2_ardour/editor_drag.cc
+++ gtk2_ardour/editor_drag.cc
@@ -3318,7 +3318,7 @@ LineDrag::motion (GdkEvent* event, bool)
 }
 
 void
-LineDrag::finished (GdkEvent* event, bool movement_occured)
+LineDrag::finished (GdkEvent* event, bool)
 {
 	if (movement_occured) {
 		motion (event, false);
