d52eb5bf2360aaaa30219aa237621c10c16be630
John Emmas <johne53@tiscali.co.uk>
2014-03-12 13:21:21 +0000

    A few days ago a #define got changed because the relevant section of code was no longer needed when building with mingw. However, it's not needed for msvc either (in fact, msvc won't compile it) so hopefully, the code fragment can be safely removed

--- libs/pbd/pbd/abstract_ui.h
+++ libs/pbd/pbd/abstract_ui.h
@@ -75,21 +75,8 @@ class ABSTRACT_UI_API AbstractUI : public BaseUI
         };
 	typedef typename RequestBuffer::rw_vector RequestBufferVector;
 
-#ifdef COMPILER_MSVC // ?? (not needed for ming32 nor mingw64)
-
-	struct pthread_cmp
-	{
-		bool operator() (const ptw32_handle_t& thread1, const ptw32_handle_t& thread2)
-		{
-			return thread1.p < thread2.p;
-		}
-	};
-	typedef typename std::map<pthread_t,RequestBuffer*, pthread_cmp>::iterator RequestBufferMapIterator;
-	typedef std::map<pthread_t,RequestBuffer*, pthread_cmp> RequestBufferMap;
-#else
 	typedef typename std::map<pthread_t,RequestBuffer*>::iterator RequestBufferMapIterator;
 	typedef std::map<pthread_t,RequestBuffer*> RequestBufferMap;
-#endif
 
 	RequestBufferMap request_buffers;
         static Glib::Threads::Private<RequestBuffer> per_thread_request_buffer;
