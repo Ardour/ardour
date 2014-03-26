af1689c0618e0ac6a93bddd788bfa974c039596c
Valeriy Kamyshniy <vkamyshniy@wavesglobal.com>
2014-03-26 10:03:30 -0500

    adding X attrib
    [git-p4: depot-paths = "//Abdaw/dev_main/tracks/": change = 446420]

--- libs/pbd/pbd/version.h
+++
@@ -1,8 +0,0 @@
-#ifndef __libpbd_version_h__
-#define __libpbd_version_h__
-#include "pbd/libpbd_visibility.h"
-LIBPBD_API extern const char* libpbd_revision;
-LIBPBD_API extern int libpbd_major_version;
-LIBPBD_API extern int libpbd_minor_version;
-LIBPBD_API extern int libpbd_micro_version;
-#endif /* __libpbd_version_h__ */
