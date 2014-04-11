5d9c2104d200aebc33a302c0018fd891e10351e6
John Emmas <johne53@tiscali.co.uk>
2014-04-11 15:36:04 +0100

    Oops - correct some typos in my previous commit
    (accidentally used G_DIR_SEPARATOR instead of G_SEARCHPATH_SEPARATOR)

--- gtk2_ardour/missing_file_dialog.cc
+++ gtk2_ardour/missing_file_dialog.cc
@@ -133,7 +133,7 @@ MissingFileDialog::add_chosen ()
                 break;
         }
 
-        split (str, dirs, G_DIR_SEPARATOR);
+        split (str, dirs, G_SEARCHPATH_SEPARATOR);
 
         newdir = chooser.get_filename ();
 
@@ -144,7 +144,7 @@ MissingFileDialog::add_chosen ()
         }
 
         if (!str.empty()) {
-                str += G_DIR_SEPARATOR;
+                str += G_SEARCHPATH_SEPARATOR;
         }
 
         str += newdir;
