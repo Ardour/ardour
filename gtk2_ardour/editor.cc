c752167ccc94793537173e1e90ac44815944733e
Paul Davis <paul@linuxaudiosystems.com>
2014-05-17 15:50:35 -0400

    change return type of Editor::axis_view_from_route()

--- gtk2_ardour/editor.cc
+++ gtk2_ardour/editor.cc
@@ -4971,7 +4971,7 @@ Editor::region_view_removed ()
 	_summary->set_dirty ();
 }
 
-TimeAxisView*
+RouteTimeAxisView*
 Editor::axis_view_from_route (boost::shared_ptr<Route> r) const
 {
 	TrackViewList::const_iterator j = track_views.begin ();
