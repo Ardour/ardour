a9239f5f064ac880ed0c3aa418c57be87ccdd865
Robin Gareus <robin@gareus.org>
2014-06-08 01:07:03 +0200

    implement midi capture alignment:
    For audio: not writing frames to the capture ringbuffer offsets
    the recording.
    For midi: we need to keep track of the record range
    and subtract the accumulated difference from the event time.

--- libs/ardour/ardour/midi_diskstream.h
+++ libs/ardour/ardour/midi_diskstream.h
@@ -173,6 +173,7 @@ class LIBARDOUR_API MidiDiskstream : public Diskstream
 	gint                         _frames_read_from_ringbuffer;
 	volatile gint                _frames_pending_write;
 	volatile gint                _num_captured_loops;
+	framepos_t                   _accumulated_capture_offset;
 
 	/** A buffer that we use to put newly-arrived MIDI data in for
 	    the GUI to read (so that it can update itself).
