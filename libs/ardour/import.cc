c7c3c1e924bc28e5c54e62ea3d9883ae2fe005fd
Robin Gareus <robin@gareus.org>
2014-06-15 20:46:06 +0200

    another edge-case, don't crash on invalid files with zero channels

--- libs/ardour/import.cc
+++ libs/ardour/import.cc
@@ -522,6 +522,11 @@ Session::import_files (ImportStatus& status)
 			}
 		}
 
+		if (channels == 0) {
+			error << _("Import: file contains no channels.") << endmsg;
+			continue;
+		}
+
 		vector<string> new_paths = get_paths_for_new_sources (config.get_native_file_header_format(),
 		                                                      status.replace_existing_source, *p,
 		                                                      get_best_session_directory_for_new_source (),
