ac39db608f5d7ec336f755f3022fb100d4feace7
nick_m <mainsbridge@gmail.com>
2014-06-17 21:44:36 +1000

    Render tracks and regions to a background image in the editor summary. Connect to editor's SelectionChanged signal to display corresponding region colour change.

--- gtk2_ardour/editor.cc
+++ gtk2_ardour/editor.cc
@@ -4807,13 +4807,13 @@ Editor::located ()
 void
 Editor::region_view_added (RegionView *)
 {
-	_summary->set_dirty ();
+	_summary->set_background_dirty ();
 }
 
 void
 Editor::region_view_removed ()
 {
-	_summary->set_dirty ();
+	_summary->set_background_dirty ();
 }
 
 RouteTimeAxisView*
