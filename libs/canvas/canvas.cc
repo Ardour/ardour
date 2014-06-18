929a787b712e909f34b8a3d1a06fac159526c5c2
nick_m <mainsbridge@gmail.com>
2014-06-19 00:06:32 +1000

    Canvas tweaks.        * Only queue a draw for changed items that are set visible and in-window.

--- libs/canvas/canvas.cc
+++ libs/canvas/canvas.cc
@@ -170,7 +170,9 @@ Canvas::item_shown_or_hidden (Item* item)
 {
 	boost::optional<Rect> bbox = item->bounding_box ();
 	if (bbox) {
-		queue_draw_item_area (item, bbox.get ());
+		if (item->item_to_window (*bbox).intersection (visible_area ())) {
+			queue_draw_item_area (item, bbox.get ());
+		}
 	}
 }
 
@@ -183,7 +185,9 @@ Canvas::item_visual_property_changed (Item* item)
 {
 	boost::optional<Rect> bbox = item->bounding_box ();
 	if (bbox) {
-		queue_draw_item_area (item, bbox.get ());
+		if (item->item_to_window (*bbox).intersection (visible_area ())) {
+			queue_draw_item_area (item, bbox.get ());
+		}
 	}
 }
 
@@ -195,15 +199,24 @@ Canvas::item_visual_property_changed (Item* item)
 void
 Canvas::item_changed (Item* item, boost::optional<Rect> pre_change_bounding_box)
 {
+	
+	Rect window_bbox = visible_area ();
+
 	if (pre_change_bounding_box) {
-		/* request a redraw of the item's old bounding box */
-		queue_draw_item_area (item, pre_change_bounding_box.get ());
+
+		if (item->item_to_window (*pre_change_bounding_box).intersection (window_bbox)) {
+			/* request a redraw of the item's old bounding box */
+			queue_draw_item_area (item, pre_change_bounding_box.get ());
+		}
 	}
 
 	boost::optional<Rect> post_change_bounding_box = item->bounding_box ();
 	if (post_change_bounding_box) {
-		/* request a redraw of the item's new bounding box */
-		queue_draw_item_area (item, post_change_bounding_box.get ());
+		
+		if (item->item_to_window (*post_change_bounding_box).intersection (window_bbox)) {
+			/* request a redraw of the item's new bounding box */
+			queue_draw_item_area (item, post_change_bounding_box.get ());
+		}
 	}
 }
 
