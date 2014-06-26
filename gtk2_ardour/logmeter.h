dfcf4a18a3fa1e5e4f2e5644fee8573c0d907197
VKamyshniy <VKamyshniy@wavesglobal.com>
2014-06-26 15:14:31 +0300

    [Summary] Changing scale for the Peak meter

--- gtk2_ardour/logmeter.h
+++ gtk2_ardour/logmeter.h
@@ -47,32 +47,56 @@ static inline float meter_deflect_k (float, float);
 static inline float
 log_meter (float db)
 {
-         gfloat def = 0.0f; /* Meter deflection %age */
+	gfloat def = 0.0f; /* Meter deflection %age */
+/* Original code from ARDOUR :
 
-         if (db < -70.0f) {
-                 def = 0.0f;
-         } else if (db < -60.0f) {
-                 def = (db + 70.0f) * 0.25f;
-         } else if (db < -50.0f) {
-                 def = (db + 60.0f) * 0.5f + 2.5f;
-         } else if (db < -40.0f) {
-                 def = (db + 50.0f) * 0.75f + 7.5f;
-         } else if (db < -30.0f) {
-                 def = (db + 40.0f) * 1.5f + 15.0f;
-         } else if (db < -20.0f) {
-                 def = (db + 30.0f) * 2.0f + 30.0f;
-         } else if (db < 6.0f) {
-                 def = (db + 20.0f) * 2.5f + 50.0f;
-         } else {
-		 def = 115.0f;
-	 }
+	if (db < -70.0f) {
+		def = 0.0f;
+	} else if (db < -60.0f) {
+		def = (db + 70.0f) * 0.25f;
+	} else if (db < -50.0f) {
+		def = (db + 60.0f) * 0.5f + 2.5f;
+	} else if (db < -40.0f) {
+		def = (db + 50.0f) * 0.75f + 7.5f;
+	} else if (db < -30.0f) {
+		def = (db + 40.0f) * 1.5f + 15.0f;
+	} else if (db < -20.0f) {
+		def = (db + 30.0f) * 2.0f + 30.0f;
+	} else if (db < 6.0f) {
+		def = (db + 20.0f) * 2.5f + 50.0f;
+	} else {
+		def = 115.0f;
+	}
 
-	 /* 115 is the deflection %age that would be
-	    when db=6.0. this is an arbitrary
-	    endpoint for our scaling.
-	 */
+	/* 115 is the deflection %age that would be
+       when db=6.0. this is an arbitrary
+       endpoint for our scaling.
+	*/
+	/*
 
-         return def/115.0f;
+	return def/115.0f;
+: end of Original code from ARDOUR */
+
+	// TRACKS specific implementation:
+	if (db < -144.0f) {
+		def = 0.0f;
+	} else if (db < -72) {
+		def = (db + 144) / 24;
+	} else if (db < -18) {
+		def = (db + 72) / 6 + 3;
+	} else if (db < 6) {
+		def = (db + 18) / 2 + 12;
+	} else {
+		def = 24.0f;
+	}
+
+	/* 24 is the deflection that would be
+       when db=6.0. this is an arbitrary
+       endpoint for our scaling.
+	*/
+
+	return def/24.0f;
+    
 }
 
 static inline float
