416abf7f1c26246cc2fbc2ff66159a690ac92d6f
Paul Davis <paul@linuxaudiosystems.com>
2014-06-30 12:31:01 -0400

    ensure that Editor::entered_track is reset during session deletion to avoid a crash when we explicitly unset it elsewhere

--- gtk2_ardour/editor.cc
+++ gtk2_ardour/editor.cc
@@ -4874,6 +4874,10 @@ Editor::add_routes (RouteList& routes)
 void
 Editor::timeaxisview_deleted (TimeAxisView *tv)
 {
+	if (tv == entered_track) {
+		entered_track = 0;
+	}
+
 	if (_session && _session->deletion_in_progress()) {
 		/* the situation is under control */
 		return;
@@ -4884,10 +4888,6 @@ Editor::timeaxisview_deleted (TimeAxisView *tv)
 	RouteTimeAxisView* rtav = dynamic_cast<RouteTimeAxisView*> (tv);
 
 	_routes->route_removed (tv);
-
-	if (tv == entered_track) {
-		entered_track = 0;
-	}
 
 	TimeAxisView::Children c = tv->get_child_list ();
 	for (TimeAxisView::Children::const_iterator i = c.begin(); i != c.end(); ++i) {
