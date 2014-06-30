46d565e7edc9311fc0f6a3740d33b52e4cf6a3be
Paul Davis <paul@linuxaudiosystems.com>
2014-06-30 10:38:03 -0400

    make verbose cursor respond dynamically to theme changes

--- gtk2_ardour/verbose_cursor.cc
+++ gtk2_ardour/verbose_cursor.cc
@@ -30,6 +30,7 @@
 #include "audio_clock.h"
 #include "editor.h"
 #include "editor_drag.h"
+#include "global_signals.h"
 #include "main_clock.h"
 #include "verbose_cursor.h"
 
@@ -44,6 +45,14 @@ VerboseCursor::VerboseCursor (Editor* editor)
 	_canvas_item = new ArdourCanvas::TrackingText (_editor->get_noscroll_group());
 	CANVAS_DEBUG_NAME (_canvas_item, "verbose canvas cursor");
 	_canvas_item->set_font_description (Pango::FontDescription (ARDOUR_UI::config()->get_canvasvar_LargerBoldFont()));
+	color_handler ();
+
+	ARDOUR_UI_UTILS::ColorsChanged.connect (sigc::mem_fun (*this, &VerboseCursor::color_handler));
+}
+
+void
+VerboseCursor::color_handler ()
+{
 	_canvas_item->set_color (ARDOUR_UI::config()->get_canvasvar_VerboseCanvasCursor());
 }
 
