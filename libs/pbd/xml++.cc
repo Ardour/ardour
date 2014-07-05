1982873b192b7be2129f40f4176fc40333ce715d
Robin Gareus <robin@gareus.org>
2014-07-05 20:53:22 +0200

    debug info to trace down "cannot rename temp session file" errors
    ..but actually, the real error may be concurrent
    calls to Session::save_state()

--- libs/pbd/xml++.cc
+++ libs/pbd/xml++.cc
@@ -155,6 +155,20 @@ XMLTree::write() const
 	xmlSetDocCompressMode(doc, _compression);
 	writenode(doc, _root, doc->children, 1);
 	result = xmlSaveFormatFileEnc(_filename.c_str(), doc, "UTF-8", 1);
+#ifndef NDEBUG
+	if (result == -1) {
+		xmlErrorPtr xerr = xmlGetLastError ();
+		if (!xerr) {
+			std::cerr << "unknown XML error during xmlSaveFormatFileEnc()." << std::endl;
+		} else {
+			std::cerr << "xmlSaveFormatFileEnc: error"
+				<< " domain: " << xerr->domain
+				<< " code: " << xerr->code
+				<< " msg: " << xerr->message
+				<< std::endl;
+		}
+	}
+#endif
 	xmlFreeDoc(doc);
 
 	if (result == -1) {
