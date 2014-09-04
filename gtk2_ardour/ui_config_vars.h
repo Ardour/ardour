aa8bbf6a03ca0fec3808a6fd8eaf75ce1d19c655
Paul Davis <paul@linuxaudiosystems.com>
2014-09-04 13:29:53 -0400

    change all GUI config files (GTK RC and our own XML config files) to use product-neutral names

--- gtk2_ardour/ui_config_vars.h
+++ gtk2_ardour/ui_config_vars.h
@@ -18,7 +18,7 @@
 */
 
 UI_CONFIG_VARIABLE(std::string, icon_set, "icon-set", "default")
-UI_CONFIG_VARIABLE(std::string, ui_rc_file, "ui-rc-file", "ardour3_ui_dark.rc")
+UI_CONFIG_VARIABLE(std::string, ui_rc_file, "ui-rc-file", "ui_dark.rc")
 UI_CONFIG_VARIABLE(bool, flat_buttons, "flat-buttons", false)
 UI_CONFIG_VARIABLE(float, waveform_gradient_depth, "waveform-gradient-depth", 0)
 UI_CONFIG_VARIABLE(float, timeline_item_gradient_depth, "timeline-item-gradient-depth", 0.5)
