00931a22b15415a65a83034e661cb9a2ff899769
Paul Davis <paul@linuxaudiosystems.com>
2014-09-11 17:00:07 -0400

    tweak,rearrange and otherwise iteratively approach PRD for DYNAMIC area at upper right of arrange view

--- gtk2_ardour/editor.h
+++ gtk2_ardour/editor.h
@@ -714,6 +714,10 @@ class Editor : public PublicEditor, public PBD::ScopedConnectionList, public ARD
 	Gtk::Container& _master_bus_ui_home;
 	MasterBusUI*    _master_bus_ui;
 	Gtk::VBox&      vpacker;
+        Gtk::VBox&      timebars_vbox;
+        Gtk::HBox&      marker_lane_hbox;
+        WavesButton&    skip_button;
+        WavesButton&    global_tracks_button;
 
 	std::stack<Gdk::Cursor*> _cursor_stack;
 	Gdk::Cursor*          current_canvas_cursor;
