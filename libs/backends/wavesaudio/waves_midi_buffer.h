000609901b6b8d81d3088578917c9b48680a28e7
Paul Davis <paul@linuxaudiosystems.com>
2014-10-07 16:09:29 -0400

    add correct copyright statements to all files in Waves backend except those derived from portaudio/portmidi
    This follows the letter sent from Waves Audio Ltd. to Paul Davis dated February 20th 2014 agreeing to release this code
    under the GNU Public License, version 2, with copyright owned by Waves Audio Ltd

--- libs/backends/wavesaudio/waves_midi_buffer.h
+++ libs/backends/wavesaudio/waves_midi_buffer.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2013 Valeriy amyshniy
+    Copyright (C) 2013 Waves Audio Ltd.
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
