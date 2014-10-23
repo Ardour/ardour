2bc3ec6ab3eeb2fb75c0ba877f65fc7140f39895
Paul Davis <paul@linuxaudiosystems.com>
2014-10-23 13:17:27 -0400

    add new Gtk2mmext::EmScale class for measuring font-based sizing

---
+++ libs/gtkmm2ext/emscale.cc
@@ -0,0 +1,73 @@
+/*
+    Copyright (C) 2014 Paul Davis, Robin Gareus
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <algorithm>
+#include <gdk/gdk.h>
+#include <pangomm/layout.h>
+
+#include "gtkmm2ext/emscale.h"
+
+#include "i18n.h"
+
+using namespace Gtkmm2ext;
+
+std::map<std::string,EmScale> EmScale::_emscales;
+
+EmScale::EmScale (const Pango::FontDescription& fd)
+	: _font (fd)
+	, _char_pixel_width (-1)
+	, _char_pixel_height (-1)
+	, _char_avg_pixel_width (-1)
+{
+}
+
+void
+EmScale::recalc_char_pixel_geometry ()
+{
+	if (_char_pixel_height > 0 && _char_pixel_width > 0) {
+		return;
+	}
+
+	Glib::RefPtr<Pango::Context> pc = Glib::wrap (gdk_pango_context_get_for_screen (gdk_screen_get_default()));
+	Glib::RefPtr<Pango::Layout> layout = Pango::Layout::create (pc);
+
+	layout->set_font_description (_font);
+
+	int w, h;
+	std::string x = _("ABCDEFGHIJLKMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+	layout->set_text (x);
+	layout->get_pixel_size (w, h);
+	_char_pixel_height = std::max(4, h);
+	// number of actual chars in the string (not bytes)
+	// Glib to the rescue.
+	Glib::ustring gx(x);
+	_char_avg_pixel_width = w / (float)gx.size();
+	_char_pixel_width = std::max(4, (int) ceil (_char_avg_pixel_width));
+}
+
+EmScale&
+EmScale::by_font (const Pango::FontDescription& fd)
+{
+	std::map<std::string,EmScale>::iterator i = _emscales.find (fd.to_string());
+
+	if (i == _emscales.end()) {
+		i = _emscales.insert (std::make_pair (fd.to_string(), EmScale (fd))).first;
+	}
+
+	return i->second;
+}
---
+++ libs/gtkmm2ext/gtkmm2ext/emscale.h
@@ -0,0 +1,56 @@
+/*
+    Copyright (C) 2014 Paul Davis, Robin Gareus
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __libgtkmm2ext_emscale_h__
+#define __libgtkmm2ext_emscale_h__
+
+#include <map>
+#include <string>
+
+#include <pangomm/fontdescription.h>
+
+#include "gtkmm2ext/visibility.h"
+
+namespace Gtkmm2ext 
+{
+
+class LIBGTKMM2EXT_API EmScale
+{
+    public:
+	EmScale (const Pango::FontDescription&);
+
+	unsigned int char_pixel_width() { if (_char_pixel_width < 1) recalc_char_pixel_geometry() ; return _char_pixel_width; }
+	unsigned int char_pixel_height() { if (_char_pixel_height < 1) recalc_char_pixel_geometry() ; return _char_pixel_height; }
+	float char_avg_pixel_width() { if (_char_pixel_width < 1) recalc_char_pixel_geometry() ; return _char_avg_pixel_width; }
+
+	static EmScale& by_font (const Pango::FontDescription&);
+
+    private:
+	Pango::FontDescription _font;
+	unsigned int           _char_pixel_width;
+	unsigned int           _char_pixel_height;
+	float                  _char_avg_pixel_width;
+
+	void recalc_char_pixel_geometry ();
+
+	static std::map<std::string,EmScale> _emscales;
+};
+
+} // namespace 
+
+#endif /* __libgtkmm2ext_emscale_h__ */
