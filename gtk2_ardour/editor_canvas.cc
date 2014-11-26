c0a21ac0f8d5796de6824aace076f4ed7bf5a737
Paul Davis <paul@linuxaudiosystems.com>
2014-11-26 13:55:02 +0200

    tweak height of the rectangle drawn while dragging in the ruler/loop bar

--- gtk2_ardour/editor_canvas.cc
+++ gtk2_ardour/editor_canvas.cc
@@ -201,7 +201,7 @@ Editor::initialize_canvas ()
 	range_bar_drag_rect->hide ();
 
 	transport_bar_drag_rect = new ArdourCanvas::Rectangle (ruler_group, ArdourCanvas::Rect (0.0, 0.0, 100, 
-                                                                                                ruler_height - ruler_divide_height));
+                                                                                                ruler_height - ruler_divide_height - 2.0));
 	CANVAS_DEBUG_NAME (transport_bar_drag_rect, "transport drag");
 	transport_bar_drag_rect->set_outline (false);
 	transport_bar_drag_rect->hide ();
