c98366c7c8a18a7987902dd418d876f2e1ee5372
Paul Davis <paul@linuxaudiosystems.com>
2014-12-12 15:28:16 -0500

    fixed merge conflict

--- gtk2_ardour/automation_line.cc
+++ gtk2_ardour/automation_line.cc
@@ -926,7 +926,7 @@ AutomationLine::set_selected_points (PointSelection const & points)
 
 void AutomationLine::set_colors ()
 {
-	set_line_color (ARDOUR_UI::config()->get_AutomationLine());
+	set_line_color (ARDOUR_UI::config()->color ("automation line"));
 	for (vector<ControlPoint*>::iterator i = control_points.begin(); i != control_points.end(); ++i) {
 		(*i)->set_color ();
 	}
