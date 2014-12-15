c49b2aed95acbbe496cdf357f46a67b01ea44c5e
Greg Zharun <grygoriiz@wavesglobal.com>
2014-12-15 14:22:07 +0200

    [Summary] Fixed indentation of new file info labels

--- gtk2_ardour/time_axis_view_item.cc
+++ gtk2_ardour/time_axis_view_item.cc
@@ -799,7 +799,11 @@ TimeAxisViewItem::manage_ioconfig_highlight ()
         return;
     }
     
-    double ioconfig_x1 = ioconfig_label_width + NAME_HIGHLIGHT_X_INDENT + name_highlight->x1 ();
+    double ioconfig_x1 = ioconfig_label_width + 2*NAME_HIGHLIGHT_X_INDENT + name_highlight->x1 ();
+#ifdef __APPLE__
+	ioconfig_x1 =- NAME_HIGHLIGHT_X_INDENT; // there is a bug in pango with text width definition on MAC
+#endif // __APPLE__
+
     if (_width < ioconfig_x1) {
         ioconfig_x1 = _width;
     }
@@ -838,7 +842,11 @@ TimeAxisViewItem::manage_sr_highlight ()
         return;
     }
     
-    double sr_x1 = sr_label_width + NAME_HIGHLIGHT_X_INDENT + ioconfig_highlight->x1 ();
+    double sr_x1 = sr_label_width + 2*NAME_HIGHLIGHT_X_INDENT + ioconfig_highlight->x1 ();
+#ifdef __APPLE__
+	sr_x1 =- NAME_HIGHLIGHT_X_INDENT; // there is a bug in pango with text width definition on MAC
+#endif // __APPLE__
+
     if (_width < sr_x1) {
         sr_x1 = _width;
     }
