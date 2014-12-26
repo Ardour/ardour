cc3305a79c722f756941c98c6b2e3ee9a1bec399
Nikolay <MPolianovskyi@wavesglobal.com>
2014-12-26 14:17:48 +0200

    [Summary] Updated copyright message

--- libs/ardour/engine_state_controller.cc
+++ libs/ardour/engine_state_controller.cc
@@ -1,10 +1,21 @@
-//
-//  engine_state_controller.cpp
-//  Tracks
-//
-//  Created by Grygorii Zharun on 4/30/14.
-//  Copyright (c) 2014 Waves. All rights reserved.
-//
+/*
+ Copyright (C) 2014 Waves Audio Ltd.
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ 
+ */
 
 #include "ardour/engine_state_controller.h"
 
