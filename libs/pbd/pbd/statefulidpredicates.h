b16c8d5ff5fa87b9058d961e63f2c0d19c92e0cd
Sakari Bergen <sakari.bergen@beatwaves.net>
2015-01-05 01:06:52 +0200

    Fix some location xml handling to work with old session files.
    Also refactor some code around it while I'm at it.

---
+++ libs/pbd/pbd/statefulidpredicates.h
@@ -0,0 +1,59 @@
+/*
+    Copyright (C) 2015 Paul Davis 
+    Author: Sakari Bergen
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef __pbd_statefulidpredicate_h__
+#define __pbd_statefulidpredicate_h__
+
+#include "pbd/libpbd_visibility.h"
+#include "pbd/id.h"
+#include "pbd/stateful.h"
+
+namespace PBD {
+
+struct LIBPBD_TEMPLATE_MEMBER_API StatefulIdEquals
+{
+	StatefulIdEquals(ID const & id)
+	  : id(id)
+	{}
+
+	template<typename StatefulPtr>
+	bool operator() (StatefulPtr p)
+	{
+		return p && compare(*p);
+	}
+
+	bool operator() (Stateful const & s)
+	{
+		return compare(s);
+	}
+
+private:
+
+	bool compare(Stateful const & s)
+	{
+		return s.id() == id;
+	}
+
+	ID const & id;
+};
+
+} // namespace PBD
+
+#endif // __pbd_statefulidpredicate_h__
