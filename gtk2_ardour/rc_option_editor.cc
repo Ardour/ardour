6cc7e9d34c79b5108c6bfeacc4573f7d5439431d
Paul Davis <paul@linuxaudiosystems.com>
2015-01-16 16:01:33 -0500

    fix copy-n-paste errors that used wrong buttons for playhead priorities

--- gtk2_ardour/rc_option_editor.cc
+++ gtk2_ardour/rc_option_editor.cc
@@ -135,7 +135,7 @@ class AutoReturnTargetOptions : public OptionEditorBox
 	}
 	void last_roll_toggled () {
 		AutoReturnTarget art = _rc_config->get_auto_return_target_list ();
-		if (range_selection_button.get_active ()) {
+		if (last_roll_button.get_active ()) {
 			_rc_config->set_auto_return_target_list (AutoReturnTarget (art | LastLocate));
 		} else {
 			_rc_config->set_auto_return_target_list (AutoReturnTarget (art & ~LastLocate));
@@ -143,7 +143,7 @@ class AutoReturnTargetOptions : public OptionEditorBox
 	}
 	void region_selection_toggled () {
 		AutoReturnTarget art = _rc_config->get_auto_return_target_list ();
-		if (range_selection_button.get_active ()) {
+		if (region_selection_button.get_active ()) {
 			_rc_config->set_auto_return_target_list (AutoReturnTarget (art | RegionSelectionStart));
 		} else {
 			_rc_config->set_auto_return_target_list (AutoReturnTarget (art & ~RegionSelectionStart));
@@ -151,7 +151,7 @@ class AutoReturnTargetOptions : public OptionEditorBox
 	}
 	void loop_toggled () {
 		AutoReturnTarget art = _rc_config->get_auto_return_target_list ();
-		if (range_selection_button.get_active ()) {
+		if (loop_button.get_active ()) {
 			_rc_config->set_auto_return_target_list (AutoReturnTarget (art | Loop));
 		} else {
 			_rc_config->set_auto_return_target_list (AutoReturnTarget (art & ~Loop));
