ab3ec791eaaa751cec5906a5e581024c0f391b0e
Paul Davis <paul@linuxaudiosystems.com>
2015-01-24 19:06:31 -0500

    Add a new signal (not yet used) to initiate session saves; alter signal name for existing signal used to allow other objects to add XML state to session

--- gtk2_ardour/video_timeline.cc
+++ gtk2_ardour/video_timeline.cc
@@ -142,7 +142,7 @@ VideoTimeLine::set_session (ARDOUR::Session *s)
 	SessionHandlePtr::set_session (s);
 	if (!_session) { return ; }
 
-	_session->SaveSession.connect_same_thread (sessionsave, boost::bind (&VideoTimeLine::save_session, this));
+	_session->SessionSaveUnderway.connect_same_thread (sessionsave, boost::bind (&VideoTimeLine::save_session, this));
 	LocaleGuard lg (X_("C"));
 
 	XMLNode* node = _session->extra_xml (X_("Videotimeline"));
