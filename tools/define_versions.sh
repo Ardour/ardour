ce9dbfce15f5c2d16d886859e15da769cc9d29bc
Paul Davis <paul@linuxaudiosystems.com>
2015-01-29 19:02:38 -0500

    Revert "new version of define-versions.sh, taken from ardour"
    This reverts commit e4ece4f61db22c4438b3cd2c7054c9dfb8b41144.

--- tools/define_versions.sh
+++ tools/define_versions.sh
@@ -8,21 +8,19 @@ else
     EXTENDED_RE=-r
 fi
 
-GIT_REV_REGEXP='([0-9][0-9]*)\.([0-9][0-9]*)\-?(rc[0-9]*)?-?([0-9][0-9]*)?(-g([a-f0-9]+))?'
+GIT_REV_REGEXP='([0-9][0-9]*)\.([0-9][0-9]*)\.([0-9][0-9]*)-?([0-9][0-9]*)?-?([a-z0-9]*)'
 
 major_version=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\1/"`
 minor_version=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\2/"`
-rc=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\3/"`
+micro_version=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\3/"`
 r=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\4/"`
-commit=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\6/"`
+commit=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\5/"`
 
-if [ "x$rc" != "x" ] ; then
-    revcount=$rc${r:+.$r}
-elif [ "x$r" != "x" ] ; then
+if [ "x$r" != "x" ] ; then
     revcount=$r
 fi
 
-release_version=${major_version}.${minor_version}${revcount:+.$revcount}
+release_version=${major_version}.${minor_version}.${micro_version}${revcount:+.$revcount}
 
 #
 # Figure out the Build Type
