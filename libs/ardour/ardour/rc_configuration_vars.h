3b888537bca1d727210d2c3b314e69311b24d458
Valeriy Kamyshniy <vkamyshniy@wavesglobal.com>
2015-02-02 12:09:00 +0200

    [Summary] Defalut for super-rapid-clock-update was switched from FALSE to TRUE

--- libs/ardour/ardour/rc_configuration_vars.h
+++ libs/ardour/ardour/rc_configuration_vars.h
@@ -160,7 +160,7 @@ CONFIG_VARIABLE (bool, show_track_meters, "show-track-meters", true)
 CONFIG_VARIABLE (bool, locate_while_waiting_for_sync, "locate-while-waiting-for-sync", false)
 CONFIG_VARIABLE (bool, disable_disarm_during_roll, "disable-disarm-during-roll", false)
 CONFIG_VARIABLE (bool, follow_edits, "follow-edits", false)
-CONFIG_VARIABLE (bool, super_rapid_clock_update, "super-rapid-clock-update", false)
+CONFIG_VARIABLE (bool, super_rapid_clock_update, "super-rapid-clock-update", true)
 CONFIG_VARIABLE (AutoReturnTarget, auto_return_target_list, "auto-return-target-list", AutoReturnTarget(LastLocate|RangeSelectionStart|Loop|RegionSelectionStart))
 
 /* metering */
