74839190d9155e149a4715368a5b80940a7c6117
Robin Gareus <robin@gareus.org>
2015-02-12 23:00:47 +0100

    win-pkg, ignore missing suil modules

--- tools/x-win/package.sh
+++ tools/x-win/package.sh
@@ -122,7 +122,7 @@ cp `find build/libs/panners/ -iname "*.dll"` $ALIBDIR/panners/
 
 cp -r build/libs/LV2 $ALIBDIR/
 cp -r build/libs/vamp-plugins/*ardourvampplugins*.dll $ALIBDIR/vamp/libardourvampplugins.dll
-cp $PREFIX/lib/suil-*/*.dll $ALIBDIR/suil/
+cp $PREFIX/lib/suil-*/*.dll $ALIBDIR/suil/ || true
 
 # lv2 core, classifications etc - TODO check if we need the complete LV2 ontology
 if test -d $PREFIX/lib/lv2/lv2core.lv2 ; then
