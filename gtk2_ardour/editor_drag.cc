cde94f002281228f96a9920a9242a1bceab30dd2
Paul Davis <paul@linuxaudiosystems.com>
2015-02-16 16:26:42 -0500

    fixes for non-overlapping fade in/out even in regions not at zero

--- gtk2_ardour/editor_drag.cc
+++ gtk2_ardour/editor_drag.cc
@@ -3150,14 +3150,12 @@ void
 FadeInDrag::motion (GdkEvent* event, bool)
 {
 	framecnt_t fade_length;
-
 	framepos_t const pos = adjusted_current_frame (event);
-
 	boost::shared_ptr<AudioRegion> region = boost::dynamic_pointer_cast<AudioRegion> (_primary->region ());
 
 	if (pos < (region->position() + 64)) {
 		fade_length = 64; // this should be a minimum defined somewhere
-	} else if (pos > region->length() - region->fade_out()->back()->when) {
+	} else if (pos > region->position() + region->length() - region->fade_out()->back()->when) {
 		fade_length = region->length() - region->fade_out()->back()->when - 1;
 	} else {
 		fade_length = pos - region->position();
@@ -3192,7 +3190,7 @@ FadeInDrag::finished (GdkEvent* event, bool movement_occurred)
 
 	if (pos < (region->position() + 64)) {
 		fade_length = 64; // this should be a minimum defined somewhere
-	} else if (pos >= region->length() - region->fade_out()->back()->when) {
+	} else if (pos >= region->position() + region->length() - region->fade_out()->back()->when) {
 		fade_length = region->length() - region->fade_out()->back()->when - 1;
 	} else {
 		fade_length = pos - region->position();
@@ -3271,10 +3269,10 @@ FadeOutDrag::motion (GdkEvent* event, bool)
 
 	if (pos > (region->last_frame() - 64)) {
 		fade_length = 64; // this should really be a minimum fade defined somewhere
-	} else if (pos <= region->fade_in()->back()->when) {
+	} else if (pos <= region->position() + region->fade_in()->back()->when) {
 		fade_length = region->length() - region->fade_in()->back()->when - 1;
 	} else {
-		fade_length = region->length() - pos;
+		fade_length = region->last_frame() - pos;
 	}
 
 	for (list<DraggingView>::iterator i = _views.begin(); i != _views.end(); ++i) {
@@ -3306,10 +3304,10 @@ FadeOutDrag::finished (GdkEvent* event, bool movement_occurred)
 
 	if (pos > (region->last_frame() - 64)) {
 		fade_length = 64; // this should really be a minimum fade defined somewhere
-	} else if (pos <= region->fade_in()->back()->when) {
+	} else if (pos <= region->position() + region->fade_in()->back()->when) {
 		fade_length = region->length() - region->fade_in()->back()->when - 1;
 	} else {
-		fade_length = region->length() - pos;
+		fade_length = region->last_frame() - pos;
 	}
 
 	_editor->begin_reversible_command (_("change fade out length"));
