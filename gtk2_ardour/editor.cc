f865d4512ef077ca126b23ba1f1794d2f013722f
YPozdnyakov <YPozdnyakov@wavesglobal.com>
2015-02-16 18:16:09 +0200

    [Summary]: Disable "Reverse" item from region context menu on every platforms except mac

--- gtk2_ardour/editor.cc
+++ gtk2_ardour/editor.cc
@@ -1970,7 +1970,9 @@ Editor::add_region_context_items (Gtk::Menu_Helpers::MenuList& edit_items)
     edit_items.push_back (SeparatorElem());
     
     edit_items.push_back (MenuElem (_("Import Audio File"), sigc::bind (sigc::mem_fun(*this, &Editor::add_external_audio_action), ImportToTrack)));
-    edit_items.push_back (*manage (ActionManager::get_action_from_name ("reverse-region")->create_menu_item ()));
+	#if defined (__APPLE__)
+		edit_items.push_back (*manage (ActionManager::get_action_from_name ("reverse-region")->create_menu_item ()));
+	#endif
 //    Tracks Live doesn't use it
 //    edit_items.push_back (*manage (ActionManager::get_action_from_name ("strip-region-silence")->create_menu_item ()));
 //    edit_items.back().set_label ("Strip Silence");
