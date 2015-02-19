19d33576627ec26dd9eedef98391cf46f106b5b4
VKamyshniy <VKamyshniy@wavesglobal.com>
2015-02-19 21:46:42 +0200

    [Summary] Just replacing "spaced" tabs with TAB like it should be.

--- gtk2_ardour/marker.h
+++ gtk2_ardour/marker.h
@@ -62,7 +62,7 @@ class Marker : public sigc::trackable
 		LoopEnd,
 		PunchIn,
 		PunchOut,
-                Range,
+		Range,
 	};
 
 
@@ -84,13 +84,13 @@ class Marker : public sigc::trackable
 
 	void set_name (const std::string&);
 
-        void set_selected (bool yn);
+	void set_selected (bool yn);
     bool selected () const { return _selected; }
     void set_color (ArdourCanvas::Color);
     void reset_color ();
-        
+	
 	void set_position (framepos_t start, framepos_t end = -1) {
-                return _set_position (start, end);
+		return _set_position (start, end);
     }
 
 	framepos_t position() const { return frame_position; }
@@ -112,69 +112,69 @@ class Marker : public sigc::trackable
 
 	bool label_on_left () const;
 
-        void set_trim_active (bool);
-        
-        static double marker_height() { return _marker_height; }
+	void set_trim_active (bool);
+	
+	static double marker_height() { return _marker_height; }
 
-        static const char * default_new_marker_prefix;
+	static const char * default_new_marker_prefix;
 
   protected:
 	PublicEditor& editor;
-        ARDOUR::Location* _location;
+	ARDOUR::Location* _location;
 
 	Pango::FontDescription name_font;
 
 	ArdourCanvas::Container* _parent;
 	ArdourCanvas::Container *group;
 	ArdourCanvas::Polygon *mark;
-        ArdourCanvas::Text *_name_item;
+	ArdourCanvas::Text *_name_item;
 	ArdourCanvas::Line* _start_line;
 	ArdourCanvas::Rectangle* _name_background;
 	ArdourCanvas::Rectangle* _scene_change_rect;
 	ArdourCanvas::Text* _scene_change_text;
     ArdourCanvas::Text* _marker_lock_text;
-        ArdourCanvas::Rectangle* left_drag_handle;
-        ArdourCanvas::Rectangle* right_drag_handle;
+	ArdourCanvas::Rectangle* left_drag_handle;
+	ArdourCanvas::Rectangle* right_drag_handle;
 
 	std::string  _name;
-	double        unit_position;
+	double	unit_position;
 	framepos_t    frame_position;
 	double       _shift;
-    bool         _selected;
-	Type         _type;
-	int           name_height;
-	bool         _shown;
+    bool	 _selected;
+	Type	 _type;
+	int	   name_height;
+	bool	 _shown;
 	double       _height;
 	uint32_t     _color;
 	double       _left_label_limit; ///< the number of pixels available to the left of this marker for a label
 	double       _right_label_limit; ///< the number of pixels available to the right of this marker for a label
 	double       _label_offset;
-        bool         _have_scene_change;
-        
-        virtual void flags_changed ();
-        virtual void bounds_changed ();
-        virtual void name_changed ();
-        virtual void scene_change_active_changed ();
-        virtual void scene_change_changed ();
-        PBD::ScopedConnection scene_change_active_connection;
-        
-        virtual void _set_position (framepos_t, framepos_t);
+	bool	 _have_scene_change;
+	
+	virtual void flags_changed ();
+	virtual void bounds_changed ();
+	virtual void name_changed ();
+	virtual void scene_change_active_changed ();
+	virtual void scene_change_changed ();
+	PBD::ScopedConnection scene_change_active_connection;
+	
+	virtual void _set_position (framepos_t, framepos_t);
 
-        void pick_basic_color (ArdourCanvas::Color);
+	void pick_basic_color (ArdourCanvas::Color);
 	virtual void use_color ();
 	virtual void reposition ();
 	virtual void setup_name_display ();
 	virtual void setup_line ();
-        
-        static const double _marker_height;
+	
+	static const double _marker_height;
 
 private:
 	/* disallow copy construction */
 	Marker (Marker const &);
 	Marker & operator= (Marker const &);
 
-        PBD::ScopedConnectionList location_connections;
-        void connect_to_scene_change_signals ();
+	PBD::ScopedConnectionList location_connections;
+	void connect_to_scene_change_signals ();
 };
 
 /** A Marker that displays a range (start+end) rather than a single location
@@ -182,15 +182,15 @@ private:
 class RangeMarker : public Marker
 {
     public:
-        RangeMarker (ARDOUR::Location*, PublicEditor& editor, ArdourCanvas::Container &, double height, guint32 rgba, const std::string& text,
-                     framepos_t start, framepos_t end);
-        ~RangeMarker ();
-        
+	RangeMarker (ARDOUR::Location*, PublicEditor& editor, ArdourCanvas::Container &, double height, guint32 rgba, const std::string& text,
+		     framepos_t start, framepos_t end);
+	~RangeMarker ();
+	
 	void setup_name_display ();
 	void use_color ();
 	void reposition ();
-        void setup_line ();
-        void canvas_height_set (double);
+	void setup_line ();
+	void canvas_height_set (double);
 
     protected:
     framepos_t _end_frame;
@@ -211,17 +211,17 @@ class RangeMarker : public Marker
 class RulerMarker: public RangeMarker 
 {
     public:
-        RulerMarker (ARDOUR::Location*, PublicEditor& editor, ArdourCanvas::Container &, double height, guint32 rgba, const std::string& text,
-                     framepos_t start, framepos_t end);
-        
+	RulerMarker (ARDOUR::Location*, PublicEditor& editor, ArdourCanvas::Container &, double height, guint32 rgba, const std::string& text,
+		     framepos_t start, framepos_t end);
+	
 	void use_color ();
-        void setup_name_display ();
+	void setup_name_display ();
 };
 
 class TempoMarker : public Marker
 {
   public:
-        TempoMarker (PublicEditor& editor, ArdourCanvas::Container &, double height, guint32 rgba, const std::string& text, ARDOUR::TempoSection&);
+	TempoMarker (PublicEditor& editor, ArdourCanvas::Container &, double height, guint32 rgba, const std::string& text, ARDOUR::TempoSection&);
 	~TempoMarker ();
 
 	ARDOUR::TempoSection& tempo() const { return _tempo; }
@@ -233,7 +233,7 @@ class TempoMarker : public Marker
 class MeterMarker : public Marker
 {
   public:
-        MeterMarker (PublicEditor& editor, ArdourCanvas::Container &, double height, guint32 rgba, const std::string& text, ARDOUR::MeterSection&);
+	MeterMarker (PublicEditor& editor, ArdourCanvas::Container &, double height, guint32 rgba, const std::string& text, ARDOUR::MeterSection&);
 	~MeterMarker ();
 
 	ARDOUR::MeterSection& meter() const { return _meter; }
