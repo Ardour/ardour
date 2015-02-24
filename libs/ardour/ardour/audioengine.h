97ca297ab8fb88db0dac6a0ecfb1f2ce31e5a3a9
GZharun <grygoriiz@wavesglobal.com>
2015-02-24 14:27:36 +0200

    [Summary] Added possibility to identify IO thread which does not have required resources initialized during process callback handling
    Conflicts:
    	libs/ardour/ardour/audioengine.h
    	libs/backends/wavesaudio/waves_audiobackend.cc
    	libs/pbd/pbd/pool.h

--- libs/ardour/ardour/audioengine.h
+++ libs/ardour/ardour/audioengine.h
@@ -209,6 +209,9 @@ class LIBARDOUR_API AudioEngine : public SessionHandlePtr, public PortManager
 	void latency_callback (bool for_playback);
 	void halted_callback (const char* reason);
 
+	/* checks if current thread is properly set up for audio processing */
+	static bool thread_initialised_for_audio_processing ();
+
 	/* sets up the process callback thread */
 	static void thread_init_callback (void *);
 
