21193c6fcab5160f4514b8f071e6cd288add033c
Valeriy Kamyshniy <vkamyshniy@wavesglobal.com>
2015-04-02 21:51:23 +0300

    [Summary] Adding option to avoid sorting the routs in Session::foreach method. Now ARDOUR_UI::every_second do not spend time for unnecessary sorting when calling update_disk_space(). [Reviewed] Paul Davis [Required review] YPozdnyakov, GZharun

--- gtk2_ardour/ardour_ui.cc
+++ gtk2_ardour/ardour_ui.cc
@@ -1420,7 +1420,7 @@ ARDOUR_UI::update_disk_space()
 		snprintf (buf, sizeof (buf), "%s", _("Disk: <span foreground=\"green\">24hrs+</span>"));
 	} else {
 		rec_enabled_streams = 0;
-		_session->foreach_route (this, &ARDOUR_UI::count_recenabled_streams);
+		_session->foreach_route (this, &ARDOUR_UI::count_recenabled_streams, false);
 
 		framecnt_t frames = opt_frames.get_value_or (0);
 
