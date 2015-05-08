511f609a148f7aceadedb7ee7d982d7dc4e1e592
Paul Davis <paul@linuxaudiosystems.com>
2015-05-08 15:03:06 -0400

    add Session::reconnect_midi_scene_ports()

--- libs/ardour/session.cc
+++ libs/ardour/session.cc
@@ -2375,6 +2375,40 @@ Session::reconnect_existing_routes (bool withLock, bool reconnect_master, bool r
          */
 }
 
+void
+Session::reconnect_midi_scene_ports(bool inputs)
+{
+	if (inputs) {
+		scene_in()->disconnect_all ();
+        
+		std::vector<EngineStateController::MidiPortState> midi_port_states;
+		EngineStateController::instance()->get_physical_midi_input_states (midi_port_states);
+        
+		std::vector<EngineStateController::MidiPortState>::iterator state_iter = midi_port_states.begin();
+        
+		for (; state_iter != midi_port_states.end(); ++state_iter) {
+			if (state_iter->active && state_iter->available && state_iter->connected) {
+				scene_in()->connect (state_iter->name);
+			}
+		}
+
+	} else {
+		scene_out()->disconnect_all ();
+
+		std::vector<EngineStateController::MidiPortState> midi_port_states;
+		EngineStateController::instance()->get_physical_midi_output_states (midi_port_states);
+        
+		std::vector<EngineStateController::MidiPortState>::iterator state_iter = midi_port_states.begin();
+        
+		for (; state_iter != midi_port_states.end(); ++state_iter) {
+			if (state_iter->active && state_iter->available && state_iter->connected) {
+				scene_out()->connect (state_iter->name);
+			}
+		}
+
+	}
+}
+
 
 /** Caller must not hold process lock
  *  @param name_template string to use for the start of the name, or "" to use "Audio".
