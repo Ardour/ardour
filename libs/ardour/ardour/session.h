3b38b0fe1bd8f6ac4253c34fea7c8e0eb8397ef5
Paul Davis <paul@linuxaudiosystems.com>
2015-05-08 15:03:06 -0400

    add Session::reconnect_midi_scene_ports()

--- libs/ardour/ardour/session.h
+++ libs/ardour/ardour/session.h
@@ -980,6 +980,8 @@ class LIBARDOUR_API Session : public PBD::StatefulDestructible, public PBD::Scop
     
 	MIDI::MachineControl& mmc() { return *_mmc; }
 
+	void reconnect_midi_scene_ports (bool);
+
   protected:
 	friend class AudioEngine;
 	void set_block_size (pframes_t nframes);
