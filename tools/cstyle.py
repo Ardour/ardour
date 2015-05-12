d0aefb28b99cb8e977a9f1d59f5df04106eb352f
Paul Davis <paul@linuxaudiosystems.com>
2015-05-12 11:25:36 -0400

    catch identifiers ending or starting with _ when checking operator spacing

--- tools/cstyle.py
+++ tools/cstyle.py
@@ -209,7 +209,7 @@ class CStyleChecker:
 			if check_re.search (line):
 				self.error (msg)
 
-		if re.search ("[a-zA-Z0-9][<>!=^/&\|]{1,2}[a-zA-Z0-9]", line):
+		if re.search ("[a-zA-Z0-9_][<>!=^/&\|]{1,2}[a-zA-Z0-9_]", line):
                         # ignore #include <foo.h> and C++ templates with indirection/pointer/reference operators
 			if not re.search (".*#include.*[a-zA-Z0-9]/[a-zA-Z]", line) and not re.search ("[a-zA-Z0-9_]>[&\*]", line):
 				self.error ("missing space around operator")
