38125c7de7a88c5259ce24bfd81ff38a782ebdfb
Paul Davis <paul@linuxaudiosystems.com>
2015-05-19 08:39:34 -0400

    add some session-scope configuration variables needed by Tracks.
    Also add some comments regarding the presence of certain variables in this file

--- libs/ardour/ardour/session_configuration_vars.h
+++ libs/ardour/ardour/session_configuration_vars.h
@@ -27,7 +27,6 @@
 
 CONFIG_VARIABLE (uint32_t, destructive_xfade_msecs,  "destructive-xfade-msecs", 2)
 CONFIG_VARIABLE (bool, use_region_fades, "use-region-fades", true)
-CONFIG_VARIABLE (bool, show_region_fades, "show-region-fades", true)
 CONFIG_VARIABLE (SampleFormat, native_file_data_format,  "native-file-data-format", ARDOUR::FormatFloat)
 CONFIG_VARIABLE (HeaderFormat, native_file_header_format,  "native-file-header-format", ARDOUR::WAVE)
 CONFIG_VARIABLE (bool, auto_play, "auto-play", false)
@@ -46,8 +45,6 @@ CONFIG_VARIABLE (std::string, take_name, "take-name", "Take1")
 CONFIG_VARIABLE (bool, jack_time_master, "jack-time-master", true)
 CONFIG_VARIABLE (bool, use_video_sync, "use-video-sync", false)
 CONFIG_VARIABLE (float, video_pullup, "video-pullup", 0.0f)
-CONFIG_VARIABLE (bool, show_summary, "show-summary", true)
-CONFIG_VARIABLE (bool, show_group_tabs, "show-group-tabs", true)
 CONFIG_VARIABLE (bool, external_sync, "external-sync", false)
 CONFIG_VARIABLE (InsertMergePolicy, insert_merge_policy, "insert-merge-policy", InsertMergeRelax)
 CONFIG_VARIABLE (framecnt_t, timecode_offset, "timecode-offset", 0)
@@ -57,6 +54,15 @@ CONFIG_VARIABLE (std::string, timecode_generator_offset, "timecode-generator-off
 CONFIG_VARIABLE (bool, glue_new_markers_to_bars_and_beats, "glue-new-markers-to-bars-and-beats", false)
 CONFIG_VARIABLE (bool, midi_copy_is_fork, "midi-copy-is-fork", false)
 CONFIG_VARIABLE (bool, glue_new_regions_to_bars_and_beats, "glue-new-regions-to-bars-and-beats", false)
+/* These are GUI-only properties and should not be present in this
+ * context. There needs to be a new GUI-level session-scoped configuration
+ * variable header.
+ */
+CONFIG_VARIABLE (double, wave_amplitude_zoom, "wave-amplitude-zoom", 0.0)
+CONFIG_VARIABLE (uint16_t, wave_zoom_factor, "wave-zoom-factor", 2)
+CONFIG_VARIABLE (bool, show_summary, "show-summary", true)
+CONFIG_VARIABLE (bool, show_group_tabs, "show-group-tabs", true)
+CONFIG_VARIABLE (bool, show_region_fades, "show-region-fades", true)
 CONFIG_VARIABLE (bool, use_video_file_fps, "use-video-file-fps", false)
 CONFIG_VARIABLE (bool, videotimeline_pullup, "videotimeline-pullup", true)
 CONFIG_VARIABLE (bool, show_busses_on_meterbridge, "show-busses-on-meterbridge", false)
@@ -68,3 +74,14 @@ CONFIG_VARIABLE (bool, show_solo_on_meterbridge, "show-solo-on-meterbridge", fal
 CONFIG_VARIABLE (bool, show_monitor_on_meterbridge, "show-monitor-on-meterbridge", false)
 CONFIG_VARIABLE (bool, show_name_on_meterbridge, "show-name-on-meterbridge", true)
 CONFIG_VARIABLE (uint32_t, meterbridge_label_height,  "meterbridge-label-height", 0)
+
+#ifdef USE_TRACKS_CODE_FEATURES
+/* This variable was not discussed with Ardour developers and is considered
+   weakly conceived. It is also poorly named, since we have "groups" already
+   present in libardour but this variable has nothing to do with it. There
+   should have been more discussion about what was required and the best 
+   way to accomplish it.
+*/
+CONFIG_VARIABLE (bool, enable_group_edit, "enable-group-edit", false)
+/* These are GUI-only properties and should not be present in this context */
+#endif
