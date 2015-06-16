03dbf7857e913823da0530bc2b8b83bf37dd79d7
Paul Davis <paul@linuxaudiosystems.com>
2015-06-16 11:11:57 -0400

    remove compilation warning (-Wunused-but-set-variable)

--- libs/qm-dsp/dsp/onsets/PeakPicking.cpp
+++ libs/qm-dsp/dsp/onsets/PeakPicking.cpp
@@ -97,8 +97,6 @@ int PeakPicking::quadEval( vector<double> &src, vector<int> &idx )
     vector <double> m_poly;
     vector <double> m_err;
 
-    double p;
-
     m_poly.push_back(0);
     m_poly.push_back(0);
     m_poly.push_back(0);
@@ -127,8 +125,6 @@ int PeakPicking::quadEval( vector<double> &src, vector<int> &idx )
 	    selMax = src[ m_maxIndex[j] + k ] ;
 	    m_maxFit.push_back(selMax);			
 	}
-
-	p = TPolyFit::PolyFit2( m_err, m_maxFit, m_poly);
 
 	double f = m_poly[0];
 	double h = m_poly[2];
