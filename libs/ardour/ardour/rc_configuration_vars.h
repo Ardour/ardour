ed72d9ddae569000cf9330e1270cc172e628203c
Robin Gareus <robin@gareus.org>
2015-06-29 18:57:21 +0200

    update system config

--- libs/ardour/ardour/rc_configuration_vars.h
+++ libs/ardour/ardour/rc_configuration_vars.h
@@ -29,7 +29,7 @@
     DO dump the config using cfgtool to system_config
     after modifying this file.
 
-    ./waf && build/cfgtool/cfgtool system_config
+    ./waf && gtk2_ardour/arcfg system_config
 *****************************************************/
 
 /* IO connection */
