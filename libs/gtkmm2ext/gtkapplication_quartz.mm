1adc5d5cf461b3c368394fea1e2d0f4ae544846b
Robin Gareus <robin@gareus.org>
2015-06-30 08:08:13 +0200

    Revert "fix previously cherry-picked commit to be Tracks-only, since Ardour/Mixbus are not affected"
    This reverts commit dddf76abf567a166c290c841894ee2b6b0917653.
    
    it is relevant for Ardour/OSX, see 58fec98a and da43652

--- libs/gtkmm2ext/gtkapplication_quartz.mm
+++ libs/gtkmm2ext/gtkapplication_quartz.mm
@@ -573,12 +573,9 @@ idle_call_activate (gpointer data)
 - (void) activate:(id) sender
 {
 	UNUSED_PARAMETER(sender);
-#ifdef USE_TRACKS_CODE_FEATURES
-	// Hot Fix. Increase Priority.
+    // Hot Fix. Increase Priority.
 	g_idle_add_full (G_PRIORITY_HIGH_IDLE, idle_call_activate, gtk_menu_item, NULL);
-#else
-	g_idle_add (idle_call_activate, gtk_menu_item);
-#endif
+//    g_idle_add (idle_call_activate, gtk_menu_item);
 }
 @end
 
