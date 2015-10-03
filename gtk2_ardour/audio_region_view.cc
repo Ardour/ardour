43b47d11690f29b63ac2a7544e91308d8254ecdc
nick_m <mainsbridge@gmail.com>
2015-10-04 03:33:37 +1100

    Add missing diff to last commit.

--- gtk2_ardour/audio_region_view.cc
+++ gtk2_ardour/audio_region_view.cc
@@ -1361,7 +1361,7 @@ AudioRegionView::add_gain_point_event (ArdourCanvas::Item *item, GdkEvent *ev, b
 
 		trackview.session()->add_command (new MementoCommand<AutomationList>(*audio_region()->envelope().get(), &before, &after));
 
-		gain_line->get_selectables (fx, fx, 0.0, 1.0, results);
+		gain_line->get_selectables (fx + region ()->position (), fx + region ()->position (), 0.0, 1.0, results);
 		trackview.editor ().get_selection ().set (results);
 
 		trackview.editor ().commit_reversible_command ();
