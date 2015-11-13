51fc8fdb1ad36e755b7e7dcf1ecb9a1e90fd5283
Johannes Mueller <github@johannes-mueller.org>
2015-11-13 07:49:13 +0100

    Add support to signal the transport state by OSC
    Implement transport_speed in the same style like transport_frame. Up to
    now transport_speed and record_enabled are implemented.

--- libs/surfaces/osc/osc.h
+++ libs/surfaces/osc/osc.h
@@ -122,6 +122,8 @@ class OSC : public ARDOUR::ControlProtocol, public AbstractUI<OSCUIRequest>
 
 	void routes_list (lo_message msg);
 	void transport_frame(lo_message msg);
+	void transport_speed(lo_message msg);
+	void record_enabled(lo_message msg);
 
 #define PATH_CALLBACK_MSG(name)					\
         static int _ ## name (const char *path, const char *types, lo_arg **argv, int argc, void *data, void *user_data) { \
@@ -134,6 +136,8 @@ class OSC : public ARDOUR::ControlProtocol, public AbstractUI<OSCUIRequest>
 
 	PATH_CALLBACK_MSG(routes_list);
 	PATH_CALLBACK_MSG(transport_frame);
+	PATH_CALLBACK_MSG(transport_speed);
+	PATH_CALLBACK_MSG(record_enabled);
 
 #define PATH_CALLBACK(name) \
         static int _ ## name (const char *path, const char *types, lo_arg **argv, int argc, void *data, void *user_data) { \
