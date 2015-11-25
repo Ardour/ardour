68da5a1402c8211758d890819249ca9fef7656ec
Paul Davis <paul@linuxaudiosystems.com>
2015-11-25 07:35:50 -0500

    faderport: put back the working version of operations.cc

--- libs/surfaces/faderport/operations.cc
+++ libs/surfaces/faderport/operations.cc
@@ -1,14 +1,35 @@
+/*
+    Copyright (C) 2015 Paul Davis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
 #include "faderport.h"
 
+using namespace ARDOUR;
 using namespace ArdourSurface;
 
 void
-FaderPort::undo()
+FaderPort::undo ()
 {
+	ControlProtocol::Undo (); /* EMIT SIGNAL */
 }
 
 void
-FaderPort::redo()
+FaderPort::redo ()
 {
+	ControlProtocol::Redo (); /* EMIT SIGNAL */
 }
-
