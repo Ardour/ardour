c2493141d983b9c4f1f35386ff21e86e686a6be9
Robin Gareus <robin@gareus.org>
2015-12-08 23:39:36 +0100

    fix "stuck marker focus" after deleting marker.

--- gtk2_ardour/editor_ops.cc
+++ gtk2_ardour/editor_ops.cc
@@ -4040,6 +4040,7 @@ Editor::cut_copy (CutCopyOp op)
 		Location* loc = find_location_from_marker (entered_marker, ignored);
 
 		if (_session && loc) {
+			entered_marker = NULL;
 			Glib::signal_idle().connect (sigc::bind (sigc::mem_fun(*this, &Editor::really_remove_marker), loc));
 		}
 
