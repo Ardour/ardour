dc4daa892d6284e996a7e2637541f451d2ce98c2
Ben Loftis <ben@harrisonconsoles.com>
2016-02-03 13:48:56 -0600

    Mackie Protocol:  Show dB values for Mixbus "Send" levels. No sense rewriting it now, but the correct way to approach    this stuff is to pass in the control, and use    the appropriate one of: interface, internal, or user values.    in this case, get_user would have returned dB for both Ardour and Mixbus sends.

--- libs/surfaces/mackie/strip.cc
+++ libs/surfaces/mackie/strip.cc
@@ -919,13 +919,19 @@ Strip::do_parameter_display (AutomationType type, float val)
 	switch (type) {
 	case GainAutomation:
 	case BusSendLevel:
-		if (val == 0.0) {
-			pending_display[1] = " -inf ";
-		} else {
-			float dB = accurate_coefficient_to_dB (val);
-			snprintf (buf, sizeof (buf), "%6.1f", dB);
+		if (Profile->get_mixbus()) {  //Mixbus sends are already stored in dB
+			snprintf (buf, sizeof (buf), "%2.1f", val);
 			pending_display[1] = buf;
 			screen_hold = true;
+		} else {
+			if (val == 0.0) {
+				pending_display[1] = " -inf ";
+			} else {
+				float dB = accurate_coefficient_to_dB (val);
+				snprintf (buf, sizeof (buf), "%6.1f", dB);
+				pending_display[1] = buf;
+				screen_hold = true;
+			}
 		}
 		break;
 
