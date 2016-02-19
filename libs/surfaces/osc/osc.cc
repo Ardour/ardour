318f3850de0821e77a7a0bdb553d38f541503ed3
Robin Gareus <robin@gareus.org>
2016-02-19 23:25:15 +0100

    third time lucky

--- libs/surfaces/osc/osc.cc
+++ libs/surfaces/osc/osc.cc
@@ -252,7 +252,7 @@ OSC::thread_init ()
 
 	if (_osc_server) {
 #ifdef PLATFORM_WINDOWS
-		Glib::RefPtr<IOChannel> iochan = Glib::IOChannel::create_from_win32_socket (lo_server_get_socket_fd (_osc_server));
+		Glib::RefPtr<IOChannel> chan = Glib::IOChannel::create_from_win32_socket (lo_server_get_socket_fd (_osc_server));
 		Glib::RefPtr<IOSource> src  = IOSource::create (chan, IO_IN|IO_HUP|IO_ERR);
 #else
 		Glib::RefPtr<IOSource> src  = IOSource::create (lo_server_get_socket_fd (_osc_server), IO_IN|IO_HUP|IO_ERR);
