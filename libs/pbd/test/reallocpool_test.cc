0e36f8e2d7d65413730947084677628e44c61608
Robin Gareus <robin@gareus.org>
2016-02-23 22:55:17 +0100

    missing include for rand() in self-tests

--- libs/pbd/test/reallocpool_test.cc
+++ libs/pbd/test/reallocpool_test.cc
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 #include "reallocpool_test.h"
 #include "pbd/reallocpool.h"
 
@@ -13,15 +14,15 @@ ReallocPoolTest::ReallocPoolTest ()
 void
 ReallocPoolTest::testBasic ()
 {
-	srand (0);
+	::srand (0);
 	PBD::ReallocPool *m = new PBD::ReallocPool("TestPool", 256 * 1024);
 
 	for (int l = 0; l < 2 * 1024 * 1024; ++l) {
 		void *x[32];
 		size_t s[32];
-		int cnt = rand() % 32;
+		int cnt = ::rand() % 32;
 		for (int i = 0; i < cnt; ++i) {
-			s[i] = rand() % 1024;
+			s[i] = ::rand() % 1024;
 			x[i] = m->malloc (s[i]);
 		}
 		for (int i = 0; i < cnt; ++i) {
