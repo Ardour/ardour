234b06a18cf3e8c07bc44fb4f8c1218e17fb65f1
Len Ovens <len@ovenwerks.net>
2016-03-29 07:58:20 -0700

    OSC: Changed gainVCA to gainfader as VCA is already used.

--- libs/surfaces/osc/osc.cc
+++ libs/surfaces/osc/osc.cc
@@ -477,7 +477,7 @@ OSC::register_callbacks()
 		REGISTER_CALLBACK (serv, "/ardour/routes/recenable", "ii", route_recenable);
 		REGISTER_CALLBACK (serv, "/ardour/routes/gainabs", "if", route_set_gain_abs);
 		REGISTER_CALLBACK (serv, "/ardour/routes/gaindB", "if", route_set_gain_dB);
-		REGISTER_CALLBACK (serv, "/ardour/routes/gainVCA", "if", route_set_gain_VCA);
+		REGISTER_CALLBACK (serv, "/ardour/routes/gainfader", "if", route_set_gain_fader);
 		REGISTER_CALLBACK (serv, "/ardour/routes/trimabs", "if", route_set_trim_abs);
 		REGISTER_CALLBACK (serv, "/ardour/routes/trimdB", "if", route_set_trim_dB);
 		REGISTER_CALLBACK (serv, "/ardour/routes/pan_stereo_position", "if", route_set_pan_stereo_position);
@@ -1115,7 +1115,7 @@ OSC::route_set_gain_dB (int rid, float dB)
 }
 
 int
-OSC::route_set_gain_VCA (int rid, float pos)
+OSC::route_set_gain_fader (int rid, float pos)
 {
 	return route_set_gain_abs (rid, slider_position_to_gain_with_max (pos, 2.0));
 }
--- libs/surfaces/osc/osc.h
+++ libs/surfaces/osc/osc.h
@@ -290,7 +290,7 @@ class OSC : public ARDOUR::ControlProtocol, public AbstractUI<OSCUIRequest>
 	PATH_CALLBACK2(route_recenable,i,i);
 	PATH_CALLBACK2(route_set_gain_abs,i,f);
 	PATH_CALLBACK2(route_set_gain_dB,i,f);
-	PATH_CALLBACK2(route_set_gain_VCA,i,f);
+	PATH_CALLBACK2(route_set_gain_fader,i,f);
 	PATH_CALLBACK2(route_set_trim_abs,i,f);
 	PATH_CALLBACK2(route_set_trim_dB,i,f);
 	PATH_CALLBACK2(route_set_pan_stereo_position,i,f);
@@ -305,7 +305,7 @@ class OSC : public ARDOUR::ControlProtocol, public AbstractUI<OSCUIRequest>
 	int route_recenable (int rid, int yn);
 	int route_set_gain_abs (int rid, float level);
 	int route_set_gain_dB (int rid, float dB);
-	int route_set_gain_VCA (int rid, float pos);
+	int route_set_gain_fader (int rid, float pos);
 	int route_set_trim_abs (int rid, float level);
 	int route_set_trim_dB (int rid, float dB);
 	int route_set_pan_stereo_position (int rid, float left_right_fraction);
