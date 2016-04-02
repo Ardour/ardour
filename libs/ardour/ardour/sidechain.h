3164c9a07b3646a60cb9433faf7ee90fdd57985e
Robin Gareus <robin@gareus.org>
2016-04-02 23:34:05 +0200

    prepare sidechain i/o processor

---
+++ libs/ardour/ardour/sidechain.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 Robin Gareus <robin@gareus.org>
+ * Copyright (C) 2006 Paul Davis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __ardour_sidechain_h__
+#define __ardour_sidechain_h__
+
+#include <string>
+
+#include "pbd/stateful.h"
+#include "ardour/ardour.h"
+#include "ardour/io_processor.h"
+
+namespace ARDOUR {
+
+class LIBARDOUR_API SideChain : public IOProcessor
+{
+public:
+	SideChain (Session&, const std::string&);
+	virtual ~SideChain ();
+
+	void run (BufferSet& bufs, framepos_t start_frame, framepos_t end_frame, pframes_t nframes, bool);
+
+	bool can_support_io_configuration (const ChanCount& in, ChanCount& out);
+	bool configure_io (ChanCount in, ChanCount out);
+
+	XMLNode& state(bool full);
+	int      set_state(const XMLNode&, int version);
+
+private:
+	/* disallow copy construction */
+	SideChain (const SideChain&);
+};
+
+} // namespace ARDOUR
+
+#endif // __ardour_sidechain_h__
---
+++ libs/ardour/sidechain.cc
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2016 Robin Gareus <robin@gareus.org>
+ * Copyright (C) 2006 Paul Davis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <algorithm>
+
+#include "pbd/xml++.h"
+
+#include "ardour/audioengine.h"
+#include "ardour/buffer.h"
+#include "ardour/buffer_set.h"
+#include "ardour/io.h"
+#include "ardour/session.h"
+#include "ardour/sidechain.h"
+
+#include "i18n.h"
+
+using namespace ARDOUR;
+using namespace PBD;
+
+
+SideChain::SideChain (Session& s, const std::string& name)
+	: IOProcessor (s, true, false, name)
+{
+
+}
+
+SideChain::~SideChain ()
+{
+}
+
+XMLNode&
+SideChain::state(bool full)
+{
+	XMLNode& node = IOProcessor::state(full);
+	node.add_property ("type", "sidechain");
+	return node;
+}
+
+
+int
+SideChain::set_state (const XMLNode& node, int version)
+{
+	XMLNodeList nlist = node.children();
+	const XMLNode* insert_node = &node;
+
+	for (XMLNodeIterator niter = nlist.begin(); niter != nlist.end(); ++niter) {
+		if ((*niter)->name() == "IOProcessor") {
+			insert_node = *niter;
+		}
+	}
+
+	IOProcessor::set_state (*insert_node, version);
+
+	return 0;
+}
+
+void
+SideChain::run (BufferSet& bufs, framepos_t start_frame, framepos_t end_frame, pframes_t nframes, bool)
+{
+	if (_input->n_ports() == ChanCount::ZERO) {
+		// inplace pass-through
+		return;
+	}
+
+	if ((!_active && !_pending_active)) {
+		// zero buffers
+		for (DataType::iterator t = DataType::begin(); t != DataType::end(); ++t) {
+			for (uint32_t out = _configured_input.get (*t); out < bufs.count().get (*t); ++out) {
+				bufs.get (*t, out).silence (nframes);
+			}
+		}
+		return;
+	}
+
+	_input->collect_input (bufs, nframes, _configured_input);
+	bufs.set_count(_configured_output);
+
+	_active = _pending_active;
+}
+
+bool
+SideChain::can_support_io_configuration (const ChanCount& in, ChanCount& out)
+{
+	out = in + _input->n_ports();
+	return true;
+}
+
+bool
+SideChain::configure_io (ChanCount in, ChanCount out)
+{
+	if (out != in + _input->n_ports()) {
+		return false;
+	}
+	Processor::configure_io(in, out);
+	return true;
+}
