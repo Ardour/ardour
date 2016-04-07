5b60070034d0a1f3e13874c0c02635da7d2bbb20
Robin Gareus <robin@gareus.org>
2016-04-07 22:13:00 +0200

    add "for each track" snippet.

---
+++ scripts/s_foreach_track.lua
@@ -0,0 +1,11 @@
+ardour { ["type"] = "Snippet", name = "foreach track" }
+
+function factory () return function ()
+	for r in Session:get_tracks():iter() do
+		print (r:name())
+		-- see http://manual.ardour.org/lua-scripting/class_reference/#ARDOUR:Track
+		-- for available methods e.g.
+		--
+		r:set_active (true, nil)
+	end
+end end
