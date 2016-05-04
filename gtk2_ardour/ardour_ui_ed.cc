9b3b1d945f72324a5ee38b7053a54e9d257c41db
Robin Gareus <robin@gareus.org>
2016-05-05 01:59:11 +0200

    fix another menu typo in fda03d1

--- gtk2_ardour/ardour_ui_ed.cc
+++ gtk2_ardour/ardour_ui_ed.cc
@@ -180,7 +180,7 @@ ARDOUR_UI::install_actions ()
 	act = global_actions.register_action (main_actions, X_("CloseVideo"), _("Remove Video"),
 					      sigc::mem_fun (*this, &ARDOUR_UI::remove_video));
 	act->set_sensitive (false);
-	act = global_actions.register_action (main_actions, X_("ExportVideo..."), _("Export to Video File"),
+	act = global_actions.register_action (main_actions, X_("ExportVideo"), _("Export to Video File..."),
 	                                      hide_return (sigc::bind (sigc::mem_fun(*this, &ARDOUR_UI::export_video), false)));
 	ActionManager::session_sensitive_actions.push_back (act);
 
