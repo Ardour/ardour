6c7a3e3d47bb63ea2a38d6b2770aa8569f40d644
Michael Beer <beerml@sigma6audio.de>
2016-05-17 21:09:01 +0200

    Get size of available disk space in FreeBSD.

--- libs/ardour/session_state.cc
+++ libs/ardour/session_state.cc
@@ -38,7 +38,7 @@
 #include <sys/vfs.h>
 #endif
 
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(__FreeBSD__) 
 #include <sys/param.h>
 #include <sys/mount.h>
 #endif
@@ -2234,7 +2234,7 @@ Session::save_template (string template_name, bool replace_existing)
 void
 Session::refresh_disk_space ()
 {
-#if __APPLE__ || (HAVE_SYS_VFS_H && HAVE_SYS_STATVFS_H)
+#if __APPLE__ || __FreeBSD__ || (HAVE_SYS_VFS_H && HAVE_SYS_STATVFS_H)
 
 	Glib::Threads::Mutex::Lock lm (space_lock);
 
