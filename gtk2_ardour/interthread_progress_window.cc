Ardour/ardour

commit 03660573e28dbc3574d5964cc0392832b8f46557

fix import-thread SNAFU
 * pressing Esc or WM close button did not cancel import thread
 * proper Abort does not wait for import thread either

It was possible to launch a 2nd (and Nth) import thread, all sharing
the Editor's ImportStatus data-structure, all having the same
registered thread-name and same thread-pool name.  Plenty of room for
crashes.

gtk2_ardour/interthread_progress_window.cc
@@ -56,6 +56,14 @@ InterthreadProgressWindow::InterthreadProgressWindow (ARDOUR::InterThreadInfo* i
 }
 
 void
+InterthreadProgressWindow::on_hide ()
+{
+	if (!_interthread_info->done) {
+		_interthread_info->cancel = true;
+	}
+}
+
+void
 InterthreadProgressWindow::cancel_clicked ()
 {
 	_interthread_info->cancel = true;

gtk2_ardour/editor_audio_import.cc
@@ -507,7 +507,10 @@ Editor::import_sndfiles (vector<string>            paths,
 		gtk_main_iteration ();
 	}
 
-	import_status.done = true;
+	// wait for thread to terminate
+	while (!import_status.done) {
+		gtk_main_iteration ();
+	}
 
 	int result = -1;
 

gtk2_ardour/interthread_progress_window.h
@@ -42,6 +42,7 @@ public:
 protected:
 
 	virtual bool update ();
+	virtual void on_hide ();
 
 	Gtk::Button _cancel_button;
 	Gtk::Label _cancel_label;

commit cd5cd2f8f29e9b99942cbb232f622ad3ab074fc1

cont'd work & fix bug introduced in 03660573e
not every code path using InterthreadProgressWindow unsets cancel
after creating the Window.

gtk2_ardour/interthread_progress_window.cc
@@ -50,7 +50,8 @@ InterthreadProgressWindow::InterthreadProgressWindow (ARDOUR::InterThreadInfo* i
 
 	set_default_size (200, 100);
 	show_all ();
-        hide ();
+	hide ();
+	_interthread_info->cancel = false; // override on_hide
 
 	Glib::signal_timeout().connect (sigc::mem_fun (*this, &InterthreadProgressWindow::update), 100);
 }
@@ -58,7 +59,8 @@ InterthreadProgressWindow::InterthreadProgressWindow (ARDOUR::InterThreadInfo* i
 void
 InterthreadProgressWindow::on_hide ()
 {
-	if (!_interthread_info->done) {
+	if (_interthread_info && !_interthread_info->done) {
+		//catch user pressing 'esc' or WM close
 		_interthread_info->cancel = true;
 	}
 }