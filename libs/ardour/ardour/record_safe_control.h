203cf3ce7c3b6130a58626082d051ade8ca90dbf
Paul Davis <paul@linuxaudiosystems.com>
2016-05-27 16:11:22 -0400

    new RecordSafeControl control

---
+++ libs/ardour/ardour/record_safe_control.h
@@ -0,0 +1,50 @@
+/*
+    Copyright (C) 2016 Paul Davis
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation; either version 2 of the License, or (at your option)
+    any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __ardour_record_safe_control_h__
+#define __ardour_record_safe_control_h__
+
+#include <string>
+
+#include <boost/shared_ptr.hpp>
+
+#include "ardour/slavable_automation_control.h"
+#include "ardour/recordable.h"
+
+#include "ardour/libardour_visibility.h"
+
+namespace ARDOUR {
+
+class Session;
+
+class LIBARDOUR_API RecordSafeControl : public SlavableAutomationControl
+{
+  public:
+	RecordSafeControl (Session& session, std::string const & name, Recordable& m);
+	~RecordSafeControl() {}
+
+  protected:
+	void actually_set_value (double val, Controllable::GroupControlDisposition gcd);
+
+  private:
+	Recordable& _recordable;
+};
+
+} /* namespace */
+
+#endif /* __libardour_record_enable_control_h__ */
---
+++ libs/ardour/record_safe_control.cc
@@ -0,0 +1,49 @@
+/*
+    Copyright (C) 2016 Paul Davis
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation; either version 2 of the License, or (at your option)
+    any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "ardour/audioengine.h"
+#include "ardour/record_safe_control.h"
+
+#include "i18n.h"
+
+using namespace ARDOUR;
+using namespace PBD;
+
+RecordSafeControl::RecordSafeControl (Session& session, std::string const & name, Recordable& r)
+	: SlavableAutomationControl (session, RecSafeAutomation, ParameterDescriptor (RecSafeAutomation),
+	                             boost::shared_ptr<AutomationList>(new AutomationList(Evoral::Parameter(RecSafeAutomation))),
+	                             name)
+	, _recordable (r)
+{
+	_list->set_interpolation(Evoral::ControlList::Discrete);
+
+	/* record-enable changes must be synchronized by the process cycle */
+	set_flags (Controllable::Flag (flags() | Controllable::RealTime));
+}
+
+void
+RecordSafeControl::actually_set_value (double val, Controllable::GroupControlDisposition gcd)
+{
+	if (val && !_recordable.can_be_record_safe()) {
+		std::cerr << "rec-enable not allowed\n";
+		return;
+	}
+
+	SlavableAutomationControl::actually_set_value (val, gcd);
+}
+
