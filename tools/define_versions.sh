db7d401435e8e8f79b2cdbe497137c9ca425c2f1
Paul Davis <paul@linuxaudiosystems.com>
2016-05-31 12:20:15 -0400

    more useful regexp to allow 5.x-preN version tags

--- tools/define_versions.sh
+++ tools/define_versions.sh
@@ -8,7 +8,7 @@ else
     EXTENDED_RE=-r
 fi
 
-GIT_REV_REGEXP='([0-9][0-9]*)\.([0-9][0-9]*)\-?(rc[0-9]*)?-?([0-9][0-9]*)?(-g([a-f0-9]+))?'
+GIT_REV_REGEXP='([0-9][0-9]*)\.([0-9][0-9]*)\-?([prec][0-9]*)?-?([0-9][0-9]*)?(-g([a-f0-9]+))?'
 
 major_version=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\1/"`
 minor_version=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\2/"`
@@ -20,6 +20,9 @@ micro_version=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE 
 
 #
 # RC e.g. 8.1-rc3 (optional)
+# OR
+# PRE e.g. 9.3-pre0 (optional)
+#
 # Tracks does not use -rcN based tagging
 #
 rc=`cut -d'"' -f2 < ../../libs/ardour/revision.cc | sed $EXTENDED_RE -e 1d -e "s/$GIT_REV_REGEXP/\3/"`
