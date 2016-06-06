36f3291a9c9da4ac798929702556ca6f735d27a2
nick_m <mainsbridge@gmail.com>
2016-06-07 06:14:47 +1000

    Ensure correct lock style before setting position.

--- libs/ardour/region_factory.cc
+++ libs/ardour/region_factory.cc
@@ -82,11 +82,12 @@ RegionFactory::create (boost::shared_ptr<const Region> region, bool announce)
 
 	if (ret) {
 		ret->set_name (new_region_name(ret->name()));
-		ret->set_position (region->position());
 
 		if (ret->session().config.get_glue_new_regions_to_bars_and_beats ()) {
 			ret->set_position_lock_style (MusicTime);
 		}
+
+		ret->set_position (region->position());
 
 		/* pure copy constructor - no property list */
 		if (announce) {
