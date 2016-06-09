f26191382f316bac32ed6dff3220528629918b15
Paul Davis <paul@linuxaudiosystems.com>
2016-06-09 12:58:54 -0400

    for now vca assignment does not apply to selection.
    This hack comment needs cleaning up later

--- gtk2_ardour/mixer_actor.cc
+++ gtk2_ardour/mixer_actor.cc
@@ -254,26 +254,28 @@ void
 MixerActor::vca_assign (boost::shared_ptr<VCA> vca)
 {
 	set_route_targets_for_operation ();
-
+#if 0
 	BOOST_FOREACH(RouteUI* r, _route_targets) {
 		MixerStrip* ms = dynamic_cast<MixerStrip*> (r);
 		if (ms) {
 			ms->vca_assign (vca);
 		}
 	}
+#endif
 }
 
 void
 MixerActor::vca_unassign (boost::shared_ptr<VCA> vca)
 {
 	set_route_targets_for_operation ();
-
+#if 0
 	BOOST_FOREACH(RouteUI* r, _route_targets) {
 		MixerStrip* ms = dynamic_cast<MixerStrip*> (r);
 		if (ms) {
 			ms->vca_unassign (vca);
 		}
 	}
+#endif
 }
 
 
