54730c2aa5f6467024f2c1429ba74d78e0c1703e
Paul Davis <paul@linuxaudiosystems.com>
2016-07-20 16:21:03 -0400

    remove long-lived debug output related to RT-setting of controls

--- libs/ardour/session_rtevents.cc
+++ libs/ardour/session_rtevents.cc
@@ -41,8 +41,6 @@ Session::set_controls (boost::shared_ptr<ControlList> cl, double val, Controllab
 		return;
 	}
 
-	std::cerr << "Session::set_controls called on " << cl->size() << " controls, group = " << enum_2_string (gcd) << std::endl;
-
 	for (ControlList::iterator ci = cl->begin(); ci != cl->end(); ++ci) {
 		/* as of july 2017 this is a no-op for everything except record enable */
 		(*ci)->do_pre_realtime_queue_stuff (val);
