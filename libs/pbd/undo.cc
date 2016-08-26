9587e11f97026b15d392f2aa8e5e8c9284cd054b
Tim Mayberry <mojofunk@gmail.com>
2016-08-26 20:57:51 +1000

    Use XMLNode::set_property API in PBD::Undo class

--- libs/pbd/undo.cc
+++ libs/pbd/undo.cc
@@ -135,13 +135,9 @@ UndoTransaction::redo ()
 XMLNode &UndoTransaction::get_state()
 {
     XMLNode *node = new XMLNode ("UndoTransaction");
-    stringstream ss;
-    ss << _timestamp.tv_sec;
-    node->add_property("tv_sec", ss.str());
-    ss.str("");
-    ss << _timestamp.tv_usec;
-    node->add_property("tv_usec", ss.str());
-    node->add_property("name", _name);
+    node->set_property("tv_sec", (int64_t)_timestamp.tv_sec);
+    node->set_property("tv_usec", (int64_t)_timestamp.tv_usec);
+    node->set_property("name", _name);
 
     list<Command*>::iterator it;
     for (it=actions.begin(); it!=actions.end(); it++)
