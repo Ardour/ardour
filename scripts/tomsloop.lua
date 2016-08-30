557bbcbfe7462182fd41538bb01e9465a1916811
Robin Gareus <robin@gareus.org>
2016-08-30 20:42:27 +0200

    tom's loop: fix arguments to add_region()

--- scripts/tomsloop.lua
+++ scripts/tomsloop.lua
@@ -95,7 +95,7 @@ function factory (params) return function ()
 
 		-- do the actual work
 		local region = track:bounce_range (loop:start (), loop:_end (), itt, proc, false)
-		playlist:add_region (region, playhead, 1.0, false, n_paste)
+		playlist:add_region (region, playhead, n_paste, false, 0)
 
 		n_regions_created = n_regions_created + 1
 
