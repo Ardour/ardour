800fdd0f0fba047b3189326b424a8ece46666880
Tim Mayberry <mojofunk@gmail.com>
2016-09-03 22:10:35 +1000

    Add pbd/types_convert.h header for PBD::to_string/string_to specialisations

---
+++ libs/pbd/pbd/types_convert.h
@@ -0,0 +1,48 @@
+/*
+    Copyright (C) 2015 Tim Mayberry
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef PBD_TYPES_CONVERT_H
+#define PBD_TYPES_CONVERT_H
+
+#include "pbd/enum_convert.h"
+
+#include "pbd/controllable.h"
+#include "pbd/id.h"
+
+namespace PBD {
+
+DEFINE_ENUM_CONVERT(Controllable::Flag)
+
+template <>
+inline bool to_string (ID val, std::string& str)
+{
+	str = val.to_s();
+	return true;
+}
+
+template <>
+inline bool string_to (const std::string& str, ID& val)
+{
+	val = str;
+	return true;
+}
+
+} // namespace PBD
+
+#endif // PBD_TYPES_CONVERT_H
