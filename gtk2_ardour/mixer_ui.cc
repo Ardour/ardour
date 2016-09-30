ea10f0a53ebe5fa26a8d234f273866eeb0e7f8c0
Ben Loftis <ben@harrisonconsoles.com>
2016-09-30 10:52:46 -0500

    fix: using arrow-keys on selected mixer strips would recursively affect grouped faders

--- gtk2_ardour/mixer_ui.cc
+++ gtk2_ardour/mixer_ui.cc
@@ -2817,10 +2817,12 @@ Mixer_UI::step_gain_up_action ()
 	set_axis_targets_for_operation ();
 
 	BOOST_FOREACH(AxisView* r, _axis_targets) {
+		r->route()->set_mix_group_override(true);
 		MixerStrip* ms = dynamic_cast<MixerStrip*> (r);
 		if (ms) {
 			ms->step_gain_up ();
 		}
+		r->route()->set_mix_group_override(false);
 	}
 }
 
@@ -2830,10 +2832,12 @@ Mixer_UI::step_gain_down_action ()
 	set_axis_targets_for_operation ();
 
 	BOOST_FOREACH(AxisView* r, _axis_targets) {
+		r->route()->set_mix_group_override(true);
 		MixerStrip* ms = dynamic_cast<MixerStrip*> (r);
 		if (ms) {
 			ms->step_gain_down ();
 		}
+		r->route()->set_mix_group_override(false);
 	}
 }
 
