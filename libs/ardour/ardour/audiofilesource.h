Ardour/ardour

commit dbf86a495b89380526f731e7bd15e0a41b91fc1e

for files imported without copy, _origin is a full path. We should use this when looking for the file at session load

libs/ardour/audiofilesource.cc
@@ -131,7 +131,10 @@ AudioFileSource::AudioFileSource (Session& s, const string& path, Source::Flag f
 }
 
 
-/** Constructor used for existing internal-to-session files via XML.  File must exist. */
+/** Constructor used for existing files via XML.  File must exist. If _origin
+ * is an absolute path after ::set_state(), then the file is external to the
+ * session.
+ */
 AudioFileSource::AudioFileSource (Session& s, const XMLNode& node, bool must_exist)
 	: Source (s, node)
 	, AudioSource (s, node)
@@ -139,6 +142,11 @@ AudioFileSource::AudioFileSource (Session& s, const XMLNode& node, bool must_exi
 {
 	if (set_state (node, Stateful::loading_state_version)) {
 		throw failed_constructor ();
+	}
+
+	if (Glib::path_is_absolute (_origin)) {
+		_path = _origin;
+		must_exist = true;
 	}
 
 	if (init (_path, must_exist)) {

commit feed9648e19f5750b102a161a1028bb143ea16c5

Allow loading sessions with missing external files #7067
The issue was introduced in dbf86a495b, forcing must_exist = true
for *all* files that have a absolute _origin, including stub
SilentFileSources.

If an external file is no longer available and a user chooses to
"ignore, skip" an SilentFile is created instead of the actual source,
The SilentFileSource has the same XML state which includes _origin.

ARDOUR::SilentFileSource::SilentFileSource () c'tor calls
AudioFileSource::AudioFileSource (,.., must_exit = false)
but since the file has an absolute _origin, the AudioFileSource c'tor sets
must_exist = true; throws and the session cannot be loaded.

libs/ardour/ardour/audiofilesource.h
@@ -88,7 +88,7 @@ protected:
 			SampleFormat samp_format, HeaderFormat hdr_format);
 
 	/** Constructor to be called for existing in-session files */
-	AudioFileSource (Session&, const XMLNode&, bool must_exist = true);
+	AudioFileSource (Session&, const XMLNode&, bool must_exist = true, bool optional_origin = false);
 
 	/** Constructor to be called for crash recovery. Final argument is not
 	 * used but exists to differentiate from the external-to-session

libs/ardour/ardour/silentfilesource.h
@@ -45,7 +45,7 @@ protected:
 
 	SilentFileSource (Session& s, const XMLNode& x, framecnt_t len, float srate)
 		: Source (s, x)
-		, AudioFileSource (s, x, false)
+		, AudioFileSource (s, x, false, true)
 		, _sample_rate(srate)
 	{
 		_length = len;

libs/ardour/audiofilesource.cc
@@ -135,7 +135,7 @@ AudioFileSource::AudioFileSource (Session& s, const string& path, Source::Flag f
  * is an absolute path after ::set_state(), then the file is external to the
  * session.
  */
-AudioFileSource::AudioFileSource (Session& s, const XMLNode& node, bool must_exist)
+AudioFileSource::AudioFileSource (Session& s, const XMLNode& node, bool must_exist, bool optional_origin)
 	: Source (s, node)
 	, AudioSource (s, node)
 	, FileSource (s, node, must_exist)
@@ -146,7 +146,7 @@ AudioFileSource::AudioFileSource (Session& s, const XMLNode& node, bool must_exi
 
 	if (Glib::path_is_absolute (_origin)) {
 		_path = _origin;
-		must_exist = true;
+		must_exist = !optional_origin;
 	}
 
 	if (init (_path, must_exist)) {