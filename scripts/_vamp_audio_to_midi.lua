4ab80e16e9e851e0e685d194625b3f5dbbce7e56
Nathan Stewart <therealnathanstewart@gmail.com>
2016-10-21 00:36:13 -0400

    vamp_audio_to_midi script now operates on a list of regions, calling qm-transcription for each region. The MIDI region holding the output is resized to the extents of the audio region list. This is because qm-transcription is a memory hog and barfs if you try to process too big a region.

--- scripts/_vamp_audio_to_midi.lua
+++ scripts/_vamp_audio_to_midi.lua
@@ -6,37 +6,53 @@ function factory () return function ()
 	local sr = Session:nominal_frame_rate ()
 	local tm = Session:tempo_map ()
 	local vamp = ARDOUR.LuaAPI.Vamp ("libardourvampplugins:qm-transcription", sr)
-	local ar, mr
+	local midi_region
+	local audio_regions = {}
+	local start_time = Session:current_end_frame ()
+	local end_time = Session:current_start_frame ()
 	for r in sel.regions:regionlist ():iter () do
 		if r:to_midiregion():isnil() then
-			ar = r
+			local st = r:position()
+			local ln = r:length()
+			local et = st + ln
+			if st < start_time then
+				start_time = st
+			end
+			if et > end_time then
+				end_time = et
+			end
+			table.insert(audio_regions, r) 
 		else
-			mr = r:to_midiregion()
+			midi_region = r:to_midiregion()
 		end
 	end
-	assert (ar and mr)
+	assert (audio_regions and midi_region)
+	midi_region:set_initial_position(start_time)
+	midi_region:set_length(end_time - start_time, 0)
 
-	local a_off = ar:position ()
-	local b_off = 4.0 * mr:pulse () - mr:start_beats ()
+	for i,ar in pairs(audio_regions) do
+		local a_off = ar:position ()
+		local b_off = 4.0 * midi_region:pulse () - midi_region:start_beats ()
 
-	vamp:analyze (ar:to_readable (), 0, nil)
-	local fl = vamp:plugin ():getRemainingFeatures ():at (0)
-	if fl and fl:size() > 0 then
-		local mm = mr:midi_source(0):model()
-		local midi_command = mm:new_note_diff_command ("Audio2Midi")
-		for f in fl:iter () do
-			local ft = Vamp.RealTime.realTime2Frame (f.timestamp, sr)
-			local fd = Vamp.RealTime.realTime2Frame (f.duration, sr)
-			local fn = f.values:at (0)
+		vamp:analyze (ar:to_readable (), 0, nil)
+		local fl = vamp:plugin ():getRemainingFeatures ():at (0)
+		if fl and fl:size() > 0 then
+			local mm = midi_region:midi_source(0):model()
+			local midi_command = mm:new_note_diff_command ("Audio2Midi")
+			for f in fl:iter () do
+				local ft = Vamp.RealTime.realTime2Frame (f.timestamp, sr)
+				local fd = Vamp.RealTime.realTime2Frame (f.duration, sr)
+				local fn = f.values:at (0)
 
-			local bs = tm:exact_qn_at_frame (a_off + ft, 0)
-			local be = tm:exact_qn_at_frame (a_off + ft + fd, 0)
+				local bs = tm:exact_qn_at_frame (a_off + ft, 0)
+				local be = tm:exact_qn_at_frame (a_off + ft + fd, 0)
 
-			local pos = Evoral.Beats (bs - b_off)
-			local len = Evoral.Beats (be - bs)
-			local note = ARDOUR.LuaAPI.new_noteptr (1, pos, len, fn + 1, 0x7f)
-			midi_command:add (note)
+				local pos = Evoral.Beats (bs - b_off)
+				local len = Evoral.Beats (be - bs)
+				local note = ARDOUR.LuaAPI.new_noteptr (1, pos, len, fn + 1, 0x7f)
+				midi_command:add (note)
+			end
+			mm:apply_command (Session, midi_command)
 		end
-		mm:apply_command (Session, midi_command)
 	end
 end end
