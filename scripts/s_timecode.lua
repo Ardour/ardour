3242bbd84c70b497810480cd7b7780cb4658d088
Robin Gareus <robin@gareus.org>
2016-12-08 15:42:11 +0100

    Add some doc/comments to the timecode example script

--- scripts/s_timecode.lua
+++ scripts/s_timecode.lua
@@ -2,15 +2,19 @@ ardour { ["type"] = "Snippet", name = "Timecode" }
 
 function factory () return function ()
 
-	-- generic convert, explicitly provide Timecode (fps) and sample-rate
-	hh, mm, ss, ff = ARDOUR.LuaAPI.sample_to_timecode (Timecode.TimecodeFormat.TC25, 48000, 1920)
-	print (ARDOUR.LuaAPI.sample_to_timecode (Timecode.TimecodeFormat.TC25, 48000, 1920))
+	local samplerate = 48000 -- samples per second
 
 	-- generic convert, explicitly provide Timecode (fps) and sample-rate
-	local s = ARDOUR.LuaAPI.timecode_to_sample (Timecode.TimecodeFormat.TC25, 48000, 10, 11, 12, 13)
-	assert (25 * (10 * 3600 + 11 * 60 + 12 ) + 13 == s * 25 / 48000)
+	-- http://manual.ardour.org/lua-scripting/class_reference/#Timecode.TimecodeFormat
+	hh, mm, ss, ff = ARDOUR.LuaAPI.sample_to_timecode (Timecode.TimecodeFormat.TC25, samplerate, 1920)
+	print (ARDOUR.LuaAPI.sample_to_timecode (Timecode.TimecodeFormat.TC25, samplerate, 1920))
 
-	-- use session-settings
+	-- generic convert, explicitly provide Timecode (fps) and sample-rate
+	local s = ARDOUR.LuaAPI.timecode_to_sample (Timecode.TimecodeFormat.TC25, samplerate, 10, 11, 12, 13)
+	assert (25 * (10 * 3600 + 11 * 60 + 12 ) + 13 == s * 25 / samplerate)
+
+	-- use session-settings: sample-rate and timecode format is taken from the
+	-- current session. Note that the sample-rate includes pull-up/down
 	print (Session:sample_to_timecode_lua (12345))
 	print (Session:timecode_to_sample_lua (10, 11, 12, 13))
 
