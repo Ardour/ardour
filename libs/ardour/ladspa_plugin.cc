ad97a069d93febff105b3d473e167c21fd6a1602
Paul Davis <paul@linuxaudiosystems.com>
2017-01-09 11:58:39 +0000

    fix ranges for LADSPA unbounded toggle parameters.
    Used to be 0..4, now 0..1. The 4 is still used as an arbitrary
    upper bound for non-toggled parameters

--- libs/ardour/ladspa_plugin.cc
+++ libs/ardour/ladspa_plugin.cc
@@ -498,7 +498,11 @@ LadspaPlugin::get_parameter_descriptor (uint32_t which, ParameterDescriptor& des
 		}
 	} else {
 		desc.max_unbound = true;
-		desc.upper = 4; /* completely arbitrary */
+		if (LADSPA_IS_HINT_TOGGLED (prh.HintDescriptor)) {
+			desc.upper = 1;
+		} else {
+			desc.upper = 4; /* completely arbitrary */
+		}
 	}
 
 	if (LADSPA_IS_HINT_HAS_DEFAULT (prh.HintDescriptor)) {
