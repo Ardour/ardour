a2d54c3d97104be1c35bc98a9e49df018e41495e
Robin Gareus <robin@gareus.org>
2017-02-22 10:17:33 +0100

    The return of the w64 debug build

--- tools/x-win/compile.sh
+++ tools/x-win/compile.sh
@@ -63,9 +63,17 @@ export WINRC=${XPREFIX}-windres
 export RANLIB=${XPREFIX}-ranlib
 export DLLTOOL=${XPREFIX}-dlltool
 
+if grep -q optimize <<<"$ARDOURCFG"; then
+	OPT=""
+else
+	#debug-build luabindings.cc, has > 60k symbols.
+	# -Wa,-mbig-obj has an unreasonable long build-time
+	# -Og to the rescue.
+	OPT=" -Og"
+fi
 
-CFLAGS="-mstackrealign" \
-CXXFLAGS="-mstackrealign" \
+CFLAGS="-mstackrealign$BIGOBJ" \
+CXXFLAGS="-mstackrealign$BIGOBJ" \
 LDFLAGS="-L${PREFIX}/lib" ./waf configure \
 	--keepflags \
 	--dist-target=mingw \
