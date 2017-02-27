Ardour/ardour

commit 123fcf3cffe048b4a16bbb7442a9cd07659f4284

fix potential crash bug in RouteProcessorSelection

gtk2_ardour/route_processor_selection.cc
@@ -119,8 +119,8 @@ RouteProcessorSelection::remove (AxisView* r)
 
 	AxisViewSelection::iterator i;
 	if ((i = find (axes.begin(), axes.end(), r)) != axes.end()) {
-		(*i)->set_selected (false);
 		axes.erase (i);
+		(*i)->set_selected (false);
 	}
 }
 

commit 007808a6ad96f38e84ce1db12d448628d80baa0a

fix crash caused by thinko in 123fcf3cf

gtk2_ardour/route_processor_selection.cc
@@ -119,8 +119,9 @@ RouteProcessorSelection::remove (AxisView* r)
 
 	AxisViewSelection::iterator i;
 	if ((i = find (axes.begin(), axes.end(), r)) != axes.end()) {
+		AxisView* av = *i;
 		axes.erase (i);
-		(*i)->set_selected (false);
+		av->set_selected (false);
 	}
 }
 