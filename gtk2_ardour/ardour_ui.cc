5524bac04bc7f4901991bd1fb4747abcb7b43db4
Robin Gareus <robin@gareus.org>
2017-03-02 20:00:15 +0100

    Don't try to auto-start engine for new sessions
    Work around logic-flow in EngineDialog which should already have
    done that, but may no longer heed this special-case if sample-rate
    dropdown is refreshed to only devices supported ones.

--- gtk2_ardour/ardour_ui.cc
+++ gtk2_ardour/ardour_ui.cc
@@ -5376,10 +5376,12 @@ ARDOUR_UI::do_audio_midi_setup (uint32_t desired_sample_rate)
 	audio_midi_setup->set_desired_sample_rate (desired_sample_rate);
 	audio_midi_setup->set_position (WIN_POS_CENTER);
 
-	if (Config->get_try_autostart_engine () || getenv ("TRY_AUTOSTART_ENGINE")) {
-		audio_midi_setup->try_autostart ();
-		if (ARDOUR::AudioEngine::instance()->running()) {
-			return 0;
+	if (desired_sample_rate != 0) {
+		if (Config->get_try_autostart_engine () || getenv ("TRY_AUTOSTART_ENGINE")) {
+			audio_midi_setup->try_autostart ();
+			if (ARDOUR::AudioEngine::instance()->running()) {
+				return 0;
+			}
 		}
 	}
 
