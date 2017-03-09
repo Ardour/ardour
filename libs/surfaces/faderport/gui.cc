f21111d7df446ccf7ce512b6b97a738b0e2e9ead
Ben Loftis <ben@harrisonconsoles.com>
2017-03-09 14:29:08 -0600

    Faderport uses the Mix and Proj buttons to summon mixer and editor; there is no longer a Toggle.

--- libs/surfaces/faderport/gui.cc
+++ libs/surfaces/faderport/gui.cc
@@ -519,7 +519,7 @@ FPGUI::build_mix_action_combo (Gtk::ComboBox& cb, FaderPort::ButtonState bs)
 {
 	vector<pair<string,string> > actions;
 
-	actions.push_back (make_pair (string (_("Toggle Editor & Mixer Windows")), string (X_("Common/toggle-editor-and-mixer"))));
+	actions.push_back (make_pair (string (_("Show Mixer Window")), string (X_("Editor/toggle-editor-to-mixer"))));
 	actions.push_back (make_pair (string (_("Show/Hide Mixer list")), string (X_("Common/ToggleMixerList"))));
 	actions.push_back (make_pair (string("Toggle Meterbridge"), string(X_("Common/toggle-meterbridge"))));
 	actions.push_back (make_pair (string (_("Show/Hide Editor mixer strip")), string (X_("Editor/show-editor-mixer"))));
@@ -532,6 +532,7 @@ FPGUI::build_proj_action_combo (Gtk::ComboBox& cb, FaderPort::ButtonState bs)
 {
 	vector<pair<string,string> > actions;
 
+	actions.push_back (make_pair (string (_("Show Editor Window")), string (X_("Mixer/toggle-mixer-to-editor"))));
 	actions.push_back (make_pair (string("Toggle Editor Lists"), string(X_("Editor/show-editor-list"))));
 	actions.push_back (make_pair (string("Toggle Summary"), string(X_("Editor/ToggleSummary"))));
 	actions.push_back (make_pair (string("Toggle Meterbridge"), string(X_("Common/toggle-meterbridge"))));
