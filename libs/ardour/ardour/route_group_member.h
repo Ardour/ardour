60ad8e21cfb59ec0021e11934f7ac788c46e8959
Robin Gareus <robin@gareus.org>
2017-03-12 20:15:55 +0100

    remove cruft; superseded by ControlGroup::use_me

--- libs/ardour/ardour/route_group_member.h
+++ libs/ardour/ardour/route_group_member.h
@@ -38,8 +38,6 @@ class LIBARDOUR_API RouteGroupMember
 	/** Emitted when this member joins or leaves a route group */
 	PBD::Signal0<void> route_group_changed;
 
-	bool use_group (PBD::Controllable::GroupControlDisposition gcd, bool (RouteGroup::*predicate)(void) const) const;
-
   protected:
 	RouteGroup* _route_group;
 
