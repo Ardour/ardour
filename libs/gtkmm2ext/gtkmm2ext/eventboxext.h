235631a618de23793f0915aa7dc9b2f669b766f9
Robin Gareus <robin@gareus.org>
2017-03-20 16:30:01 +0100

    Add a EventBox wrapper to forward unmap events.

---
+++ libs/gtkmm2ext/gtkmm2ext/eventboxext.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2017 Robin Gareus <robin@gareus.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __gtkmm2ext_eventbox_ext_h__
+#define __gtkmm2ext_eventbox_ext_h__
+
+#include <gtkmm/eventbox.h>
+
+#include "gtkmm2ext/visibility.h"
+
+namespace Gtkmm2ext {
+
+class LIBGTKMM2EXT_API EventBoxExt : public Gtk::EventBox
+{
+public:
+	virtual ~EventBoxExt () {}
+
+protected:
+	/* gtk2's gtk/gtkcontainer.c does not
+	 * unmap child widgets if the container has a window.
+	 *
+	 * (this is for historical reasons and optimization
+	 * because back in the day each GdkWindow was backed by
+	 * an actual windowing system surface).
+	 *
+	 * In Ardour's case an EventBox is used in the Editor's top-level
+	 * and child-widgets (e.g. Canvas::GtkCanvas never receive an unmap.
+	 *
+	 * However, when switching Tabbable pages, we do need to hide overlays
+	 * such as ArdourCanvasOpenGLView
+	 *
+	 */
+	void on_unmap () {
+		Gtk::EventBox::on_unmap();
+		if (get_child ()) {
+			get_child()->unmap();
+		}
+	}
+};
+
+} /* namespace */
+
+#endif
