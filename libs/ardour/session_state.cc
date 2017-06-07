af4bc3fea98872225bfc89c052720f61c6654693
Paul Davis <paul@linuxaudiosystems.com>
2017-06-07 19:03:08 -0400

    NO-OP explanatory comment

--- libs/ardour/session_state.cc
+++ libs/ardour/session_state.cc
@@ -1810,7 +1810,9 @@ Session::XMLRouteFactory_2X (const XMLNode& node, int version)
 
 	if (ds_prop) {
 
-		// XXX DISK .... how to load 2.x diskstreams ?
+		/* see comment in current ::set_state() regarding diskstream
+		 * state and DiskReader/DiskWRiter.
+		 */
 
 		error << _("Could not find diskstream for route") << endmsg;
 		return boost::shared_ptr<Route> ();
@@ -1830,8 +1832,6 @@ Session::XMLRouteFactory_2X (const XMLNode& node, int version)
 		if (track->set_state (node, version)) {
 			return ret;
 		}
-
-                // XXX DISK NEED TO SET UP DISKSTREAM ???
 
 		BOOST_MARK_TRACK (track);
 		ret = track;
