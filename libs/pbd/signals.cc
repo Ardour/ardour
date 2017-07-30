ea48c56061aa7324b076ab01ee408005a46bae8e
Robin Gareus <robin@gareus.org>
2017-07-30 02:27:52 +0200

    NO-OP: name-change (_list and _lock are also used in other places)
    Derived classes need to explicitly specify namespace and class
    to avoid ambiguities (even for private members)

--- libs/pbd/signals.cc
+++ libs/pbd/signals.cc
@@ -34,19 +34,19 @@ ScopedConnectionList::~ScopedConnectionList()
 void
 ScopedConnectionList::add_connection (const UnscopedConnection& c)
 {
-	Glib::Threads::Mutex::Lock lm (_lock);
-	_list.push_back (new ScopedConnection (c));
+	Glib::Threads::Mutex::Lock lm (_scoped_connection_lock);
+	_scoped_connection_list.push_back (new ScopedConnection (c));
 }
 
 void
 ScopedConnectionList::drop_connections ()
 {
-	Glib::Threads::Mutex::Lock lm (_lock);
+	Glib::Threads::Mutex::Lock lm (_scoped_connection_lock);
 
-	for (ConnectionList::iterator i = _list.begin(); i != _list.end(); ++i) {
+	for (ConnectionList::iterator i = _scoped_connection_list.begin(); i != _scoped_connection_list.end(); ++i) {
 		delete *i;
 	}
 
-	_list.clear ();
+	_scoped_connection_list.clear ();
 }
 
