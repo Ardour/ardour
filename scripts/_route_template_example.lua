39ffe6d95e20ff4ec66ea9c8eb10865db33893a0
Robin Gareus <robin@gareus.org>
2017-08-17 21:12:29 +0200

    Add an example script for prototyping

---
+++ scripts/_route_template_example.lua
@@ -0,0 +1,11 @@
+ardour {
+	["type"]    = "TrackSetup",
+	name        = "Live Recording Session",
+	description = [[ FOR TESTING AND PROTOTYING ONLY ]]
+}
+
+-- DON'T COUNT ON THIS TO REMAIN AS IS.
+-- This may turn into a factory method, re-usable as ActionScript.
+function session_setup ()
+	Session:new_audio_track (1, 1, nil, 1, "Hello",  ARDOUR.PresentationInfo.max_order, ARDOUR.TrackMode.Normal)
+end
