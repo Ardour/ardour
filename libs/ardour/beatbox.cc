7f6b6eef4f3cee9a85300462c85c52b6b0680b31
Paul Davis <paul@linuxaudiosystems.com>
2017-08-19 19:36:28 -0400

    add whitespace

--- libs/ardour/beatbox.cc
+++ libs/ardour/beatbox.cc
@@ -271,7 +271,6 @@ BeatBox::run (BufferSet& bufs, framepos_t /*start_frame*/, framepos_t /*end_fram
 
 	for (Events::iterator ee = _current_events.begin(); ee != _current_events.end(); ++ee) {
 		Event* e = (*ee);
-
 		if (e->size && (e->time >= process_start && e->time < process_end)) {
 			const framepos_t sample_offset_in_buffer = superclock_to_samples (offset + e->time - process_start, _session.frame_rate());
 			/* e->buf is guaranteed to live through this process * * cycle, so do not alloc for a copy*/
