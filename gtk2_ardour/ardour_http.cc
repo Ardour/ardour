511216a587e66c45f8ae48a58a50b123e8252eeb
Robin Gareus <robin@gareus.org>
2017-09-01 00:36:19 +0200

    Disable ArdourHTTP debug

--- gtk2_ardour/ardour_http.cc
+++ gtk2_ardour/ardour_http.cc
@@ -15,7 +15,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-#define ARDOURCURLDEBUG
+
+//#define ARDOURCURLDEBUG
 
 #include <assert.h>
 #include <stdlib.h>
