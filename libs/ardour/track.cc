9775c5c9f1b81340f3177ede038f02faed71c887
Robin Gareus <robin@gareus.org>
2017-10-01 20:54:40 +0200

    set owner or disk-i/o processors
    This fixes a race-condition. These plugins may be run w/o being
    re-configured which sets the ownershi. currently debug msgs use
    owner()->name()

--- libs/ardour/track.cc
+++ libs/ardour/track.cc
@@ -96,10 +96,12 @@ Track::init ()
 	_disk_reader.reset (new DiskReader (_session, name(), dflags));
 	_disk_reader->set_block_size (_session.get_block_size ());
 	_disk_reader->set_route (boost::dynamic_pointer_cast<Route> (shared_from_this()));
+	_disk_reader->set_owner (this);
 
 	_disk_writer.reset (new DiskWriter (_session, name(), dflags));
 	_disk_writer->set_block_size (_session.get_block_size ());
 	_disk_writer->set_route (boost::dynamic_pointer_cast<Route> (shared_from_this()));
+	_disk_writer->set_owner (this);
 
 	set_align_choice_from_io ();
 
