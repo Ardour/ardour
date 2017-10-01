Ardour/ardour

commit a4a87f56e9dc8e2351101439aeea7a87064fa146

mega-commit to save state of first "it compilesand links" state for separated disk i/o changes.
THIS WILL NOT RUN. THIS REQUIRES MANY CHANGES

libs/ardour/ardour/disk_io.h
@@ -65,12 +65,11 @@ class LIBARDOUR_API DiskIOProcessor : public Processor
 	bool configure_io (ChanCount in, ChanCount out);
 	bool can_support_io_configuration (const ChanCount& in, ChanCount& out);
 
-	/** @return A number between 0 and 1, where 0 indicates that the playback buffer
+	/** @return A number between 0 and 1, where 0 indicates that the playback/capture buffer
 	 *  is dry (ie the disk subsystem could not keep up) and 1 indicates that the
 	 *  buffer is full.
 	 */
-	virtual float playback_buffer_load() const = 0;
-	virtual float capture_buffer_load() const = 0;
+	virtual float buffer_load() const = 0;
 
 	void set_flag (Flag f)   { _flags = Flag (_flags | f); }
 	void unset_flag (Flag f) { _flags = Flag (_flags & ~f); }
@@ -88,8 +87,6 @@ class LIBARDOUR_API DiskIOProcessor : public Processor
 
 	virtual void punch_in()  {}
 	virtual void punch_out() {}
-
-	virtual float buffer_load() const = 0;
 
 	bool slaved() const      { return _slaved; }
 	void set_slaved(bool yn) { _slaved = yn; }
@@ -113,8 +110,6 @@ class LIBARDOUR_API DiskIOProcessor : public Processor
 
 	virtual void playlist_modified () {}
 	virtual int use_playlist (DataType, boost::shared_ptr<Playlist>);
-	virtual int use_new_playlist (DataType);
-	virtual int use_copy_playlist (DataType);
 
 	PBD::Signal1<void,DataType>   PlaylistChanged;
 
@@ -135,6 +130,8 @@ class LIBARDOUR_API DiskIOProcessor : public Processor
 	bool         _slaved;
 	Location*     loop_location;
 	bool          in_set_state;
+	framepos_t     file_frame;
+	framepos_t     playback_sample;
 	framecnt_t    wrap_buffer_size;
 	framecnt_t    speed_buffer_size;
 	bool         _need_butler;
@@ -202,7 +199,6 @@ class LIBARDOUR_API DiskIOProcessor : public Processor
 	virtual void playlist_changed (const PBD::PropertyChange&) {}
 	virtual void playlist_deleted (boost::weak_ptr<Playlist>);
 	virtual void playlist_ranges_moved (std::list< Evoral::RangeMove<framepos_t> > const &, bool) {}
-	int find_and_use_playlist (DataType, std::string const &);
 
 	/* The MIDI stuff */
 
@@ -210,6 +206,8 @@ class LIBARDOUR_API DiskIOProcessor : public Processor
 	gint                         _frames_written_to_ringbuffer;
 	gint                         _frames_read_from_ringbuffer;
 	CubicMidiInterpolation        midi_interpolation;
+
+	static void get_location_times (const Location* location, framepos_t* start, framepos_t* end, framepos_t* length);
 };
 
 } // namespace ARDOUR

libs/ardour/ardour/disk_writer.h
@@ -24,12 +24,14 @@
 #include <vector>
 
 #include "ardour/disk_io.h"
+#include "ardour/midi_buffer.h"
 
 namespace ARDOUR
 {
 
 class AudioFileSource;
 class SMFSource;
+class MidiSource;
 
 class LIBARDOUR_API DiskWriter : public DiskIOProcessor
 {
@@ -49,8 +51,6 @@ class LIBARDOUR_API DiskWriter : public DiskIOProcessor
 
 	virtual XMLNode& state (bool full);
 	int set_state (const XMLNode&, int version);
-
-	virtual int use_new_write_source (uint32_t n=0) = 0;
 
 	std::string write_source_name () const {
 		if (_write_source_name.empty()) {
@@ -72,6 +72,8 @@ class LIBARDOUR_API DiskWriter : public DiskIOProcessor
 	boost::shared_ptr<SMFSource> midi_write_source () { return _midi_write_source; }
 
 	virtual std::string steal_write_source_name () { return std::string(); }
+	int use_new_write_source (DataType, uint32_t n = 0);
+	void reset_write_sources (bool, bool force = false);
 
 	AlignStyle  alignment_style() const { return _alignment_style; }
 	AlignChoice alignment_choice() const { return _alignment_choice; }
@@ -87,8 +89,8 @@ class LIBARDOUR_API DiskWriter : public DiskIOProcessor
 
 	bool         record_enabled() const { return g_atomic_int_get (const_cast<gint*>(&_record_enabled)); }
 	bool         record_safe () const { return g_atomic_int_get (const_cast<gint*>(&_record_safe)); }
-	virtual void set_record_enabled (bool yn) = 0;
-	virtual void set_record_safe (bool yn) = 0;
+	virtual void set_record_enabled (bool yn);
+	virtual void set_record_safe (bool yn);
 
 	bool destructive() const { return _flags & Destructive; }
 	int set_destructive (bool yn);
@@ -109,16 +111,31 @@ class LIBARDOUR_API DiskWriter : public DiskIOProcessor
 	framecnt_t   capture_offset() const { return _capture_offset; }
 	virtual void set_capture_offset ();
 
+	int seek (framepos_t frame, bool complete_refill);
+
 	static PBD::Signal0<void> Overrun;
+
+	void set_note_mode (NoteMode m);
+
+	/** Emitted when some MIDI data has been received for recording.
+	 *  Parameter is the source that it is destined for.
+	 *  A caller can get a copy of the data with get_gui_feed_buffer ()
+	 */
+	PBD::Signal1<void, boost::weak_ptr<MidiSource> > DataRecorded;
 
 	PBD::Signal0<void> RecordEnableChanged;
 	PBD::Signal0<void> RecordSafeChanged;
 
+	void check_record_status (framepos_t transport_frame, bool can_record);
+
+	void transport_looped (framepos_t transport_frame);
+	void transport_stopped_wallclock (struct tm&, time_t, bool abort);
+
   protected:
-	virtual int do_flush (RunContext context, bool force = false) = 0;
+	friend class Track;
+	int do_flush (RunContext context, bool force = false);
 
 	void get_input_sources ();
-	void check_record_status (framepos_t transport_frame, bool can_record);
 	void prepare_record_status (framepos_t /*capture_start_frame*/);
 	void set_align_style_from_io();
 	void setup_destructive_playlist ();
@@ -137,9 +154,6 @@ class LIBARDOUR_API DiskWriter : public DiskIOProcessor
 		Evoral::OverlapType ot, framepos_t transport_frame, framecnt_t nframes,
 		framecnt_t& rec_nframes, framecnt_t& rec_offset
 		);
-
-	static framecnt_t disk_read_chunk_frames;
-	static framecnt_t disk_write_chunk_frames;
 
 	struct CaptureInfo {
 		framepos_t start;
@@ -168,13 +182,19 @@ class LIBARDOUR_API DiskWriter : public DiskIOProcessor
 
 	std::list<boost::shared_ptr<Source> > _last_capture_sources;
 	std::vector<boost::shared_ptr<AudioFileSource> > capturing_sources;
-	
+
 	static framecnt_t _chunk_frames;
 
 	NoteMode                     _note_mode;
 	volatile gint                _frames_pending_write;
 	volatile gint                _num_captured_loops;
 	framepos_t                   _accumulated_capture_offset;
+
+	/** A buffer that we use to put newly-arrived MIDI data in for
+	    the GUI to read (so that it can update itself).
+	*/
+	MidiBuffer                   _gui_feed_buffer;
+	mutable Glib::Threads::Mutex _gui_feed_buffer_mutex;
 
 	void finish_capture (boost::shared_ptr<ChannelList> c);
 };

libs/ardour/ardour/midi_track.h
@@ -54,10 +54,6 @@ public:
 	bool can_be_record_enabled ();
 	bool can_be_record_safe ();
 
-	DataType data_type () const {
-		return DataType::MIDI;
-	}
-
 	void freeze_me (InterThreadInfo&);
 	void unfreeze ();
 
@@ -155,8 +151,6 @@ private:
 	MidiChannelFilter          _capture_filter;
 
 	virtual boost::shared_ptr<Diskstream> diskstream_factory (XMLNode const &);
-
-	boost::shared_ptr<MidiDiskstream> midi_diskstream () const;
 
 	void write_out_of_band_data (BufferSet& bufs, framepos_t start_frame, framepos_t end_frame, framecnt_t nframes);
 

libs/ardour/audio_track.cc
@@ -32,6 +32,8 @@
 #include "ardour/boost_debug.h"
 #include "ardour/buffer_set.h"
 #include "ardour/delivery.h"
+#include "ardour/disk_reader.h"
+#include "ardour/disk_writer.h"
 #include "ardour/meter.h"
 #include "ardour/monitor_control.h"
 #include "ardour/playlist_factory.h"
@@ -63,96 +65,47 @@ AudioTrack::~AudioTrack ()
 	}
 }
 
-boost::shared_ptr<Diskstream>
-AudioTrack::create_diskstream ()
-{
-	AudioDiskstream::Flag dflags = AudioDiskstream::Flag (AudioDiskstream::Recordable);
-
-	if (_mode == Destructive && !Profile->get_trx()) {
-		dflags = AudioDiskstream::Flag (dflags | AudioDiskstream::Destructive);
-	} else if (_mode == NonLayered){
-		dflags = AudioDiskstream::Flag(dflags | AudioDiskstream::NonLayered);
-	}
-
-	return boost::shared_ptr<AudioDiskstream> (new AudioDiskstream (_session, name(), dflags));
-}
-
-void
-AudioTrack::set_diskstream (boost::shared_ptr<Diskstream> ds)
-{
-	Track::set_diskstream (ds);
-
-	_diskstream->set_track (this);
-
-	if (audio_diskstream()->deprecated_io_node) {
-
-		if (!IO::connecting_legal) {
-			IO::ConnectingLegal.connect_same_thread (*this, boost::bind (&AudioTrack::deprecated_use_diskstream_connections, this));
-		} else {
-			deprecated_use_diskstream_connections ();
-		}
-	}
-
-	_diskstream->set_record_enabled (false);
-	_diskstream->request_input_monitoring (false);
-
-	DiskstreamChanged (); /* EMIT SIGNAL */
-}
-
-boost::shared_ptr<AudioDiskstream>
-AudioTrack::audio_diskstream() const
-{
-	return boost::dynamic_pointer_cast<AudioDiskstream>(_diskstream);
-}
-
+#ifdef XXX_OLD_DESTRUCTIVE_API_XXX
 int
-AudioTrack::deprecated_use_diskstream_connections ()
+AudioTrack::set_mode (TrackMode m)
 {
-	boost::shared_ptr<AudioDiskstream> diskstream = audio_diskstream();
+	if (m != _mode) {
 
-	if (diskstream->deprecated_io_node == 0) {
-		return 0;
-	}
-
-	XMLNode& node (*diskstream->deprecated_io_node);
-
-	/* don't do this more than once. */
-
-	diskstream->deprecated_io_node = 0;
-
-	float val;
-	if (node.get_property ("gain", val)) {
-		_amp->gain_control()->set_value (val, PBD::Controllable::NoGroup);
-	}
-
-	std::string str;
-	if (node.get_property ("input-connection", str)) {
-		boost::shared_ptr<Bundle> c = _session.bundle_by_name (str);
-
-		if (c == 0) {
-			error << string_compose(_("Unknown bundle \"%1\" listed for input of %2"), str, _name) << endmsg;
-
-			if ((c = _session.bundle_by_name (_("in 1"))) == 0) {
-				error << _("No input bundles available as a replacement")
-			        << endmsg;
-				return -1;
-			} else {
-				info << string_compose (_("Bundle %1 was not available - \"in 1\" used instead"), str)
-			       << endmsg;
-			}
-		}
-
-		_input->connect_ports_to_bundle (c, true, this);
-
-	} else if (node.get_property ("inputs", str)) {
-		if (_input->set_ports (str)) {
-			error << string_compose(_("improper input channel list in XML node (%1)"), str) << endmsg;
+		if (!Profile->get_trx() && _diskstream->set_destructive (m == Destructive)) {
 			return -1;
 		}
+
+		_diskstream->set_non_layered (m == NonLayered);
+		_mode = m;
+
+		TrackModeChanged (); /* EMIT SIGNAL */
 	}
 
 	return 0;
 }
+
+bool
+AudioTrack::can_use_mode (TrackMode m, bool& bounce_required)
+{
+	switch (m) {
+	case NonLayered:
+	case Normal:
+		bounce_required = false;
+		return true;
+
+	case Destructive:
+		if (Profile->get_trx()) {
+			return false;
+		} else {
+			return _diskstream->can_become_destructive (bounce_required);
+		}
+		break;
+
+	default:
+		return false;
+	}
+}
+#endif
 
 int
 AudioTrack::set_state (const XMLNode& node, int version)
@@ -279,17 +232,8 @@ AudioTrack::roll (pframes_t nframes, framepos_t start_frame, framepos_t end_fram
 	Glib::Threads::RWLock::ReaderLock lm (_processor_lock, Glib::Threads::TRY_LOCK);
 
 	if (!lm.locked()) {
-		boost::shared_ptr<AudioDiskstream> diskstream = audio_diskstream();
-		framecnt_t playback_distance = diskstream->calculate_playback_distance(nframes);
-		if (can_internal_playback_seek(::llabs(playback_distance))) {
-			/* TODO should declick */
-			internal_playback_seek(playback_distance);
-		}
 		return 0;
 	}
-
-	framepos_t transport_frame;
-	boost::shared_ptr<AudioDiskstream> diskstream = audio_diskstream();
 
 	if (n_outputs().n_total() == 0 && _processors.empty()) {
 		return 0;
@@ -297,29 +241,10 @@ AudioTrack::roll (pframes_t nframes, framepos_t start_frame, framepos_t end_fram
 
 	if (!_active) {
 		silence (nframes);
-		if (_meter_point == MeterInput && ((_monitoring_control->monitoring_choice() & MonitorInput) || _diskstream->record_enabled())) {
+		if (_meter_point == MeterInput && ((_monitoring_control->monitoring_choice() & MonitorInput) || _disk_writer->record_enabled())) {
 			_meter->reset();
 		}
 		return 0;
-	}
-
-	transport_frame = _session.transport_frame();
-
-	int dret;
-	framecnt_t playback_distance;
-
-	if ((nframes = check_initial_delay (nframes, transport_frame)) == 0) {
-
-		/* need to do this so that the diskstream sets its
-		   playback distance to zero, thus causing diskstream::commit
-		   to do nothing.
-		*/
-
-		BufferSet bufs; /* empty set, no matter - nothing will happen */
-
-		dret = diskstream->process (bufs, transport_frame, 0, playback_distance, false);
-		need_butler = diskstream->commit (playback_distance);
-		return dret;
 	}
 
 	_silent = false;
@@ -333,17 +258,9 @@ AudioTrack::roll (pframes_t nframes, framepos_t start_frame, framepos_t end_fram
 		_meter->run (bufs, start_frame, end_frame, 1.0 /*speed()*/, nframes, true);
 	}
 
-	if ((dret = diskstream->process (bufs, transport_frame, nframes, playback_distance, (monitoring_state() == MonitoringDisk))) != 0) {
-		need_butler = diskstream->commit (playback_distance);
-		silence (nframes);
-		return dret;
-	}
-
-	process_output_buffers (bufs, start_frame, end_frame, nframes, declick, (!diskstream->record_enabled() && _session.transport_rolling()));
+	process_output_buffers (bufs, start_frame, end_frame, nframes, declick, (!_disk_writer->record_enabled() && _session.transport_rolling()));
 
 	flush_processor_buffers_locked (nframes);
-
-	need_butler = diskstream->commit (playback_distance);
 
 	return 0;
 }
@@ -354,11 +271,10 @@ AudioTrack::export_stuff (BufferSet& buffers, framepos_t start, framecnt_t nfram
 {
 	boost::scoped_array<gain_t> gain_buffer (new gain_t[nframes]);
 	boost::scoped_array<Sample> mix_buffer (new Sample[nframes]);
-	boost::shared_ptr<AudioDiskstream> diskstream = audio_diskstream();
 
 	Glib::Threads::RWLock::ReaderLock rlock (_processor_lock);
 
-	boost::shared_ptr<AudioPlaylist> apl = boost::dynamic_pointer_cast<AudioPlaylist>(diskstream->playlist());
+	boost::shared_ptr<AudioPlaylist> apl = boost::dynamic_pointer_cast<AudioPlaylist>(playlist());
 
 	assert(apl);
 	assert(buffers.count().n_audio() >= 1);
@@ -373,7 +289,7 @@ AudioTrack::export_stuff (BufferSet& buffers, framepos_t start, framecnt_t nfram
 	BufferSet::audio_iterator bi = buffers.audio_begin();
 	++bi;
 	for ( ; bi != buffers.audio_end(); ++bi, ++n) {
-		if (n < diskstream->n_channels().n_audio()) {
+		if (n < _disk_reader->output_streams().n_audio()) {
 			if (apl->read (bi->data(), mix_buffer.get(), gain_buffer.get(), start, nframes, n) != nframes) {
 				return -1;
 			}
@@ -468,9 +384,8 @@ AudioTrack::freeze_me (InterThreadInfo& itt)
 	boost::shared_ptr<Playlist> new_playlist;
 	string dir;
 	string region_name;
-	boost::shared_ptr<AudioDiskstream> diskstream = audio_diskstream();
 
-	if ((_freeze_record.playlist = boost::dynamic_pointer_cast<AudioPlaylist>(diskstream->playlist())) == 0) {
+	if ((_freeze_record.playlist = boost::dynamic_pointer_cast<AudioPlaylist>(playlist())) == 0) {
 		return;
 	}
 
@@ -555,8 +470,8 @@ AudioTrack::freeze_me (InterThreadInfo& itt)
 	new_playlist->set_frozen (true);
 	region->set_locked (true);
 
-	diskstream->use_playlist (boost::dynamic_pointer_cast<AudioPlaylist>(new_playlist));
-	diskstream->set_record_enabled (false);
+	use_playlist (DataType::AUDIO, boost::dynamic_pointer_cast<AudioPlaylist>(new_playlist));
+	_disk_writer->set_record_enabled (false);
 
 	_freeze_record.playlist->use(); // prevent deletion
 
@@ -576,7 +491,7 @@ AudioTrack::unfreeze ()
 {
 	if (_freeze_record.playlist) {
 		_freeze_record.playlist->release();
-		audio_diskstream()->use_playlist (_freeze_record.playlist);
+		use_playlist (DataType::AUDIO, _freeze_record.playlist);
 
 		{
 			Glib::Threads::RWLock::ReaderLock lm (_processor_lock); // should this be a write lock? jlc
@@ -601,13 +516,6 @@ AudioTrack::unfreeze ()
 boost::shared_ptr<AudioFileSource>
 AudioTrack::write_source (uint32_t n)
 {
-	boost::shared_ptr<AudioDiskstream> ds = boost::dynamic_pointer_cast<AudioDiskstream> (_diskstream);
-	assert (ds);
-	return ds->write_source (n);
-}
-
-boost::shared_ptr<Diskstream>
-AudioTrack::diskstream_factory (XMLNode const & node)
-{
-	return boost::shared_ptr<Diskstream> (new AudioDiskstream (_session, node));
+	assert (_disk_writer);
+	return _disk_writer->audio_write_source (n);
 }

commit 05ab7a46b5cf4482ca999625da7405d47fd700a6

Fix recording MIDI
- Fix API call to add region(midi_region) -- set count to "1"
- Forward DataRecorded() signal
- remove botched merge/rebase"
  a4a87f56 accidentally brought back code from old-destructive API
  which was removed in af103cf3 and 08c13007
  There is no per track NonLayered record mode anymore, it's session global.
- set can_record correctly to not accidentally clear last capture sources
  for cont'd recording (toggle track's rec-arm)

libs/ardour/ardour/disk_io.h
@@ -50,7 +50,7 @@ class LIBARDOUR_API DiskIOProcessor : public Processor
 		Recordable  = 0x1,
 		Hidden      = 0x2,
 		Destructive = 0x4,
-		NonLayered   = 0x8
+		NonLayered  = 0x8 // deprecated (kept only for enum compat)
 	};
 
 	static const std::string state_node_name;
@@ -77,7 +77,6 @@ class LIBARDOUR_API DiskIOProcessor : public Processor
 
 	bool           hidden()      const { return _flags & Hidden; }
 	bool           recordable()  const { return _flags & Recordable; }
-	bool           non_layered()  const { return _flags & NonLayered; }
 
 	virtual void non_realtime_locate (samplepos_t);
 

libs/ardour/ardour/disk_writer.h
@@ -97,9 +97,6 @@ public:
 	virtual void set_record_safe (bool yn);
 
 	bool destructive() const { return _flags & Destructive; }
-	int  set_destructive (bool yn);
-	int  set_non_layered (bool yn);
-	bool can_become_destructive (bool& requires_bounce) const;
 
 	/** @return Start position of currently-running capture (in session samples) */
 	samplepos_t current_capture_start() const { return capture_start_sample; }

libs/ardour/ardour/midi_track.h
@@ -161,6 +161,8 @@ private:
 	void track_input_active (IOChange, void*);
 	void map_input_active (bool);
 
+	void data_recorded (boost::weak_ptr<MidiSource> src);
+
 	/** Update automation controls to reflect any changes in buffers. */
 	void update_controls (BufferSet const& bufs);
 	void restore_controls ();

libs/ardour/audio_track.cc
@@ -64,48 +64,6 @@ AudioTrack::~AudioTrack ()
 	}
 }
 
-#ifdef XXX_OLD_DESTRUCTIVE_API_XXX
-int
-AudioTrack::set_mode (TrackMode m)
-{
-	if (m != _mode) {
-
-		if (!Profile->get_trx() && _diskstream->set_destructive (m == Destructive)) {
-			return -1;
-		}
-
-		_diskstream->set_non_layered (m == NonLayered);
-		_mode = m;
-
-		TrackModeChanged (); /* EMIT SIGNAL */
-	}
-
-	return 0;
-}
-
-bool
-AudioTrack::can_use_mode (TrackMode m, bool& bounce_required)
-{
-	switch (m) {
-	case NonLayered:
-	case Normal:
-		bounce_required = false;
-		return true;
-
-	case Destructive:
-		if (Profile->get_trx()) {
-			return false;
-		} else {
-			return _diskstream->can_become_destructive (bounce_required);
-		}
-		break;
-
-	default:
-		return false;
-	}
-}
-#endif
-
 int
 AudioTrack::set_state (const XMLNode& node, int version)
 {

libs/ardour/disk_writer.cc
@@ -359,7 +359,7 @@ DiskWriter::run (BufferSet& bufs, samplepos_t start_sample, samplepos_t end_samp
 	bool re = record_enabled ();
 	bool punch_in = _session.config.get_punch_in () && _session.locations()->auto_punch_location ();
 	bool can_record = _session.actively_recording ();
-	can_record |= _session.get_record_enabled () && punch_in && _session.transport_sample () <= _session.locations()->auto_punch_location ()->start ();
+	can_record |= speed != 0 && _session.get_record_enabled () && punch_in && _session.transport_sample () <= _session.locations()->auto_punch_location ()->start ();
 
 	_need_butler = false;
 
@@ -1371,14 +1371,8 @@ DiskWriter::use_destructive_playlist ()
 		assert((*chan)->write_source);
 		(*chan)->write_source->set_allow_remove_if_empty (false);
 
-		/* this might be false if we switched modes, so force it */
-
-#ifdef XXX_OLD_DESTRUCTIVE_API_XXX
-		(*chan)->write_source->set_destructive (true);
-#else
 		// should be set when creating the source or loading the state
 		assert ((*chan)->write_source->destructive());
-#endif
 	}
 
 	/* the source list will never be reset for a destructive track */

libs/ardour/midi_track.cc
@@ -98,7 +98,15 @@ MidiTrack::init ()
 	_disk_writer->set_note_mode (_note_mode);
 	_disk_reader->reset_tracker ();
 
+	_disk_writer->DataRecorded.connect_same_thread (*this, boost::bind (&MidiTrack::data_recorded, this, _1));
+
 	return 0;
+}
+
+void
+MidiTrack::data_recorded (boost::weak_ptr<MidiSource> src)
+{
+	DataRecorded (src); /* EMIT SIGNAL */
 }
 
 bool

libs/ardour/track.cc
@@ -91,8 +91,6 @@ Track::init ()
 
 	if (_mode == Destructive && !Profile->get_trx()) {
 		dflags = DiskIOProcessor::Flag (dflags | DiskIOProcessor::Destructive);
-	} else if (_mode == NonLayered){
-		dflags = DiskIOProcessor::Flag(dflags | DiskIOProcessor::NonLayered);
 	}
 
 	_disk_reader.reset (new DiskReader (_session, name(), dflags));
@@ -1035,9 +1033,9 @@ Track::use_captured_midi_sources (SourceList& srcs, CaptureInfos const & capture
 			continue; /* XXX is this OK? */
 		}
 
-		// cerr << "add new region, buffer position = " << buffer_position << " @ " << (*ci)->start << endl;
+		cerr << "add new region, len = " << (*ci)->samples << " @ " << (*ci)->start << endl;
 
-		pl->add_region (midi_region, (*ci)->start + preroll_off, _disk_writer->non_layered());
+		pl->add_region (midi_region, (*ci)->start + preroll_off, 1, _session.config.get_layered_record_mode ());
 	}
 
 	pl->thaw ();
@@ -1139,7 +1137,7 @@ Track::use_captured_audio_sources (SourceList& srcs, CaptureInfos const & captur
 			continue; /* XXX is this OK? */
 		}
 
-		pl->add_region (region, (*ci)->start + preroll_off, 1, _disk_writer->non_layered());
+		pl->add_region (region, (*ci)->start + preroll_off, 1, _session.config.get_layered_record_mode());
 		pl->set_layer (region, DBL_MAX);
 
 		buffer_position += (*ci)->samples;