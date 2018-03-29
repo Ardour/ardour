39c01540dfc91d4b1614411cc5ff98fa4a9d9d85
Guenther Humer <dojoy@avlinux8>
2018-03-29 21:36:41 +0200

    removed wrong info from other bug

--- gtk2_ardour/duplicate_routes_dialog.h
+++ gtk2_ardour/duplicate_routes_dialog.h
@@ -45,9 +45,6 @@ public:
 
 	int restart (ARDOUR::Session*);
 
-protected:
-	 void get_ordered_selected_route_list(ARDOUR::RouteList&);
-
 private:
 	Gtk::Entry name_template_entry;
 	Gtk::VBox playlist_button_box;
