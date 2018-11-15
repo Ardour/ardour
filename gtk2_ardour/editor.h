b409c58ee67f2779a0ee8ef66f9ead093a911058
Ben Loftis <ben@harrisonconsoles.com>
2018-11-15 09:30:22 -0600

    (Source List) Fix drag-n-drop.

--- gtk2_ardour/editor.h
+++ gtk2_ardour/editor.h
@@ -1979,7 +1979,8 @@ private:
 	        gint                                  y,
 	        const Gtk::SelectionData&             data,
 	        guint                                 info,
-	        guint                                 time);
+	        guint                                 time,
+	        bool                                  from_region_list);
 
 	void drop_routes (
 	        const Glib::RefPtr<Gdk::DragContext>& context,
