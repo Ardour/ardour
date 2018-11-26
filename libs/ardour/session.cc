5531a440c990391241292a864f9c71d3b7625c45
Robin Gareus <robin@gareus.org>
2018-11-26 21:45:06 +0100

    Fix stuck connection block when template load fails.
    Session::new_route_from_template() unconditionally calls
    IO::disable_connecting(), and needs to be reverted likewise
    (no RAII here).

--- libs/ardour/session.cc
+++ libs/ardour/session.cc
@@ -3451,6 +3451,11 @@ Session::new_route_from_template (uint32_t how_many, PresentationInfo::order_t i
 			goto out;
 		}
 
+		catch (...) {
+			IO::enable_connecting ();
+			throw;
+		}
+
 		--how_many;
 	}
 
@@ -3462,8 +3467,9 @@ Session::new_route_from_template (uint32_t how_many, PresentationInfo::order_t i
 		} else {
 			add_routes (ret, true, true, false, insert_at);
 		}
-		IO::enable_connecting ();
 	}
+
+	IO::enable_connecting ();
 
 	return ret;
 }
