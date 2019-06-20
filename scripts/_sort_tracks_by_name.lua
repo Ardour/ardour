8a78b19e1b29ee9661f0bd5967397622a51b0751
Robin Gareus <robin@gareus.org>
2019-06-21 00:43:29 +0200

    Add Lua script to sort tracks by name

---
+++ scripts/_sort_tracks_by_name.lua
@@ -0,0 +1,30 @@
+ardour {
+    ["type"] = "EditorAction",
+    name = "Track Sort",
+    author = "Ardour Lua Taskforce",
+    description = [[Sort tracks alphabetically by name]]
+}
+
+function factory () return function ()
+
+	function tsort (a, b)
+		return a:name() < b:name()
+	end
+
+	local tracklist = {}
+   for t in Session:get_tracks():iter() do
+		 table.insert(tracklist, t)
+		 print (t:name(), t:presentation_info_ptr():order())
+	 end
+
+	 table.sort(tracklist, tsort)
+
+	 local pos = 1;
+	 for _, t in ipairs(tracklist) do
+		 t:set_presentation_order(pos)
+		 pos = pos + 1
+	 end
+
+	 tracklist = nil
+	 collectgarbage ()
+ end end
