132e895b186da7747ebbb6513ce09e600a4cce79
Nikolaus Gullotta <nik@harrisonconsoles.com>
2019-08-13 09:41:33 -0500

    fix GPL info

--- gtk2_ardour/mixer_snapshots.h
+++ gtk2_ardour/mixer_snapshots.h
@@ -1,10 +1,10 @@
 /*
-    Copyright (C) 2000-2019 Paul Davis
+    Copyright (C) 2019 Nikolaus Gullotta
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,8 +13,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #ifndef __gtk_ardour_mixer_snapshots_h__
