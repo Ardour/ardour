706a9ab59f070024b50d4f504113eb57fc49d316
Paul Davis <paul@linuxaudiosystems.com>
2019-10-28 17:21:46 -0600

    fix mistakenly-placed semi-colon

--- libs/ardour/port_insert.cc
+++ libs/ardour/port_insert.cc
@@ -236,7 +236,7 @@ PortInsert::signal_latency() const
 
 	if (_measured_latency == 0) {
 		return _session.engine().samples_per_cycle()
-		       + _input->connected_latency (false);
+		       + _input->connected_latency (false)
 		       + _output->connected_latency (true);
 	} else {
 		return _measured_latency;
