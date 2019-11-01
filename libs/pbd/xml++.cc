7852047dca6312a668edb1ba3fd0d315b3f83ac8
Robin Gareus <robin@gareus.org>
2019-11-01 15:39:44 +0100

    Fix XML-writer edge-case (empty content)

--- libs/pbd/xml++.cc
+++ libs/pbd/xml++.cc
@@ -482,6 +482,12 @@ XMLNode::attribute_value()
 XMLNode*
 XMLNode::add_content(const string& c)
 {
+	if (c.empty ()) {
+		/* this would add a "</>" child, leading to invalid XML.
+		 * Also in XML, empty string content is equivalent to no content.
+		 */
+		return NULL;
+	}
 	return add_child_copy(XMLNode (string(), c));
 }
 
