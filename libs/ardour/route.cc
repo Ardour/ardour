f61f938503c53f77eba7a423af3ffdd7be189e54
Paul Davis <paul@linuxaudiosystems.com>
2019-11-06 09:28:23 -0700

    remove debug message

--- libs/ardour/route.cc
+++ libs/ardour/route.cc
@@ -5960,8 +5960,6 @@ Route::set_disk_io_point (DiskIOPoint diop)
 {
 	bool display = false;
 
-	cerr << "set disk io to " << enum_2_string (diop) << endl;
-
 	switch (diop) {
 	case DiskIOCustom:
 		display = true;
