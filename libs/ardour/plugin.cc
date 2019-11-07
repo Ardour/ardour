dad47e445cb3496c0f904fef3090e6469f0cc1a4
Paul Davis <paul@linuxaudiosystems.com>
2019-11-06 23:54:47 -0700

    remove debug output

--- libs/ardour/plugin.cc
+++ libs/ardour/plugin.cc
@@ -393,11 +393,9 @@ Plugin::realtime_handle_transport_stopped ()
 void
 Plugin::realtime_locate (bool for_loop_end)
 {
-	std::cerr << name() << " RL fle = " << for_loop_end << std::endl;
-
-	//if (!for_loop_end) {
+	if (!for_loop_end) {
 		resolve_midi ();
-//}
+	}
 }
 
 void
