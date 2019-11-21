6ee21fb77e12ca11f0fc29af9b34a1b64501e5a1
Robin Gareus <robin@gareus.org>
2019-11-21 17:49:20 +0100

    GUI: Use new boost::optional API
    get_value_or() has been deprecated since boost 1.56

--- gtk2_ardour/ardour_ui.cc
+++ gtk2_ardour/ardour_ui.cc
@@ -1329,13 +1329,13 @@ ARDOUR_UI::update_disk_space()
 	if (!opt_samples) {
 		/* Available space is unknown */
 		format_disk_space_label (-1);
-	} else if (opt_samples.get_value_or (0) == max_samplecnt) {
+	} else if (opt_samples.value_or (0) == max_samplecnt) {
 		format_disk_space_label (max_samplecnt);
 	} else {
 		rec_enabled_streams = 0;
 		_session->foreach_route (this, &ARDOUR_UI::count_recenabled_streams, false);
 
-		samplecnt_t samples = opt_samples.get_value_or (0);
+		samplecnt_t samples = opt_samples.value_or (0);
 
 		if (rec_enabled_streams) {
 			samples /= rec_enabled_streams;
