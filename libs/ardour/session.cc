cafd1b792c26397202f77a25e7c3085cebe065bc
Paul Davis <paul@linuxaudiosystems.com>
2019-12-16 16:13:02 -0700

    expand DEBUG_TRACE output

--- libs/ardour/session.cc
+++ libs/ardour/session.cc
@@ -6403,7 +6403,7 @@ Session::update_latency (bool playback)
 	 * but may indirectly be triggered from
 	 * Session::update_latency_compensation -> _engine.update_latencies
 	 */
-	DEBUG_TRACE (DEBUG::LatencyCompensation, string_compose ("Engine latency callback: %1\n", (playback ? "PLAYBACK" : "CAPTURE")));
+	DEBUG_TRACE (DEBUG::LatencyCompensation, string_compose ("Engine latency callback: %1 (initial/deletion? %2)\n", (playback ? "PLAYBACK" : "CAPTURE"), inital_connect_or_deletion_in_progress()));
 
 	if (inital_connect_or_deletion_in_progress () || _adding_routes_in_progress || _route_deletion_in_progress) {
 		return;
