64b43a6b95557610b5126fa14844849e30c4239c
Robin Gareus <robin@gareus.org>
2020-01-06 16:59:14 +0100

    Fix windows fallback for LV2 state:freePath feature

--- libs/plugins/a-fluidsynth.lv2/a-fluidsynth.cc
+++ libs/plugins/a-fluidsynth.lv2/a-fluidsynth.cc
@@ -751,11 +751,11 @@ save (LV2_Handle                instance,
 		free_path->free_path (free_path->handle, apath);
 	} else
 #endif
-#ifndef _WIN32 // https://github.com/drobilla/lilv/issues/14
 	{
+#ifndef _WIN32 // https://github.com/drobilla/lilv/issues/14
 		free (apath);
-	}
 #endif
+	}
 
 	return LV2_STATE_SUCCESS;
 }
@@ -808,11 +808,11 @@ restore (LV2_Handle                  instance,
 			free_path->free_path (free_path->handle, apath);
 		} else
 #endif
-#ifndef _WIN32 // https://github.com/drobilla/lilv/issues/14
 		{
+#ifndef _WIN32 // https://github.com/drobilla/lilv/issues/14
 			free (apath);
-		}
 #endif
+		}
 	}
 	return LV2_STATE_SUCCESS;
 }
