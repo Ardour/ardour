79f0ad0533e8a8fd0cc93331db4748c15eda24ea
Paul Davis <paul@linuxaudiosystems.com>
2020-02-12 09:23:06 -0700

    fix missing MIDI playback by using correct (expanded) logic in DiskReader::declick_in_progress()
    If use_transport_fades() is false, then the declick_amp will have its gain always set to the current target (no declick).
    Therefore only testing if it has reached zero is not enough, we need to check if we are declicking at all.

--- libs/ardour/disk_reader.cc
+++ libs/ardour/disk_reader.cc
@@ -499,7 +499,7 @@ DiskReader::run (BufferSet& bufs, samplepos_t start_sample, samplepos_t end_samp
 bool
 DiskReader::declick_in_progress () const
 {
-	return _declick_amp.gain() != 0; // declick-out
+	return _session.cfg()->get_use_transport_fades() && (_declick_amp.gain() != 0); // declick-out
 }
 
 bool
