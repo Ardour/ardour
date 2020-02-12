a463a1e0ea359e9dbf5344991ba2a2d9921b33c9
John Emmas <john@creativepost.co.uk>
2020-02-12 09:56:29 +0000

    Revert commit #39975dc3c6 from Feb 7th
    'using namespace ARDOUR;' is needed for the MSVC build.
    
    (I guess it could be enclosed by a #ifdef if it's causing issues for the gcc build)

--- libs/ardour/convolver.cc
+++ libs/ardour/convolver.cc
@@ -35,10 +35,11 @@
 
 #include "pbd/i18n.h"
 
+using namespace ARDOUR;
 using namespace ARDOUR::DSP;
 using namespace ArdourZita;
 
-Convolution::Convolution (ARDOUR::Session& session, uint32_t n_in, uint32_t n_out)
+Convolution::Convolution (Session& session, uint32_t n_in, uint32_t n_out)
     : SessionHandleRef (session)
     , _n_samples (0)
     , _max_size (0)
@@ -217,7 +218,7 @@ Convolution::run (BufferSet& bufs, ChanMapping const& in_map, ChanMapping const&
 /* ****************************************************************************/
 
 Convolver::Convolver (
-    ARDOUR::Session&   session,
+    Session&           session,
     std::string const& path,
     IRChannelConfig    irc,
     IRSettings         irs)
