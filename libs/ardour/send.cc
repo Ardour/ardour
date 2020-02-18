Ardour/ardour

commit d4e023e1cbd783c2a46f8b4b231798c05f6b6708

Make BusSendLevel 1st class citizen (1/2)
Equivalent to Gain and Trim (gain-coefficient, not dB) and use
it for Sends.

libs/ardour/send.cc
@@ -101,7 +101,7 @@ Send::Send (Session& s, boost::shared_ptr<Pannable> p, boost::shared_ptr<MuteMas
 	//boost_debug_shared_ptr_mark_interesting (this, "send");
 
 	boost::shared_ptr<AutomationList> gl (new AutomationList (Evoral::Parameter (GainAutomation)));
-	_gain_control = boost::shared_ptr<GainControl> (new GainControl (_session, Evoral::Parameter(GainAutomation), gl));
+	_gain_control = boost::shared_ptr<GainControl> (new GainControl (_session, Evoral::Parameter(BusSendLevel), gl));
 	add_control (_gain_control);
 
 	_amp.reset (new Amp (_session, _("Fader"), _gain_control, true));

libs/ardour/ardour/value_as_string.h
@@ -53,7 +53,7 @@ value_as_string(const ARDOUR::ParameterDescriptor& desc,
 	// Value is not a scale point, print it normally
 	if (desc.unit == ARDOUR::ParameterDescriptor::MIDI_NOTE) {
 		snprintf(buf, sizeof(buf), "%s", ParameterDescriptor::midi_note_name (rint(v)).c_str());
-	} else if (desc.type == GainAutomation || desc.type == TrimAutomation || desc.type == EnvelopeAutomation) {
+	} else if (desc.type == GainAutomation || desc.type == BusSendLevel || desc.type == TrimAutomation || desc.type == EnvelopeAutomation) {
 		snprintf(buf, sizeof(buf), "%.1f dB", accurate_coefficient_to_dB (v));
 	} else if (desc.type == PanWidthAutomation) {
 		snprintf (buf, sizeof (buf), "%d%%", (int) floor (100.0 * v));

libs/ardour/automatable.cc
@@ -190,6 +190,8 @@ Automatable::describe_parameter (Evoral::Parameter param)
 
 	if (param == Evoral::Parameter(GainAutomation)) {
 		return _("Fader");
+	} else if (param.type() == BusSendLevel) {
+		return _("Send");
 	} else if (param.type() == TrimAutomation) {
 		return _("Trim");
 	} else if (param.type() == MuteAutomation) {
@@ -541,6 +543,8 @@ Automatable::control_factory(const Evoral::Parameter& param)
 	} else if (param.type() == GainAutomation) {
 		control = new GainControl(_a_session, param);
 	} else if (param.type() == TrimAutomation) {
+		control = new GainControl(_a_session, param);
+	} else if (param.type() == BusSendLevel) {
 		control = new GainControl(_a_session, param);
 	} else if (param.type() == PanAzimuthAutomation || param.type() == PanWidthAutomation || param.type() == PanElevationAutomation) {
 		Pannable* pannable = dynamic_cast<Pannable*>(this);

libs/ardour/automation_list.cc
@@ -153,6 +153,7 @@ AutomationList::create_curve_if_necessary()
 {
 	switch (_parameter.type()) {
 	case GainAutomation:
+	case BusSendLevel:
 	case TrimAutomation:
 	case PanAzimuthAutomation:
 	case PanElevationAutomation:

libs/ardour/event_type_map.cc
@@ -135,6 +135,8 @@ EventTypeMap::from_symbol(const string& str) const
 
 	if (str == "gain") {
 		p_type = GainAutomation;
+	} else if (str == "send") {
+		p_type = BusSendLevel;
 	} else if (str == "trim") {
 		p_type = TrimAutomation;
 	} else if (str == "solo") {
@@ -235,6 +237,8 @@ EventTypeMap::to_symbol(const Evoral::Parameter& param) const
 
 	if (t == GainAutomation) {
 		return "gain";
+	} else if (t == BusSendLevel) {
+		return "send";
 	} else if (t == TrimAutomation) {
                 return "trim";
 	} else if (t == PanAzimuthAutomation) {

libs/ardour/gain_control.cc
@@ -38,7 +38,7 @@ using namespace std;
 GainControl::GainControl (Session& session, const Evoral::Parameter &param, boost::shared_ptr<AutomationList> al)
 	: SlavableAutomationControl (session, param, ParameterDescriptor(param),
 	                             al ? al : boost::shared_ptr<AutomationList> (new AutomationList (param)),
-	                             param.type() == GainAutomation ? X_("gaincontrol") : X_("trimcontrol"),
+	                             (param.type() == GainAutomation || param.type() == BusSendLevel) ? X_("gaincontrol") : X_("trimcontrol"),
 	                             Controllable::GainLike)
 {
 }

libs/ardour/luabindings.cc
@@ -1865,6 +1865,7 @@ LuaBindings::common (lua_State* L)
 
 		.beginNamespace ("AutomationType")
 		.addConst ("GainAutomation", ARDOUR::AutomationType(GainAutomation))
+		.addConst ("BusSendLevel", ARDOUR::AutomationType(BusSendLevel))
 		.addConst ("PluginAutomation", ARDOUR::AutomationType(PluginAutomation))
 		.addConst ("SoloAutomation", ARDOUR::AutomationType(SoloAutomation))
 		.addConst ("SoloIsolateAutomation", ARDOUR::AutomationType(SoloIsolateAutomation))

libs/ardour/parameter_descriptor.cc
@@ -195,7 +195,7 @@ ParameterDescriptor::update_steps()
 	if (unit == ParameterDescriptor::MIDI_NOTE) {
 		step      = smallstep = 1;  // semitone
 		largestep = 12;             // octave
-	} else if (type == GainAutomation || type == TrimAutomation) {
+	} else if (type == GainAutomation || type == TrimAutomation || type == BusSendLevel) {
 		/* dB_coeff_step gives a step normalized for [0, max_gain].  This is
 		   like "slider position", so we convert from "slider position" to gain
 		   to have the correct unit here. */

commit 720664d9d37bc272014d7d435468d01180eb666b

Fix loading send gain automation
This cleans up various issues that have been accumulated over the past
and fixes bugs introduced in d4e023e1 and e31f5d99.

Previously GainControl as saved as part of the Amp, however the
automation was saved via Send (is-a Automatable).

In d4e023e1cb, the GainControl was changed a "BusSendLevel"
parameter, but AutomationList was not updated. This prevented
loading existing automation (control parameter was not found).

libs/ardour/send.cc
@@ -30,6 +30,7 @@
 #include "ardour/buffer_set.h"
 #include "ardour/debug.h"
 #include "ardour/delayline.h"
+#include "ardour/event_type_map.h"
 #include "ardour/gain_control.h"
 #include "ardour/io.h"
 #include "ardour/meter.h"
@@ -100,7 +101,7 @@ Send::Send (Session& s, boost::shared_ptr<Pannable> p, boost::shared_ptr<MuteMas
 
 	//boost_debug_shared_ptr_mark_interesting (this, "send");
 
-	boost::shared_ptr<AutomationList> gl (new AutomationList (Evoral::Parameter (GainAutomation)));
+	boost::shared_ptr<AutomationList> gl (new AutomationList (Evoral::Parameter (BusSendLevel)));
 	_gain_control = boost::shared_ptr<GainControl> (new GainControl (_session, Evoral::Parameter(BusSendLevel), gl));
 	_gain_control->set_flags (Controllable::Flag ((int)_gain_control->flags() | Controllable::InlineControl));
 	add_control (_gain_control);
@@ -279,7 +280,7 @@ Send::state ()
 
 	node.set_property ("selfdestruct", _remove_on_disconnect);
 
-	node.add_child_nocopy (_amp->get_state ());
+	node.add_child_nocopy (_gain_control->get_state());
 
 	return node;
 }
@@ -291,11 +292,51 @@ Send::set_state (const XMLNode& node, int version)
 		return set_state_2X (node, version);
 	}
 
-	XMLProperty const * prop;
+	XMLNode* gain_node;
+	if ((gain_node = node.child (Controllable::xml_node_name.c_str ())) != 0) {
+		_gain_control->set_state (*gain_node, version);
+	}
+
+	if (version <= 6000) {
+		/* convert GainAutomation to BusSendLevel
+		 *
+		 * (early Ardour 6.0-pre0 and Mixbus 6.0 used "BusSendLevel"
+		 *  control with GainAutomation, so we check version <= 6000.
+		 *  New A6 sessions do not have a GainAutomation parameter,
+		 *  so this is safe.)
+		 *
+		 * Normally this is restored via
+		 * Delivery::set_state() -> Processor::set_state()
+		 * -> Automatable::set_automation_xml_state()
+		 */
+		XMLNodeList nlist;
+		XMLNode* automation = node.child ("Automation");
+		if (automation) {
+			nlist = automation->children();
+		}
+		for (XMLNodeIterator i = nlist.begin(); i != nlist.end(); ++i) {
+			if ((*i)->name() != "AutomationList") {
+				continue;
+			}
+			XMLProperty const* id_prop = (*i)->property("automation-id");
+			if (!id_prop) {
+				continue;
+			}
+			Evoral::Parameter param = EventTypeMap::instance().from_symbol (id_prop->value());
+			if (param.type() != GainAutomation) {
+				continue;
+			}
+			XMLNode xn (**i);
+			xn.set_property ("automation-id", EventTypeMap::instance().to_symbol(Evoral::Parameter (BusSendLevel)));
+			_gain_control->alist()->set_state (xn, version);
+			break;
+		}
+	}
 
 	Delivery::set_state (node, version);
 
 	if (node.property ("ignore-bitslot") == 0) {
+		XMLProperty const* prop;
 
 		/* don't try to reset bitslot if there is a node for it already: this can cause
 		   issues with the session's accounting of send ID's
@@ -327,13 +368,6 @@ Send::set_state (const XMLNode& node, int version)
 	}
 
 	node.get_property (X_("selfdestruct"), _remove_on_disconnect);
-
-	XMLNodeList nlist = node.children();
-	for (XMLNodeIterator i = nlist.begin(); i != nlist.end(); ++i) {
-		if ((*i)->name() == X_("Processor")) {
-			_amp->set_state (**i, version);
-		}
-	}
 
 	_send_delay->set_name ("Send-" + name());
 	_thru_delay->set_name ("Thru-" + name());