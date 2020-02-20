587fc5005930cb6b18e37444883a80ffd3d02b2b
Robin Gareus <robin@gareus.org>
2020-02-20 22:52:26 +0100

    Fix Inline-control visibility state
    This fixes an issue when copy/pasting plugins or aux-sends from
    one track to another. After copying the processor, the state
    is copied, however the Controllable state did save
    the InlineControl flag, so this as lost.
    
    (amend 93180ceea)

--- libs/pbd/enums.cc
+++ libs/pbd/enums.cc
@@ -40,6 +40,7 @@ setup_libpbd_enums ()
 	REGISTER_CLASS_ENUM (Controllable, GainLike);
 	REGISTER_CLASS_ENUM (Controllable, RealTime);
 	REGISTER_CLASS_ENUM (Controllable, NotAutomatable);
+	REGISTER_CLASS_ENUM (Controllable, InlineControl);
 	REGISTER_BITS (controllable_flags);
 
 	REGISTER_CLASS_ENUM (Controllable, InverseGroup);
