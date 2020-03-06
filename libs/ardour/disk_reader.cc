Ardour/ardour

commit 128a45954cfbfeaa0fd70c626a5d0c3c86e2ff7a

Do not de-click during freewheel export

libs/ardour/disk_reader.cc
@@ -283,7 +283,7 @@ DiskReader::run (BufferSet& bufs, samplepos_t start_sample, samplepos_t end_samp
 	const bool declicked_out = (_declick_amp.gain() == target_gain) && target_gain == 0.0;
 	const bool declick_out = (_declick_amp.gain() != target_gain) && target_gain == 0.0;
 
-	if (!_session.cfg ()->get_use_transport_fades ()) {
+	if (!_session.cfg ()->get_use_transport_fades () || (_session.exporting () && ! _session.realtime_export ())) {
 		_declick_amp.set_gain (target_gain);
 	}
 

libs/ardour/ardour/session.h
@@ -1126,6 +1126,10 @@ public:
 		return _exporting;
 	}
 
+	bool realtime_export() const {
+		return _realtime_export;
+	}
+
 	bool bounce_processing() const {
 		return _bounce_processing_active;
 	}

commit d142619cf4990822db6414cb0722d6df1674c682

Fix MIDI export and post-export transport issues
Bug was introduced in 128a45954cf, declick-amp gain was
overridden, but declick not cleared. For some reason this did
not affect audio-only exports nor all session exports.

libs/ardour/disk_reader.cc
@@ -499,7 +499,10 @@ DiskReader::run (BufferSet& bufs, samplepos_t start_sample, samplepos_t end_samp
 bool
 DiskReader::declick_in_progress () const
 {
-	return _session.cfg()->get_use_transport_fades() && (_declick_amp.gain() != 0); // declick-out
+	if (!_session.cfg()->get_use_transport_fades () || (_session.exporting () && ! _session.realtime_export ())) {
+		return false;
+	}
+	return (_declick_amp.gain() != 0); // declick-out
 }
 
 bool