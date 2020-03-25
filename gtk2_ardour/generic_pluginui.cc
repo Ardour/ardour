Ardour/ardour

commit a44fecb740d3b3086e29ce0dd9f292bf21d12d02

Hide hidden Controls in GUI

gtk2_ardour/generic_pluginui.cc
@@ -359,6 +359,10 @@ GenericPluginUI::build ()
 				= boost::dynamic_pointer_cast<ARDOUR::AutomationControl>(
 					insert->control(param));
 
+			if (c->flags () & Controllable::HiddenControl) {
+				continue;
+			}
+
 			ParameterDescriptor desc;
 			plugin->get_parameter_descriptor(i, desc);
 			if ((cui = build_control_ui (param, desc, c, value, plugin->parameter_is_input(i), hint.knob)) == 0) {

gtk2_ardour/route_time_axis.cc
@@ -2067,6 +2067,11 @@ RouteTimeAxisView::add_existing_processor_automation_curves (boost::weak_ptr<Pro
 		Evoral::Parameter param (*i);
 		boost::shared_ptr<AutomationLine> al;
 
+		boost::shared_ptr<AutomationControl> control = boost::dynamic_pointer_cast<AutomationControl>(processor->control(*i, false));
+		if (!control || control->flags () & Controllable::HiddenControl) {
+			continue;
+		}
+
 		if ((al = find_processor_automation_curve (processor, param)) != 0) {
 			al->queue_reset ();
 		} else {

commit 89904468b317c3198af376de3048abbe4b3526e2

Fix generic UI crash when plugin has ctrl outputs -- #7937
build_control_ui() special cases `AutomationControl mcontrol = NULL`
to create read-only output display. So Iterating over
Evoral::Control that are not AutomationControl must not crash.

This fixes a bug introduced in a44fecb740d3.
"Edit with generic controls" caused crashes for plugins with ctrl outs.

gtk2_ardour/generic_pluginui.cc
@@ -359,7 +359,7 @@ GenericPluginUI::build ()
 				= boost::dynamic_pointer_cast<ARDOUR::AutomationControl>(
 					insert->control(param));
 
-			if (c->flags () & Controllable::HiddenControl) {
+			if (c && c->flags () & Controllable::HiddenControl) {
 				continue;
 			}
 