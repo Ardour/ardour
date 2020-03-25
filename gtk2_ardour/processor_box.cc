4a24cc5083fd0b693ef09d29b8e42fb4d5aded3e
Robin Gareus <robin@gareus.org>
2020-03-25 21:01:32 +0100

    Fix Aux-Send names on copy/paste
    Sends do not have any ports, so a unique name is not required.
    Since 82541b33a4a custom aux names are kept when setting state.
    
    Previously this "worked" because set_state() change the name
    of the new aux-send to the name of the target-bus.

--- gtk2_ardour/processor_box.cc
+++ gtk2_ardour/processor_box.cc
@@ -3427,8 +3427,6 @@ ProcessorBox::paste_processor_state (const XMLNodeList& nlist, boost::shared_ptr
 				InternalSend* s = new InternalSend (*_session, sendpan, _route->mute_master(),
 						_route, boost::shared_ptr<Route>(), Delivery::Aux);
 
-				IOProcessor::prepare_for_reset (n, s->name());
-
 				if (s->set_state (n, Stateful::loading_state_version)) {
 					delete s;
 					return;
