c5332ddd0092c3a73315923a90c41024c0ad7758
Robin Gareus <robin@gareus.org>
2020-04-09 20:25:25 +0200

    Another attempt at fixing multi-range export
    See also
      * 4f3a95a1da9
      * cfd95340b18
      * 61e7f3176bf

--- libs/ardour/session_transport.cc
+++ libs/ardour/session_transport.cc
@@ -1569,12 +1569,10 @@ Session::non_realtime_stop (bool abort, int on_entry, bool& finished)
 	TransportStateChange (); /* EMIT SIGNAL */
 	AutomationWatch::instance().transport_stop_automation_watches (_transport_sample);
 
-#if 0
 	if (_exporting && !_realtime_export) {
 		ptw = PostTransportWork (ptw & ~(PostTransportStop));
 		set_post_transport_work (ptw);
 	}
-#endif
 }
 
 void
