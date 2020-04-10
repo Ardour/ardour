9b19fad18070e0c4395e8df89542f786d8dad5de
Luciano Iam <lucianito@gmail.com>
2020-04-10 08:43:06 +0200

    WebSockets: always return -1 from send_index_body() unless there is a write error

--- libs/surfaces/websockets/server.cc
+++ libs/surfaces/websockets/server.cc
@@ -405,7 +405,7 @@ WebsocketsServer::send_index_body (Client wsi)
 	/* lws_http_transaction_completed() returns 1 if the HTTP connection must close now
 	   Returns 0 and resets connection to wait for new HTTP header / transaction if possible */
 	if (lws_http_transaction_completed (wsi) != 1) {
-		return 1;
+		return -1;
 	}
 
 	return -1;	// end connection
