Ardour/ardour

commit 7434478a3567a0757791e1d231f9f60dbfdd8c10

Update Waveform display when channel-count changes

gtk2_ardour/route_time_axis.cc
@@ -2404,6 +2404,15 @@ RouteTimeAxisView::io_changed (IOChange /*change*/, void */*src*/)
 {
 	reset_meter ();
 	if (_route && !no_redraw) {
+		AudioStreamView* asv = dynamic_cast<AudioStreamView*>(_view);
+		if (asv) {
+			/* this needs to happen with the disk-reader's I/O changed,
+			 * however there is no dedicated signal for this, and in almost
+			 * call cases it follows I/O changes.
+			 * This is similar to ARDOUR_UI::cleanup_peakfiles, and
+			 * re-loads wave-form displays. */
+			asv->reload_waves ();
+		}
 		request_redraw ();
 	}
 }

gtk2_ardour/audio_streamview.cc
@@ -178,6 +178,20 @@ AudioStreamView::redisplay_track ()
 }
 
 void
+AudioStreamView::reload_waves ()
+{
+	list<RegionView *>::iterator i;
+	for (i = region_views.begin(); i != region_views.end(); ++i) {
+		AudioRegionView* arv = dynamic_cast<AudioRegionView*> (*i);
+		if (!arv) {
+			continue;
+		}
+		arv->delete_waves();
+		arv->create_waves();
+	}
+}
+
+void
 AudioStreamView::setup_rec_box ()
 {
 	//cerr << _trackview.name() << " streamview SRB region_views.size() = " << region_views.size() << endl;

gtk2_ardour/audio_streamview.h
@@ -67,6 +67,8 @@ public:
 	RegionView* create_region_view (boost::shared_ptr<ARDOUR::Region>, bool, bool);
 	void set_selected_points (PointSelection&);
 
+	void reload_waves ();
+
 private:
 	void setup_rec_box ();
 	void rec_peak_range_ready (samplepos_t start, ARDOUR::samplecnt_t cnt, boost::weak_ptr<ARDOUR::Source> src);

commit 134b2162ba4327508e4eea4b3e3d16d5c2764c30

Fix waveform update when channel-count changes
7434478a35 introduced a race, IOchange triggers updating the
GUI and re-configuring processors. The latter needs to complete
first, otherwise Track::n_channels() returns the previous
channel count.

gtk2_ardour/route_time_axis.cc
@@ -321,6 +321,8 @@ RouteTimeAxisView::set_route (boost::shared_ptr<Route> rt)
 		track()->FreezeChange.connect (*this, invalidator (*this), boost::bind (&RouteTimeAxisView::map_frozen, this), gui_context());
 		track()->SpeedChanged.connect (*this, invalidator (*this), boost::bind (&RouteTimeAxisView::speed_changed, this), gui_context());
 
+		track()->ChanCountChanged.connect (*this, invalidator (*this), boost::bind (&RouteTimeAxisView::chan_count_changed, this), gui_context());
+
 		/* pick up the correct freeze state */
 		map_frozen ();
 
@@ -2404,15 +2406,19 @@ RouteTimeAxisView::io_changed (IOChange /*change*/, void */*src*/)
 {
 	reset_meter ();
 	if (_route && !no_redraw) {
-		AudioStreamView* asv = dynamic_cast<AudioStreamView*>(_view);
-		if (asv) {
-			/* this needs to happen with the disk-reader's I/O changed,
-			 * however there is no dedicated signal for this, and in almost
-			 * call cases it follows I/O changes.
-			 * This is similar to ARDOUR_UI::cleanup_peakfiles, and
-			 * re-loads wave-form displays. */
-			asv->reload_waves ();
-		}
+		request_redraw ();
+	}
+}
+
+void
+RouteTimeAxisView::chan_count_changed ()
+{
+	AudioStreamView* asv = dynamic_cast<AudioStreamView*>(_view);
+	if (_route && !no_redraw && asv) {
+		/* This is similar to ARDOUR_UI::cleanup_peakfiles, and
+		 * re-loads wave-form displays. */
+		asv->reload_waves ();
+		reset_meter ();
 		request_redraw ();
 	}
 }

gtk2_ardour/route_time_axis.h
@@ -145,6 +145,7 @@ public:
 	void reset_meter ();
 	void clear_meter ();
 	void io_changed (ARDOUR::IOChange, void *);
+	void chan_count_changed ();
 	void meter_changed ();
 	void effective_gain_display () { gm.effective_gain_display(); }
 