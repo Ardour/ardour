d99dd1e3ecb530388a44efb5ce80d82d9196e7e4
Robin Gareus <robin@gareus.org>
2020-07-07 05:26:06 +0200

    Fix auto-play when locating via drag/click in the editor

--- gtk2_ardour/editor_drag.cc
+++ gtk2_ardour/editor_drag.cc
@@ -4131,7 +4131,7 @@ CursorDrag::finished (GdkEvent* event, bool movement_occurred)
 	Session* s = _editor->session ();
 	if (s) {
 		_editor->_pending_locate_request = true;
-		s->request_locate (_editor->playhead_cursor->current_sample (), _was_rolling ? MustRoll : MustStop);
+		s->request_locate (_editor->playhead_cursor->current_sample (), _was_rolling ? MustRoll : RollIfAppropriate);
 		s->request_resume_timecode_transmission ();
 	}
 }
