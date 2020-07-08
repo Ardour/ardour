c12cabd418020db7fbbe1daed50ba5d88ea675bd
David Robillard <d@drobilla.net>
2020-07-08 20:53:37 +0200

    Remove support for the long-deprecated LV2 uri-map extension
    This has been supplanted by the urid extension.

--- libs/ardour/uri_map.cc
+++ libs/ardour/uri_map.cc
@@ -85,28 +85,6 @@ URIMap::instance()
 	return *URIMap::uri_map;
 }
 
-static uint32_t
-c_uri_map_uri_to_id(LV2_URI_Map_Callback_Data callback_data,
-                    const char*               map,
-                    const char*               uri)
-{
-	URIMap* const me = (URIMap*)callback_data;
-	const uint32_t id = me->uri_to_id(uri);
-
-	/* The event context with the uri-map extension guarantees a value in the
-	   range of uint16_t.  Ardour used to map to a separate range to achieve
-	   this, but unfortunately some plugins are broken and use the incorrect
-	   context.  To compensate, we simply use the same context for everything
-	   and hope that anything in the event context gets mapped before
-	   UINT16_MAX is reached (which will be fine unless something seriously
-	   weird is going on).  If this fails there is nothing we can do, die.
-	*/
-	assert(!map || strcmp(map, "http://lv2plug.in/ns/ext/event")
-	       || id < UINT16_MAX);
-
-	return id;
-}
-
 static LV2_URID
 c_urid_map(LV2_URID_Map_Handle handle,
            const char*         uri)
@@ -125,11 +103,6 @@ c_urid_unmap(LV2_URID_Unmap_Handle handle,
 
 URIMap::URIMap()
 {
-	_uri_map_feature_data.uri_to_id     = c_uri_map_uri_to_id;
-	_uri_map_feature_data.callback_data = this;
-	_uri_map_feature.URI                = LV2_URI_MAP_URI;
-	_uri_map_feature.data               = &_uri_map_feature_data;
-
 	_urid_map_feature_data.map    = c_urid_map;
 	_urid_map_feature_data.handle = this;
 	_urid_map_feature.URI         = LV2_URID_MAP_URI;
