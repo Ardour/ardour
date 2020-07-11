8c093e9080b21ad9b6cd4e5d17a8c8956d49c391
Iurie Nistor <iur.nistor@gmail.com>
2020-07-11 11:42:58 +0300

    double click to expand / contract the track

--- gtk2_ardour/time_axis_view.cc
+++ gtk2_ardour/time_axis_view.cc
@@ -426,6 +426,14 @@ TimeAxisView::controls_ebox_button_press (GdkEventButton* event)
 
 	}
 
+	if (event->button == 1 && event->type == GDK_2BUTTON_PRESS) {
+		if (_effective_height < preset_height (HeightLargest)) {
+			set_height_enum (HeightLargest);
+		} else {
+			set_height_enum (HeightNormal);
+		}
+	}
+
 	_ebox_release_can_act = true;
 
 	if (maybe_set_cursor (event->y) > 0) {
