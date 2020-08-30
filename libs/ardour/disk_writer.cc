b326b8e4628ddb71e5a0479e0f8e8248f237e66c
Robin Gareus <robin@gareus.org>
2020-08-31 01:19:41 +0200

    Fix MIDI Capture alignment
    When recording audio, simply not writing to the ringbuffer
    offsets the recording accordingly.
    When recording MIDI, absolute timestamps are used, so the recording
    has to be offset by the accumulated difference.
    
    Previously this went unnoticed because tests using the Dummy
    backend the accumulated offset never exceeded 1 cycle.

--- libs/ardour/disk_writer.cc
+++ libs/ardour/disk_writer.cc
@@ -484,6 +484,8 @@ DiskWriter::run (BufferSet& bufs, samplepos_t start_sample, samplepos_t end_samp
 		 */
 		if (rec_nframes) {
 			_accumulated_capture_offset += rec_offset;
+		} else {
+			_accumulated_capture_offset += nframes;
 		}
 
 	}
