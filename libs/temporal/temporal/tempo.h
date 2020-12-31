6a9330b2b0cbd8e3cfb4f7d4a149c3df52d3fd93
Paul Davis <paul@linuxaudiosystems.com>
2020-12-31 12:20:43 -0700

    libtemporal: fix TempoMap::fetch_writable() so that it actually does what its name describes

--- libs/temporal/temporal/tempo.h
+++ libs/temporal/temporal/tempo.h
@@ -644,7 +644,7 @@ class LIBTEMPORAL_API TempoMap : public PBD::StatefulDestructible
 	static SharedPtr fetch() { update_thread_tempo_map(); return use(); }
 
 	static SharedPtr write_copy() { return _map_mgr.write_copy(); }
-	static void fetch_writable() { _tempo_map_p = fetch(); }
+	static void fetch_writable() { _tempo_map_p = write_copy(); }
 	static void update (SharedPtr m) { _map_mgr.update (m); _tempo_map_p = _map_mgr.reader(); }
 
 	/* and now on with the rest of the show ... */
