4f87d59576fa6bccb4d9b1fabdb5530b4a829ec5
Robin Gareus <robin@gareus.org>
2021-02-01 03:17:30 +0100

    Reduce InternalReturn inheritance
    InternalReturn makes no use of Return or IOProcessor methods
    or members. It is a truly independent Processor.

--- libs/ardour/ardour/internal_return.h
+++ libs/ardour/ardour/internal_return.h
@@ -2,7 +2,7 @@
  * Copyright (C) 2009-2011 David Robillard <d@drobilla.net>
  * Copyright (C) 2009-2012 Carl Hetherington <carl@carlh.net>
  * Copyright (C) 2009-2017 Paul Davis <paul@linuxaudiosystems.com>
- * Copyright (C) 2013-2017 Robin Gareus <robin@gareus.org>
+ * Copyright (C) 2013-2021 Robin Gareus <robin@gareus.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,19 +22,19 @@
 #ifndef __ardour_internal_return_h__
 #define __ardour_internal_return_h__
 
+#include <list>
 
-#include "ardour/ardour.h"
-#include "ardour/return.h"
 #include "ardour/buffer_set.h"
+#include "ardour/processor.h"
 
 namespace ARDOUR {
 
 class InternalSend;
 
-class LIBARDOUR_API InternalReturn : public Return
+class LIBARDOUR_API InternalReturn : public Processor
 {
 public:
-	InternalReturn (Session&);
+	InternalReturn (Session&, std::string const& name = "Return");
 
 	void run (BufferSet& bufs, samplepos_t start_sample, samplepos_t end_sample, double speed, pframes_t nframes, bool);
 	bool configure_io (ChanCount, ChanCount);
