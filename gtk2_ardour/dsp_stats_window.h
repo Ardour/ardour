51df20796baab807e03fd5946fc541cd4ce56850
Paul Davis <paul@linuxaudiosystems.com>
2021-06-11 21:02:06 -0600

    skeleton for dsp stats window

---
+++ gtk2_ardour/dsp_stats_window.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2021 Paul Davis <paul@linuxaudiosystems.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __ardour_dspstats_window_h__
+#define __ardour_dspstats_window_h__
+
+
+#include "ardour_window.h"
+
+class PluginLoadStatsGui;
+
+class DspStatisticsWindow : public ArdourWindow
+{
+public:
+	DspStatisticsWindow ();
+	~DspStatisticsWindow ();
+
+	void set_session (ARDOUR::Session*);
+
+protected:
+
+	void on_show ();
+	void on_hide ();
+
+private:
+};
+
+#endif
+
