40c9b1482a4ccd2908106a531a1f939323f44c45
Paul Davis <paul@linuxaudiosystems.com>
2021-07-04 09:42:36 -0600

    region move drag now requires named argument

--- gtk2_ardour/editor_drag.cc
+++ gtk2_ardour/editor_drag.cc
@@ -1701,7 +1701,8 @@ RegionMoveDrag::clear_draggingview_list ()
 	_views.clear();
 }
 
-RegionMoveDrag::finished_copy (bool const changed_position, bool const /*changed_tracks*/, timepos_t const & last_position, int32_t const ev_state)
+void
+RegionMoveDrag::finished_copy (bool const changed_position, bool const changed_tracks, timepos_t const & last_position, int32_t const ev_state)
 {
 	RegionSelection new_views;
 	PlaylistSet modified_playlists;
