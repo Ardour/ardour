f2ea9461f23c553c31df3b65a8618b3be6cf59d8
Robin Gareus <robin@gareus.org>
2021-09-14 21:18:24 +0200

    Fix API, Route::apply_latency_compensation() is not virtual

--- libs/ardour/ardour/route.h
+++ libs/ardour/ardour/route.h
@@ -352,7 +352,7 @@ public:
 	bool remove_sidechain (boost::shared_ptr<Processor> proc) { return add_remove_sidechain (proc, false); }
 
 	samplecnt_t  update_signal_latency (bool apply_to_delayline = false, bool* delayline_update_needed = NULL);
-	virtual void apply_latency_compensation ();
+	void apply_latency_compensation ();
 
 	samplecnt_t  set_private_port_latencies (bool playback) const;
 	void         set_public_port_latencies (samplecnt_t, bool playback, bool with_latcomp) const;
