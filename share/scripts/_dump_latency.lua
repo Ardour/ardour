bc4eb334d980d132627b1d580247937196f58869
Robin Gareus <robin@gareus.org>
2021-09-14 21:39:41 +0200

    Align printed port/latencies in debug script output

--- share/scripts/_dump_latency.lua
+++ share/scripts/_dump_latency.lua
@@ -17,7 +17,7 @@ function factory () return function ()
 	print (" -- Routes --")
 	for t in Session:get_routes ():iter () do
 		print (string.format ("%-30s  signal-latency: %4d align: %4d play: %4d || in: %4d out: %4d",
-		t:name(),
+		string.sub (t:name(), 0, 30),
 		t:signal_latency (), t:playback_latency (false), t:playback_latency (true),
 		t:input():latency(), t:output():latency()))
 		local i = 0
@@ -55,7 +55,7 @@ function factory () return function ()
 			local bpl = p:public_latency_range (true)
 			local bcl = p:public_latency_range (false)
 			print (string.format ("%-30s  play: (%4d, %4d) (%4d, %4d) (%4d, %4d)  capt: (%4d, %4d) (%4d, %4d) (%4d, %4d)",
-			p:name(),
+			string.sub (p:name(), 0, 30),
 			lp[1].min, lp[1].max, ppl.min, ppl.max, bpl.min, bpl.max,
 			lc[1].min, lc[1].max, pcl.min, pcl.max, bcl.min, bcl.max))
 		end
@@ -69,7 +69,7 @@ function factory () return function ()
 			local bpl = p:public_latency_range (true)
 			local bcl = p:public_latency_range (false)
 			print (string.format ("%-30s  play: (%4d, %4d) (%4d, %4d) (%4d, %4d)  capt: (%4d, %4d) (%4d, %4d) (%4d, %4d)",
-			p:name(),
+			string.sub (p:name(), 0, 30),
 			lp[1].min, lp[1].max, ppl.min, ppl.max, bpl.min, bpl.max,
 			lc[1].min, lc[1].max, pcl.min, pcl.max, bcl.min, bcl.max))
 		end
