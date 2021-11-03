62c80e143098905410a0635814e4d717d6c379b3
Paul Davis <paul@linuxaudiosystems.com>
2021-11-03 15:18:59 -0600

    Add the definition of Session::AfterConnect

--- libs/ardour/session.cc
+++ libs/ardour/session.cc
@@ -167,6 +167,7 @@ PBD::Signal0<void> Session::Quit;
 PBD::Signal0<void> Session::FeedbackDetected;
 PBD::Signal0<void> Session::SuccessfulGraphSort;
 PBD::Signal2<void,std::string,std::string> Session::VersionMismatch;
+PBD::Signal0<void> Session::AfterConnect;
 
 const samplecnt_t Session::bounce_chunk_size = 8192;
 static void clean_up_session_event (SessionEvent* ev) { delete ev; }
