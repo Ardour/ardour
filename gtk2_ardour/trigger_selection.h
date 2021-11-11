85fc3d5629c41538a6f1ae479ecc3141e9652503
Paul Davis <paul@linuxaudiosystems.com>
2021-11-10 21:16:27 -0700

    add missing file

---
+++ gtk2_ardour/trigger_selection.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2007-2017 Paul Davis <paul@linuxaudiosystems.com>
+ * Copyright (C) 2015-2016 Robin Gareus <robin@gareus.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __ardour_gtk_trigger_selection_h__
+#define __ardour_gtk_trigger_selection_h__
+
+#include <list>
+
+#include "triggerbox_ui.h"
+
+class TriggerSelection : public std::list<TriggerEntry*>
+{
+   public:
+};
+
+#endif /* __ardour_gtk_trigger_selection_h__ */
