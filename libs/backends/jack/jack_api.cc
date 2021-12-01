4f59dd8e3de921ad47ea73c69e74b2481fd43e7d
Houston4444 <picotmathieu@gmail.com>
2021-12-01 11:36:04 +0100

    replace trailing spaces with tabulations

--- libs/backends/jack/jack_api.cc
+++ libs/backends/jack/jack_api.cc
@@ -58,7 +58,7 @@ instantiate (const std::string& arg1, const std::string& arg2)
 {
 	try {
 		jack_connection.reset (new JackConnection (arg1, arg2));
-        backend.reset ();
+		backend.reset ();
 	} catch (...) {
 		return -1;
 	}
