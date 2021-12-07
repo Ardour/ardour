992b5179338db1f45c675f3d805e5b07404124b1
Paul Davis <paul@linuxaudiosystems.com>
2021-12-06 17:03:32 -0700

    clip editor: add bg color

--- gtk2_ardour/audio_clip_editor.cc
+++ gtk2_ardour/audio_clip_editor.cc
@@ -31,7 +31,7 @@ using namespace ArdourCanvas;
 
 AudioClipEditor::AudioClipEditor ()
 {
-
+	set_background_color (UIConfiguration::instance().color (X_("theme:bg")));
 }
 
 AudioClipEditor::~AudioClipEditor ()
