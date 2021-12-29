10d367fdae64062efcee01e59493627b0adbcaf3
Florian Walpen <dev@submerge.ch>
2021-12-29 23:07:58 +0100

    Fix plugin manager build with VST3 disabled.
    ARDOUR::SystemExec is also used for scanning VST2 and AU plugins, yet
    the corresponding header ardour/system_exec.h is not included when VST3
    support is disabled in the build configuration.

--- libs/ardour/plugin_manager.cc
+++ libs/ardour/plugin_manager.cc
@@ -87,6 +87,7 @@
 #include "ardour/search_paths.h"
 
 #if (defined WINDOWS_VST_SUPPORT || defined MACVST_SUPPORT || defined LXVST_SUPPORT)
+#include "ardour/system_exec.h"
 #include "ardour/vst2_scan.h"
 #endif
 
@@ -113,6 +114,7 @@
 
 #include "ardour/audio_unit.h"
 #include "ardour/auv2_scan.h"
+#include "ardour/system_exec.h"
 #include <Carbon/Carbon.h>
 #endif
 
