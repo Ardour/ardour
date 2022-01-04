8a49d274fbf914493b895e0411569b25e35aeff6
Paul Davis <paul@linuxaudiosystems.com>
2022-01-04 15:23:43 -0700

    fix luabindings for Editor.mouse_add_new_marker(), since API changed

--- share/scripts/add_cdmarker.lua
+++ share/scripts/add_cdmarker.lua
@@ -2,6 +2,6 @@ ardour { ["type"] = "EditorAction", name = "Add CD marker" }
 function 
 factory () 
 	return function () 
-		Editor:mouse_add_new_marker (Session:transport_sample(), true)
+		Editor:mouse_add_new_marker (Session:transport_sample(), Location.IsCDMarker)
 	end 
 end
