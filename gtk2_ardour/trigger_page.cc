2147ec6da7b77557778aea66f0e26c34bf5cd723
Ben Loftis <ben@harrisonconsoles.com>
2022-01-18 10:14:08 -0600

    trigger_ui: hide midi clip properties (pgm, ccs) until implemented

--- gtk2_ardour/trigger_page.cc
+++ gtk2_ardour/trigger_page.cc
@@ -144,9 +144,11 @@ TriggerPage::TriggerPage ()
 	table->attach (_audio_trig_box, col, col + 1, 0, 1, Gtk::FILL, Gtk::SHRINK);
 	++col;
 
+#if MIDI_PROPERTIES_BOX_IMPLEMENTED
 	col = 2;
 	table->attach (_midi_trig_box, col, col + 1, 0, 1, Gtk::FILL, Gtk::SHRINK);
 	++col;
+#endif
 
 	_parameter_box.pack_start (*table);
 
