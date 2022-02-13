c5d7883538fe6192ea54cff848b1a05a360cfc3b
Paul Davis <paul@linuxaudiosystems.com>
2022-02-12 20:47:32 -0700

    temporal: add skeleton for Timeline (timepos_t/timecnt_t) tests

---
+++ libs/temporal/test/TimelineTest.cc
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+#include "temporal/bbt_time.h"
+
+#include "TimelineTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TimelineTest);
+
+using namespace Temporal;
+
+void
+TimelineTest::createTest()
+{
+}
+
+void
+TimelineTest::addTest()
+{
+}
+
+void
+TimelineTest::subtractTest()
+{
+}
+
+void
+TimelineTest::multiplyTest()
+{
+}
+
+void
+TimelineTest::roundTest()
+{
+}
+
+void
+TimelineTest::convertTest()
+{
+}
+
