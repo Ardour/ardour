0762ef4f35506d46ea9af54a2fed1dd82680e228
Paul Davis <paul@linuxaudiosystems.com>
2022-02-18 12:50:56 -0700

    triggerbox: remove Playout enum from enums registation

--- libs/ardour/enums.cc
+++ libs/ardour/enums.cc
@@ -853,7 +853,6 @@ setup_enum_writer ()
 	REGISTER_CLASS_ENUM (Trigger, Stopped);
 	REGISTER_CLASS_ENUM (Trigger, WaitingToStart);
 	REGISTER_CLASS_ENUM (Trigger, Running);
-	REGISTER_CLASS_ENUM (Trigger, Playout);
 	REGISTER_CLASS_ENUM (Trigger, WaitingForRetrigger);
 	REGISTER_CLASS_ENUM (Trigger, WaitingToStop);
 	REGISTER_CLASS_ENUM (Trigger, Stopping);
