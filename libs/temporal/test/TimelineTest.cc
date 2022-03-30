1d10408bd176bdd8d6a7992283f68f43d13fd539
Paul Davis <paul@linuxaudiosystems.com>
2022-03-30 15:13:46 -0600

    temporal: add some relatively useless code to one of the timeline test functions

--- libs/temporal/test/TimelineTest.cc
+++ libs/temporal/test/TimelineTest.cc
@@ -1,6 +1,7 @@
+#include <iostream>
 #include <stdlib.h>
 
-#include "temporal/bbt_time.h"
+#include "temporal/timeline.h"
 
 #include "TimelineTest.h"
 
@@ -11,6 +12,30 @@ using namespace Temporal;
 void
 TimelineTest::createTest()
 {
+	using namespace Temporal;
+	using namespace std;
+
+	srandom (time (0));
+
+	vector<timepos_t> times;
+
+	for (int n = 0; n < 32767; ++n) {
+		times.push_back (timepos_t (random() % 32120321));
+	}
+
+	uint32_t lessthan = 0;
+	uint32_t greaterthan = 0;
+
+	for (vector<timepos_t>::iterator t = times.begin(); t != times.end(); ++t) {
+		if ((*t) + timepos_t (random() % 20207) < timepos_t (2299307)) {
+			lessthan++;
+		} else {
+			greaterthan++;
+		}
+	}
+
+	std::cerr << "LT " << lessthan << " GT " << greaterthan << std::endl;
+
 }
 
 void
