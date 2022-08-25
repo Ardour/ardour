6379c28b767e9a17a19f0de532b6380f5c76b0bd
Paul Davis <paul@linuxaudiosystems.com>
2022-08-25 15:15:45 -0600

    remove debug output

--- libs/ardour/region.cc
+++ libs/ardour/region.cc
@@ -999,7 +999,6 @@ Region::trim_to_internal (timepos_t const & pos, timecnt_t const & len)
 		if (!property_changes_suspended()) {
 			_last_length = _length;
 		}
-		std::cerr << "change len\n";
 		set_length_internal (nl);
 		what_changed.add (Properties::length);
 	}
