05595be5281c003e87f88ef19ae52c7e8a822ad5
Mads Kiilerich <mads@kiilerich.com>
2022-10-22 01:24:02 +0200

    Revert "Another attempt for ssl-cert support on RHEL/Fedora #9005"
    This reverts commit b75be7f975cb5f695789bdc4e5c10bc39cbf0a61.

--- gtk2_ardour/ardour_http.cc
+++ gtk2_ardour/ardour_http.cc
@@ -63,6 +63,7 @@ HttpGet::ca_setopt (CURL* c)
 	}
 
 	if (ca_info && strlen (ca_info) == 0) {
+		/* not hat for you */
 		curl_easy_setopt (c, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_easy_setopt (c, CURLOPT_SSL_VERIFYHOST, 0);
 	} else if (ca_info || ca_path) {
@@ -102,9 +103,9 @@ HttpGet::setup_certificate_paths ()
 		ca_info = ""; // disable cert check
 	}
 
-	if (Glib::file_test ("/etc/pki/tls/certs/ca-bundle.crt", Glib::FILE_TEST_EXISTS|Glib::FILE_TEST_IS_REGULAR)) {
+	if (Glib::file_test ("/etc/pki/tls/certs/ca-bundle.crt", Glib::FILE_TEST_EXISTS|Glib::FILE_TEST_IS_DIR)) {
 		// we're on RHEL // https://bugzilla.redhat.com/show_bug.cgi?id=1053882
-		ca_path = "/nonexistent_path"; // don't try "/etc/ssl/certs" it's a trap
+		ca_path = "/nonexistent_path"; // don't try "/etc/ssl/certs" in case it's curl's default
 	}
 	else if (Glib::file_test ("/etc/ssl/certs", Glib::FILE_TEST_EXISTS|Glib::FILE_TEST_IS_DIR)) {
 		// Debian and derivs + OpenSuSe
