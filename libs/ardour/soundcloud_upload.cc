5d1061c92b714a7e960220c5e8aeffaa5eace83f
Gonsolo <gonsolo@gmail.com>
2022-11-22 18:28:34 +0100

    Update soundcloud_upload.cc

--- libs/ardour/soundcloud_upload.cc
+++ libs/ardour/soundcloud_upload.cc
@@ -35,7 +35,6 @@ using namespace PBD;
 static size_t
 WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *data)
 {
-
 	size_t realsize = (size * nmemb);
 	struct MemoryStruct *mem = (struct MemoryStruct *)data;
 
