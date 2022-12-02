81ccb685b2a71100aea6b03a956539847b882abd
Robin Gareus <robin@gareus.org>
2022-12-02 13:06:29 +0100

    Update CD marker Lua script, use new API

--- share/scripts/add_cdmarker.lua
+++ share/scripts/add_cdmarker.lua
@@ -2,6 +2,6 @@ ardour { ["type"] = "EditorAction", name = "Add CD marker" }
 function 
 factory () 
 	return function () 
-		Editor:mouse_add_new_marker (Session:transport_sample(), Location.IsCDMarker)
+		Editor:mouse_add_new_marker (Temporal.timepos_t (Session:transport_sample()), ARDOUR.LocationFlags.IsCDMarker, 0)
 	end 
 end
