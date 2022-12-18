99cbc3c30d413ccde7456134a5d22d03a50929a9
Paul Davis <paul@linuxaudiosystems.com>
2022-12-18 16:43:11 -0700

    describe velocity parameter and implement velocity control

--- libs/ardour/automatable.cc
+++ libs/ardour/automatable.cc
@@ -209,6 +209,8 @@ Automatable::describe_parameter (Evoral::Parameter param)
 		return _("Elevation");
 	} else if (param.type() == PhaseAutomation) {
 		return _("Polarity Invert");
+	} else if (param.type() == MidiVelocityAutomation) {
+		return string_compose("Velocity [%1]", int(param.channel()) + 1);
 	} else if (param.type() == MidiCCAutomation) {
 		return string_compose("Controller %1 [%2]", param.id(), int(param.channel()) + 1);
 	} else if (param.type() == MidiPgmChangeAutomation) {
