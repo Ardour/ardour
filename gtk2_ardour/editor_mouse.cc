e32d4f7b71f88fd279775af4c3008e00d2fa51f9
Paul Davis <paul@linuxaudiosystems.com>
2022-12-19 15:03:57 -0700

    try to get BBTRulerDrag to work on all platforms

--- gtk2_ardour/editor_mouse.cc
+++ gtk2_ardour/editor_mouse.cc
@@ -838,8 +838,7 @@ Editor::button_press_handler_1 (ArdourCanvas::Item* item, GdkEvent* event, ItemT
 		if (!Keyboard::modifier_state_equals (event->button.state, Keyboard::PrimaryModifier)
 		    && !ArdourKeyboard::indicates_constraint (event->button.state)) {
 			_drags->set (new CursorDrag (this, *_playhead_cursor, false), event);
-		} else if (ArdourKeyboard::indicates_constraint (event->button.state)
-		           && Keyboard::modifier_state_contains (event->button.state, Keyboard::PrimaryModifier)) {
+		} else if (Keyboard::modifier_state_equals (event->button.state, Keyboard::PrimaryModifier|Keyboard::TertiaryModifier)) {
 			_drags->set (new TempoTwistDrag (this, item), event);
 		} else if (Keyboard::modifier_state_contains (event->button.state, Keyboard::PrimaryModifier)) {
 			_drags->set (new BBTRulerDrag (this, item), event);
