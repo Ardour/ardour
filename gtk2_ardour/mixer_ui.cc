Ardour/ardour

commit 7942897d931369d87802598053e2f0dd47e677c7

VST3: cleanup, drop references (1/2)
VST3PluginInfo holds a shared_ptr reference to
VST3PluginModule. In order to cleanly unload a plugin, all
references have to be dropped.

Unloading a plugin cleanly before exit is needed in some
cases (e.g. Waves shell otherwise crashes)

gtk2_ardour/mixer_ui.cc
@@ -1223,6 +1223,7 @@ Mixer_UI::set_session (Session* sess)
 	_group_tabs->set_session (sess);
 
 	if (!_session) {
+		favorite_plugins_model->clear ();
 		_selection.clear ();
 		return;
 	}

gtk2_ardour/ardour_ui.cc
@@ -209,6 +209,7 @@ typedef uint64_t microseconds_t;
 #include "virtual_keyboard_window.h"
 #include "add_video_dialog.h"
 #include "transcode_video_dialog.h"
+#include "plugin_selector.h"
 
 #include "pbd/i18n.h"
 
@@ -851,7 +852,11 @@ ARDOUR_UI::~ARDOUR_UI ()
 		delete main_window_visibility;
 		FastMeter::flush_pattern_cache ();
 		ArdourFader::flush_pattern_cache ();
+	} else if (mixer) {
+		/* drop references to any PluginInfoPtr */
+		delete mixer->plugin_selector ();
 	}
+
 
 #ifndef NDEBUG
 	/* Small trick to flush main-thread event pool.

commit a4d9fb4935be2789c5bf5be2b880e20d4713373c

Fix saving favorite plugin order
This broke in 7942897d931. Clearing the list triggered
favorite_plugins_deleted() for each plugin. This resulted
in an empty list being saved to disk.

gtk2_ardour/mixer_ui.cc
@@ -1251,6 +1251,7 @@ Mixer_UI::set_session (Session* sess)
 	update_scene_buttons();
 
 	if (!_session) {
+		PBD::Unwinder<bool> uw (ignore_plugin_reorder, true);
 		favorite_plugins_model->clear ();
 		_selection.clear ();
 		return;