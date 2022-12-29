fef691f0fb7fe53674cb2dd5d1d7cee6f16d7361
Marijn Kruisselbrink <mek@google.com>
2022-12-29 15:34:11 -0800

    Fix AutomationListPropertyTest.
    These tests use reference files that were generated with a particular
    value for superclock_ticks_per_second. The default for that has changed
    since the last time the reference files were updated though, causing
    tests to fail. Rather than updating the reference files for the new
    default value, this makes the test not depend on the default value by
    hardcoding the value that was used to generate the reference files.

--- libs/ardour/test/automation_list_property_test.cc
+++ libs/ardour/test/automation_list_property_test.cc
@@ -46,11 +46,17 @@ write_automation_list_xml (XMLNode* node, std::string filename)
 void
 AutomationListPropertyTest::setUp ()
 {
+	// The reference files were created with this superclock ticks per second
+	// value. Setting this here makes sure we don't need to update all the
+	// reference files whenever the default value changes.
+	saved_superclock_ticks_per_second_ = Temporal::superclock_ticks_per_second();
+	Temporal::set_superclock_ticks_per_second(56448000);
 }
 
 void
 AutomationListPropertyTest::tearDown ()
 {
+	Temporal::set_superclock_ticks_per_second(saved_superclock_ticks_per_second_);
 }
 
 void
