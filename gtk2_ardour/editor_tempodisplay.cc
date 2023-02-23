dbd779bf85aedfaf6521993d90b9118df453090d
Paul Davis <paul@linuxaudiosystems.com>
2023-02-23 12:14:39 -0700

    NOOP: remove commented debug output

--- gtk2_ardour/editor_tempodisplay.cc
+++ gtk2_ardour/editor_tempodisplay.cc
@@ -338,9 +338,6 @@ Editor::tempo_map_changed ()
 
 	TempoMap::SharedPtr current_map = TempoMap::fetch ();
 
-	// std::cerr << "\n\n MAP CHANGE @ " << get_microseconds() << std::endl;
-	// current_map->dump (std::cerr);
-
 	/* If the tempo map was changed by something other than the Editor, we
 	 * will need to reassociate all visual elements used for tempo display
 	 * with the new map.
