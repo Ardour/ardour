54fac9abacab99851f281524f78b5a17ab417908
Robin Gareus <robin@gareus.org>
2023-06-05 20:04:41 +0200

    Allow user to not be reminded about tempo-map not being pasted

--- gtk2_ardour/editor_ops.cc
+++ gtk2_ardour/editor_ops.cc
@@ -2709,11 +2709,17 @@ Editor::cut_copy_section (bool copy)
 	}
 #if 1
 	TempoMap::SharedPtr tmap (TempoMap::use());
-	if (tmap->tempos ().size () > 1 || tmap->meters ().size () > 1 || tmap->bartimes ().size () > 1) {
+	if ((tmap->tempos ().size () > 1 || tmap->meters ().size () > 1 || tmap->bartimes ().size () > 1) && UIConfiguration::instance().get_ask_cut_copy_section_tempo_map ()) {
 		ArdourMessageDialog msg (_("Cut/Copy Section does not yet include the Tempo Map\nDo you still want to proceed?"), false, MESSAGE_QUESTION, BUTTONS_YES_NO, true)  ;
 		msg.set_title (_("Cut/Copy without Tempo Map"));
+		Gtk::CheckButton cb (_("Do not show this dialog again."));
+		msg.get_vbox()->pack_start (cb);
+		cb.show ();
 		if (msg.run () != RESPONSE_YES) {
 			return;
+		}
+		if (cb.get_active ()) {
+			UIConfiguration::instance().set_ask_cut_copy_section_tempo_map (false);
 		}
 	}
 #endif
