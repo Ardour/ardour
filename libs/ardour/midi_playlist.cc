Ardour/ardour

commit e79d8b00f98700194df786288792362268a8bea7

Speed up playlist rendering of non-overlapping MIDI Regions

libs/ardour/midi_playlist.cc
@@ -335,6 +335,9 @@ MidiPlaylist::render (MidiChannelFilter* filter)
 	regs.sort (cmp);
 
 	bool all_transparent = true;
+	bool no_layers = true;
+
+	layer_t layer = regs.front()->layer ();
 
 	/* skip bottom-most region, transparency is irrelevant */
 	for (auto i = ++regs.begin(); i != regs.end(); ++i) {
@@ -342,11 +345,14 @@ MidiPlaylist::render (MidiChannelFilter* filter)
 			all_transparent = false;
 			break;
 		}
+		if ((*i)->layer () != layer) {
+			no_layers = false;
+		}
 	}
 
 	Evoral::EventList<samplepos_t> evlist;
 
-	if (all_transparent) {
+	if (all_transparent || no_layers) {
 
 		DEBUG_TRACE (DEBUG::MidiPlaylistIO, string_compose ("\t%1 regions to read\n", regs.size()));
 

commit a2975bb4b7a46aeb04e7205ba8e00590a2eea709

Fix opaque MIDI region rendering
This issue was introduced in e79d8b00f98700194df786288792362268a8bea7.

libs/ardour/midi_playlist.cc
@@ -329,10 +329,13 @@ MidiPlaylist::render (MidiChannelFilter* filter)
 	for (auto i = ++regs.begin(); i != regs.end(); ++i) {
 		if ((*i)->opaque ()) {
 			all_transparent = false;
-			break;
 		}
 		if ((*i)->layer () != layer) {
 			no_layers = false;
+		}
+		if (!all_transparent && !no_layers) {
+			/* no need to check further */
+			break;
 		}
 	}
 