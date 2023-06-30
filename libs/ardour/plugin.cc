0a15527a25f9c845a267ed421ff8b74dc88831b6
Robin Gareus <robin@gareus.org>
2023-06-30 04:35:35 +0200

    Plugin: track only events for current cycle

--- libs/ardour/plugin.cc
+++ libs/ardour/plugin.cc
@@ -410,7 +410,7 @@ int
 Plugin::connect_and_run (BufferSet& bufs,
 		samplepos_t /*start*/, samplepos_t /*end*/, double /*speed*/,
 		ChanMapping const& /*in_map*/, ChanMapping const& /*out_map*/,
-		pframes_t nframes, samplecnt_t /*offset*/)
+		pframes_t nframes, samplecnt_t offset)
 {
 	if (bufs.count().n_midi() > 0) {
 
@@ -421,7 +421,11 @@ Plugin::connect_and_run (BufferSet& bufs,
 		/* Track notes that we are sending to the plugin */
 		const MidiBuffer& b = bufs.get_midi (0);
 
-		_tracker.track (b.begin(), b.end());
+		for (auto const ev : b) {
+			if (ev.time () >= offset && ev.time () < nframes + offset) {
+				_tracker.track (ev);
+			}
+		}
 
 		if (_have_pending_stop_events) {
 			/* Transmit note-offs that are pending from the last transport stop */
