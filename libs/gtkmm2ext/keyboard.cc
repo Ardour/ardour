86a5aec276aef25b802084bf5bf0de1e38e7b864
Paul Davis <paul@linuxaudiosystems.com>
2023-07-26 16:40:34 -0600

    change "use selection" modifier to Win/Opt (Level4 modifier)

--- libs/gtkmm2ext/keyboard.cc
+++ libs/gtkmm2ext/keyboard.cc
@@ -237,7 +237,7 @@ Keyboard::format_modifier (uint32_t mod)
 
 guint Keyboard::GainFineScaleModifier      = Keyboard::PrimaryModifier;
 guint Keyboard::GainExtraFineScaleModifier = Keyboard::SecondaryModifier;
-guint Keyboard::UseSelectionModifier = Keyboard::TertiaryModifier;
+guint Keyboard::UseSelectionModifier = Keyboard::Level4Modifier;
 
 guint Keyboard::ScrollZoomVerticalModifier   = Keyboard::SecondaryModifier;
 guint Keyboard::ScrollZoomHorizontalModifier = Keyboard::PrimaryModifier;
