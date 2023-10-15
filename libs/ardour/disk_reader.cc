Ardour/ardour

commit 295dbd8e1e8465fb74d94e4eb0cfa35b09761cfc

Make RCU reader return a const pointer (omnibus commit)

libs/ardour/disk_reader.cc
@@ -219,7 +219,7 @@ DiskReader::buffer_load () const
 	 * are so low compared to audio, just use the audio value here.
 	 */
 
-	std::shared_ptr<ChannelList> c = channels.reader ();
+	std::shared_ptr<ChannelList const> c = channels.reader ();
 
 	if (c->empty ()) {
 		/* no channels, so no buffers, so completely full and ready to playback, sir! */
@@ -233,10 +233,10 @@ DiskReader::buffer_load () const
 void
 DiskReader::adjust_buffering ()
 {
-	std::shared_ptr<ChannelList> c = channels.reader ();
+	std::shared_ptr<ChannelList const> c = channels.reader ();
 
-	for (ChannelList::iterator chan = c->begin (); chan != c->end (); ++chan) {
-		(*chan)->resize (_session.butler ()->audio_playback_buffer_size ());
+	for (auto const& chan : *c) {
+		chan->resize (_session.butler ()->audio_playback_buffer_size ());
 	}
 }
 
@@ -273,13 +273,13 @@ DiskReader::use_playlist (DataType dt, std::shared_ptr<Playlist> playlist)
 void
 DiskReader::run (BufferSet& bufs, samplepos_t start_sample, samplepos_t end_sample, double speed, pframes_t nframes, bool result_required)
 {
-	uint32_t                       n;
-	std::shared_ptr<ChannelList> c = channels.reader ();
-	ChannelList::iterator          chan;
-	sampleoffset_t                 disk_samples_to_consume;
-	MonitorState                   ms = _track.monitoring_state ();
-	const bool                     midi_only = (c->empty() || !_playlists[DataType::AUDIO]);
-	bool                           no_disk_output = _no_disk_output.load () != 0;
+	uint32_t                           n;
+	std::shared_ptr<ChannelList const> c = channels.reader ();
+	ChannelList::const_iterator        chan;
+	sampleoffset_t                     disk_samples_to_consume;
+	MonitorState                       ms = _track.monitoring_state ();
+	const bool                         midi_only = (c->empty() || !_playlists[DataType::AUDIO]);
+	bool                               no_disk_output = _no_disk_output.load () != 0;
 
 	if (!check_active()) {
 		return;
@@ -358,9 +358,9 @@ DiskReader::run (BufferSet& bufs, samplepos_t start_sample, samplepos_t end_samp
 		/* no need for actual disk data, just advance read pointer */
 
 		if (!still_locating || no_disk_output) {
-			for (ChannelList::iterator chan = c->begin (); chan != c->end (); ++chan) {
-				assert ((*chan)->rbuf);
-				(*chan)->rbuf->increment_read_ptr (disk_samples_to_consume);
+			for (auto const& chan : *c) {
+				assert (chan->rbuf);
+				chan->rbuf->increment_read_ptr (disk_samples_to_consume);
 			}
 		}
 
@@ -533,7 +533,7 @@ DiskReader::declick_in_progress () const
 void
 DiskReader::configuration_changed ()
 {
-	std::shared_ptr<ChannelList> c = channels.reader ();
+	std::shared_ptr<ChannelList const> c = channels.reader ();
 	if (!c->empty ()) {
 		ReaderChannelInfo* chaninfo = dynamic_cast<ReaderChannelInfo*> (c->front ());
 		if (!chaninfo->initialized) {
@@ -553,7 +553,7 @@ DiskReader::pending_overwrite () const
 void
 DiskReader::set_pending_overwrite (OverwriteReason why)
 {
-	std::shared_ptr<ChannelList> c = channels.reader ();
+	std::shared_ptr<ChannelList const> c = channels.reader ();
 
 	/* called from audio thread, so we can use the read ptr and playback sample as we wish */
 
@@ -567,7 +567,7 @@ DiskReader::set_pending_overwrite (OverwriteReason why)
 			 * ::refill_audio() and ::overwrite_existing_audio() expect
 			 * that read-pointers and fill_level of all buffers are in sync.
 			 */
-			ChannelList::iterator chan = c->begin ();
+			ChannelList::const_iterator chan = c->begin ();
 			for (++chan; chan != c->end (); ++chan) {
 				ReaderChannelInfo* chaninfo = dynamic_cast<ReaderChannelInfo*> (*chan);
 				if (!chaninfo->initialized) {
@@ -678,7 +678,7 @@ DiskReader::overwrite_existing_audio ()
 	 * simply replace the contents of the buffer.
 	 */
 
-	std::shared_ptr<ChannelList> c = channels.reader ();
+	std::shared_ptr<ChannelList const> c = channels.reader ();
 
 	if (c->empty ()) {
 		return true;
@@ -716,10 +716,10 @@ DiskReader::overwrite_existing_audio ()
 	bool                        ret = true;
 	samplepos_t                 start;
 
-	for (ChannelList::iterator chan = c->begin (); chan != c->end (); ++chan, ++n) {
+	for (auto const& chan : *c) {
 
-		Sample*            buf = (*chan)->rbuf->buffer ();
-		ReaderChannelInfo* rci = dynamic_cast<ReaderChannelInfo*> (*chan);
+		Sample*            buf = chan->rbuf->buffer ();
+		ReaderChannelInfo* rci = dynamic_cast<ReaderChannelInfo*> (chan);
 
 		/* Note that @p start is passed by reference and will be
 		 * updated by the ::audio_read() call
@@ -744,8 +744,8 @@ DiskReader::overwrite_existing_audio ()
 		}
 
 		if (!rci->initialized) {
-			DEBUG_TRACE (DEBUG::DiskIO, string_compose ("Init ReaderChannel '%1' overwriting at: %2, avail: %3\n", name (), overwrite_sample, (*chan)->rbuf->read_space ()));
-			if ((*chan)->rbuf->read_space () > 0) {
+			DEBUG_TRACE (DEBUG::DiskIO, string_compose ("Init ReaderChannel '%1' overwriting at: %2, avail: %3\n", name (), overwrite_sample, chan->rbuf->read_space ()));
+			if (chan->rbuf->read_space () > 0) {
 				rci->initialized = true;
 			}
 		}
@@ -818,7 +818,7 @@ DiskReader::seek (samplepos_t sample, bool complete_refill)
 	const bool read_reversed = !_session.transport_will_roll_forwards ();
 	const bool read_loop     = (bool)_loop_location;
 
-	std::shared_ptr<ChannelList> c = channels.reader ();
+	std::shared_ptr<ChannelList const> c = channels.reader ();
 
 	if (c->empty ()) {
 		return 0;
@@ -853,9 +853,9 @@ DiskReader::seek (samplepos_t sample, bool complete_refill)
 		return 0;
 	}
 
-	for (ChannelList::iterator chan = c->begin (); chan != c->end (); ++chan) {
-		(*chan)->rbuf->reset ();
-		assert ((*chan)->rbuf->reserved_size () == 0);
+	for (auto const& chan : *c) {
+		chan->rbuf->reset ();
+		assert (chan->rbuf->reserved_size () == 0);
 	}
 
 	/* move the intended read target, so that after the refill is done,
@@ -907,8 +907,8 @@ DiskReader::seek (samplepos_t sample, bool complete_refill)
 
 		shift = abs (shift);
 
-		for (ChannelList::iterator chan = c->begin (); chan != c->end (); ++chan) {
-			(*chan)->rbuf->increment_read_ptr (shift);
+		for (auto const& chan : *c) {
+			chan->rbuf->increment_read_ptr (shift);
 		}
 	}
 
@@ -920,11 +920,10 @@ DiskReader::can_internal_playback_seek (sampleoffset_t distance)
 {
 	/* 1. Audio */
 
-	ChannelList::iterator          chan;
-	std::shared_ptr<ChannelList> c = channels.reader ();
+	std::shared_ptr<ChannelList const> c = channels.reader ();
 
-	for (chan = c->begin (); chan != c->end (); ++chan) {
-		if (!(*chan)->rbuf->can_seek (distance)) {
+	for (auto const& chan : *c) {
+		if (!chan->rbuf->can_seek (distance)) {
 			return false;
 		}
 	}
@@ -943,13 +942,12 @@ DiskReader::internal_playback_seek (sampleoffset_t distance)
 
 	sampleoffset_t off = distance;
 
-	ChannelList::iterator          chan;
-	std::shared_ptr<ChannelList> c = channels.reader ();
-	for (chan = c->begin (); chan != c->end (); ++chan) {
+	std::shared_ptr<ChannelList const> c = channels.reader ();
+	for (auto const& chan : *c) {
 		if (distance < 0) {
-			off = 0 - (sampleoffset_t) (*chan)->rbuf->decrement_read_ptr (::llabs (distance));
+			off = 0 - (sampleoffset_t) chan->rbuf->decrement_read_ptr (::llabs (distance));
 		} else {
-			off = (*chan)->rbuf->increment_read_ptr (distance);
+			off = chan->rbuf->increment_read_ptr (distance);
 		}
 	}
 
@@ -1159,11 +1157,11 @@ DiskReader::refill_audio (Sample* sum_buffer, Sample* mixdown_buffer, float* gai
 		return 0;
 	}
 
-	int32_t                        ret = 0;
-	samplecnt_t                    zero_fill;
-	uint32_t                       chan_n;
-	ChannelList::iterator          i;
-	std::shared_ptr<ChannelList> c = channels.reader ();
+	int32_t                            ret = 0;
+	samplecnt_t                        zero_fill;
+	uint32_t                           chan_n;
+	ChannelList::const_iterator        i;
+	std::shared_ptr<ChannelList const> c = channels.reader ();
 
 	_last_read_reversed = reversed;
 
@@ -1936,7 +1934,7 @@ DiskReader::setup_preloop_buffer ()
 		return;
 	}
 
-	std::shared_ptr<ChannelList> c = channels.reader ();
+	std::shared_ptr<ChannelList const> c = channels.reader ();
 
 	if (c->empty () || !_playlists[DataType::AUDIO]) {
 		return;
@@ -1950,8 +1948,8 @@ DiskReader::setup_preloop_buffer ()
 
 	uint32_t channel = 0;
 
-	for (ChannelList::iterator chan = c->begin (); chan != c->end (); ++chan, ++channel) {
-		ReaderChannelInfo* rci = dynamic_cast<ReaderChannelInfo*> (*chan);
+	for (auto const& chan : *c) {
+		ReaderChannelInfo* rci = dynamic_cast<ReaderChannelInfo*> (chan);
 
 		rci->resize_preloop (loop_fade_length);
 
@@ -1960,5 +1958,6 @@ DiskReader::setup_preloop_buffer ()
 		} else {
 			memset (rci->pre_loop_buffer, 0, sizeof (Sample) * loop_fade_length);
 		}
+		++channel;
 	}
 }

gtk2_ardour/bundle_manager.cc
@@ -283,9 +283,9 @@ BundleManager::BundleManager (Session* session)
 	_tree_view.append_column (_("Name"), _list_model_columns.name);
 	_tree_view.set_headers_visible (false);
 
-	std::shared_ptr<BundleList> bundles = _session->bundles ();
-	for (BundleList::iterator i = bundles->begin(); i != bundles->end(); ++i) {
-		add_bundle (*i);
+	std::shared_ptr<BundleList const> bundles = _session->bundles ();
+	for (auto const& i : *bundles) {
+		add_bundle (i);
 	}
 
 	/* New / Edit / Delete buttons */

gtk2_ardour/cuebox_ui.cc
@@ -304,9 +304,8 @@ CueBoxUI::context_menu (uint64_t idx)
 void
 CueBoxUI::get_slots (TriggerList &triggerlist, uint64_t idx)
 {
-	std::shared_ptr<RouteList> rl = _session->get_routes();
-	for (RouteList::iterator r = rl->begin(); r != rl->end(); ++r) {
-		std::shared_ptr<Route> route = *r;
+	std::shared_ptr<RouteList const> rl = _session->get_routes();
+	for (auto const& route : *rl) {
 		std::shared_ptr<TriggerBox> box = route->triggerbox();
 #warning @Ben disambiguate processor *active* vs *visibility*
 		if (box /*&& box.active*/) {

gtk2_ardour/editor_canvas.cc
@@ -645,9 +645,9 @@ Editor::session_gui_extents (bool use_extra) const
 	 * NOTE: we should listen to playlists, and cache these values so we don't calculate them every time.
 	 */
 	{
-		std::shared_ptr<RouteList> rl = _session->get_routes();
-		for (RouteList::iterator r = rl->begin(); r != rl->end(); ++r) {
-			std::shared_ptr<Track> tr = std::dynamic_pointer_cast<Track> (*r);
+		std::shared_ptr<RouteList const> rl = _session->get_routes();
+		for (auto const& r : *rl) {
+			std::shared_ptr<Track> tr = std::dynamic_pointer_cast<Track> (r);
 
 			if (!tr) {
 				continue;

gtk2_ardour/editor_tempodisplay.cc
@@ -756,7 +756,7 @@ Editor::real_remove_meter_marker (Temporal::MeterPoint const * section)
 Temporal::TempoMap::WritableSharedPtr
 Editor::begin_tempo_mapping ()
 {
-	TempoMap::WritableSharedPtr wmap = TempoMap::fetch_writable ();
+	TempoMap::WritableSharedPtr wmap = TempoMap::write_copy ();
 	reassociate_metric_markers (wmap);
 	(void) Temporal::DomainSwapInformation::start (Temporal::BeatTime);
 	_session->globally_change_time_domain (Temporal::BeatTime, Temporal::AudioTime);
@@ -787,7 +787,7 @@ Editor::commit_tempo_mapping (TempoMap::WritableSharedPtr& new_map)
 Temporal::TempoMap::WritableSharedPtr
 Editor::begin_tempo_map_edit ()
 {
-	TempoMap::WritableSharedPtr wmap = TempoMap::fetch_writable ();
+	TempoMap::WritableSharedPtr wmap = TempoMap::write_copy ();
 	reassociate_metric_markers (wmap);
 	return wmap;
 }

gtk2_ardour/group_tabs.cc
@@ -218,7 +218,7 @@ GroupTabs::on_button_release_event (GdkEventButton*)
 			if (_dragging_new_tab) {
 				run_new_group_dialog (&routes, false);
 			} else {
-				std::shared_ptr<RouteList> r = _session->get_routes ();
+				std::shared_ptr<RouteList const> r = _session->get_routes ();
 				/* First add new ones, then remove old ones.
 				 * We cannot allow the group to become temporarily empty, because
 				 * Session::route_removed_from_route_group() will delete empty groups.
@@ -227,16 +227,15 @@ GroupTabs::on_button_release_event (GdkEventButton*)
 					/* RouteGroup::add () ignores routes already present in the set */
 					_dragging->group->add (*i);
 				}
-				for (RouteList::const_iterator i = r->begin(); i != r->end(); ++i) {
+				for (auto const& i : *r) {
 
 					bool const was_in_tab = find (
-						_initial_dragging_routes.begin(), _initial_dragging_routes.end(), *i
-						) != _initial_dragging_routes.end ();
+						_initial_dragging_routes.begin(), _initial_dragging_routes.end(), i) != _initial_dragging_routes.end ();
 
-					bool const now_in_tab = find (routes.begin(), routes.end(), *i) != routes.end();
+					bool const now_in_tab = find (routes.begin(), routes.end(), i) != routes.end();
 
 					if (was_in_tab && !now_in_tab) {
-						_dragging->group->remove (*i);
+						_dragging->group->remove (i);
 					}
 				}
 
@@ -584,12 +583,12 @@ GroupTabs::get_rec_enabled ()
 		return rec_enabled;
 	}
 
-	std::shared_ptr<RouteList> rl = _session->get_routes ();
+	std::shared_ptr<RouteList const> rl = _session->get_routes ();
 
-	for (RouteList::iterator i = rl->begin(); i != rl->end(); ++i) {
-		std::shared_ptr<Track> trk (std::dynamic_pointer_cast<Track> (*i));
+	for (auto const& i : *rl) {
+		std::shared_ptr<Track> trk (std::dynamic_pointer_cast<Track> (i));
 		if (trk && trk->rec_enable_control()->get_value()) {
-			rec_enabled.push_back (*i);
+			rec_enabled.push_back (i);
 		}
 	}
 

gtk2_ardour/io_button.cc
@@ -252,7 +252,7 @@ IOButtonBase::set_label (IOButtonBase& self, ARDOUR::Session& session, std::shar
 
 	/* Are all main-typed channels connected to the same route ? */
 	if (!have_label) {
-		std::shared_ptr<ARDOUR::RouteList> routes = session.get_routes ();
+		std::shared_ptr<ARDOUR::RouteList const> routes = session.get_routes ();
 		for (auto const& route : *routes) {
 			std::shared_ptr<IO> dest_io = input ? route->output () : route->input ();
 			if (io->bundle ()->connected_to (dest_io->bundle (), session.engine (), dt, true)) {
@@ -276,9 +276,9 @@ IOButtonBase::set_label (IOButtonBase& self, ARDOUR::Session& session, std::shar
 
 	/* Are all main-typed channels connected to the same (user) bundle ? */
 	if (!have_label) {
-		std::shared_ptr<ARDOUR::BundleList> bundles       = session.bundles ();
-		std::shared_ptr<ARDOUR::Port>       ap            = std::dynamic_pointer_cast<ARDOUR::Port> (session.vkbd_output_port ());
-		std::string                           vkbd_portname = AudioEngine::instance ()->make_port_name_non_relative (ap->name ());
+		std::shared_ptr<ARDOUR::BundleList const> bundles       = session.bundles ();
+		std::shared_ptr<ARDOUR::Port>             ap            = std::dynamic_pointer_cast<ARDOUR::Port> (session.vkbd_output_port ());
+		std::string                               vkbd_portname = AudioEngine::instance ()->make_port_name_non_relative (ap->name ());
 		for (auto const& bundle : *bundles) {
 			if (std::dynamic_pointer_cast<UserBundle> (bundle) == 0) {
 				if (!bundle->offers_port (vkbd_portname)) {
@@ -576,20 +576,20 @@ IOButton::button_press (GdkEventButton* ev)
 	uint32_t const n_with_separator = citems.size ();
 
 	_menu_bundles.clear ();
-	ARDOUR::BundleList                    current = io ()->bundles_connected ();
-	std::shared_ptr<ARDOUR::BundleList> b       = _route->session ().bundles ();
+	ARDOUR::BundleList                        current = io ()->bundles_connected ();
+	std::shared_ptr<ARDOUR::BundleList const> b       = _route->session ().bundles ();
 
 	if (_input) {
 		/* give user bundles first chance at being in the menu */
-		for (ARDOUR::BundleList::iterator i = b->begin (); i != b->end (); ++i) {
-			if (std::dynamic_pointer_cast<UserBundle> (*i)) {
-				maybe_add_bundle_to_menu (*i, current);
+		for (auto const& i : *b) {
+			if (std::dynamic_pointer_cast<UserBundle> (i)) {
+				maybe_add_bundle_to_menu (i, current);
 			}
 		}
 
-		for (ARDOUR::BundleList::iterator i = b->begin (); i != b->end (); ++i) {
-			if (std::dynamic_pointer_cast<UserBundle> (*i) == 0) {
-				maybe_add_bundle_to_menu (*i, current);
+		for (auto const& i : *b) {
+			if (std::dynamic_pointer_cast<UserBundle> (i) == 0) {
+				maybe_add_bundle_to_menu (i, current);
 			}
 		}
 	} else {
@@ -603,8 +603,7 @@ IOButton::button_press (GdkEventButton* ev)
 		}
 	}
 
-	std::shared_ptr<ARDOUR::RouteList> routes = _route->session ().get_routes ();
-	RouteList                            copy   = *routes;
+	RouteList copy = *_route->session ().get_routes ();
 	copy.sort (RouteCompareByName ());
 
 	if (_input) {
@@ -642,16 +641,16 @@ IOButton::button_press (GdkEventButton* ev)
 		}
 
 		/* then try adding user output bundles, often labeled/grouped physical inputs */
-		for (ARDOUR::BundleList::iterator i = b->begin (); i != b->end (); ++i) {
-			if (std::dynamic_pointer_cast<UserBundle> (*i)) {
-				maybe_add_bundle_to_menu (*i, current, intended_type);
+		for (auto const& i : *b) {
+			if (std::dynamic_pointer_cast<UserBundle> (i)) {
+				maybe_add_bundle_to_menu (i, current, intended_type);
 			}
 		}
 
 		/* then all other bundles, including physical outs or other software */
-		for (ARDOUR::BundleList::iterator i = b->begin (); i != b->end (); ++i) {
-			if (std::dynamic_pointer_cast<UserBundle> (*i) == 0) {
-				maybe_add_bundle_to_menu (*i, current, intended_type);
+		for (auto const& i : *b) {
+			if (std::dynamic_pointer_cast<UserBundle> (i) == 0) {
+				maybe_add_bundle_to_menu (i, current, intended_type);
 			}
 		}
 

gtk2_ardour/io_plugin_window.cc
@@ -125,7 +125,7 @@ IOPluginWindow::refill ()
 	if (!_session) {
 		return;
 	}
-	std::shared_ptr<IOPlugList> iop (_session->io_plugs ());
+	std::shared_ptr<IOPlugList const> iop (_session->io_plugs ());
 	for (auto& i : *iop) {
 		IOPlugUI* iopup = manage (new IOPlugUI (i));
 		if (i->is_pre ()) {
@@ -588,9 +588,9 @@ IOPluginWindow::IOButton::button_press (GdkEventButton* ev)
 
 	uint32_t const n_with_separator = citems.size ();
 
-	std::shared_ptr<ARDOUR::BundleList> b      = _io->session ().bundles ();
-	std::shared_ptr<ARDOUR::RouteList>  routes = _io->session ().get_routes ();
-	RouteList                             copy   = *routes;
+	std::shared_ptr<ARDOUR::BundleList const> b      = _io->session ().bundles ();
+	std::shared_ptr<ARDOUR::RouteList const>  routes = _io->session ().get_routes ();
+	RouteList                                 copy   = *routes;
 	copy.sort (RouteCompareByName ());
 
 	if (_io->direction () == IO::Input) {

commit fd3f475b3f24c390214def90885277422221f17f

Fix mono audio after pending overwrite (#9483)
This bug was introduced in 295dbd8e1e when replacing

```diff
-for (ChannelList::iterator chan = c->begin (); chan != c->end (); ++chan, ++n) {
+for (auto const& chan : *c) {
```

see also https://discourse.ardour.org/t/first-10-seconds-of-modified-stereo-region-goes-mono-output/109322

libs/ardour/disk_reader.cc
@@ -728,6 +728,7 @@ DiskReader::overwrite_existing_audio ()
 			if (audio_read (buf + chunk1_offset, mixdown_buffer.get (), gain_buffer.get (), start, chunk1_cnt, rci, n, reversed) != (samplecnt_t)chunk1_cnt) {
 				error << string_compose (_("DiskReader %1: when overwriting(1), cannot read %2 from playlist at sample %3"), id (), chunk1_cnt, overwrite_sample) << endmsg;
 				ret = false;
+				++n;
 				continue;
 			}
 		}
@@ -745,6 +746,7 @@ DiskReader::overwrite_existing_audio ()
 				rci->initialized = true;
 			}
 		}
+		++n;
 	}
 
 	file_sample[DataType::AUDIO] = start;