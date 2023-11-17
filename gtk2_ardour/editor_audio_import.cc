b76c3b11d9a28b4a08368a92791011f6032d8605
Paul Davis <paul@linuxaudiosystems.com>
2023-11-17 10:02:34 -0700

    when importing SMF tempo map, use copy&paste to keep position

--- gtk2_ardour/editor_audio_import.cc
+++ gtk2_ardour/editor_audio_import.cc
@@ -285,15 +285,13 @@ Editor::import_smf_tempo_map (Evoral::SMF const & smf, timepos_t const & pos)
 		return;
 	}
 
-	TempoMapChange tmc (*this, _("import SMF tempo map"));
-
 	/* cannot create an empty TempoMap, so create one with "default" single
 	   values for tempo and meter, then overwrite them.
 	*/
 
 	TempoMap::WritableSharedPtr new_map (new TempoMap (Tempo (120, 4), Meter (4, 4)));
 
-	Meter last_meter (4.0, 4.0);
+	Meter last_meter (4, 4);
 	bool have_initial_meter = false;
 
 	for (size_t n = 0; n < num_tempos; ++n) {
@@ -302,13 +300,7 @@ Editor::import_smf_tempo_map (Evoral::SMF const & smf, timepos_t const & pos)
 		assert (t);
 
 		Tempo tempo (t->tempo(), 32.0 / (double) t->notes_per_note);
-
-		cerr << "new tempo from SMF : " << tempo << endl;
-
 		Meter meter (t->numerator, t->denominator);
-
-		cerr << "new meter from SMF : " << meter << endl;
-
 
 		Temporal::BBT_Argument bbt; /* 1|1|0 which is correct for the no-meter case */
 
@@ -330,7 +322,22 @@ Editor::import_smf_tempo_map (Evoral::SMF const & smf, timepos_t const & pos)
 		last_meter = meter;
 	}
 
-	tmc.use_new_map (new_map);
+	TempoMap::WritableSharedPtr wmap = TempoMap::write_copy ();
+	TempoMapCutBuffer* tmcb;
+	// XMLNode& tm_before (wmap->get_state());
+
+	tmcb = new_map->copy (timepos_t (0), timepos_t::max (Temporal::AudioTime));
+
+	if (tmcb && !tmcb->empty()) {
+		wmap->paste (*tmcb, pos, false, _("import"));
+		TempoMap::update (wmap);
+		delete tmcb;
+		// XMLNode& tm_after (wmap->get_state());
+		// _session->add_command (new TempoCommand (_("cut tempo map"), &tm_before, &tm_after));
+	} else {
+		// delete &tm_before;
+		TempoMap::abort_update ();
+	}
 }
 
 void
