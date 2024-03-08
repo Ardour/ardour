43c08dd2f51b08154292daa42988ae5aedb2b5e7
Robin Gareus <robin@gareus.org>
2024-03-08 16:14:44 +0100

    Do not automatically bind ADM import script

--- gtk2_ardour/luainstance.cc
+++ gtk2_ardour/luainstance.cc
@@ -1546,9 +1546,6 @@ LuaInstance::pre_seed_scripts ()
 	int id = 0;
 	pre_seed_script ("Mixer Screenshot", id);
 	pre_seed_script ("List Plugins", id);
-#if defined MIXBUS && !defined PLATFORM_WINDOWS
-	pre_seed_script ("Import ADM BWF File", id);
-#endif
 }
 
 bool
