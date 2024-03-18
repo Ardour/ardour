0c4eada414939548ce4d007f26b573f25df33be7
Paul Davis <paul@linuxaudiosystems.com>
2024-03-18 10:48:38 -0600

    make editor canvas use multi-expose redraws
    This is not required for any functionality or performance fixes, but may be useful
    and also helps tracking redraw issues with other related changes

--- gtk2_ardour/editor_canvas.cc
+++ gtk2_ardour/editor_canvas.cc
@@ -83,7 +83,8 @@ Editor::initialize_canvas ()
 
 	_track_canvas->set_background_color (UIConfiguration::instance().color ("arrange base"));
 	_track_canvas->use_nsglview (UIConfiguration::instance().get_nsgl_view_mode () == NSGLHiRes);
-
+	_track_canvas->set_single_exposure (false);
+	
 	/* scroll group for items that should not automatically scroll
 	 *  (e.g verbose cursor). It shares the canvas coordinate space.
 	*/
