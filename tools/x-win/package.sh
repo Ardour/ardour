Ardour/ardour

commit 75167ac347bb7bc73ad8b94f7332e47001276d07

Bundle/package script updates for LiveTrax

tools/linux_packaging/build
@@ -22,6 +22,7 @@ MIXBUS=
 WITH_HARRISON_LV2=
 WITH_COMMERCIAL_X42_LV2=
 WITH_GRATIS_X42_LV2=
+WITH_GMSYNYTH=
 WITH_HARVID=
 STRIP=all
 PRINT_SYSDEPS=
@@ -58,6 +59,7 @@ while [ $# -gt 0 ] ; do
 		WITH_HARRISON_LV2=1 ;
 		WITH_COMMERCIAL_X42_LV2=1
 		WITH_GRATIS_X42_LV2=1
+		WITH_GMSYNYTH=1
 		WITH_NLS=1 ;
 		STRIP=all
 		APPNAME=Mixbus ;
@@ -69,6 +71,7 @@ while [ $# -gt 0 ] ; do
 		WITH_HARRISON_LV2=1 ;
 		WITH_COMMERCIAL_X42_LV2=1
 		WITH_GRATIS_X42_LV2=1
+		WITH_GMSYNYTH=1
 		WITH_NLS=1 ;
 		STRIP=all
 		APPNAME=Mixbus32C ;
@@ -76,8 +79,16 @@ while [ $# -gt 0 ] ; do
 		EXENAME=mixbus32c ;
 		major_version=""
 		shift ;;
+	--livetrax)
+		LIVETRAX=1 ;
+		STRIP=all ;
+		WITH_NLS=1 ;
+		APPNAME=LiveTrax ;
+		WITHOUT_GMSYNYTH=1 ;
+		shift ;;
 	--public)
 		WITH_HARRISON_LV2=1 ;
+		WITH_GMSYNYTH=1
 		WITH_NLS=1 ;
 		STRIP=all ;
 		APPNAME=Ardour ;
@@ -676,7 +687,7 @@ if [ -d specialSauce ] ; then
 fi
 
 # install bundled LV2s to <app>/lib/LV2/
-cp -R $BUILD_ROOT/libs/LV2 $APPLIB/
+cp -R $BUILD_ROOT/libs/LV2 $APPLIB/ || true
 
 # lv2 core, classifications
 for file in $GTKSTACK_ROOT/lib/lv2/*.lv2; do
@@ -740,7 +751,7 @@ if test -n "$MIXBUS"; then
 		rm -f "${MediaClips}/"*.*
 		unzip -q -o -d "${MediaClips}" "${CACHEDIR}/MixbusBundledMedia.zip"
 	fi
-else
+elif test -z "$LIVETRAX"; then
     echo "Fetching Ardour bundled content"
 
     curl -s -S --fail -#  \
@@ -758,7 +769,7 @@ echo "Setting Bundled Media dir/file permissions"
 find "${MediaClips}" -type d -exec chmod 755 "{}" \;
 find "${MediaClips}" -type f -exec chmod 644 "{}" \;
 
-if true ; then
+if test x$WITH_GMSYNYTH != x ; then
 	mkdir -p $APPLIB/LV2
 
 	echo "Adding General MIDI Synth LV2"

tools/osx_packaging/osx_build
@@ -19,6 +19,7 @@ WITH_HARVID=1
 WITH_HARRISON_LV2=
 WITH_COMMERCIAL_X42_LV2=
 WITH_GRATIS_X42_LV2=
+WITH_GMSYNTH=
 STRIP=1
 PRINT_SYSDEPS=
 WITH_NLS=1
@@ -41,6 +42,7 @@ while [ $# -gt 0 ] ; do
 		WITH_HARRISON_LV2=1 ;
 		WITH_COMMERCIAL_X42_LV2=1
 		WITH_GRATIS_X42_LV2=1
+		WITH_GMSYNTH=1
 		STRIP= ;
 		PRODUCT_PKG_DIR=Mixbus;
 		APPNAME=Mixbus ;
@@ -53,6 +55,7 @@ while [ $# -gt 0 ] ; do
 		WITH_HARRISON_LV2=1 ;
 		WITH_COMMERCIAL_X42_LV2=1
 		WITH_GRATIS_X42_LV2=1
+		WITH_GMSYNTH=1
 		STRIP= ;
 		PRODUCT_PKG_DIR=Mixbus32C;
 		lower_case_appname=mixbus32c;
@@ -60,8 +63,18 @@ while [ $# -gt 0 ] ; do
 		APPNAME=Mixbus32C ;
 		BUNDLENAME=Mixbus32C-${major_version} ;
 		shift ;;
+	--livetrax)
+		LIVETRAX=1 ;
+		WITH_HARVID= ;
+		APPNAME=LiveTrax ;
+		PRODUCT_PKG_DIR=LiveTrax;
+		BUNDLENAME=LiveTrax${major_version} ;
+		lower_case_appname=livetrax;
+		BUNDLE_ID_BASE=com.harrisonconsoles
+		shift ;;
 	--public)
 		WITH_HARRISON_LV2=1 ;
+		WITH_GMSYNTH=1
 		PRODUCT_PKG_DIR=Ardour;
 		APPNAME=Ardour ;
 		BUNDLENAME=Ardour${major_version} ;
@@ -537,7 +550,7 @@ done
 rm $Frameworks/ardour-util.sh
 
 # install bundled LV2s to <app>/Contents/lib/LV2/
-cp -R $BUILD_ROOT/libs/LV2 $Frameworks/
+cp -R $BUILD_ROOT/libs/LV2 $Frameworks/ || true
 
 # lv2 core, classifications
 for file in $GTKSTACK_ROOT/lib/lv2/*.lv2; do
@@ -743,7 +756,7 @@ fi
 
 ################################################################################
 ### Mixbus plugins, etc
-if true; then
+if test x$WITH_GMSYNYTH != x ; then
 	echo "Bundling General MIDI Synth LV2"
 	mkdir -p "${PRODUCT_PKG_DIR}/${APPROOT}/lib/LV2"
 
@@ -828,7 +841,7 @@ if test -n "$MIXBUS"; then
 		bsdtar -C "${PRODUCT_PKG_DIR}/${MediaClips}" -xf \
 			"${CACHEDIR}/MixbusBundledMedia.zip"
 	fi
-else
+elif test -z "$LIVETRAX"; then
 	echo "Fetching Ardour bundled content"
 	curl -s -S --fail -#  \
 		-z "${CACHEDIR}/ArdourBundledMedia.zip" \
@@ -949,7 +962,9 @@ if test -n "${APPLE_DEVELOPER_ID_FOR_APPLICATION}"; then
 
 		rm $ZIP_PATH
 	fi
+fi
 
+if test -n "${APPLE_DEVELOPER_ID_FOR_APPLICATION}" -a -n "$WITH_HARVID"; then
 	#############
 	## Sign Jadeo
 

tools/x-win/package.sh
@@ -33,6 +33,7 @@ PRODUCT_VERSION=${major_version}
 WITH_HARRISON_LV2=1 ;
 WITH_COMMERCIAL_X42_LV2=
 WITH_GRATIS_X42_LV2=
+WITH_GMSYNTH=1
 
 # TODO: grep from build/config.log instead
 while [ $# -gt 0 ] ; do
@@ -58,6 +59,15 @@ while [ $# -gt 0 ] ; do
 			PROGRAM_NAME=Mixbus32C-${PROGRAM_VERSION}
 			PROGRAM_VERSION=""
 			MANUAL_NAME="mixbus32c-live-manual"
+			shift ;;
+		--livetrax)
+			LIVETRAX=1 ;
+			NOVIDEOTOOLS=1 ;
+			WITH_HARRISON_LV2="" ;
+			WITH_GMSYNTH="" ;
+			PROGRAM_NAME=LiveTrax
+			PROGRAM_KEY=LiveTrax
+			PRODUCT_NAME=LiveTrax
 			shift ;;
 		--chanstrip) HARRISONCHANNELSTRIP=$2 ; shift; shift ;;
 	esac
@@ -185,7 +195,7 @@ cp `find build/libs/surfaces/ -iname "*.dll"` $ALIBDIR/surfaces/
 cp `find build/libs/backends/ -iname "*.dll"` $ALIBDIR/backends/
 cp `find build/libs/panners/ -iname "*.dll"` $ALIBDIR/panners/
 
-cp -r build/libs/LV2 $ALIBDIR/
+cp -r build/libs/LV2 $ALIBDIR/ || true
 cp -r build/libs/vamp-plugins/*ardourvampplugins*.dll $ALIBDIR/vamp/libardourvampplugins.dll
 cp -r build/libs/vamp-pyin/*ardourvamppyin*.dll $ALIBDIR/vamp/libardourvamppyin.dll
 
@@ -232,7 +242,7 @@ cp gtk2_ardour/icons/cursor_square/* $DESTDIR/share/${LOWERCASE_DIRNAME}/icons/
 # clean build-dir after depoyment
 echo " === bundle completed, cleaning up"
 ./waf uninstall
-find $DESTDIR -name "*.dll.a" | xargs rm
+find $DESTDIR -name "*.dll.a" -print0 | xargs -0 -r rm
 echo " === complete"
 du -sh $DESTDIR
 
@@ -299,7 +309,7 @@ fi
 
 ################################################################################
 ### Mixbus plugins, etc
-if true ; then
+if test x$WITH_GMSYNYTH != x ; then
 	mkdir -p $ALIBDIR/LV2
 
 	echo "Adding General MIDI Synth LV2"
@@ -387,7 +397,7 @@ if test -n "$MIXBUS"; then
 		rm -f $DESTDIR/share/${LOWERCASE_DIRNAME}/media/*.*
 		unzip -q -o -d "$DESTDIR/share/${LOWERCASE_DIRNAME}/media/" "${SRCCACHE}/MixbusBundledMedia.zip"
 	fi
-else
+elif test -z "$LIVETRAX"; then
 	echo "Fetching Ardour bundled media"
 	curl -s -S --fail -#  \
 		-z "${SRCCACHE}/ArdourBundledMedia.zip" \

tools/linux_packaging/package
@@ -37,6 +37,9 @@ while [ $# -gt 0 ] ; do
 	--mixbus32c)
 		APPNAME=Mixbus32C ;
 		shift ;;
+	--livetrax)
+		APPNAME=LiveTrax ;
+		shift ;;
 	--public)
 		APPNAME=Ardour ;
 		shift ;;

commit 3ce07580cb3d501618e6ce6841fea9da03aacaa7

Fix packaging gmsynth
Bug introduced in 75167ac347b

tools/linux_packaging/build
@@ -74,7 +74,6 @@ while [ $# -gt 0 ] ; do
 		STRIP=all ;
 		WITH_NLS=1 ;
 		APPNAME=LiveTrax ;
-		WITHOUT_GMSYNYTH=1 ;
 		shift ;;
 	--public)
 		WITH_HARRISON_LV2=1 ;

tools/osx_packaging/osx_build
@@ -741,7 +741,7 @@ fi
 
 ################################################################################
 ### Mixbus plugins, etc
-if test x$WITH_GMSYNYTH != x ; then
+if test x$WITH_GMSYNTH != x ; then
 	echo "Bundling General MIDI Synth LV2"
 	mkdir -p "${PRODUCT_PKG_DIR}/${APPROOT}/lib/LV2"
 

tools/x-win/package.sh
@@ -303,7 +303,7 @@ fi
 
 ################################################################################
 ### Mixbus plugins, etc
-if test x$WITH_GMSYNYTH != x ; then
+if test x$WITH_GMSYNTH != x ; then
 	mkdir -p $ALIBDIR/LV2
 
 	echo "Adding General MIDI Synth LV2"