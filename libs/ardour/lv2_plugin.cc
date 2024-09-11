Ardour/ardour

commit c0b61d5a2856b215761911da0e7569658ed52154

Fix LV2 optional connect port logging #9790

libs/ardour/lv2_plugin.cc
@@ -3979,6 +3979,7 @@ LV2PluginInfo::discover (boost::function <void (std::string const&, PluginScanLo
 				}
 			}
 			else if (lilv_port_has_property(p, port, world.lv2_connectionOptional)) {
+				LilvNode* name = lilv_port_get_name(p, port);
 				cb (uri, PluginScanLogEntry::OK, string_compose (_("Ignored optional port %1 ('%2') which has unsupported data type."), i, lilv_node_as_string (name)), false);
 			}
 			else if (!lilv_port_is_a(p, port, world.lv2_AudioPort)) {

commit ab533c38a3cd8779ab8873bf64df715a017de537

LV2 fix a tiny memory leak introduced in c0b61d5a285

libs/ardour/lv2_plugin.cc
@@ -3981,6 +3981,7 @@ LV2PluginInfo::discover (boost::function <void (std::string const&, PluginScanLo
 			else if (lilv_port_has_property(p, port, world.lv2_connectionOptional)) {
 				LilvNode* name = lilv_port_get_name(p, port);
 				cb (uri, PluginScanLogEntry::OK, string_compose (_("Ignored optional port %1 ('%2') which has unsupported data type."), i, lilv_node_as_string (name)), false);
+				lilv_node_free(name);
 			}
 			else if (!lilv_port_is_a(p, port, world.lv2_AudioPort)) {
 				err = 1;