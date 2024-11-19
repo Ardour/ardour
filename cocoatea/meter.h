bfffe101de88602e72629a860919a6e1ee490bb8
Paul Davis <paul@linuxaudiosystems.com>
2024-11-19 12:11:02 -0700

    clean up single-view version of cocoatea

---
+++ cocoatea/meter.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <AppKit/AppKit.h>
+
+#include "view.h"
+
+struct Meter {
+	double x;
+	double y;
+	double width;
+	double height;
+	double r;
+	double g;
+	double b;
+	double a;
+	double level;
+	NSView* view;
+
+	Meter (NSView* view, double ax, double ay, double aw, double ah, double ar, double ag, double ab, double aa);
+
+	void set_level (double);
+	void draw (CGContextRef);
+};
--- cocoatea/view.h
+++ cocoatea/view.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <AppKit/AppKit.h>
 
 @interface CTView : NSView
