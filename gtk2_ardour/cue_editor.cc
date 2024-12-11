38f5546327af44b497f6f94161cd9077b39eec76
Paul Davis <paul@linuxaudiosystems.com>
2024-12-11 08:33:51 -0700

    actually implement ::reposition_and_zoom() for midi cue editor

--- gtk2_ardour/cue_editor.cc
+++ gtk2_ardour/cue_editor.cc
@@ -168,8 +168,13 @@ CueEditor::get_current_zoom () const
 }
 
 void
-CueEditor::reposition_and_zoom (samplepos_t, double)
+CueEditor::reposition_and_zoom (samplepos_t pos, double spp)
 {
+	set_samples_per_pixel (spp);
+
+	horizontal_adjustment.set_value (sample_to_pixel (pos));
+	/* correct rounding errors */
+	_leftmost_sample = pos;
 }
 
 void
