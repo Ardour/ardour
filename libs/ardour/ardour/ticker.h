Ardour/ardour

commit f67029bd02448fecb81bc84353fddc5225975090

random commit to facilitate trivial move of work from laptop back to main system

libs/ardour/ticker.cc
@@ -38,6 +38,7 @@
 
 using namespace ARDOUR;
 using namespace PBD;
+using namespace Temporal;
 
 MidiClockTicker::MidiClockTicker (Session* s)
 {
@@ -166,7 +167,8 @@ MidiClockTicker::tick (samplepos_t start_sample, samplepos_t end_sample, pframes
 			uint32_t    beat_pos;
 			samplepos_t clk_pos;
 
-			_session->tempo_map ().midi_clock_beat_at_of_after (start_sample + _mclk_out_latency.max, clk_pos, beat_pos);
+#warning NUTEMPO need to reimplement this in TempoMap
+			// _session->tempo_map ().midi_clock_beat_at_of_after (start_sample + _mclk_out_latency.max, clk_pos, beat_pos);
 
 			_beat_pos      = beat_pos;
 			_next_tick     = clk_pos - _mclk_out_latency.max;
@@ -213,8 +215,8 @@ out:
 double
 MidiClockTicker::one_ppqn_in_samples (samplepos_t transport_position) const
 {
-	const double samples_per_quarter_note = _session->tempo_map ().samples_per_quarter_note_at (transport_position, _session->nominal_sample_rate ());
-
+	Tempo const & tempo (_session->tempo_map().metric_at (transport_position).tempo());
+	const double samples_per_quarter_note = tempo.samples_per_quarter_note (_session->nominal_sample_rate());
 	return samples_per_quarter_note / 24.0;
 }
 

gtk2_ardour/automation_controller.cc
@@ -284,7 +284,7 @@ AutomationController::set_freq_beats(double beats)
 	const ARDOUR::ParameterDescriptor& desc    = _controllable->desc();
 	const ARDOUR::Session&             session = _controllable->session();
 	const samplepos_t                  pos     = session.transport_sample();
-	const ARDOUR::Tempo&               tempo   = session.tempo_map().tempo_at_sample (pos);
+	const Temporal::Tempo&             tempo   = session.tempo_map().metric_at (pos).tempo();
 	const double                       bpm     = tempo.note_types_per_minute();
 	const double                       bps     = bpm / 60.0;
 	const double                       freq    = bps / beats;

gtk2_ardour/automation_line.cc
@@ -77,7 +77,7 @@ using namespace std;
 using namespace ARDOUR;
 using namespace PBD;
 using namespace Editing;
-
+using namespace Temporal;
 
 #define TIME_TO_SAMPLES(x) (_distance_measure (x, Temporal::AudioTime))
 #define SAMPLES_TO_TIME(x) (_distance_measure (x, alist->time_domain()))
@@ -496,9 +496,8 @@ AutomationLine::ContiguousControlPoints::compute_x_bounds (PublicEditor& e)
 			before_x = line.nth (front()->view_index() - 1)->get_x();
 
 			const samplepos_t pos = e.pixel_to_sample(before_x);
-			const Meter& meter = map.meter_at_sample (pos);
-			const samplecnt_t len = ceil (meter.samples_per_bar (map.tempo_at_sample (pos), e.session()->sample_rate())
-			                              / (Temporal::ticks_per_beat * meter.divisions_per_bar()) );
+			const TempoMetric& metric = map.metric_at (pos);
+			const samplecnt_t len = ceil (metric.samples_per_bar (pos) / (Temporal::ticks_per_beat * metric.meter().divisions_per_bar()));
 			const double one_tick_in_pixels = e.sample_to_pixel_unrounded (len);
 
 			before_x += one_tick_in_pixels;
@@ -512,9 +511,8 @@ AutomationLine::ContiguousControlPoints::compute_x_bounds (PublicEditor& e)
 			after_x = line.nth (back()->view_index() + 1)->get_x();
 
 			const samplepos_t pos = e.pixel_to_sample(after_x);
-			const Meter& meter = map.meter_at_sample (pos);
-			const samplecnt_t len = ceil (meter.samples_per_bar (map.tempo_at_sample (pos), e.session()->sample_rate())
-			                              / (Temporal::ticks_per_beat * meter.divisions_per_bar()));
+			const TempoMetric& metric = map.metric_at (pos);
+			const samplecnt_t len = ceil (metric.samples_per_bar (pos) / (Temporal::ticks_per_beat * metric.meter().divisions_per_bar()));
 			const double one_tick_in_pixels = e.sample_to_pixel_unrounded (len);
 
 			after_x -= one_tick_in_pixels;

gtk2_ardour/editor.h
@@ -1805,9 +1805,9 @@ private:
 	Curves tempo_curves;
 
 	void remove_metric_marks ();
-	void draw_metric_marks (const ARDOUR::Metrics& metrics);
+	void draw_metric_marks (const Temporal::TempoMap::Metrics& metrics);
 
-	void compute_current_bbt_points (std::vector<ARDOUR::TempoMap::BBTPoint>& grid, samplepos_t left, samplepos_t right);
+	void compute_current_bbt_points (std::vector<Temporal::Point>& grid, samplepos_t left, samplepos_t right);
 
 	void tempo_map_changed (const PBD::PropertyChange&);
 	void tempometric_position_changed (const PBD::PropertyChange&);

gtk2_ardour/editor_audio_import.cc
@@ -74,6 +74,8 @@ using namespace PBD;
 using namespace Gtk;
 using namespace Gtkmm2ext;
 using namespace Editing;
+using namespace Temporal;
+
 using std::string;
 
 /* Functions supporting the incorporation of external (non-captured) audio material into ardour */
@@ -280,7 +282,9 @@ Editor::import_smf_tempo_map (Evoral::SMF const & smf, timepos_t const & pos)
 	}
 
 	const samplecnt_t sample_rate = _session->sample_rate ();
-	TempoMap new_map (sample_rate);
+#warning NUTEMPO need to be able to create a tempo map with no entries
+	// TempoMap new_map (sample_rate);
+	TempoMap new_map (Tempo (120), Meter (4, 4), sample_rate);
 	Meter last_meter (4.0, 4.0);
 	bool have_initial_meter = false;
 
@@ -294,10 +298,12 @@ Editor::import_smf_tempo_map (Evoral::SMF const & smf, timepos_t const & pos)
 		Temporal::BBT_Time bbt; /* 1|1|0 which is correct for the no-meter case */
 
 		if (have_initial_meter) {
-			new_map.add_tempo (tempo, t->time_pulses/ (double)smf.ppqn() / 4.0, 0, MusicTime);
+#warning NUTEMPO check API for this
+			// new_map.add_tempo (tempo, t->time_pulses/ (double)smf.ppqn() / 4.0, 0, BeatTime);
 			if (!(meter == last_meter)) {
-				bbt = new_map.bbt_at_quarter_note (t->time_pulses/(double)smf.ppqn());
-				new_map.add_meter (meter, bbt, 0, MusicTime);
+				bbt = new_map.bbt_at (Beats::from_double (t->time_pulses/(double)smf.ppqn()));
+#warning NUTEMPO check API for this
+				//new_map.add_meter (meter, bbt, 0, MusicTime);
 			}
 
 		} else {
@@ -311,7 +317,8 @@ Editor::import_smf_tempo_map (Evoral::SMF const & smf, timepos_t const & pos)
 		last_meter = meter;
 	}
 
-	_session->tempo_map() = new_map;
+#warning NUTEMPO need an assignment API for TempoMap
+	//_session->tempo_map() = new_map;
 }
 
 void

gtk2_ardour/editor_drag.cc
@@ -3717,7 +3717,7 @@ BBTRulerDrag::start_grab (GdkEvent* event, Gdk::Cursor* cursor)
 #warning NUTEMPO fixme needs new tempo map
 #if 0
 	TempoMap& map (_editor->session()->tempo_map());
-	_tempo = const_cast<TempoSection*> (&map.tempo_section_at_sample (raw_grab_sample()));
+	_tempo = const_cast<TempoSection*> (&map.metric_at (raw_grab_sample().tempo()));
 
 	if (adjusted_current_time (event, false) <= _tempo->time()) {
 		_drag_valid = false;
@@ -3748,7 +3748,7 @@ BBTRulerDrag::setup_pointer_offset ()
 	/* get current state */
 	_before_state = &map.get_state();
 
-	const double beat_at_sample = max (0.0, map.beat_at_sample (raw_grab_sample()));
+	const double beat_at_sample = max (0.0, map.beat_at (raw_grab_sample()));
 	const uint32_t divisions = _editor->get_grid_beat_divisions (0);
 	double beat = 0.0;
 
@@ -3762,7 +3762,7 @@ BBTRulerDrag::setup_pointer_offset ()
 		beat = floor (beat_at_sample) + (floor (((beat_at_sample - floor (beat_at_sample)) * 4)) / 4);
 	}
 
-	_grab_qn = map.quarter_note_at_beat (beat);
+	_grab_qn = map.quarter_note_at (beat);
 
 	_pointer_offset = raw_grab_sample() - map.sample_at_quarter_note (_grab_qn);
 

gtk2_ardour/editor_drag.h
@@ -875,7 +875,7 @@ private:
 
 	bool _copy;
 	bool _movable;
-	ARDOUR::Tempo _grab_bpm;
+	Temporal::Tempo _grab_bpm;
 	double _grab_qn;
 	XMLNode* _before_state;
 };
@@ -931,9 +931,9 @@ public:
 
 private:
 	double _grab_qn;
-	ARDOUR::Tempo  _grab_tempo;
-	ARDOUR::TempoSection* _tempo;
-	ARDOUR::TempoSection* _next_tempo;
+	Temporal::Tempo  _grab_tempo;
+	Temporal::TempoPoint* _tempo;
+	Temporal::TempoPoint* _next_tempo;
 	bool _drag_valid;
 	XMLNode* _before_state;
 };
@@ -962,7 +962,7 @@ public:
 
 private:
 	double _grab_qn;
-	ARDOUR::TempoSection* _tempo;
+	Temporal::TempoPoint* _tempo;
 	XMLNode* _before_state;
 	bool _drag_valid;
 };

gtk2_ardour/export_format_dialog.cc
@@ -1032,7 +1032,7 @@ ExportFormatDialog::update_time (AnyTime& time, AudioClock const& clock)
 		break;
 	case AudioClock::BBT:
 		time.type = AnyTime::BBT;
-		_session->bbt_time (samples, time.bbt);
+		_session->bbt_time (timepos_t (samples), time.bbt);
 		break;
 	case AudioClock::Seconds:
 	case AudioClock::MinSec:

commit 6e2d34df2d8066bccd011ba26482e09b4d8a452f

Fix MIDI Clock generator (rounding issues)
This broke in f67029bd0 and notably bcc1aeeb8615.

next_tick needs to accommodate for sub-sample accuracy when
PPQN are not integer samples. e.g. 110 bpm @ 48kHz

PS. Instead of `double` we could use superclock next_tick, and
`one_ppqn_in_superclocks` respectively. This would provide us
with 62 bit significand (instead of 52 bit using double).

Yet for the case of MIDI clock, this will have no real world
effect.

libs/ardour/ardour/ticker.h
@@ -47,7 +47,7 @@ private:
 	ARDOUR::Session&      _session;
 	std::shared_ptr<MidiPort> _midi_port;
 	bool                  _rolling;
-	samplepos_t           _next_tick;
+	double                _next_tick;
 	uint32_t              _beat_pos;
 	uint32_t              _clock_cnt;
 	samplepos_t           _transport_pos;

libs/ardour/ticker.cc
@@ -219,7 +219,8 @@ double
 MidiClockTicker::one_ppqn_in_samples (samplepos_t transport_position) const
 {
 	TempoPoint const & tempo (TempoMap::use()->metric_at (timepos_t (transport_position)).tempo());
-	const double samples_per_quarter_note = superclock_to_samples (tempo.superclocks_per_note_type_at (timepos_t (transport_position)), _session.nominal_sample_rate());
+	/* un-rounded superclock_to_samples (tempo.superclocks_per_note_type_at (timepos_t (transport_position)), _session.nominal_sample_rate()) */
+	const double samples_per_quarter_note = tempo.superclocks_per_note_type_at (timepos_t (transport_position)) * _session.nominal_sample_rate() / (double)superclock_ticks_per_second ();
 	return samples_per_quarter_note / 24.0;
 }
 