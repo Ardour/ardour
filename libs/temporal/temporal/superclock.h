90795ea138ac8103a2c0735905be149ad1855cbc
Paul Davis <paul@linuxaudiosystems.com>
2025-01-30 11:26:37 -0700

    lighter, simpler math for sample-aligned superclocks

--- libs/temporal/temporal/superclock.h
+++ libs/temporal/temporal/superclock.h
@@ -48,7 +48,6 @@ static inline superclock_t superclock_ticks_per_second() { return _superclock_ti
 
 static inline superclock_t superclock_to_samples (superclock_t s, int sr) { return PBD::muldiv_floor (s, sr, superclock_ticks_per_second()); }
 static inline superclock_t samples_to_superclock (int64_t samples, int sr) { return PBD::muldiv_round (samples, superclock_ticks_per_second(), superclock_t (sr)); }
-static inline superclock_t sample_aligned_superclock (superclock_t s, int sr) { return PBD::muldiv_floor (s, sr, sr); }
 
 LIBTEMPORAL_API extern int most_recent_engine_sample_rate;
 
