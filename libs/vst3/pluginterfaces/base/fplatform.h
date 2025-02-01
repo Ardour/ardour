b4bf87725e7eafa9f39ab6b6680e481a8a398aab
Robin Gareus <robin@gareus.org>
2025-02-01 11:27:03 +0100

    VST3 SDK update for NetBSD (#9886)

--- libs/vst3/pluginterfaces/base/fplatform.h
+++ libs/vst3/pluginterfaces/base/fplatform.h
@@ -86,7 +86,7 @@
 //-----------------------------------------------------------------------------
 // LINUX
 //-----------------------------------------------------------------------------
-#elif __gnu_linux__ || __linux__
+#elif __gnu_linux__ || __linux__ || __FreeBSD__ || __NetBSD__ || __OpenBSD__
 	#define SMTG_OS_LINUX	1
 	#define SMTG_OS_MACOS	0
 	#define SMTG_OS_WINDOWS	0
