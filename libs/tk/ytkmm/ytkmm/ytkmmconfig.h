8d7fc1591783ea4ea9f82afdc47f15376c4705fa
John Emmas <john@creativepost.co.uk>
2025-02-24 12:42:53 +0000

    Fix an issue where 'ytkmm' wouldn't run properly if built with MSVC

--- libs/tk/ytkmm/ytkmm/ytkmmconfig.h
+++ libs/tk/ytkmm/ytkmm/ytkmmconfig.h
@@ -12,10 +12,21 @@
 /* Defined when the --enable-maemo-extensions configure argument was given */
 /* #undef GTKMM_MAEMO_EXTENSIONS_ENABLED */
 
+/* Define when building gtkmm as a static library */
+/* #undef GTKMM_STATIC_LIB */
+
+/* Enable DLL-specific stuff only when not building a static library */
+# if (!defined(GTKMM_STATIC_LIB) && defined(_WIN32) && !defined(__CYGWIN__) && !defined(COMPILER_MINGW))
+#  if !defined(GTKMM_DLL)
+#   define GTKMM_DLL 1
+#  endif
+# endif
+
 #ifdef GTKMM_DLL
 # if defined(GTKMM_BUILD) && defined(_WINDLL)
-   /* Do not dllexport as it is handled by gendef on MSVC */
-#  define GTKMM_API
+   /* GTKMM_API was previously undefined here.  It was getting handled  */
+   /* by 'gendef' on MSVC but 'gendef' stopped working a long time ago  */
+#  define GTKMM_API __declspec(dllexport)
 # elif !defined(GTKMM_BUILD)
 #  define GTKMM_API __declspec(dllimport)
 # else
