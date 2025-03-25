e12d8b3769ab688f10d5a34b86a6bfa4102faaf6
Robin Gareus <robin@gareus.org>
2025-03-25 03:02:06 +0100

    Add API to tap signal from a Delivery to RTA
    Rater than having each Delivery Object provide a Ringbuffer,
    which is unused most of the time, the GUI will provide
    them on demand.
    
    We need to be careful of lifetime, and use `rt_safe_delete` in
    case the Delivery holds the last instance.

--- libs/ardour/ardour/delivery.h
+++ libs/ardour/ardour/delivery.h
@@ -24,6 +24,8 @@
 
 #include <string>
 
+#include "pbd/ringbuffer.h"
+
 #include "ardour/libardour_visibility.h"
 #include "ardour/types.h"
 #include "ardour/chan_count.h"
@@ -110,6 +112,17 @@ public:
 
 	void set_gain_control (std::shared_ptr<GainControl> gc);
 
+	using RTARingBuffer    = PBD::RingBuffer<ARDOUR::Sample>;
+	using RTARingBufferPtr = std::shared_ptr<RTARingBuffer>;
+	using RTABufferList    = std::vector<RTARingBufferPtr>;
+	using RTABufferListPtr = std::shared_ptr<RTABufferList>;
+
+	void set_analysis_buffers (RTABufferListPtr rb) {
+		_rtabuffers = rb;
+	}
+	bool analysis_active () const;
+	void set_analysis_active (bool);
+
 	void set_polarity_control (std::shared_ptr<AutomationControl> ac) {
 		_polarity_control = ac;
 	}
@@ -152,6 +165,9 @@ private:
 	std::shared_ptr<GainControl>       _gain_control;
 	std::shared_ptr<AutomationControl> _polarity_control;
 
+	RTABufferListPtr  _rtabuffers;
+	std::atomic<bool> _rta_active;
+
 	static bool panners_legal;
 	static PBD::Signal<void()> PannersLegal;
 
@@ -164,5 +180,3 @@ private:
 
 
 } // namespace ARDOUR
-
-
