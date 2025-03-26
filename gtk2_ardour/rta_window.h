18d7d3962357e25c06e1bd03370bf7a46c2691d8
Robin Gareus <robin@gareus.org>
2025-03-26 23:57:52 +0100

    Commence work on actual RTA GUI

--- gtk2_ardour/rta_window.h
+++ gtk2_ardour/rta_window.h
@@ -22,9 +22,15 @@
 #include <ytkmm/box.h>
 #include <ytkmm/drawingarea.h>
 
+#include "pbd/ringbuffer.h"
+
 #include "ardour/ardour.h"
+#include "ardour/dsp_filter.h"
 #include "ardour/session_handle.h"
 #include "ardour/types.h"
+
+#include "widgets/ardour_button.h"
+#include "widgets/ardour_dropdown.h"
 
 #include "ardour_window.h"
 
@@ -39,18 +45,99 @@ public:
 	void set_session (ARDOUR::Session*);
 	XMLNode& get_state () const;
 
+	/* TODO: consider moving this out of the RTAWindow class,
+	 * allow to share std::list<RTA> to be used in different
+	 * contexts (mixer-strip, RTAWindow, ...)
+	 */
+	void attach (std::shared_ptr<ARDOUR::Route>);
+	void remove (std::shared_ptr<ARDOUR::Route>);
+	bool attached (std::shared_ptr<ARDOUR::Route>) const;
+
+	class RTA {
+	public:
+		RTA (std::shared_ptr<ARDOUR::Route>);
+		~RTA ();
+
+		RTA (RTA const&) = delete;
+
+		using RTARingBuffer    = PBD::RingBuffer<ARDOUR::Sample>;
+		using RTARingBufferPtr = std::shared_ptr<RTARingBuffer>;
+
+		std::shared_ptr<ARDOUR::Route>  route;
+		ARDOUR::samplecnt_t             rate;
+		size_t                          blocksize;
+		size_t                          stepsize;
+		size_t                          offset;
+		ARDOUR::DSP::PerceptualAnalyzer analyzer;
+		RTARingBufferPtr                ringbuffer;
+	};
+
 private:
 	RTAWindow ();
 	static RTAWindow* _instance;
 
+	void on_map ();
+	void on_unmap ();
+	bool hide_window (GdkEventAny*);
+
 	void session_going_away ();
 	void update_title ();
 	void on_theme_changed ();
+	void route_removed (std::weak_ptr<ARDOUR::Route>);
 
 	void darea_size_request (Gtk::Requisition*);
 	void darea_size_allocate (Gtk::Allocation&);
 	bool darea_expose_event (GdkEventExpose*);
 
-	Gtk::VBox         _vpacker;
-	Gtk::DrawingArea  _darea;
+	bool darea_button_press_event (GdkEventButton*);
+	bool darea_button_release_event (GdkEventButton*);
+	bool darea_motion_notify_event (GdkEventMotion*);
+	bool darea_leave_notify_event (GdkEventCrossing*);
+
+	void set_rta_speed (ARDOUR::DSP::PerceptualAnalyzer::Speed);
+	void set_rta_warp (ARDOUR::DSP::PerceptualAnalyzer::Warp);
+
+	void fast_update ();
+	void pause_toggled ();
+	void set_active (bool);
+	bool run_rta ();
+
+	enum DragStatus {
+		DragNone,
+		DragUpper,
+		DragLower,
+		DragRange
+	};
+
+	const float _dB_range = 86; // +6 .. -80 dB
+	const float _dB_min   = -80;
+
+	Gtk::VBox                              _vpacker;
+	Gtk::HBox                              _ctrlbox;
+	Gtk::DrawingArea                       _darea;
+	Gtk::Label                             _pointer_info;
+	ArdourWidgets::ArdourButton            _pause;
+	ArdourWidgets::ArdourDropdown          _speed_dropdown;
+	ArdourWidgets::ArdourDropdown          _warp_dropdown;
+	Cairo::RefPtr<Cairo::ImageSurface>     _grid;
+	bool                                   _visible;
+	bool                                   _active;
+	std::list<RTA>                         _rta;
+	ARDOUR::DSP::PerceptualAnalyzer::Speed _speed;
+	ARDOUR::DSP::PerceptualAnalyzer::Warp  _warp;
+	std::vector<std::string>               _speed_strings;
+	std::vector<std::string>               _warp_strings;
+	std::map<int, float>                   _xpos;
+	Gtkmm2ext::Color                       _basec;
+	Gtkmm2ext::Color                       _gridc;
+	Gtkmm2ext::Color                       _textc;
+	int                                    _margin;
+	int                                    _min_dB;
+	int                                    _max_dB;
+	bool                                   _hovering_dB;
+	DragStatus                             _dragging_dB;
+	float                                  _dragstart_y;
+	float                                  _dragstart_dB;
+
+	sigc::connection _update_connection;
 };
