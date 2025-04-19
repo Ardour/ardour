Ardour/ardour

commit 91f95619581d0ec91beabd1d4f27d5949e46cef7

no track record enable if triggerbox is rec-enabled

libs/ardour/track.cc
@@ -431,7 +431,7 @@ Track::can_be_record_safe ()
 bool
 Track::can_be_record_enabled ()
 {
-	return !_record_safe_control->get_value() && _disk_writer && !_disk_writer->record_safe() && _session.writable() && (_freeze_record.state != Frozen);
+	return !_record_safe_control->get_value() && _disk_writer && !_disk_writer->record_safe() && _session.writable() && (_freeze_record.state != Frozen) && (!_triggerbox || !_triggerbox->record_enabled());
 }
 
 void

commit 5f1bea0b6bcd90e6ee106da6ac92bdbe576f73fe

Fix rec-arm disable
This broke in 91f95619581, when the condition for clip
recording was added.

libs/ardour/track.cc
@@ -334,7 +334,7 @@ Track::prep_record_enabled (bool yn)
 		return -1;
 	}
 
-	if (!can_be_record_enabled()) {
+	if (yn && !can_be_record_enabled()) {
 		return -1;
 	}
 