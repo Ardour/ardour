feb80d39f4ae163f225b446cd16968a14a640978
Paul Davis <paul@linuxaudiosystems.com>
2025-07-23 15:09:39 -0600

    NO-OP: braces/newline/space style

--- gtk2_ardour/ardour_ui.cc
+++ gtk2_ardour/ardour_ui.cc
@@ -2273,8 +2273,7 @@ ARDOUR_UI::save_template_dialog_response (int response, SaveTemplateDialog* d)
 
 			if (overwrite) {
 				_session->save_template (name, desc, true);
-			}
-			else {
+			}  else {
 				d->show ();
 				return;
 			}
