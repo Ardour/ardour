Ardour/ardour

commit a550b6482f1d22c03223075406299fdc7f924767

start using the now-compilable MidiView
Nothing yet derives from this, but MIDI display items and drags
do use it.

gtk2_ardour/midi_streamview.cc
@@ -110,11 +110,11 @@ MidiStreamView::create_region_view (std::shared_ptr<Region> r, bool /*wfd*/, boo
 	if (recording) {
 		region_view = new MidiRegionView (
 			_region_group, _trackview.editor(), _trackview, region,
-			_samples_per_pixel, region_color, recording,
+			_samples_per_pixel, MidiViewBackground::region_color(), recording,
 			TimeAxisViewItem::Visibility(TimeAxisViewItem::ShowFrame));
 	} else {
 		region_view = new MidiRegionView (_region_group, _trackview.editor(), _trackview, region,
-		                                  _samples_per_pixel, region_color);
+		                                  _samples_per_pixel, MidiViewBackground::region_color());
 	}
 
 	region_view->init (false);
@@ -494,3 +494,20 @@ MidiStreamView::get_regions_with_selected_data (RegionSelection& rs)
 	}
 }
 
+void
+MidiStreamView::set_note_highlight (bool yn)
+{
+	dynamic_cast<MidiTimeAxisView*> (&_trackview)->set_note_highlight (yn);
+}
+
+uint8_t
+MidiStreamView::get_preferred_midi_channel () const
+{
+	return dynamic_cast<MidiTimeAxisView*> (&_trackview)->get_preferred_midi_channel();
+}
+
+void
+MidiStreamView::record_layer_check (std::shared_ptr<ARDOUR::Region> r, samplepos_t t)
+{
+	check_record_layers (r, t);
+}

gtk2_ardour/cue_editor.h
@@ -92,8 +92,6 @@ class CueEditor : public EditingContext
 	}
 	VerboseCursor* verbose_cursor () const;
 
-
-
 	void set_snapped_cursor_position (Temporal::timepos_t const & pos);
 
 	std::vector<MidiRegionView*> filter_to_unique_midi_region_views (RegionSelection const & ms) const;

gtk2_ardour/edit_note_dialog.cc
@@ -25,7 +25,7 @@
 #include "gtkmm2ext/utils.h"
 
 #include "edit_note_dialog.h"
-#include "midi_region_view.h"
+#include "midi_view.h"
 #include "note_base.h"
 
 #include "pbd/i18n.h"
@@ -41,7 +41,7 @@ using namespace Gtkmm2ext;
  *    @param n Notes to edit.
  */
 
-EditNoteDialog::EditNoteDialog (MidiRegionView* rv, set<NoteBase*> n)
+EditNoteDialog::EditNoteDialog (MidiView* rv, set<NoteBase*> n)
 	: ArdourDialog (_("Note"))
 	, _region_view (rv)
 	, _events (n)
@@ -94,12 +94,11 @@ EditNoteDialog::EditNoteDialog (MidiRegionView* rv, set<NoteBase*> n)
 	table->attach (_time_all, 2, 3, r, r + 1);
 	++r;
 
-	_time_clock.set_session (_region_view->get_time_axis_view().session ());
+	// XXXX _time_clock.set_session (_region_view->get_time_axis_view().session ());
 	_time_clock.set_mode (AudioClock::BBT);
 
 	/* Calculate absolute position of the event on time timeline */
-	std::shared_ptr<ARDOUR::Region> region (_region_view->region ());
-	timepos_t const pos = region->source_position() + timecnt_t ((*_events.begin())->note()->time ());
+	timepos_t const pos = _region_view->current_slice().source_position() + timecnt_t ((*_events.begin())->note()->time ());
 
 	_time_clock.set (pos, true);
 
@@ -109,7 +108,7 @@ EditNoteDialog::EditNoteDialog (MidiRegionView* rv, set<NoteBase*> n)
 	table->attach (_length_all, 2, 3, r, r + 1);
 	++r;
 
-	_length_clock.set_session (_region_view->get_time_axis_view().session ());
+	// XXXX _length_clock.set_session (_region_view->get_time_axis_view().session ());
 	_length_clock.set_mode (AudioClock::BBT);
 	_length_clock.set_duration (timecnt_t ((*_events.begin())->note()->length()), true);
 
@@ -201,10 +200,8 @@ EditNoteDialog::done (int r)
 		}
 	}
 
-	std::shared_ptr<ARDOUR::Region> region (_region_view->region ());
-
 	/* convert current clock time into an offset from the start of the source */
-	timecnt_t const time_clock_source_relative = region->source_position ().distance (_time_clock.last_when ());
+	timecnt_t const time_clock_source_relative = _region_view->current_slice().source_position ().distance (_time_clock.last_when ());
 
 	/* convert that into a position in Beats - this will be the new note time (as an offset inside the source) */
 	Beats const new_note_time_source_relative_beats = time_clock_source_relative.beats ();

gtk2_ardour/edit_note_dialog.h
@@ -22,18 +22,18 @@
 #include "ardour_dialog.h"
 #include "audio_clock.h"
 
-class MidiRegionView;
+class MidiView;
 class NoteBase;
 
 class EditNoteDialog : public ArdourDialog
 {
 public:
-	EditNoteDialog (MidiRegionView* rv, std::set<NoteBase*> n);
+	EditNoteDialog (MidiView* rv, std::set<NoteBase*> n);
 
 	void done (int);
 
   private:
-	MidiRegionView* _region_view;
+	MidiView* _region_view;
 	std::set<NoteBase*> _events;
 	Gtk::SpinButton _channel;
 	Gtk::CheckButton _channel_all;

gtk2_ardour/editor.cc
@@ -137,6 +137,7 @@
 #include "marker.h"
 #include "midi_region_view.h"
 #include "midi_time_axis.h"
+#include "midi_view.h"
 #include "mixer_strip.h"
 #include "mixer_ui.h"
 #include "mouse_cursors.h"
@@ -6281,7 +6282,7 @@ Editor::popup_note_context_menu (ArdourCanvas::Item* item, GdkEvent* event)
 	   popping up the menu will cause a region leave event which clears
 	   entered_regionview. */
 
-	MidiRegionView&       mrv = note->region_view();
+	MidiView&       mrv = note->region_view();
 	const RegionSelection rs  = get_regions_from_selection_and_entered ();
 	const uint32_t sel_size = mrv.selection_size ();
 
@@ -6290,7 +6291,7 @@ Editor::popup_note_context_menu (ArdourCanvas::Item* item, GdkEvent* event)
 
 	if (sel_size > 0) {
 		items.push_back(MenuElem(_("Delete"),
-					 sigc::mem_fun(mrv, &MidiRegionView::delete_selection)));
+					 sigc::mem_fun(mrv, &MidiView::delete_selection)));
 	}
 
 	items.push_back(MenuElem(_("Edit..."),
@@ -6508,7 +6509,7 @@ Editor::track_dragging() const
 }
 
 void
-Editor::snap_to_internal (timepos_t& start, Temporal::RoundMode direction, SnapPref pref, bool ensure_snap)
+Editor::snap_to_internal (timepos_t& start, Temporal::RoundMode direction, SnapPref pref, bool ensure_snap) const
 {
 	UIConfiguration const& uic (UIConfiguration::instance ());
 	const timepos_t presnap = start;
@@ -6545,8 +6546,8 @@ Editor::snap_to_internal (timepos_t& start, Temporal::RoundMode direction, SnapP
 
 		if (!region_boundary_cache.empty ()) {
 
-			vector<timepos_t>::iterator prev = region_boundary_cache.begin ();
-			vector<timepos_t>::iterator next = std::upper_bound (region_boundary_cache.begin (), region_boundary_cache.end (), presnap);
+			auto prev = region_boundary_cache.begin ();
+			auto next = std::upper_bound (region_boundary_cache.begin (), region_boundary_cache.end (), presnap);
 			if (next != region_boundary_cache.begin ()) {
 				prev = next;
 				prev--;

gtk2_ardour/editor.h
@@ -130,6 +130,7 @@ class EditorSummary;
 class GUIObjectState;
 class ArdourMarker;
 class MidiRegionView;
+class MidiView;
 class MidiExportDialog;
 class MixerStrip;
 class MouseCursors;
@@ -1726,7 +1727,7 @@ private:
 	void edit_meter_marker (MeterMarker&);
 	void edit_bbt_marker (BBTMarker&);
 	void edit_control_point (ArdourCanvas::Item*);
-	void edit_notes (MidiRegionView*);
+	void edit_notes (MidiView*);
 	void edit_region (RegionView*);
 
 	void edit_current_meter ();

gtk2_ardour/editor_drag.cc
@@ -72,6 +72,7 @@
 #include "midi_region_view.h"
 #include "midi_selection.h"
 #include "midi_time_axis.h"
+#include "midi_view.h"
 #include "mouse_cursors.h"
 #include "note_base.h"
 #include "patch_change.h"
@@ -2384,12 +2385,14 @@ NoteResizeDrag::start_grab (GdkEvent* event, Gdk::Cursor* /*ignored*/)
 
 	Drag::start_grab (event, cursor);
 
+#warning paul fix me MRV/MV
+#if 0
 	region = &cnote->region_view ();
 
 	double temp;
 	temp        = region->snap_to_pixel (cnote->x0 (), true);
 	_snap_delta = temp - cnote->x0 ();
-
+#endif
 	_item->grab ();
 
 	if (event->motion.state & ArdourKeyboard::note_size_relative_modifier ()) {
@@ -6157,13 +6160,13 @@ NoteDrag::NoteDrag (EditingContext& ec, ArdourCanvas::Item* i)
 	_primary = reinterpret_cast<NoteBase*> (_item->get_data ("notebase"));
 	assert (_primary);
 	_region      = &_primary->region_view ();
-	_note_height = _region->midi_stream_view ()->note_height ();
+	_note_height = _region->midi_context().note_height ();
 }
 
 void
 NoteDrag::setup_pointer_offset ()
 {
-	_pointer_offset = _region->region ()->source_beats_to_absolute_time (_primary->note ()->time ()).distance (raw_grab_time ());
+	_pointer_offset = _region->current_slice().source_beats_to_absolute_time (_primary->note ()->time ()).distance (raw_grab_time ());
 }
 
 void
@@ -6177,7 +6180,7 @@ NoteDrag::start_grab (GdkEvent* event, Gdk::Cursor*)
 		_copy = false;
 	}
 
-	setup_snap_delta (_region->region ()->source_beats_to_absolute_time (_primary->note ()->time ()));
+	setup_snap_delta (_region->current_slice().source_beats_to_absolute_time (_primary->note ()->time ()));
 
 	if (!(_was_selected = _primary->selected ())) {
 		/* tertiary-click means extend selection - we'll do that on button release,
@@ -6216,7 +6219,7 @@ NoteDrag::total_dx (GdkEvent* event) const
 	timecnt_t dx = t2.distance (t1);
 
 	/* primary note time in quarter notes */
-	timepos_t const n_qn = _region->region ()->source_beats_to_absolute_time (_primary->note ()->time ());
+	timepos_t const n_qn = _region->current_slice().source_beats_to_absolute_time (_primary->note ()->time ());
 
 	/* prevent (n_qn + dx) from becoming negative */
 	if (-dx.distance() > timecnt_t(n_qn).distance ()) {
@@ -6241,8 +6244,8 @@ NoteDrag::total_dx (GdkEvent* event) const
 	timecnt_t ret (snap.earlier (n_qn).earlier (snap_delta (event->button.state)), n_qn);
 
 	/* prevent the earliest note being dragged earlier than the region's start position */
-	if (_earliest + ret < _region->region ()->start ()) {
-		ret -= (ret + _earliest) - _region->region ()->start ();
+	if (_earliest + ret < _region->current_slice().start ()) {
+		ret -= (ret + _earliest) - _region->current_slice().start ();
 	}
 
 	return ret;
@@ -6256,13 +6259,13 @@ NoteDrag::total_dy () const
 		return 0;
 	}
 
-	double const y = _region->midi_view ()->y_position ();
+	double const y = _region->midi_context().y_position ();
 	/* new current note */
 	uint8_t n = _region->y_to_note (current_pointer_y () - y);
 	/* clamp */
-	MidiStreamView* msv = _region->midi_stream_view ();
-	n                   = max (msv->lowest_note (), n);
-	n                   = min (msv->highest_note (), n);
+	MidiViewBackground& mvb = _region->midi_context ();
+	n                   = max (mvb.lowest_note (), n);
+	n                   = min (mvb.highest_note (), n);
 	/* and work out delta */
 	return n - _region->y_to_note (grab_y () - y);
 }
@@ -6308,7 +6311,7 @@ NoteDrag::motion (GdkEvent* event, bool first_move)
 
 		_region->show_verbose_cursor_for_new_note_value (_primary->note (), new_note);
 
-		editing_context.set_snapped_cursor_position (_region->region ()->region_beats_to_absolute_time (_primary->note ()->time ()) + dx_qn);
+		editing_context.set_snapped_cursor_position (_region->current_slice().region_beats_to_absolute_time (_primary->note ()->time ()) + dx_qn);
 	}
 }
 
@@ -6675,21 +6678,21 @@ DraggingView::DraggingView (RegionView* v, RegionDrag* parent, TimeAxisView* ita
 	initial_end      = v->region ()->position () + v->region ()->length ();
 }
 
-PatchChangeDrag::PatchChangeDrag (EditingContext& ec, PatchChange* i, MidiRegionView* r)
+PatchChangeDrag::PatchChangeDrag (EditingContext& ec, PatchChange* i, MidiView* r)
 	: Drag (ec, i->canvas_item (), Temporal::BeatTime, true, false)
 	, _region_view (r)
 	, _patch_change (i)
 	, _cumulative_dx (0)
 {
 	DEBUG_TRACE (DEBUG::Drags, string_compose ("New PatchChangeDrag, patch @ %1, grab @ %2\n",
-	                                           _region_view->region ()->source_beats_to_absolute_time (_patch_change->patch ()->time ()),
+	                                           _region_view->current_slice().source_beats_to_absolute_time (_patch_change->patch ()->time ()),
 	                                           grab_time ()));
 }
 
 void
 PatchChangeDrag::motion (GdkEvent* ev, bool)
 {
-	std::shared_ptr<Region> r = _region_view->region ();
+	std::shared_ptr<Region> r = _region_view->midi_region ();
 
 	timepos_t f = adjusted_current_time (ev);
 	f           = max (f, r->position ());
@@ -6713,13 +6716,13 @@ PatchChangeDrag::finished (GdkEvent* ev, bool movement_occurred)
 		return;
 	}
 
-	std::shared_ptr<Region> r (_region_view->region ());
+	std::shared_ptr<Region> r (_region_view->midi_region ());
 
 	timepos_t f = adjusted_current_time (ev);
 	f           = max (f, r->position ());
 	f           = min (f, r->nt_last ());
 
-	_region_view->move_patch_change (*_patch_change, _region_view->region ()->absolute_time_to_source_beats (f));
+	_region_view->move_patch_change (*_patch_change, r->absolute_time_to_source_beats (f));
 }
 
 void
@@ -6731,8 +6734,7 @@ PatchChangeDrag::aborted (bool)
 void
 PatchChangeDrag::setup_pointer_offset ()
 {
-	std::shared_ptr<Region> region = _region_view->region ();
-	_pointer_offset                = region->source_beats_to_absolute_time (_patch_change->patch ()->time ()).distance (raw_grab_time ());
+	_pointer_offset = _region_view->current_slice().source_beats_to_absolute_time (_patch_change->patch ()->time ()).distance (raw_grab_time ());
 }
 
 MidiRubberbandSelectDrag::MidiRubberbandSelectDrag (EditingContext& ec, MidiRegionView* rv)

gtk2_ardour/editor_drag.h
@@ -77,6 +77,7 @@ class TimeAxisView;
 class RouteTimeAxisView;
 class RegionSelection;
 class MidiRegionView;
+class MidiView;
 class MeterMarker;
 class ArdourMarker;
 class TempoMarker;
@@ -643,7 +644,7 @@ private:
 	Temporal::timecnt_t total_dx (GdkEvent * event) const; // total movement in quarter notes
 	int8_t total_dy () const;
 
-	MidiRegionView* _region;
+	MidiView* _region;
 	NoteBase* _primary;
 	Temporal::timecnt_t _cumulative_dx;
 	double _cumulative_dy;
@@ -727,7 +728,7 @@ private:
 class PatchChangeDrag : public Drag
 {
 public:
-	PatchChangeDrag (EditingContext&, PatchChange *, MidiRegionView *);
+	PatchChangeDrag (EditingContext&, PatchChange *, MidiView *);
 
 	void motion (GdkEvent *, bool);
 	void finished (GdkEvent *, bool);
@@ -744,7 +745,7 @@ public:
 	void setup_pointer_offset ();
 
 private:
-	MidiRegionView* _region_view;
+	MidiView* _region_view;
 	PatchChange* _patch_change;
 	double _cumulative_dx;
 };

commit 3320e7d5f9069dadb6ba530cf281963fe2b5405d

Set MIDI region color to use track's color or default fg.
This broke in a550b6482f1

gtk2_ardour/midi_streamview.cc
@@ -122,11 +122,11 @@ MidiStreamView::create_region_view (std::shared_ptr<Region> r, bool /*wfd*/, boo
 	if (recording) {
 		region_view = new MidiRegionView (
 			_region_group, _trackview.editor(), _trackview, region,
-			_samples_per_pixel, MidiViewBackground::region_color(), recording,
+			_samples_per_pixel, StreamView::region_color, recording,
 			TimeAxisViewItem::Visibility(TimeAxisViewItem::ShowFrame));
 	} else {
 		region_view = new MidiRegionView (_region_group, _trackview.editor(), _trackview, region,
-		                                  _samples_per_pixel, MidiViewBackground::region_color());
+		                                  _samples_per_pixel, StreamView::region_color);
 	}
 
 	region_view->init (false);