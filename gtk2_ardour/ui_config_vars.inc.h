9d36bace2d5ae7e36e2c88d6b366b9c9e8c24d0c
Paul Davis <paul@linuxaudiosystems.com>
2025-08-19 11:35:55 -0600

    add move threshold sensitivity parameter for UI

--- gtk2_ardour/ui_config_vars.inc.h
+++ gtk2_ardour/ui_config_vars.inc.h
@@ -160,7 +160,7 @@ UI_CONFIG_VARIABLE (bool, sandbox_all_lua_scripts, "sandbox-all-lua-scripts", fa
 UI_CONFIG_VARIABLE (bool, update_action_scripts, "update-action-scripts", true)
 UI_CONFIG_VARIABLE (bool, use_cocoa_invalidation, "use-cocoa-invalidation", true)
 UI_CONFIG_VARIABLE (Editing::RegionEditDisposition, region_edit_disposition, "region-edit-disposition", Editing::PreferBottomPane)
-
+UI_CONFIG_VARIABLE (int, drag_sensitivity, "drag-sensitivity", 1)
 
 /* these are visibility-type selections in the New Track dialog that we should make persistent for the user's choices */
 UI_CONFIG_VARIABLE (bool, show_on_cue_page, "show-on-cue-page", true)
