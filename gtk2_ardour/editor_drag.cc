54bea422162715386e928e634854444bdd091e6a
Paul Davis <paul@linuxaudiosystems.com>
2025-08-19 11:59:58 -0600

    scale move threshold by UIConfiguration parameter

--- gtk2_ardour/editor_drag.cc
+++ gtk2_ardour/editor_drag.cc
@@ -319,9 +319,6 @@ Drag::move_threshold () const
 	x *= UIConfiguration::instance().get_drag_sensitivity ();
 	y *= UIConfiguration::instance().get_drag_sensitivity ();
 
-	x *= UIConfiguration::instance().get_ui_scale ();
-	y *= UIConfiguration::instance().get_ui_scale ();
-
 	return std::make_pair (x, y);
 }
 
