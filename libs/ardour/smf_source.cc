637e59a14f6cefe7d2e7eb67ad58b09074aa33f1
Paul Davis <paul@linuxaudiosystems.com>
2025-11-14 16:36:28 -0700

    SMFSource: auto-ify a knarly container iteration

--- libs/ardour/smf_source.cc
+++ libs/ardour/smf_source.cc
@@ -835,10 +835,9 @@ SMFSource::load_model_unlocked (bool force_reload)
 
 	eventlist.sort(compare_eventlist);
 
-	std::list< std::pair< Evoral::Event<Temporal::Beats>*, gint > >::iterator it;
-	for (it=eventlist.begin(); it!=eventlist.end(); ++it) {
-		_model->append (*it->first, it->second);
-		delete it->first;
+	for (auto & it : eventlist) {
+		_model->append (*it.first, it.second);
+		delete it.first;
 	}
 
 	/* Length ought to be based on data in the file (TrkEnd meta-event, not
