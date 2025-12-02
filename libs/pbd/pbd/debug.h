6efe8ceeff4ff0a24dce194f0d9b0876ade84332
EZ4Stephen <ez4stephen@tutamail.com>
2025-12-02 21:45:34 +0400

    Update conditional include for pthread.h
    Further exclusion of the use of ardourext/pthread.h

--- libs/pbd/pbd/debug.h
+++ libs/pbd/pbd/debug.h
@@ -30,7 +30,7 @@
 #include "pbd/timing.h"
 
 /* check for PTW32_VERSION */
-#ifdef COMPILER_MSVC
+#if defined(COMPILER_MSVC) && !defined(OTHER1)
 #include <ardourext/pthread.h>
 #else
 #include <pthread.h>
