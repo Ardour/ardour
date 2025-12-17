Ardour/ardour

commit 557058f362f294cc46ca4c854b2439a410097fe0

Fix crash when an early callback changes selection during session load
This can happen e.g. from a VST3 plugin's state restore.

gtk2_ardour/editor_selection.cc
@@ -1083,6 +1083,15 @@ Editor::set_selected_regionview_from_map_event (GdkEventAny* /*ev*/, StreamView*
 void
 Editor::presentation_info_changed (PropertyChange const & what_changed)
 {
+	if (!_session || _session->deletion_in_progress()) {
+		/* static signal, that the editor c'tor subscribes to.
+		 * It may be received during connect_dependents_to_session() when
+		 * signals are processed in BootMessage -> GUIIdle, just
+		 * just before Editor::set_session();
+		 */
+		return;
+	}
+
 	uint32_t n_tracks = 0;
 	uint32_t n_busses = 0;
 	uint32_t n_vcas = 0;

commit 167359b300b8cb063b3c2bc47089847a1da6e8cd

Fix crashes caused by retaining references in 557058f362f29

gtk2_ardour/editor_selection.cc
@@ -1083,7 +1083,7 @@ Editor::set_selected_regionview_from_map_event (GdkEventAny* /*ev*/, StreamView*
 void
 Editor::presentation_info_changed (PropertyChange const & what_changed)
 {
-	if (!_session || _session->deletion_in_progress()) {
+	if (!_session) {
 		/* static signal, that the editor c'tor subscribes to.
 		 * It may be received during connect_dependents_to_session() when
 		 * signals are processed in BootMessage -> GUIIdle, just

gtk2_ardour/recorder_ui.cc
@@ -1139,7 +1139,7 @@ struct TrackRecordAxisSorter {
 void
 RecorderUI::presentation_info_changed (PBD::PropertyChange const& what_changed)
 {
-	if (!_session || _session->deletion_in_progress()) {
+	if (!_session) {
 		return;
 	}
 	if (what_changed.contains (Properties::hidden)) {