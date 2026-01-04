65190ebe67150a172c2afbe30726cf39f2abd5a1
Robin Gareus <robin@gareus.org>
2026-01-04 16:48:17 +0100

    Update unit-test for mutex

--- libs/pbd/test/mutex_test.cc
+++ libs/pbd/test/mutex_test.cc
@@ -1,4 +1,7 @@
 #include "mutex_test.h"
+#include <cppunit/TestAssert.h>
+#include <thread>
+#include <sys/time.h>
 
 CPPUNIT_TEST_SUITE_REGISTRATION (MutexTest);
 
@@ -9,16 +12,35 @@ MutexTest::MutexTest ()
 }
 
 void
-MutexTest::testBasic ()
+MutexTest::test_mutex ()
 {
-	Glib::Threads::Mutex::Lock lm (m_mutex);
+	PBD::Mutex::Lock lm (_mutex);
 
-	CPPUNIT_ASSERT (lm.locked());
+	CPPUNIT_ASSERT (lm.locked( ));
 
 	/* This will fail on POSIX systems but not on some older versions of glib
 	 * on win32 as TryEnterCriticalSection is used and it will return true
 	 * as CriticalSection is reentrant and fail the assertion.
 	 */
-	CPPUNIT_ASSERT (!m_mutex.trylock());
+	CPPUNIT_ASSERT (!_mutex.trylock());
+}
 
+void
+MutexTest::test_cond ()
+{
+	PBD::Mutex::Lock lm (_mutex);
+
+	CPPUNIT_ASSERT (!_cond.wait_for (_mutex, std::chrono::milliseconds (1000)));
+
+	thread t1 ([&]() { _cond.signal (); });
+	_cond.wait (_mutex);
+	t1.join();
+
+	thread t2 ([&]() { PBD::Mutex::Lock lm (_mutex); _cond.signal (); });
+	if (_cond.wait_for (_mutex, std::chrono::milliseconds (1000))) {
+		t2.join();
+	} else {
+		t2.detach();
+		CPPUNIT_ASSERT_MESSAGE ("_cond.wait_until failed", false);
+	}
 }
