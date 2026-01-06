7436776bad3f2bac27a2c78ff52e2f7eea17664a
EZ4Stephen <ez4stephen@tutamail.com>
2026-01-06 11:40:38 +0400

    Remove an extra line in license

--- gtk2_ardour/bundle_env_msvc2.cc
+++ gtk2_ardour/bundle_env_msvc2.cc
@@ -1,5 +1,4 @@
 /*
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -181,3 +180,4 @@ load_custom_fonts ()
 		std::cerr << _("Failed to set fontconfig configuration.") << std::endl;
 	}
 }
+
