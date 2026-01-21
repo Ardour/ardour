84428bab62a3d8704d99dc456fe4305604ad2537
Robin Gareus <robin@gareus.org>
2026-01-21 23:28:13 +0100

    Fix SNAFU, setting active dropdown item

--- libs/widgets/ardour_dropdown.cc
+++ libs/widgets/ardour_dropdown.cc
@@ -83,8 +83,9 @@ ArdourDropdown::on_button_press_event (GdkEventButton* ev)
 void
 ArdourDropdown::set_active (int c)
 {
+	int n = c;
 	for (auto& i : _menu.items()) {
-		if (0 == c--) {
+		if (0 == n--) {
 			_menu.set_active (c);
 			_menu.activate_item (i);
 			break;
