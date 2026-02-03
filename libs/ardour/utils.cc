6e9c8f001f17efbbfe73ca9b29049a9f2b3c5a83
Paul Davis <paul@linuxaudiosystems.com>
2026-02-03 11:17:34 -0700

    add ARDOUR::maybe_clean_pipewire_midi_port_name() to centralize this logic

--- libs/ardour/utils.cc
+++ libs/ardour/utils.cc
@@ -913,3 +913,32 @@ ARDOUR::estimate_audio_tempo_source (TimelineRange const & range, std::shared_pt
 	return true;
 }
 
+std::string
+ARDOUR::maybe_clean_pipewire_midi_port_name (std::string pn)
+{
+	string::size_type pos = pn.find (X_("Midi-Bridge:"));
+
+	if (pos == string::npos) {
+		return pn;
+	}
+
+	/* Remove "Midi-Bridge:" prefix */
+
+	pn = pn.erase (pos, 12);
+
+	/* Remove capture/playback suffix, which we don't need because we show
+	 * the right ports in the right context
+	 */
+
+	pos = pn.find (X_(" (capture)"));
+	if (pos != string::npos) {
+		pn.erase (pos);
+	} else {
+		pos = pn.find (X_(" (playback)"));
+		if (pos != string::npos) {
+			pn.erase (pos);
+		}
+	}
+
+	return pn;
+}
