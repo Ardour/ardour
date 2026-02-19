c177913c2fa6987b41b9d5023cebd5aa52401bc0
Robin Gareus <robin@gareus.org>
2026-02-19 22:54:48 +0100

    Mark VCA Solo control as master

--- libs/ardour/vca.cc
+++ libs/ardour/vca.cc
@@ -79,7 +79,7 @@ VCA::VCA (Session& s, int32_t num, const string& name)
 int
 VCA::init ()
 {
-	_solo_control.reset (new SoloControl (_session, X_("solo"), *this, *this, *this));
+	_solo_control.reset (new SoloControl (_session, X_("solo"), *this, *this, *this, true));
 	_mute_control.reset (new MuteControl (_session, X_("mute"), *this, *this));
 
 	add_control (_gain_control);
