e0b345f67c56d35d4e75c6fab6adb05a2dcc0886
Frankie Doo <frank.povazanj@gmail.com>
2026-02-23 13:31:43 -0500

    mcp_http: add usability README and author headers

--- libs/surfaces/mcp_http/mcp_http_server.cc
+++ libs/surfaces/mcp_http/mcp_http_server.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026
+ * Copyright (C) 2026 Frank Povazanj <frank.povazanj@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
