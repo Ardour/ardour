ce9b645b7a4cb454310b191efba354aa02a9bb1f
Robin Gareus <robin@gareus.org>
2026-03-08 02:42:19 +0100

    Add additional RWLock unit test

--- libs/pbd/test/rwlock_test.cc
+++ libs/pbd/test/rwlock_test.cc
@@ -30,18 +30,20 @@ RWLockTest::single_thread_test ()
 	/* leaving the scope releases ReaderLock */
 }
 
+/* ****************************************************************************/
+
 #define N_WORKERS 3
 
 void*
-RWLockTest::launch_reader (void* d)
+RWLockTest::launch_worker (void* d)
 {
 	RWLockTest* self = static_cast<RWLockTest*> (d);
-	self->reader_thread ();
+	self->worker_thread ();
 	return NULL;
 }
 
 void
-RWLockTest::reader_thread ()
+RWLockTest::worker_thread ()
 {
 	RWLock::ReaderLock rl (_rwlock);
 	g_usleep (1000000); // 1 sec
@@ -52,7 +54,7 @@ RWLockTest::run_thread_test ()
 {
 	pthread_t workers[N_WORKERS];
 	for (int i = 0; i < N_WORKERS; ++i) {
-		CPPUNIT_ASSERT (0 == pthread_create (&workers[i], NULL, &RWLockTest::launch_reader, this));
+		CPPUNIT_ASSERT (0 == pthread_create (&workers[i], NULL, &RWLockTest::launch_worker, this));
 	}
 
 	sched_yield ();
@@ -76,4 +78,113 @@ RWLockTest::run_thread_test ()
 	CPPUNIT_ASSERT (wl.try_acquire());
 
 	CPPUNIT_ASSERT (!trl.try_acquire());
+}
+
+/* ****************************************************************************/
+
+void*
+RWLockTest::launch_reader_a (void* d)
+{
+	RWLockTest* self = static_cast<RWLockTest*> (d);
+	self->reader_thread_a ();
+	return NULL;
+}
+
+void*
+RWLockTest::launch_reader_b (void* d)
+{
+	RWLockTest* self = static_cast<RWLockTest*> (d);
+	self->reader_thread_b ();
+	return NULL;
+}
+
+void
+RWLockTest::reader_thread_a ()
+{
+	RWLock::ReaderLock rl1 (_rwlock);
+	/* release main thread */
+	_sequence++; // = 1
+
+	/* wait for main thread to ack .. */
+	int want = 2;
+	while (!_sequence.compare_exchange_strong (want, 3)) {
+		want = 2;
+		sched_yield ();
+		g_usleep (1000);
+	}
+
+	/* wait for main thread to ask for the writer-lock */
+	sched_yield ();
+	g_usleep (1000000); // 1 sec
+
+	/* Now test if 2nd read-lock can be taken while writer-lock waits on rl1 */
+
+	RWLock::ReaderLock rl2 (_rwlock, RWLock::TryLock);
+	CPPUNIT_ASSERT (rl2.locked());
+
+	_sequence++; // = 4
+
+	want = 6;
+	while (!_sequence.compare_exchange_strong (want, 7)) {
+		want = 6;
+		sched_yield ();
+		g_usleep (1000);
+	}
+}
+
+void
+RWLockTest::reader_thread_b ()
+{
+	int want = 4;
+	while (!_sequence.compare_exchange_strong (want, 5)) {
+		want = 4;
+		sched_yield ();
+		g_usleep (1000);
+	}
+
+	RWLock::ReaderLock rl (_rwlock, RWLock::TryLock);
+	CPPUNIT_ASSERT (rl.locked());
+	_sequence++; // = 6
+
+	want = 7;
+	while (!_sequence.compare_exchange_strong (want, 8)) {
+		want = 7;
+		sched_yield ();
+		g_usleep (1000);
+	}
+}
+
+
+void
+RWLockTest::run_thread_sequence_test ()
+{
+	/* 1. Thread A takes read-lock
+	 * 2. Thread M asks for write-lock (blocks)
+	 * 3. Thread A takes another read-lock
+	 * 4. Thread B takes another read-lock
+	 *
+	 * Test that additional reader-lock can still be
+	 * taken while writer-lock waits.
+	 */
+
+	_sequence = 0;
+
+	pthread_t r[2];
+	CPPUNIT_ASSERT (0 == pthread_create (&r[0], NULL, &RWLockTest::launch_reader_a, this));
+	CPPUNIT_ASSERT (0 == pthread_create (&r[1], NULL, &RWLockTest::launch_reader_b, this));
+
+	int want = 1;
+	while (!_sequence.compare_exchange_strong (want, 2)) {
+		want = 1;
+		sched_yield ();
+		g_usleep (1000);
+	}
+
+	/* worker thread(s) should hold read-lock by now */
+	RWLock::WriterLock wl (_rwlock);
+	CPPUNIT_ASSERT (wl.locked());
+	CPPUNIT_ASSERT (_sequence == 8);
+
+	CPPUNIT_ASSERT (0 == pthread_join (r[0], NULL));
+	CPPUNIT_ASSERT (0 == pthread_join (r[1], NULL));
 }
--- libs/pbd/test/rwlock_test.h
+++ libs/pbd/test/rwlock_test.h
@@ -1,5 +1,7 @@
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
+#include <atomic>
+
 #include "pbd/rwlock.h"
 
 class RWLockTest : public CppUnit::TestFixture
@@ -7,16 +9,24 @@ class RWLockTest : public CppUnit::TestFixture
 	CPPUNIT_TEST_SUITE (RWLockTest);
 	CPPUNIT_TEST (single_thread_test);
 	CPPUNIT_TEST (run_thread_test);
+	CPPUNIT_TEST (run_thread_sequence_test);
 	CPPUNIT_TEST_SUITE_END ();
 
 public:
 	RWLockTest ();
 	void single_thread_test ();
 	void run_thread_test ();
+	void run_thread_sequence_test ();
 
 private:
-	void reader_thread ();
-	static void* launch_reader (void*);
+	void worker_thread ();
+	static void* launch_worker (void*);
 
-	PBD::RWLock _rwlock;
+	void reader_thread_a ();
+	void reader_thread_b ();
+	static void* launch_reader_a (void*);
+	static void* launch_reader_b (void*);
+
+	PBD::RWLock      _rwlock;
+	std::atomic<int> _sequence;
 };
