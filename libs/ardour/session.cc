8c05ef0b8a61264a13677a6968b7a2d345140f18
Paul Davis <paul@linuxaudiosystems.com>
2026-05-28 09:17:34 -0600

    for semitone/wholetone step-defined scales, root is a MIDI note, not Hz

--- libs/ardour/session.cc
+++ libs/ardour/session.cc
@@ -376,7 +376,7 @@ Session::Session (AudioEngine &eng,
 	_seek_counter.store (0);
 	_butler_seek_counter.store (0);
 
-	ScaleProvider::set_key (MusicalKey (440.0, ARDOUR::MusicalMode (ARDOUR::MusicalMode::IonianMajor)));
+	ScaleProvider::set_key (MusicalKey (60, ARDOUR::MusicalMode (ARDOUR::MusicalMode::IonianMajor)));
 
 	created_with = string_compose ("%1 %2", PROGRAM_NAME, revision);
 
