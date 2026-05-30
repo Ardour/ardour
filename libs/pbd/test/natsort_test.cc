4549e31919fc70784f1c21b4fe2890715b36f8af
Gian Fontanilla <fontanillagian@gmail.com>
2026-05-31 02:31:10 +0800

    Add unit tests

--- libs/pbd/test/natsort_test.cc
+++ libs/pbd/test/natsort_test.cc
@@ -18,3 +18,17 @@ NatSortTest::testBasic ()
 	CPPUNIT_ASSERT ( PBD::naturally_less ("abc", "abcd"));
 	CPPUNIT_ASSERT (!PBD::naturally_less ("abc", "abc"));
 }
+
+void
+NatSortTest::testStringLiteral ()
+{
+
+	CPPUNIT_ASSERT (!PBD::naturally_less ("a32"s, "a4"s));
+	CPPUNIT_ASSERT (!PBD::naturally_less ("a32"s, "a04"s));
+	CPPUNIT_ASSERT ( PBD::naturally_less ("a32"s, "a40"s));
+	CPPUNIT_ASSERT ( PBD::naturally_less ("a32a"s, "a32b"s));
+	CPPUNIT_ASSERT (!PBD::naturally_less ("a32b"s, "a32a"s));
+	CPPUNIT_ASSERT (!PBD::naturally_less ("abcd"s, "abc"s));
+	CPPUNIT_ASSERT ( PBD::naturally_less ("abc"s, "abcd"s));
+	CPPUNIT_ASSERT (!PBD::naturally_less ("abc"s, "abc"s));
+}
