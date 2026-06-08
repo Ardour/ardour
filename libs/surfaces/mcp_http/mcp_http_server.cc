Ardour/ardour

commit 00cac5436228cc608261d99950918289cc0bd852

Remove unread variables
These variables aren't read at all, so their declaration / maintenance is
"effectively dead" code (most of these are leftovers from earlier code
removals).  Flagged by GCC's Wunused-but-set-variable.

libs/surfaces/mcp_http/mcp_http_server.cc
@@ -5190,8 +5190,7 @@ dispatch_tracks_tool_call (ARDOUR::Session& session, const std::string& tool_nam
 static std::string
 handle_plugin_tool_call (ARDOUR::Session& session, PBD::EventLoop* event_loop, const std::string& tool_name, const pt::ptree& root, const std::string& id)
 {
-	ARDOUR::Session& _session    = session;
-	PBD::EventLoop*  _event_loop = event_loop;
+	ARDOUR::Session& _session = session;
 
 	if (tool_name == "plugin/list_available") {
 		const std::string search           = root.get<std::string> ("params.arguments.search", "");

gtk2_ardour/audio_region_view.cc
@@ -1514,7 +1514,6 @@ AudioRegionView::add_gain_point_event (ArdourCanvas::Item *item, GdkEvent *ev, b
 		return;
 	}
 
-	uint32_t before_p, after_p;
 	double mx = ev->button.x;
 	double my = ev->button.y;
 

gtk2_ardour/cue_editor.cc
@@ -1816,80 +1816,6 @@ CueEditor::metric_get_bbt (std::vector<ArdourCanvas::Ruler::Mark>& marks, sample
 		return;
 	}
 
-	/* we can accent certain lines depending on the user's Grid choice */
-	/* for example, even in a 4/4 meter we can draw a grid with triplet-feel */
-	/* and in this case you will want the accents on '3s' not '2s' */
-	uint32_t bbt_divisor = 2;
-
-	using namespace Editing;
-
-	switch (grid_type()) {
-	case GridTypeBeatDiv3:
-		bbt_divisor = 3;
-		break;
-	case GridTypeBeatDiv5:
-		bbt_divisor = 5;
-		break;
-	case GridTypeBeatDiv6:
-		bbt_divisor = 3;
-		break;
-	case GridTypeBeatDiv7:
-		bbt_divisor = 7;
-		break;
-	case GridTypeBeatDiv10:
-		bbt_divisor = 5;
-		break;
-	case GridTypeBeatDiv12:
-		bbt_divisor = 3;
-		break;
-	case GridTypeBeatDiv14:
-		bbt_divisor = 7;
-		break;
-	case GridTypeBeatDiv16:
-		break;
-	case GridTypeBeatDiv20:
-		bbt_divisor = 5;
-		break;
-	case GridTypeBeatDiv24:
-		bbt_divisor = 6;
-		break;
-	case GridTypeBeatDiv28:
-		bbt_divisor = 7;
-		break;
-	case GridTypeBeatDiv32:
-		break;
-	default:
-		bbt_divisor = 2;
-		break;
-	}
-
-	uint32_t bbt_beat_subdivision = 1;
-	switch (bbt_ruler_scale) {
-	case bbt_show_quarters:
-		bbt_beat_subdivision = 1;
-		break;
-	case bbt_show_eighths:
-		bbt_beat_subdivision = 1;
-		break;
-	case bbt_show_sixteenths:
-		bbt_beat_subdivision = 2;
-		break;
-	case bbt_show_thirtyseconds:
-		bbt_beat_subdivision = 4;
-		break;
-	case bbt_show_sixtyfourths:
-		bbt_beat_subdivision = 8;
-		break;
-	case bbt_show_onetwentyeighths:
-		bbt_beat_subdivision = 16;
-		break;
-	default:
-		bbt_beat_subdivision = 1;
-		break;
-	}
-
-	bbt_beat_subdivision *= bbt_divisor;
-
 	switch (bbt_ruler_scale) {
 
 	case bbt_show_many:

gtk2_ardour/editor_ops.cc
@@ -5981,7 +5981,6 @@ Editor::apply_filter (Filter& filter, string command, ProgressReporter* progress
 	CursorRAII cr (*this, _cursors->wait);
 	bool in_command = false;
 
-	int n = 0;
 	int const N = rs.size ();
 
 	for (RegionSelection::iterator r = rs.begin(); r != rs.end(); ) {
@@ -6043,7 +6042,6 @@ Editor::apply_filter (Filter& filter, string command, ProgressReporter* progress
 		}
 
 		r = tmp;
-		++n;
 	}
 
 	if (in_command) {

gtk2_ardour/level_meter.cc
@@ -189,9 +189,8 @@ LevelMeterBase::parameter_changed (string p)
 
 	if (p == "meter-hold") {
 		vector<MeterInfo>::iterator i;
-		uint32_t n;
 
-		for (n = 0, i = meters.begin(); i != meters.end(); ++i, ++n) {
+		for (i = meters.begin(); i != meters.end(); ++i) {
 			(*i).meter->set_hold_count ((uint32_t) floor(UIConfiguration::instance().get_meter_hold()));
 		}
 	}
@@ -205,9 +204,8 @@ LevelMeterBase::parameter_changed (string p)
 	}
 	else if (p == "meter-peak") {
 		vector<MeterInfo>::iterator i;
-		uint32_t n;
 
-		for (n = 0, i = meters.begin(); i != meters.end(); ++i, ++n) {
+		for (i = meters.begin(); i != meters.end(); ++i) {
 			(*i).max_peak = minus_infinity();
 		}
 	}

gtk2_ardour/midi_time_axis.cc
@@ -111,7 +111,6 @@ using namespace Editing;
 using namespace std;
 
 // Minimum height at which a control is displayed
-static const uint32_t MIDI_CONTROLS_BOX_MIN_HEIGHT = 160;
 static const uint32_t KEYBOARD_MIN_HEIGHT = 130;
 
 #define DEFAULT_MIDNAM_MODEL (X_("Generic"))

gtk2_ardour/midi_util.cc
@@ -67,7 +67,6 @@ build_controller_menu (Gtk::Menu& menu, InstrumentInfo const & instrument_info, 
 		using namespace MIDI::Name;
 
 		unsigned n_items  = 0;
-		unsigned n_groups = 0;
 
 		/* keep track of CC numbers that are added */
 		uint16_t ctl_start = 1;
@@ -139,7 +138,6 @@ build_controller_menu (Gtk::Menu& menu, InstrumentInfo const & instrument_info, 
 									ctl_start, ctl_end), *ctl_menu));
 					ctl_menu = nullptr;
 					n_items  = 0;
-					++n_groups;
 				}
 			}
 		}

gtk2_ardour/midi_view.cc
@@ -1057,7 +1057,6 @@ MidiView::chord_is_selected () const
 		return false;
 	}
 
-	size_t cnt = 1;
 	auto s = _selection.begin();
 
 	Temporal::Beats start = (*s)->note()->time();
@@ -1068,7 +1067,6 @@ MidiView::chord_is_selected () const
 		if (delta.abs() > Temporal::Beats (0, 15)) { /* 1/128th note */
 			return false;
 		}
-		++cnt;
 		++s;
 	}
 	return true;

commit 10b28ee944f39dc268dd4ac26fbd327dbb6c6d53

Restore removed variable that's used in the Mac build
Broken by 00cac5436228cc608.

libs/surfaces/mcp_http/mcp_http_server.cc
@@ -5190,7 +5190,8 @@ dispatch_tracks_tool_call (ARDOUR::Session& session, const std::string& tool_nam
 static std::string
 handle_plugin_tool_call (ARDOUR::Session& session, PBD::EventLoop* event_loop, const std::string& tool_name, const pt::ptree& root, const std::string& id)
 {
-	ARDOUR::Session& _session = session;
+	ARDOUR::Session& _session    = session;
+	PBD::EventLoop*  _event_loop = event_loop;
 
 	if (tool_name == "plugin/list_available") {
 		const std::string search           = root.get<std::string> ("params.arguments.search", "");