Ardour/ardour

commit fe67539c6dcf4dcc996e5284a402fb7fc8f0cc53

when adding routes to an otherwise empty session, select on the first one added
For all subsequent additions, select all that are added.

Note that we also do not select the master bus as the very first state as the (new)
session is loaded.

gtk2_ardour/editor.cc
@@ -4743,7 +4743,30 @@ Editor::add_stripables (StripableList& sl)
 	std::shared_ptr<Route> r;
 	TrackViewList new_selection;
 	bool changed = false;
-	bool from_scratch = (track_views.size() == 0);
+	int32_t real_routes = 0;
+
+	/* Count the number of "normal" tracks & busses */
+
+	{
+		std::shared_ptr<RouteList const> rl (_session->get_routes());
+
+		for (auto const & r : *rl) {
+			if (r->is_normal_route()) {
+				real_routes++;
+			}
+		}
+	}
+
+	/* Session::get_routes() will include the ones we just added */
+
+	for (auto & s : sl) {
+		if (s->is_normal_route()) {
+			real_routes--;
+		}
+	}
+
+	bool from_scratch = (real_routes <= 0);
+	bool initial_selection = (from_scratch || _no_not_select_reimported_tracks);
 
 	sl.sort (Stripable::Sorter());
 
@@ -4784,7 +4807,11 @@ Editor::add_stripables (StripableList& sl)
 			}
 
 			track_views.push_back (rtv);
-			new_selection.push_back (rtv);
+			if (!initial_selection || new_selection.empty()) {
+				if (r->is_normal_route()) {
+					new_selection.push_back (rtv);
+				}
+			}
 
 			rtv->effective_gain_display ();
 
@@ -4803,7 +4830,7 @@ Editor::add_stripables (StripableList& sl)
 	 * than just VCAs
 	 */
 
-	if (!from_scratch && !_no_not_select_reimported_tracks && !new_selection.empty()) {
+	if (!new_selection.empty()) {
 		selection->set (new_selection);
 		begin_selection_op_history();
 	}

commit 3d533c4061338242e527b89d2bc77f4581090acd

Fix restoring selectoin on session load
this broke in fe67539c6dc, which selected the first
track when tracks were added during session load.

gtk2_ardour/editor.cc
@@ -4835,7 +4835,7 @@ Editor::add_stripables (StripableList& sl)
 	 * than just VCAs
 	 */
 
-	if (!new_selection.empty()) {
+	if (!new_selection.empty() && !ARDOUR_UI::instance()->loading_session ()) {
 		selection->set (new_selection);
 		begin_selection_op_history();
 	}