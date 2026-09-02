dd852ccb4be8f5ad56097f916a149cb9effe913f
Paul Davis <paul@linuxaudiosystems.com>
2026-09-01 20:42:14 -0600

    introduce concept of "transience" for routes, sources, regions
    This could also be called "temporariness" but that has more letters.
    
    Commit also includes some loop-autofication in session state code

--- libs/ardour/ardour/region.h
+++ libs/ardour/ardour/region.h
@@ -283,6 +283,7 @@ public:
 	bool sync_marked ()      const { return _sync_marked; }
 	bool external ()         const { return _external; }
 	bool import ()           const { return _import; }
+	bool transient ()        const;
 
 	Trimmable::CanTrim can_trim () const;
 
