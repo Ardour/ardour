6c096b7c3a9213fd2307d12132df82b0054c0879
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2026-09-12 13:04:43 +0200

    miditracker: restore polypressure state according to what's found in the timeline, like with other events

--- libs/ardour/midi_state_tracker.cc
+++ libs/ardour/midi_state_tracker.cc
@@ -492,8 +492,10 @@ MidiStateTracker::resolve_state (Evoral::EventSink<samplepos_t>& dst, Evoral::Ev
 			if ((poly_pressure[chn][k] & 0x80) == 0) {
 				buf[0] = MIDI_CMD_NOTE_PRESSURE | chn;
 				buf[1] = k;
-				buf[2] = poly_pressure[chn][k];
-				dst.write (time, Evoral::MIDI_EVENT, 3, buf);
+				buf[2] = 0x80;
+				if (find_event (evlist, time, buf) > 0) {
+					dst.write (time, Evoral::MIDI_EVENT, 3, buf);
+				}
 			}
 			if (reset) {
 				poly_pressure[chn][k] = 0x80;
